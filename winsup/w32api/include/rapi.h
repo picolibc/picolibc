7408c2434339992b31f52a2c194766c7102b3dbb
Christopher Faylor <me@cgf.cx>
2000-02-24 18:04:23 +0000

    Adhere to w32api copyright conventions.

--- winsup/w32api/include/rapi.h
+++ winsup/w32api/include/rapi.h
@@ -1,13 +1,8 @@
-/*
-	rapi.h - main header file for the RAPI API
+/* rapi.h - main header file for the RAPI API
 
-	Copyright 1999 Cygnus Solutions.
+        NOTE: This strictly does not belong in the Win32 API since it's
+        really part of Platform SDK.
 
-	This file is part of Cygwin.
-
-	This software is a copyrighted work licensed under the terms of the
-	Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
-	details.
 */
 
 #ifndef _RAPI_H
@@ -22,43 +17,35 @@ typedef struct IRAPIStreamVtbl IRAPIStreamVtbl;
 
 typedef enum tagRAPISTREAMFLAG
 {
-	STREAM_TIMEOUT_READ
+  STREAM_TIMEOUT_READ
 } RAPISTREAMFLAG;
 
 struct IRAPIStreamVtbl
 {
-  HRESULT (__stdcall * SetRapiStat)( IRAPIStream * This, RAPISTREAMFLAG Flag, DWORD dwValue) ;
-  HRESULT (__stdcall * GetRapiStat)( IRAPIStream * This, RAPISTREAMFLAG Flag, DWORD *pdwValue) ;
+  HRESULT (__stdcall * SetRapiStat)( IRAPIStream *, RAPISTREAMFLAG, DWORD);
+  HRESULT (__stdcall * GetRapiStat)( IRAPIStream *, RAPISTREAMFLAG, DWORD *);
 };
 
-// RAPI extension on Windows CE (e.g., MyFunctionFOO) called via CeRapiInvoke should be declared as:
-// EXTERN_C RAPIEXT MyFunctionFOO;
-typedef  HRESULT (STDAPICALLTYPE RAPIEXT)(
-		 DWORD			cbInput,			// [IN]
-		 BYTE			*pInput,			// [IN]
-		 DWORD			*pcbOutput,	 		// [OUT]
-		 BYTE			**ppOutput,			// [OUT]
-		 IRAPIStream	*pIRAPIStream		// [IN]
-		 );
+typedef  HRESULT (STDAPICALLTYPE RAPIEXT)(DWORD, BYTE, DWORD, BYTE, IRAPIStream	*);
 
 typedef struct _RAPIINIT
 {
-    DWORD cbSize;
-    HANDLE heRapiInit;
-    HRESULT hrRapiInit;
+  DWORD cbSize;
+  HANDLE heRapiInit;
+  HRESULT hrRapiInit;
 } RAPIINIT;
 
-STDAPI CeRapiInit();
-STDAPI CeRapiInitEx(RAPIINIT*);
-STDAPI_(BOOL) CeCreateProcess(LPCWSTR, LPCWSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES,
-			      BOOL, DWORD, LPVOID, LPWSTR, LPSTARTUPINFO, LPPROCESS_INFORMATION);
-STDAPI CeRapiUninit();
+STDAPI CeRapiInit ();
+STDAPI CeRapiInitEx (RAPIINIT*);
+STDAPI_(BOOL) CeCreateProcess (LPCWSTR, LPCWSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES,
+			       BOOL, DWORD, LPVOID, LPWSTR, LPSTARTUPINFO, LPPROCESS_INFORMATION);
+STDAPI CeRapiUninit ();
 
-STDAPI_(BOOL) CeWriteFile(HANDLE, LPCVOID, DWORD, LPDWORD, LPOVERLAPPED);
-STDAPI_(HANDLE) CeCreateFile(LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE); 
-STDAPI_(BOOL) CeCreateDirectory(LPCWSTR, LPSECURITY_ATTRIBUTES); 
-STDAPI_(DWORD) CeGetLastError(void);
-STDAPI_(BOOL) CeGetFileTime(HANDLE, LPFILETIME, LPFILETIME, LPFILETIME); 
-STDAPI_(BOOL) CeCloseHandle(HANDLE); 
+STDAPI_(BOOL) CeWriteFile (HANDLE, LPCVOID, DWORD, LPDWORD, LPOVERLAPPED);
+STDAPI_(HANDLE) CeCreateFile (LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE); 
+STDAPI_(BOOL) CeCreateDirectory (LPCWSTR, LPSECURITY_ATTRIBUTES); 
+STDAPI_(DWORD) CeGetLastError (void);
+STDAPI_(BOOL) CeGetFileTime (HANDLE, LPFILETIME, LPFILETIME, LPFILETIME); 
+STDAPI_(BOOL) CeCloseHandle (HANDLE); 
 
 #endif /* _RAPI_H */
