2cf1695b81783e64b0572ed67b9b11cf99f3cea7
Tim Wall <twall@redhat.com>
2000-04-07 17:06:58 +0000

    BFD and include/coff support for tic54x target.

---
+++ include/coff/tic54x.h
@@ -0,0 +1,11 @@
+/*** COFF information for Texas Instruments TMS320C54X */
+#define TIC54X_TARGET_ID 0x98
+#define TIC54XALGMAGIC  0x009B  /* c54x algebraic assembler output */
+#define TIC5X_TARGET_ID  0x92
+#define TI_TARGET_ID TIC54X_TARGET_ID
+#define OCTETS_PER_BYTE_POWER 1 /* octets per byte, as a power of two */
+#define HOWTO_BANK 6 /* add to howto to get absolute/sect-relative version */
+#define TICOFF_TARGET_ARCH bfd_arch_tic54x
+#define TICOFF_DEFAULT_MAGIC TICOFF1MAGIC /* we use COFF1 for compatibility */
+#include "coff/ti.h"
+/*EOF*/
