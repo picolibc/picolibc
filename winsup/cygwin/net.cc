046069e4491987af37326d4563e55fcb0698b88c
DJ Delorie <dj@redhat.com>
2000-04-24 15:44:11 +0000

    * net.cc (cygwin_inet_network): new function. * cygwin.din (inet_network): new export

--- winsup/cygwin/net.cc
+++ winsup/cygwin/net.cc
@@ -127,6 +127,17 @@ cygwin_inet_addr (const char *cp)
   return res;
 }
 
+/* undocumented in wsock32.dll */
+extern "C" unsigned int	 inet_network (const char *);
+
+extern "C"
+unsigned int 
+cygwin_inet_network (const char *cp)
+{
+  unsigned int res = inet_network (cp);
+  return res;
+}
+
 /* inet_netof is in the standard BSD sockets library.  It is useless
    for modern networks, since it assumes network values which are no
    longer meaningful, but some existing code calls it.  */
@@ -1811,6 +1822,7 @@ LoadDLLfunc (getservbyport, getservbyport@8, wsock32)
 LoadDLLfunc (getsockname, getsockname@12, wsock32)
 LoadDLLfunc (getsockopt, getsockopt@20, wsock32)
 LoadDLLfunc (inet_addr, inet_addr@4, wsock32)
+LoadDLLfunc (inet_network, inet_network@4, wsock32)
 LoadDLLfunc (inet_ntoa, inet_ntoa@4, wsock32)
 LoadDLLfunc (ioctlsocket, ioctlsocket@12, wsock32)
 LoadDLLfunc (listen, listen@8, wsock32)
