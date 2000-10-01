7d278f3da361403e846eeeaf6b32b884917180d7
Corinna Vinschen <corinna@vinschen.de>
2000-10-01 14:22:14 +0000

    * lib/iphlpapi.def: New stub for iphlpapi.dll.         * include/iptypes.h: New header file.         * include/ipexport.h: Ditto.         * include/iphlpapi.h: Ditto.         * include/iprtrmib.h: Ditto.

---
+++ winsup/w32api/include/ipexport.h
@@ -0,0 +1,22 @@
+#ifndef _IPEXPORT_H_
+#define _IPEXPORT_H_
+
+#ifndef ANY_SIZE
+#define ANY_SIZE 1
+#endif
+
+#define MAX_ADAPTER_NAME 128
+
+typedef unsigned long IPAddr, IPMask, IP_STATUS;
+
+typedef struct {
+  ULONG Index;
+  WCHAR  Name[MAX_ADAPTER_NAME];
+} IP_ADAPTER_INDEX_MAP, *PIP_ADAPTER_INDEX_MAP;
+
+typedef struct {
+  LONG NumAdapters;
+  IP_ADAPTER_INDEX_MAP Adapter[ANY_SIZE];
+} IP_INTERFACE_INFO, *PIP_INTERFACE_INFO;
+
+#endif /* _IPEXPORT_H_ */
---
+++ winsup/w32api/include/iptypes.h
@@ -0,0 +1,70 @@
+#ifndef _IPTYPES_H_
+#define _IPTYPES_H_
+
+#define DEFAULT_MINIMUM_ENTITIES 32
+
+#define MAX_ADAPTER_ADDRESS_LENGTH 8
+#define MAX_ADAPTER_DESCRIPTION_LENGTH 128
+#define MAX_ADAPTER_NAME_LENGTH 256
+#define MAX_DOMAIN_NAME_LEN 128
+#define MAX_HOSTNAME_LEN 128
+#define MAX_SCOPE_ID_LEN 256
+
+#define BROADCAST_NODETYPE 1
+#define PEER_TO_PEER_NODETYPE 2
+#define MIXED_NODETYPE 4
+#define HYBRID_NODETYPE 8
+
+#define IF_OTHER_ADAPTERTYPE 0
+#define IF_ETHERNET_ADAPTERTYPE 1
+#define IF_TOKEN_RING_ADAPTERTYPE 2
+#define IF_FDDI_ADAPTERTYPE 3
+#define IF_PPP_ADAPTERTYPE 4
+#define IF_LOOPBACK_ADAPTERTYPE 5
+
+typedef struct {
+  char String[4 * 4];
+} IP_ADDRESS_STRING, *PIP_ADDRESS_STRING,
+  IP_MASK_STRING, *PIP_MASK_STRING;
+
+typedef struct __IP_ADDR_STRING {
+  struct __IP_ADDR_STRING* Next;
+  IP_ADDRESS_STRING IpAddress;
+  IP_MASK_STRING IpMask;
+  DWORD Context;
+} IP_ADDR_STRING, *PIP_ADDR_STRING;
+
+typedef struct __IP_ADAPTER_INFO {
+  struct __IP_ADAPTER_INFO* Next;
+  DWORD ComboIndex;
+  char AdapterName[MAX_ADAPTER_NAME_LENGTH+4];
+  char Description[MAX_ADAPTER_DESCRIPTION_LENGTH+4];
+  UINT AddressLength;
+  BYTE Address[MAX_ADAPTER_ADDRESS_LENGTH];
+  DWORD Index;
+  UINT Type;
+  UINT DhcpEnabled;
+  PIP_ADDR_STRING CurrentIpAddress;
+  IP_ADDR_STRING IpAddressList;
+  IP_ADDR_STRING GatewayList;
+  IP_ADDR_STRING DhcpServer;
+  BOOL HaveWins;
+  IP_ADDR_STRING PrimaryWinsServer;
+  IP_ADDR_STRING SecondaryWinsServer;
+  time_t LeaseObtained;
+  time_t LeaseExpires;
+} IP_ADAPTER_INFO, *PIP_ADAPTER_INFO;
+
+typedef struct {
+  char HostName[MAX_HOSTNAME_LEN + 4] ;
+  char DomainName[MAX_DOMAIN_NAME_LEN + 4];
+  PIP_ADDR_STRING CurrentDnsServer;
+  IP_ADDR_STRING DnsServerList;
+  UINT NodeType;
+  char ScopeId[MAX_SCOPE_ID_LEN + 4];
+  UINT EnableRouting;
+  UINT EnableProxy;
+  UINT EnableDns;
+} FIXED_INFO, *PFIXED_INFO;
+
+#endif /* _IPTYPES_H_ */
