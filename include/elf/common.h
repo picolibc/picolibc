b63f2ed24489ee951d2dc19ef5e188669935b81d
H.J. Lu <hjl.tools@gmail.com>
2000-11-20 23:45:42 +0000

    2000-11-20  H.J. Lu  <hjl@gnu.org>
    	* common.h (ELFOSABI_MONTEREY): Renamed to ...
    	(ELFOSABI_AIX): This.

--- include/elf/common.h
+++ include/elf/common.h
@@ -64,7 +64,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #define ELFOSABI_LINUX  3       /* GNU/Linux */
 #define ELFOSABI_HURD   4       /* GNU/Hurd */
 #define ELFOSABI_SOLARIS 6      /* Solaris */
-#define ELFOSABI_MONTEREY 7     /* Monterey */
+#define ELFOSABI_AIX    7       /* AIX */
 #define ELFOSABI_IRIX   8       /* IRIX */
 #define ELFOSABI_FREEBSD 9      /* FreeBSD */
 #define ELFOSABI_TRU64  10      /* TRU64 UNIX */
