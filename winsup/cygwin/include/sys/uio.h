fdb352765e9529c58140ec425360582b7f909272
Corinna Vinschen <corinna@vinschen.de>
2000-12-05 16:08:48 +0000

    * include/sys/uio.h: Include <sys/cdefs.h>. Use __BEGIN_DECL and         __END_DECL.         Add declarations for `readv' and `writev'.

--- winsup/cygwin/include/sys/uio.h
+++ winsup/cygwin/include/sys/uio.h
@@ -1,14 +1,14 @@
 #ifndef _UIO_H_
 #define _UIO_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
 /* For size_t */
 #include <stddef.h>
 /* For ssize_t */
 #include <sys/types.h>
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
 
 /*
  * Define the uio buffers used for writev, readv.
@@ -19,7 +19,9 @@ struct iovec {
 	int iov_len;
 };
 
-#ifdef __cplusplus
-};
-#endif /* __cplusplus */
+extern int readv __P ((int filedes, const struct iovec *vector, size_t count));
+extern int writev __P ((int filedes, const struct iovec *vector, size_t count));
+
+__END_DECLS
+
 #endif /* _UIO_H_ */
