3112d7a41c20a4f12ba57ad266a984bbb04331dc
Corinna Vinschen <corinna@vinschen.de>
2000-12-19 20:43:41 +0000

    * syscalls.cc (remove): New function, overriding the newlib 	implementation.

--- winsup/cygwin/syscalls.cc
+++ winsup/cygwin/syscalls.cc
@@ -166,6 +166,25 @@ done:
   return res;
 }
 
+extern "C" int
+remove (const char *ourname)
+{
+  path_conv win32_name (ourname, PC_SYM_NOFOLLOW | PC_FULL);
+
+  if (win32_name.error)
+    {
+      set_errno (win32_name.error);
+      syscall_printf ("-1 = remove (%s)", ourname);
+      return -1;
+    }
+
+  DWORD atts = win32_name.file_attributes ();
+  if (atts != 0xffffffff && atts & FILE_ATTRIBUTE_DIRECTORY)
+    return rmdir (ourname);
+
+  return _unlink (ourname);
+}
+
 extern "C" pid_t
 _getpid ()
 {
