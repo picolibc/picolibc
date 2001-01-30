4f42df7bc43f874642b8da13c44438ca0d450c5f
Christopher Faylor <me@cgf.cx>
2001-01-30 23:19:19 +0000

    * include/cygwin/version.h: Bump version to 1.3.0.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -41,10 +41,10 @@ details. */
 	 the Cygwin library".  This version is used to track important
 	 changes to the DLL and is mainly informative in nature. */
 
-  /* The current cygwin version is 1.1.0 */
+  /* The current cygwin version is 1.3.0 */
 
-#define CYGWIN_VERSION_DLL_MAJOR 1001
-#define CYGWIN_VERSION_DLL_MINOR 8
+#define CYGWIN_VERSION_DLL_MAJOR 1003
+#define CYGWIN_VERSION_DLL_MINOR 0
 
       /* Major numbers before CYGWIN_VERSION_DLL_EPOCH are
 	 incompatible. */
