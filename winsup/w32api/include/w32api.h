c7193585d3b923462f862d02fe373f42b615a494
Earnie Boyd <earnie@users.sf.net>
2001-02-21 21:44:59 +0000

    * include/w32api.h: (_W32API_VERSION): Remove. 	(__W32API_VERSION): Add. 	(__W32API_MAJOR_VERSION): Ditto. 	(__W32API_MINOR_VERSION): Ditto.

--- winsup/w32api/include/w32api.h
+++ winsup/w32api/include/w32api.h
@@ -1,6 +1,8 @@
 #ifndef _W32API_H_
 #define _W32API_H_
 
-#define _W32API_VERSION 0.4
+#define __W32API_VERSION 0.5
+#define __W32API_MAJOR_VERSION 0
+#define __W32API_MINOR_VERSION 5
 
 #endif /* ndef _W32API_H_ */
