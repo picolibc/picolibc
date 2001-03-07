8f98585d511db77f20e361f323c0be2f719e6ab0
Christopher Faylor <me@cgf.cx>
2001-03-07 17:45:03 +0000

    Update copyright.

--- winsup/cygwin/strace.cc
+++ winsup/cygwin/strace.cc
@@ -1,6 +1,6 @@
 /* strace.cc: system/windows tracing
 
-   Copyright 1996, 1997, 1998, 1999, 2000 Cygnus Solutions.
+   Copyright 1996, 1997, 1998, 1999, 2000, 2001 Red Hat, Inc.
 
 This file is part of Cygwin.
 
