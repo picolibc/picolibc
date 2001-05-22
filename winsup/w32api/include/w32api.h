1f6e71f34849129101c26e907d90c43a780cd3c5
Earnie Boyd <earnie@users.sf.net>
2001-05-22 20:51:47 +0000

    * include/w32api.h: Update version. 	* Makefile.in: Ditto.

--- winsup/w32api/include/w32api.h
+++ winsup/w32api/include/w32api.h
@@ -1,8 +1,8 @@
 #ifndef _W32API_H_
 #define _W32API_H_
 
-#define __W32API_VERSION 0.5
-#define __W32API_MAJOR_VERSION 0
-#define __W32API_MINOR_VERSION 5
+#define __W32API_VERSION 1.0
+#define __W32API_MAJOR_VERSION 1
+#define __W32API_MINOR_VERSION 0
 
 #endif /* ndef _W32API_H_ */
