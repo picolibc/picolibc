2b6b730c986a53e9e10ef22508541bdb3151a5fe
Nick Clifton <nickc@redhat.com>
2001-06-28 10:40:09 +0000

    Handle ARMs with big endian IEEE words but little endian IEEE bytes

--- newlib/libc/include/machine/ieeefp.h
+++ newlib/libc/include/machine/ieeefp.h
@@ -21,9 +21,11 @@
 
 #if defined(__arm__) || defined(__thumb__)
 /* ARM always has big-endian words.  Within those words the byte ordering
-   appears to be big or little endian.  Newlib doesn't seem to care about
-   the byte ordering within words.  */
+   will be big or little endian depending upon the target.  */
 #define __IEEE_BIG_ENDIAN
+#ifdef __ARMEL__
+#define __IEEE_BYTES_LITTLE_ENDIAN
+#endif
 #endif
 
 #ifdef __hppa__
