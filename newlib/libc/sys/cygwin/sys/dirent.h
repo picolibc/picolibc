a3e67ddfe6806949e6f441e89a1dbe5f0b92ed1a
Corinna Vinschen <corinna@vinschen.de>
2001-09-03 19:04:05 +0000

    * libc/sys/cygwin/sys/dirent.h (struct dirent): Add version number         field.         (__DIRENT_VERSION): New define.

--- newlib/libc/sys/cygwin/sys/dirent.h
+++ newlib/libc/sys/cygwin/sys/dirent.h
@@ -13,9 +13,12 @@
 
 #include <sys/types.h>
 
+#define __DIRENT_VERSION	1
+
 struct dirent
 {
-  long __d_reserved[3];
+  long d_version; /* Used since Cygwin 1.3.3. */
+  long __d_reserved[2];
   long d_fd; /* File descriptor of open directory. Used since Cygwin 1.3.3. */
   ino_t d_ino; /* Just for compatibility, it's junk */
   char d_name[256];		/* FIXME: use NAME_MAX? */
