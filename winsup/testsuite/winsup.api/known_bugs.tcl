d961def403a334176adb4c79cb02385bfca230e7
Egor Duda <deo@logos-m.ru>
2001-09-09 13:38:17 +0000

    * winsup.api/ltp/dup03.c: New test. * winsup.api/ltp/lseek03.c: Ditto. * winsup.api/ltp/mmap001.c: Ditto. * winsup.api/ltp/read01.c: Ditto. * winsup.api/ltp/readdir01.c: Ditto. * winsup.api/ltp/rmdir05.c: Ditto. * winsup.api/ltp/sbrk01.c: Ditto. * winsup.api/ltp/select02.c: Ditto. * winsup.api/ltp/select03.c: Ditto. * winsup.api/ltp/signal03.c: Ditto. * winsup.api/ltp/stat06.c: Ditto. * winsup.api/ltp/unlink08.c: Ditto. * winsup.api/known_bugs.tcl: Update to reflect new test's known failures. * winsup.api/winsup.exp: Don't delete executable in case of unexpected pass, as well as in case of unexpected failure. * README: Update paragraph about expected failures.

--- winsup/testsuite/winsup.api/known_bugs.tcl
+++ winsup/testsuite/winsup.api/known_bugs.tcl
@@ -1,8 +1,8 @@
 set xfail_list [list dup03 dup05 \
     fcntl05 fcntl07B fcntl09 fcntl10 \
-    fsync01 gethostid01 lseek04 mknod01 \
+    fsync01 gethostid01 lseek04 mknod01 select03 \
     setgroups01 setregid01 setreuid01 setuid02 \
-    ulimit01 unlink06 \
+    ulimit01 unlink06 unlink08 \
     sample-fail sample-miscompile]
 
 
