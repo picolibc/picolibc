64bd8a83af6b848e1a0af97f724ccbed6f57a72c
Christopher Faylor <me@cgf.cx>
2001-11-12 00:19:17 +0000

    * path.h: New file.

---
+++ winsup/utils/path.h
@@ -0,0 +1,11 @@
+/* path.h
+
+   Copyright 2001 Red Hat, Inc.
+
+This file is part of Cygwin.
+
+This software is a copyrighted work licensed under the terms of the
+Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
+details. */
+
+char * cygpath (const char *s, ...);
