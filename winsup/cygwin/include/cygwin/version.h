dbe67a81032765d702b73691d11406984d47f25a
Christopher Faylor <me@cgf.cx>
2002-04-09 23:47:26 +0000

    * cygwin.din: Add strptime. * include/cygwin/version.h: Increment API version number.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -149,12 +149,13 @@ details. */
        49: Export setutent, endutent, utmpname, getutent, getutid, getutline.
        50: Export fnmatch.
        51: Export recvmsg, sendmsg.
+       52: Export strptime
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 51
+#define CYGWIN_VERSION_API_MINOR 52
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
