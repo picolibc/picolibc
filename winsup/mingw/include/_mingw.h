4e4871b389bc938d24e0c892e52182a81f2a1c7d
Earnie Boyd <earnie@users.sf.net>
2002-04-09 23:47:01 +0000

    * include/_mingw.h: Increment version. 	* Makefile.in: Ditto.

--- winsup/mingw/include/_mingw.h
+++ winsup/mingw/include/_mingw.h
@@ -60,9 +60,9 @@
 # define __int64 long long
 #endif /* __GNUC__ */
 
-#define __MINGW32_VERSION 1.3
+#define __MINGW32_VERSION 1.4
 #define __MINGW32_MAJOR_VERSION 1
-#define __MINGW32_MINOR_VERSION 3
+#define __MINGW32_MINOR_VERSION 4
 
 #endif /* __MINGW_H */
 
