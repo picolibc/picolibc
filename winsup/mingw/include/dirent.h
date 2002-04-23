1d401c02afb3f6b96350a39997763427da752355
Danny Smith <dannysmith@users.sourceforge.net>
2002-04-23 01:59:06 +0000

    Make wide char versions of opendir and friends. 	* include/dirent.h (_wdirent, _WDIR): Define wide versions of 	struct dirent, DIR. 	(_wopendir,_wreaddir,_wclosedir,_wrewinddir,_wtelldir, 	_wseekdir): Add prototypes for wide versions of corresponding 	standard functions. 	* include/tchar.h; Add _UNICODE mappings for dirent.h 	structures and functions. 	* mingwex/dirent.c: Make _UNICODE neutral. 	* mingwex/wdirent.c: New file to define _UNICODE before 	including dirent.c. 	* mingwex/Makefile.in (DISTFILES): Add wdirent.c/ 	(POSIX_OBJS): Add wdirent.o. 	(wdirent.o): Specify dependency on dirent.c as well as 	wdirent.c. 	* samples/dirent/wtest.c: New file, wide version of test.c.

--- winsup/mingw/include/dirent.h
+++ winsup/mingw/include/dirent.h
@@ -77,12 +77,65 @@ typedef struct
 } DIR;
 
 
+
 DIR*		opendir (const char*);
 struct dirent*	readdir (DIR*);
 int		closedir (DIR*);
 void		rewinddir (DIR*);
 long		telldir (DIR*);
 void		seekdir (DIR*, long);
+
+
+/* wide char versions */
+
+struct _wdirent
+{
+	long		d_ino;		/* Always zero. */
+	unsigned short	d_reclen;	/* Always zero. */
+	unsigned short	d_namlen;	/* Length of name in d_name. */
+	wchar_t*	d_name;		/* File name. */
+	/* NOTE: The name in the dirent structure points to the name in the
+	 *       wfinddata_t structure in the _WDIR. */
+};
+
+/*
+ * This is an internal data structure. Good programmers will not use it
+ * except as an argument to one of the functions below.
+ */
+typedef struct
+{
+	/* disk transfer area for this dir */
+	struct _wfinddata_t	dd_dta;
+
+	/* dirent struct to return from dir (NOTE: this makes this thread
+	 * safe as long as only one thread uses a particular DIR struct at
+	 * a time) */
+	struct _wdirent		dd_dir;
+
+	/* _findnext handle */
+	long			dd_handle;
+
+	/*
+         * Status of search:
+	 *   0 = not started yet (next entry to read is first entry)
+	 *  -1 = off the end
+	 *   positive = 0 based index of next entry
+	 */
+	short			dd_stat;
+
+	/* given path for dir with search pattern (struct is extended) */
+	wchar_t			dd_name[1];
+} _WDIR;
+
+
+
+_WDIR*		_wopendir (const wchar_t*);
+struct _wdirent* _wreaddir (_WDIR*);
+int		_wclosedir (_WDIR*);
+void		_wrewinddir (_WDIR*);
+long		_wtelldir (_WDIR*);
+void		_wseekdir (_WDIR*, long);
+
 
 #ifdef	__cplusplus
 }
