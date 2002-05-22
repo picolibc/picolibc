1d2b71d6a351727a83db3807fce1b45a3d7f47db
Corinna Vinschen <corinna@vinschen.de>
2002-05-22 12:47:54 +0000

    * include/cygwin/types.h: Revert previous patch.

--- winsup/cygwin/include/cygwin/types.h
+++ winsup/cygwin/include/cygwin/types.h
@@ -50,7 +50,6 @@ typedef __gid16_t gid_t;
 #endif
 
 #if !defined(__INSIDE_CYGWIN__) || !defined(__cplusplus)
-#if defined(_POSIX_THREADS)
 
 typedef void *pthread_t;
 typedef void *pthread_mutex_t;
@@ -88,7 +87,6 @@ typedef class pthread_rwlockattr *pthread_rwlockattr_t;
 
 /* semaphores types */
 typedef class semaphore *sem_t;
-#endif /* defined(_POSIX_THREADS) */
 #endif /* __INSIDE_CYGWIN__ */
 #endif /* _CYGWIN_TYPES_H */
 
