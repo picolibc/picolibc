f6b2b56722de6c25dff893c19392a1b446c7f4ae
Jeff Johnston <jjohnstn@redhat.com>
2002-05-23 19:52:46 +0000

    2002-05-23  Jeff Johnston  <jjohnstn@redhat.com>
            * libc/sys/linux/Makefile.am: Add resource.c.
            * libc/sys/linux/Makefile.in: Regenerated.
            * libc/sys/linux/resource.c: New file.
            * libc/sys/linux/time.c: Add settimeofday, getitimer, and setitimer.
            * libc/sys/linux/machine/i386/Makefile.am: Remove syscalls.c.
            * libc/sys/linux/machine/i386/Makefile.in: Regenerated.
            * libc/sys/linux/machine/i386/syscalls.c: Removed as functions
            are now found in libc/sys/linux/resource.c.

--- newlib/libc/sys/linux/time.c
+++ newlib/libc/sys/linux/time.c
@@ -12,7 +12,10 @@
 
 _syscall1(time_t,time,time_t *,t)
 _syscall1(clock_t,times,struct tms *,buf)
+_syscall2(int,getitimer,int,type,struct itimerval *,old)
+_syscall3(int,setitimer,int,type,const struct itimerval *,new,struct itimerval *,old)
 _syscall2(int,gettimeofday,struct timeval *,tv,struct timezone *,tz)
+_syscall2(int,settimeofday,const struct timeval *,tv,const struct timezone *,tz)
 _syscall2(int,nanosleep,const struct timespec *,req,struct timespec *,rem)
 
 weak_alias(__libc_gettimeofday,__gettimeofday);
