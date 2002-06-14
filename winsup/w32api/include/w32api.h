ced1e577b77b5bf0abab7fdcce453fdf0fb249ce
Earnie Boyd <earnie@users.sf.net>
2002-06-14 13:46:12 +0000

    * include/w32api.h: Change to version 2.0 to reflect the change 	in the license. 	* README: Renamed. 	* README.w32api: Renamed from README.  Modified license to remove 	the restriction of notifying the author based on the fact that the 	author is unreachable at the notified address. 	* Makefile.in (VERSION): Change to 2.0.

--- winsup/w32api/include/w32api.h
+++ winsup/w32api/include/w32api.h
@@ -4,8 +4,8 @@
 #pragma GCC system_header
 #endif
 
-#define __W32API_VERSION 1.5
-#define __W32API_MAJOR_VERSION 1
-#define __W32API_MINOR_VERSION 5
+#define __W32API_VERSION 2.0
+#define __W32API_MAJOR_VERSION 2
+#define __W32API_MINOR_VERSION 0
 
 #endif /* ndef _W32API_H_ */
