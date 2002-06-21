55105f8660765dfc0e48a4c6005eba93f49c3fe1
Jeff Johnston <jjohnstn@redhat.com>
2002-06-21 18:44:09 +0000

    2002-06-21  Richard Earnshaw  (rearnsha@arm.com)
            * libc/sys/arm/sys/param.h (BIG_ENDIAN, LITTLE_ENDIAN): Define.
            (BYTE_ORDER): Define as appropriate for the target.

--- newlib/libc/sys/arm/sys/param.h
+++ newlib/libc/sys/arm/sys/param.h
@@ -7,4 +7,13 @@
 # define NOFILE	(60)
 # define PATHSIZE (1024)
 
+#define BIG_ENDIAN      4321
+#define LITTLE_ENDIAN   1234
+
+#ifdef __ARMEB__
+#define BYTE_ORDER BIG_ENDIAN
+#else
+#define BYTE_ORDER LITTLE_ENDIAN
+#endif
+
 #endif
