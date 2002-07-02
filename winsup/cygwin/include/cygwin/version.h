b7e51146bfef4e3677633a8c18d943551ccf0c86
Corinna Vinschen <corinna@vinschen.de>
2002-07-02 09:40:07 +0000

    * include/cygwin/version.h: Bump API minor version.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -151,12 +151,13 @@ details. */
        51: Export recvmsg, sendmsg.
        52: Export strptime
        53: Export strlcat, strlcpy.
+       54: Export __fpclassifyd, __fpclassifyf, __signbitd, __signbitf.
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 53
+#define CYGWIN_VERSION_API_MINOR 54
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
