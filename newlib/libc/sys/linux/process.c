a6cac46c1741cc686da46585c16b17c2acb9f416
Thomas Fitzsimmons <fitzsim@redhat.com>
2002-07-04 19:57:57 +0000

    * libtool.m4: New file. 	* libc/sys/linux/process.c: Implement vfork in terms of fork, 	rather than as a syscall.

--- newlib/libc/sys/linux/process.c
+++ newlib/libc/sys/linux/process.c
@@ -4,6 +4,7 @@
 
 
 #include <sys/unistd.h>
+#include <sys/wait.h>
 #include <machine/syscall.h>
 
 
@@ -11,7 +12,6 @@
 #define __NR__execve __NR_execve
 
 _syscall0(int,fork)
-_syscall0(pid_t,vfork)
 _syscall3(int,_execve,const char *,file,char * const *,argv,char * const *,envp)
 _syscall0(int,getpid)
 _syscall2(int,setpgid,pid_t,pid,pid_t,pgid)
@@ -20,6 +20,25 @@ _syscall0(pid_t,getpgrp)
 _syscall0(pid_t,setsid)
 
 weak_alias(__libc_getpid,__getpid);
+
+pid_t vfork(void)
+{
+  pid_t pid;
+
+  pid = fork();
+  
+  if(!pid)
+    {
+      /* In child. */
+      return 0;
+    }
+  else
+    {
+      /* In parent.  Wait for child to finish. */
+      if (waitpid (pid, NULL, 0) < 0)
+        return pid;
+    }
+}
 
 /* FIXME: get rid of noreturn warning */
 
