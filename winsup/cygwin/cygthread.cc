92655eab172c728429b5b0f3f786b443663a3f3e
Christopher Faylor <me@cgf.cx>
2002-08-01 16:27:29 +0000

    * cygthread.cc: Remove cruft.

--- winsup/cygwin/cygthread.cc
+++ winsup/cygwin/cygthread.cc
@@ -6,28 +6,6 @@ This software is a copyrighted work licensed under the terms of the
 Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
 details. */
 
-class cygthread
-{
-  DWORD avail;
-  DWORD id;
-  HANDLE h;
-  HANDLE ev;
-  const char *__name;
-  LPTHREAD_START_ROUTINE func;
-  VOID *arg;
-  static DWORD main_thread_id;
-  static DWORD WINAPI runner (VOID *);
-  static DWORD WINAPI stub (VOID *);
- public:
-  static const char * name (DWORD = 0);
-  cygthread (LPTHREAD_START_ROUTINE, LPVOID, const char *);
-  cygthread () {};
-  static void init ();
-  void detach ();
-  operator HANDLE ();
-  static bool is ();
-  void * operator new (size_t);
-};
 #include "winsup.h"
 #include "exceptions.h"
 #include "security.h"
