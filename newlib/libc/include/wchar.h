7d08241b68428bba98089eb841cef8eddd87f27b
Jeff Johnston <jjohnstn@redhat.com>
2002-08-27 21:20:10 +0000

    2002-08-27  Egor Duda  <deo@logos-m.ru>
            * libc/stdlib/wmemchr.c: New file.
            * libc/stdlib/wmemcmp.c: Ditto.
            * libc/stdlib/wmemcpy.c: Ditto.
            * libc/stdlib/wmemmove.c: Ditto.
            * libc/stdlib/wmemset.c: Ditto.
            * libc/stdlib/Makefile.am (GENERAL_SOURCES): Add new files.
            * configure.host: Default -DMB_CAPABLE for cygwin.
            * libc/include/wchar.h: Declare wmemchr(), wmemcmp(), wmemcpy(),
            wmemmove() and wmemset().  Add include of <_ansi.h>.
            * libc/stdlib/Makefile.in: Regenerate.

--- newlib/libc/include/wchar.h
+++ newlib/libc/include/wchar.h
@@ -1,6 +1,8 @@
 #ifndef _WCHAR_H_
 #define _WCHAR_H_
 
+#include <_ansi.h>
+
 #define __need_size_t
 #define __need_wchar_t
 #define __need_wint_t
@@ -26,5 +28,10 @@ size_t mbrtowc(wchar_t *pwc, const char *s, size_t n, mbstate_t *ps);
 size_t wcrtomb(char *s, wchar_t wc, mbstate_t *ps);
 size_t mbsrtowcs(wchar_t *dst, const char **src, size_t len, mbstate_t *ps);
 size_t wcsrtombs(char *dst, const wchar_t **src, size_t len, mbstate_t *ps);
+wchar_t *wmemchr(const wchar_t *ws, wchar_t wc, size_t n);
+int wmemcmp(const wchar_t *ws1, const wchar_t *ws2, size_t n);
+wchar_t *wmemcpy(wchar_t *ws1, const wchar_t *ws2, size_t n);
+wchar_t *wmemmove(wchar_t *ws1, const wchar_t *ws2, size_t n);
+wchar_t *wmemset(wchar_t *ws, wchar_t wc, size_t n);
 
 #endif /* _WCHAR_H_ */
