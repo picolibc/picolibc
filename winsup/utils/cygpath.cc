9b566b9695a3324e0856ae4c6e33eb0799e6682f
Joshua Daniel Franklin <joshuadfranklin@yahoo.com>
2002-09-15 15:19:49 +0000

    2002-09-12  Igor Pechtchanski <pechtcha@cs.nyu.edu> 	* cygpath.cc (options) New global variable. 	(main) Make short options global for easier change. 	(print_version) Add a missing newline.

--- winsup/utils/cygpath.cc
+++ winsup/utils/cygpath.cc
@@ -57,6 +57,8 @@ static struct option long_options[] = {
   {0, no_argument, 0, 0}
 };
 
+static char options[] = "ac:df:hilmopst:uvwADHPSW";
+
 static void
 usage (FILE * stream, int status)
 {
@@ -534,7 +536,8 @@ print_version ()
 cygpath (cygwin) %.*s\n\
 Path Conversion Utility\n\
 Copyright 1998, 1999, 2000, 2001, 2002 Red Hat, Inc.\n\
-Compiled on %s", len, v, __DATE__);
+Compiled on %s\n\
+", len, v, __DATE__);
 }
 
 int
@@ -562,7 +565,7 @@ main (int argc, char **argv)
   options_from_file_flag = 0;
   allusers_flag = 0;
   output_flag = 0;
-  while ((c = getopt_long (argc, argv, (char *) "ac:df:hilmopst:uvwADHPSW",
+  while ((c = getopt_long (argc, argv, options,
 			   long_options, (int *) NULL)) != EOF)
     {
       switch (c)
