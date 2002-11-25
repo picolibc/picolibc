4e85569d1146c519fdb51758df7d8ca5c92e4311
Earnie Boyd <earnie@users.sf.net>
2002-11-25 21:15:52 +0000

    * include/w32api.h: Increment to version 2.2 	* Makefile.in: Ditto.

--- winsup/w32api/include/w32api.h
+++ winsup/w32api/include/w32api.h
@@ -4,8 +4,8 @@
 #pragma GCC system_header
 #endif
 
-#define __W32API_VERSION 2.1
+#define __W32API_VERSION 2.2
 #define __W32API_MAJOR_VERSION 2
-#define __W32API_MINOR_VERSION 1
+#define __W32API_MINOR_VERSION 2
 
 #endif /* ndef _W32API_H_ */
