aa6ad99be8a625886de3e411bf7ff82e6a416ba7
Danny Smith <dannysmith@users.sourceforge.net>
2002-12-26 05:05:46 +0000

    * include/unknwn.h: Include windows.h and ole2.h before header 	guard to avoid circular inclusion of COM headers.

--- winsup/w32api/include/unknwn.h
+++ winsup/w32api/include/unknwn.h
@@ -1,12 +1,12 @@
+#ifndef COM_NO_WINDOWS_H
+#include <windows.h>
+#include <ole2.h>
+#endif
+
 #ifndef _UNKNWN_H
 #define _UNKNWN_H
 #if __GNUC__ >=3
 #pragma GCC system_header
-#endif
-
-#ifndef COM_NO_WINDOWS_H
-#include <windows.h>
-#include <ole2.h>
 #endif
 
 #ifdef __cplusplus
