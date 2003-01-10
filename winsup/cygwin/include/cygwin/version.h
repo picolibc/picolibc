70d61f30fe4b853f2d2c3d9a53ec261a0a9873dc
Corinna Vinschen <corinna@vinschen.de>
2003-01-10 12:19:17 +0000

    * cygwin.din: add asprintf and vasprintf, as well as the reentrant 	versions and underscore variants. 	* include/cygwin/version.h: bump CYGWIN_VERSION_API_MINOR.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -167,12 +167,13 @@ details. */
        67: Export pthread_getsequence_np
        68: Export netdb stuff
        69: Export strtof
+       70: Export asprintf, _asprintf_r, vasprintf, _vasprintf_r
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 69
+#define CYGWIN_VERSION_API_MINOR 70
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
