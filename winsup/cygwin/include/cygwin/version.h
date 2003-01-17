afa378e74a5bf8126053ee47a1be536d7cdcd822
Corinna Vinschen <corinna@vinschen.de>
2003-01-17 13:08:06 +0000

    * cygwin.din: Add strerror_r. 	* include/cygwin/version.h: Bump API minor number.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -168,12 +168,13 @@ details. */
        68: Export netdb stuff
        69: Export strtof
        70: Export asprintf, _asprintf_r, vasprintf, _vasprintf_r
+       71: Export strerror_r
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 70
+#define CYGWIN_VERSION_API_MINOR 71
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
