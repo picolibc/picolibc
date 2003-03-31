5c5e8b1dad73741fed064ec134ca18d2e6795338
Danny Smith <dannysmith@users.sourceforge.net>
2003-03-31 22:36:41 +0000

    * include/stdarg.h: Get rid of namespaces.

--- winsup/mingw/include/stdarg.h
+++ winsup/mingw/include/stdarg.h
@@ -1,40 +1,8 @@
-/* 
- * stdarg.h 
- * Forwarding header to get gcc's stdarg.h and keep std namespace clean
+/*
+ * This is just an RC_INVOKED guard for the real stdarg.h
+ * included in gcc system dir.  One day we will delete this file.
  */
 
 #ifndef RC_INVOKED 
-
-#include <_mingw.h>
-
-#ifdef __cplusplus
-
-namespace __ginclude
-{
-  /* Include gcc's stdarg.h.  */
-# include_next<stdarg.h>
-}
-
-/* If invocation was from the user program, push va_list into std. */  
-#if defined  (_STDARG_H)
-
-__BEGIN_CSTD_NAMESPACE
-  using __ginclude::va_list;
-__END_CSTD_NAMESPACE
-
-/* Otherwise inject __gnuc_va_list into global.  */
-
-#else
-
-using __ginclude::__gnuc_va_list;
-
-#endif /* _STDARG_H */
-
-#else /* __cplusplus */
-
-/* Just include gcc's stdarg.h.  */
 #include_next<stdarg.h>
-
-#endif
-
 #endif /* RC_INVOKED */ 
