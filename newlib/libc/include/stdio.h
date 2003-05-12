b47fcdf03dee9b496d37763c6abebcc1e95cbb8c
Corinna Vinschen <corinna@vinschen.de>
2003-05-12 11:26:11 +0000

    * libc/include/stdio.h: Change one __CYGWIN__ to __CYGWIN32__.

--- newlib/libc/include/stdio.h
+++ newlib/libc/include/stdio.h
@@ -79,7 +79,7 @@ typedef _fpos64_t fpos64_t;
 #define	__SNPT	0x0800		/* do not do fseek() optimisation */
 #define	__SOFF	0x1000		/* set iff _offset is in fact correct */
 #define	__SMOD	0x2000		/* true => fgetline modified _p text */
-#if defined(__CYGWIN__) || defined(__CYGWIN__)
+#if defined(__CYGWIN__) || defined(__CYGWIN32__)
 #  define __SCLE  0x4000        /* convert line endings CR/LF <-> NL */
 #endif
 #define	__SL64	0x8000		/* is 64-bit offset large file */
