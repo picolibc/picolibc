5b429f26744bb7babfb7dd0075ebe6d865bfb069
Christopher Faylor <me@cgf.cx>
2003-07-17 05:27:03 +0000

    update copyright

--- winsup/cygwin/times.cc
+++ winsup/cygwin/times.cc
@@ -1,6 +1,6 @@
 /* times.cc
 
-   Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002 Red Hat, Inc.
+   Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003 Red Hat, Inc.
 
 This file is part of Cygwin.
 
