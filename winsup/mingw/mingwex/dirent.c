c536f54adab29e9646a91639ce205ec91b1f9234
Danny Smith <dannysmith@users.sourceforge.net>
2003-10-02 21:29:51 +0000

    * mingwex/dirent.c (_treaddir): Reset errno to 0 if end 	of directory.

--- winsup/mingw/mingwex/dirent.c
+++ winsup/mingw/mingwex/dirent.c
@@ -165,7 +165,12 @@ _treaddir (_TDIR * dirp)
       /* Get the next search entry. */
       if (_tfindnext (dirp->dd_handle, &(dirp->dd_dta)))
 	{
-	  /* We are off the end or otherwise error. */
+	  /* We are off the end or otherwise error.	
+	     _findnext sets errno to ENOENT if no more file
+	     Undo this. */ 
+	  DWORD winerr = GetLastError();
+	  if (winerr == ERROR_NO_MORE_FILES)
+	    errno = 0;	
 	  _findclose (dirp->dd_handle);
 	  dirp->dd_handle = -1;
 	  dirp->dd_stat = -1;
