84f08ba160fd0a6233865f501866c6bf46f8d7b0
Christopher Faylor <me@cgf.cx>
2003-11-25 19:24:46 +0000

    * winsup.api/known_bugs.tcl: Remove lseek10 since mknod now works.

--- winsup/testsuite/winsup.api/known_bugs.tcl
+++ winsup/testsuite/winsup.api/known_bugs.tcl
@@ -1,6 +1,6 @@
 set xfail_list [list dup03 dup05 \
     fcntl05 fcntl07B fcntl09 fcntl10 \
-    lseek04 lseek10 mknod01 select03 \
+    lseek04 mknod01 select03 \
     setgroups01 setuid02 \
     ulimit01 unlink06 unlink08 \
     sample-fail sample-miscompile]
