ff41498a1931f86da1089e8d0bb88dc4f3973273
Jeff Johnston <jjohnstn@redhat.com>
2004-01-30 20:32:04 +0000

    2004-01-30  Thomas Pfaff  <tpfaff@gmx.net>             Jeff Johnston  <jjohnstn@redhat.com>
            * libc/stdio/fwalk.c (_fwalk): Traverse the given reentrancy
            struct for std streams and traverse the global reeentrancy
            struct for all other streams.

--- newlib/libc/stdio/fwalk.c
+++ newlib/libc/stdio/fwalk.c
@@ -35,9 +35,17 @@ _fwalk (ptr, function)
   register int n, ret = 0;
   register struct _glue *g;
 
+  /* Must traverse given list for std streams.  */
   for (g = &ptr->__sglue; g != NULL; g = g->_next)
     for (fp = g->_iobs, n = g->_niobs; --n >= 0; fp++)
       if (fp->_flags != 0)
 	ret |= (*function) (fp);
+
+  /* Must traverse global list for all other streams.  */
+  for (g = &_GLOBAL_REENT->__sglue; g != NULL; g = g->_next)
+    for (fp = g->_iobs, n = g->_niobs; --n >= 0; fp++)
+      if (fp->_flags != 0)
+	ret |= (*function) (fp);
+
   return ret;
 }
