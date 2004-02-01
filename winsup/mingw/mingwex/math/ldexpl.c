f6565cd1a68b4a4b7c43b588513cd802597bc5d3
Danny Smith <dannysmith@users.sourceforge.net>
2004-02-01 02:52:21 +0000

    * mingwex/math/ldexpl.c (ldexpl): Call __asm__("fscale") 	directly, rather than via scabnl.

--- winsup/mingw/mingwex/math/ldexpl.c
+++ winsup/mingw/mingwex/math/ldexpl.c
@@ -1,14 +1,19 @@
 #include <math.h>
 #include <errno.h>
-
-
+  
 long double ldexpl(long double x, int expn)
 {
-  if (isfinite (x) && x != 0.0L)
-    {
-      x = scalbnl (x , expn);
-      if (!isfinite (x) || x == 0.0) errno = ERANGE;
-    }
-  return x;
+  long double res;
+  if (!isfinite (x) || x == 0.0L)
+    return x;
+
+  __asm__ ("fscale"
+  	    : "=t" (res)
+	    : "0" (x), "u" ((long double) expn));
+
+  if (!isfinite (res) || res == 0.0L)
+    errno = ERANGE;
+
+  return res;
 }
 
