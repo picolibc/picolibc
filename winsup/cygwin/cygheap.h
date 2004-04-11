1ed8d18733295428499ee4ee620ad22acb172c27
Christopher Faylor <me@cgf.cx>
2004-04-11 17:36:05 +0000

    * cygheap.h: Replace inexplicably removed closing brace.

--- winsup/cygwin/cygheap.h
+++ winsup/cygwin/cygheap.h
@@ -376,3 +376,4 @@ char *__stdcall cstrdup1 (const char *) __attribute__ ((regparm(1)));
 void __stdcall cfree_and_set (char *&, char * = NULL) __attribute__ ((regparm(2)));
 void __stdcall cygheap_init ();
 extern DWORD _cygheap_start;
+}
