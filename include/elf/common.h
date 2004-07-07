49e7d4dbb387fbd94b9d656aa46b845c6622461a
Nick Clifton <nickc@redhat.com>
2004-07-07 17:28:50 +0000

    Add new port: crx-elf

--- include/elf/common.h
+++ include/elf/common.h
@@ -182,6 +182,7 @@
 #define EM_IP2K		101	/* Ubicom IP2022 micro controller */
 #define EM_CR		103	/* National Semiconductor CompactRISC */
 #define EM_MSP430	105	/* TI msp430 micro controller */
+#define EM_CRX		114	/* National Semiconductor CRX */
 
 /* If it is necessary to assign new unofficial EM_* values, please pick large
    random numbers (0x8523, 0xa7f2, etc.) to minimize the chances of collision
