42347b0a3bc3f74f7d6761e678997e97522bb9e6
Corinna Vinschen <corinna@vinschen.de>
2005-01-03 18:11:00 +0000

    * signal.cc: Fix copyright date.

--- winsup/cygwin/signal.cc
+++ winsup/cygwin/signal.cc
@@ -1,6 +1,7 @@
 /* signal.cc
 
-   Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004 Red Hat, Inc.
+   Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
+   2005 Red Hat, Inc.
 
    Written by Steve Chamberlain of Cygnus Support, sac@cygnus.com
    Significant changes by Sergey Okhapkin <sos@prospect.com.ru>
