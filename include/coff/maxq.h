c4a0585f6034feb31c2a99f3d79465786c572da2
Nick Clifton <nickc@redhat.com>
2005-01-10 16:33:51 +0000

    Add support for maxq10 and maxq20 machine values

--- include/coff/maxq.h
+++ include/coff/maxq.h
@@ -1,6 +1,6 @@
 /* COFF spec for MAXQ
 
-   Copyright 2004 Free Software Foundation, Inc.
+   Copyright 2004, 2005 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the Free 
@@ -25,13 +25,18 @@
 
 /* Bits for f_flags: F_RELFLG relocation info stripped from file F_EXEC file
    is executable (no unresolved external references) F_LNNO line numbers
-   stripped from file F_LSYMS local symbols stripped from file F_AR32WR file
-   has byte ordering of an AR32WR machine (e.g. vax).  */
+   stripped from file F_LSYMS local symbols stripped from file.  */
 
 #define F_RELFLG        (0x0001)
 #define F_EXEC          (0x0002)
 #define F_LNNO          (0x0004)
 #define F_LSYMS         (0x0008)
+
+/* Variant Specific Flags for MAXQ10 and MAXQ20.  */
+#define F_MAXQ10	(0x0030)
+#define F_MAXQ20	(0x0040)
+
+#define F_MACHMASK	(0x00F0)
 
 /* Magic numbers for maxq.  */
 #define MAXQ20MAGIC      0xa0
