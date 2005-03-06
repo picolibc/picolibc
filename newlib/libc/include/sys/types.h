2e7d9bf906c37584962e7d89b6de55c1fec9eeb3
Jeff Johnston <jjohnstn@redhat.com>
2005-03-06 20:32:16 +0000

    2005-03-06  Ralf Corsepious  <ralf.corsepius@rtems.org>
            * libc/include/machine/_types.h: New file.
            * libc/include/sys/types.h: Do not check for __rtems__
            when including <machine/_types.h>.  Remove some redundant
            declarations now that <machine/_types.h> is included.
            * libc/sys/rtems/machine/_types.h: Removed.  Replaced with
            shared header file.

--- newlib/libc/include/sys/types.h
+++ newlib/libc/include/sys/types.h
@@ -22,34 +22,14 @@
 #ifndef __INTTYPES_DEFINED__
 #define __INTTYPES_DEFINED__
 
-#if !defined (__rtems__)
-typedef short int __int16_t;
-typedef unsigned short int __uint16_t;
+#include <machine/_types.h>
 
-#if __INT_MAX__ == 32767
-typedef long int __int32_t;
-typedef unsigned long int __uint32_t;
-#else
-typedef int __int32_t;
-typedef unsigned int __uint32_t;
-#endif
-
-#if __LONG_MAX__ > 2147483647 || !defined(__GNUC__)
-typedef long int __int64_t;
-typedef unsigned long int __uint64_t;
-#else
-__extension__ typedef long long __int64_t;
-__extension__ typedef unsigned long long __uint64_t;
-#endif
-#else /* __rtems__ */
-
+#if defined(__rtems__)
 /*
  *  The following section is RTEMS specific and is needed to more
  *  closely match the types defined in the BSD sys/types.h.
  *  This is needed to let the RTEMS/BSD TCP/IP stack compile.
  */
-
-#include <machine/_types.h>
 
 /* deprecated */
 #if ___int8_t_defined
