5f8e4efa678ccb7b699a04f980b2a60e776c90da
Christopher Faylor <me@cgf.cx>
2005-03-06 18:00:03 +0000

    * cygpath.cc (usage): Pass in one more copy of program_name to printf to avoid a SEGV.

--- winsup/utils/cygpath.cc
+++ winsup/utils/cygpath.cc
@@ -89,7 +89,7 @@ System information:\n\
   -P, --smprograms	output Start Menu `Programs' directory and exit\n\
   -S, --sysdir		output system directory and exit\n\
   -W, --windir		output `Windows' directory and exit\n\
-", prog_name, prog_name);
+", prog_name, prog_name, prog_name);
   if (ignore_flag)
     /* nothing to do */;
   else if (stream != stdout)
