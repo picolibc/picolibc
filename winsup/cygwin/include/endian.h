071f64d93433fddcd1cb39bcaa1d08e5a8d97c48
Christopher Faylor <me@cgf.cx>
2005-05-19 19:44:31 +0000

    * include/endian.h: New file.

---
+++ winsup/cygwin/include/endian.h
@@ -0,0 +1,17 @@
+/* io.h
+
+   Copyright 2005 Red Hat, Inc.
+
+This file is part of Cygwin.
+
+This software is a copyrighted work licensed under the terms of the
+Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
+details. */
+
+#ifndef _ENDIAN_H_
+#define _ENDIAN_H_
+
+#include <machine/endian.h>
+
+#endif /* _IO_H_ */
+
