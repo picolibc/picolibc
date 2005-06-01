31d0f220adb060b792367d956b59a934b91cffa0
Christopher Faylor <me@cgf.cx>
2005-06-01 18:24:31 +0000

    * include/machine/stdlib.h: New file.

---
+++ winsup/cygwin/include/machine/stdlib.h
@@ -0,0 +1,22 @@
+/* machine/stdlib.h
+
+  Copyright 2005 Red Hat, Inc.
+
+  This file is part of Cygwin.
+
+  This software is a copyrighted work licensed under the terms of the
+  Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
+  details. */
+
+#ifndef _MACHSTDLIB_H_
+#define _MACHSTDLIB_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char *mkdtemp (char *);
+
+#ifdef __cplusplus
+}
+#endif
+#endif  /* _MACHSTDLIB_H_ */
