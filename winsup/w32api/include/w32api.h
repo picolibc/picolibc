8cb66eee67a63093a87494d9a9baeb86401e7b7f
Chris Sutcliffe <ir0nh34d@users.sourceforge.net>
2005-08-11 02:13:36 +0000

    * include/w32api.h: Increment version to 3.3.         * Makefile.in: Ditto.

--- winsup/w32api/include/w32api.h
+++ winsup/w32api/include/w32api.h
@@ -4,9 +4,9 @@
 #pragma GCC system_header
 #endif
 
-#define __W32API_VERSION 3.2
+#define __W32API_VERSION 3.3
 #define __W32API_MAJOR_VERSION 3
-#define __W32API_MINOR_VERSION 2
+#define __W32API_MINOR_VERSION 3
 
 /* The following defines are for documentation purposes.  The following defines
  * identify the versions of Windows and Internet Explorer.  They are not to be
