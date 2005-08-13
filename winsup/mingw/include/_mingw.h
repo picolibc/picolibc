50f196b57f00d411133ef72165015c63562b9c3e
Chris Sutcliffe <ir0nh34d@users.sourceforge.net>
2005-08-13 12:52:43 +0000

    * Include/_mingw.h: Increment version to 3.8.         * Makefile.in: Ditto.

--- winsup/mingw/include/_mingw.h
+++ winsup/mingw/include/_mingw.h
@@ -158,8 +158,8 @@
 # define __MSVCRT_VERSION__ 0x0600
 #endif
 
-#define __MINGW32_VERSION 3.7
+#define __MINGW32_VERSION 3.8
 #define __MINGW32_MAJOR_VERSION 3
-#define __MINGW32_MINOR_VERSION 7
+#define __MINGW32_MINOR_VERSION 8
 
 #endif /* __MINGW_H */
