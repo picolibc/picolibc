58e78add8bc0082c64a05f64c91bb4e0e6ec6c40
Jeff Johnston <jjohnstn@redhat.com>
2005-10-04 20:47:39 +0000

    2005-10-04  James E Wilson  <wilson@specifix.com>
    	* libc/include/sys/dirent.h (_DIRENT_H_): Delete #include_next.  Add
    	#error.

--- newlib/libc/include/sys/dirent.h
+++ newlib/libc/include/sys/dirent.h
@@ -1,15 +1,13 @@
 /* <dirent.h> includes <sys/dirent.h>, which is this file.  On a
    system which supports <dirent.h>, this file is overridden by
    dirent.h in the libc/sys/.../sys directory.  On a system which does
-   not support <dirent.h>, we will get this file which tries to find
-   any other <dirent.h> which may be lurking around.  If there isn't
-   one, the user will get an error indicating that there is no
-   <dirent.h>.  */
+   not support <dirent.h>, we will get this file which uses #error to force
+   an error.  */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-#include_next <dirent.h>
+#error "<dirent.h> not supported"
 #ifdef __cplusplus
 }
 #endif
