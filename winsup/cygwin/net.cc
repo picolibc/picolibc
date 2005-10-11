f2e8d7b0f062b86286b7969d7b7ec1d82ffd4a18
Christopher Faylor <me@cgf.cx>
2005-10-11 16:35:31 +0000

    * net.cc (cygwin_gethostbyname): Remove debugging.

--- winsup/cygwin/net.cc
+++ winsup/cygwin/net.cc
@@ -969,10 +969,7 @@ cygwin_gethostbyname (const char *name)
 
   hostent *res = (hostent *) dup_ent (hostent_buf, h, t_hostent);
   if (res)
-{
     debug_printf ("h_name %s", res->h_name);
-debug_printf ("HERE");
-}
   else
     {
       debug_printf ("dup_ent returned NULL for name %s, h %p", name, h);
