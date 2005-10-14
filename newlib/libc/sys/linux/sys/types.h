e9154015a09ac328f94c126e3253c9ebcff7b004
Jeff Johnston <jjohnstn@redhat.com>
2005-10-14 20:01:55 +0000

    2005-10-14  Jeff Johnston  <jjohnstn@redhat.com>
            * libc/sys/linux/sys/types.h (int8_t): Add type.

--- newlib/libc/sys/linux/sys/types.h
+++ newlib/libc/sys/linux/sys/types.h
@@ -166,6 +166,7 @@ typedef __uint8_t u_int8_t;
 typedef __uint16_t u_int16_t;
 typedef __uint32_t u_int32_t;
 typedef __uint64_t u_int64_t;
+typedef __int8_t int8_t;
 typedef __int16_t int16_t;
 typedef __int32_t int32_t;
 typedef __int64_t int64_t;
