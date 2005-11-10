c845f0c182fa2a9bd2d8f9961e1968ce8d725f54
Christopher Faylor <me@cgf.cx>
2005-11-10 21:17:15 +0000

    * include/endian.h: Remove USE_BSD ifdef since it is never defined on Cygwin.

--- winsup/cygwin/include/endian.h
+++ winsup/cygwin/include/endian.h
@@ -24,12 +24,12 @@ details. */
 # define __BYTE_ORDER __LITTLE_ENDIAN
 #endif
 
-#ifdef  __USE_BSD
+/*#ifdef  __USE_BSD*/
 # define LITTLE_ENDIAN  __LITTLE_ENDIAN
 # define BIG_ENDIAN     __BIG_ENDIAN
 # define PDP_ENDIAN     __PDP_ENDIAN
 # define BYTE_ORDER     __BYTE_ORDER
-#endif
+/*#endif*/
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
 # define __LONG_LONG_PAIR(HI, LO) LO, HI
