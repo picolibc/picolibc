808139f1af2a37d898f06664cc2f8124f2b61e70
Christopher Faylor <me@cgf.cx>
2005-11-18 15:45:51 +0000

    * include/cygwin/sys_time.h: Rename from include/cygwin/time.h. * include/cygwin/time.h: New file.

--- winsup/cygwin/include/cygwin/time.h
+++ winsup/cygwin/include/cygwin/time.h
@@ -1,4 +1,4 @@
-/* sys_time.h
+/* time.h
 
    Copyright 2005 Red Hat Inc.
 
@@ -8,18 +8,18 @@ This software is a copyrighted work licensed under the terms of the
 Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
 details. */
 
-#ifndef _CYGWIN_SYS_TIME_H
-#define _CYGWIN_SYS_TIME_H
-#include <sys/select.h>
-
+#ifndef _CYGWIN_TIME_H
+#define _CYGWIN_TIME_H
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+int nanosleep (const struct timespec  *, struct timespec *);
 int clock_setres (clockid_t, struct timespec *);
+int clock_getres (clockid_t, struct timespec *);
 
 #ifdef __cplusplus
 }
 #endif
-#endif /*_CYGWIN_SYS_TIME_H*/
+#endif /*_CYGWIN_TIME_H*/
