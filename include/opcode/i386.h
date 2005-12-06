466fc394b8b9cfe04689de9fff53a8b49fb3444a
H.J. Lu <hjl.tools@gmail.com>
2005-12-06 12:40:57 +0000

    gas/
    2005-12-06  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR gas/1874
    	* config/tc-i386.c (match_template): Handle monitor.
    	(process_suffix): Likewise.
    
    gas/testsuite/
    
    2005-12-06  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR gas/1874
    	* gas/i386/i386.exp: Add x86-64-prescott for 64bit.
    
    	* gas/i386/prescott.s: Test address size override for monitor.
    	* gas/i386/prescott.d: Updated.
    
    	* gas/i386/x86-64-prescott.d: New file.
    	* gas/i386/x86-64-prescott.s: Likewise.
    
    include/opcode/
    
    2005-12-06  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR gas/1874
    	* i386.h (i386_optab): Add 64bit support for monitor and mwait.
    
    opcodes/
    
    2005-12-06  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR gas/1874
    	* i386-dis.c (address_mode): New enum type.
    	(address_mode): New variable.
    	(mode_64bit): Removed.
    	(ckprefix): Updated to check address_mode instead of mode_64bit.
    	(prefix_name): Likewise.
    	(print_insn): Likewise.
    	(putop): Likewise.
    	(print_operand_value): Likewise.
    	(intel_operand_size): Likewise.
    	(OP_E): Likewise.
    	(OP_G): Likewise.
    	(set_op): Likewise.
    	(OP_REG): Likewise.
    	(OP_I): Likewise.
    	(OP_I64): Likewise.
    	(OP_OFF): Likewise.
    	(OP_OFF64): Likewise.
    	(ptr_reg): Likewise.
    	(OP_C): Likewise.
    	(SVME_Fixup): Likewise.
    	(print_insn): Set address_mode.
    	(PNI_Fixup): Add 64bit and address size override support for
    	monitor and mwait.

--- include/opcode/i386.h
+++ include/opcode/i386.h
@@ -1346,14 +1346,24 @@ static const template i386_optab[] =
 {"hsubps",    2, 0xf20f7d,  X, CpuPNI, NoSuf|IgnoreSize|Modrm,	{ RegXMM|LLongMem, RegXMM, 0 } },
 {"lddqu",     2, 0xf20ff0,  X, CpuPNI, NoSuf|IgnoreSize|Modrm,	{ LLongMem, RegXMM, 0 } },
 {"monitor",   0, 0x0f01, 0xc8, CpuPNI, NoSuf|ImmExt,	{ 0, 0, 0} },
-/* Need to ensure only "monitor %eax,%ecx,%edx" is accepted. */
-{"monitor",   3, 0x0f01, 0xc8, CpuPNI, NoSuf|ImmExt,	{ Reg32, Reg32, Reg32} },
+/* monitor is very special. CX and DX are always 64bits with zero upper
+   32bits in 64bit mode, and 32bits in 16bit and 32bit modes. The
+   address size override prefix can be used to overrride the AX size in
+   all modes.  */
+/* Need to ensure only "monitor %eax/%ax,%ecx,%edx" is accepted. */
+{"monitor",   3, 0x0f01, 0xc8, CpuPNI|CpuNo64, NoSuf|ImmExt,	{ Reg16|Reg32, Reg32, Reg32 } },
+/* Need to ensure only "monitor %rax/%eax,%rcx,%rdx" is accepted. */
+{"monitor",   3, 0x0f01, 0xc8, CpuPNI|Cpu64, NoSuf|ImmExt|NoRex64,	{ Reg32|Reg64, Reg64, Reg64 } },
 {"movddup",   2, 0xf20f12,  X, CpuPNI, NoSuf|IgnoreSize|Modrm,	{ RegXMM|LLongMem, RegXMM, 0 } },
 {"movshdup",  2, 0xf30f16,  X, CpuPNI, NoSuf|IgnoreSize|Modrm,	{ RegXMM|LLongMem, RegXMM, 0 } },
 {"movsldup",  2, 0xf30f12,  X, CpuPNI, NoSuf|IgnoreSize|Modrm,	{ RegXMM|LLongMem, RegXMM, 0 } },
 {"mwait",     0, 0x0f01, 0xc9, CpuPNI, NoSuf|ImmExt,	{ 0, 0, 0} },
+/* mwait is very special. AX and CX are always 64bits with zero upper
+   32bits in 64bit mode, and 32bits in 16bit and 32bit modes.  */
 /* Need to ensure only "mwait %eax,%ecx" is accepted.  */
-{"mwait",     2, 0x0f01, 0xc9, CpuPNI, NoSuf|ImmExt,	{ Reg32, Reg32, 0} },
+{"mwait",     2, 0x0f01, 0xc9, CpuPNI|CpuNo64, NoSuf|ImmExt,	{ Reg32, Reg32, 0} },
+/* Need to ensure only "mwait %rax,%rcx" is accepted.  */
+{"mwait",     2, 0x0f01, 0xc9, CpuPNI|Cpu64, NoSuf|ImmExt|NoRex64,	{ Reg64, Reg64, 0} },
 
 /* VMX instructions.  */
 {"vmcall",    0, 0x0f01, 0xc1, CpuVMX, NoSuf|ImmExt,	{ 0, 0, 0} },
