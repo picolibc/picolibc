dfe2700797bc3606ab11922ca177181d1373d9e4
Dimitri Papadopoulos <dimitri_at@users.sf.net>
2006-05-11 15:58:39 +0000

    * include/ipxrtdef.h: Cleanup. 	(IPX_INTERFACE_INFO_TYPE,IPX_STATIC_ROUTE_INFO_TYPE) 	(IPX_STATIC_SERVICE_INFO_TYPE,IPX_SERVICE_FILTER_INFO_TYPE) 	(IPX_ROUTE_FILTER_INFO_TYPE,IPX_IN_TRAFFIC_FILTER_INFO_TYPE) 	(IPX_ADAPTER_INFO_TYPE,IPXWAN_INTERFACE_INFO_TYPE,IPX_GLOBAL_INFO_TYPE) 	(IPX_STATIC_NETBIOS_NAME_INFO_TYPE) 	(IPX_IN_TRAFFIC_FILTER_GLOBAL_INFO_TYPE) 	(IPX_OUT_TRAFFIC_FILTER_INFO_TYPE) 	(IPX_OUT_TRAFFIC_FILTER_GLOBAL_INFO_TYPE): Define. 	* include/routprot.h: Cleanup. 	(PROTO_IP_*,IPRTRMGR_PID,IPX_PROTOCOL_*): Define. 	* include/fltdefs.h: New file. 	* include/ipinfoid.h: New file.

--- winsup/w32api/include/ipxrtdef.h
+++ winsup/w32api/include/ipxrtdef.h
@@ -1,3 +1,12 @@
+/*
+	ipxrtdef.h - Routing and Remote Access Services
+
+	This file is part of a free library for the Win32 API.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
 #ifndef _IPXRTDEF_H
 #define _IPXRTDEF_H
 #if __GNUC__ >= 3
@@ -10,6 +19,21 @@
 extern "C" {
 #endif
 
+/*--- Router Management Reference - Router Information Enumeration Types */
+#define IPX_INTERFACE_INFO_TYPE 0x0001
+#define IPX_STATIC_ROUTE_INFO_TYPE 0x0002
+#define IPX_STATIC_SERVICE_INFO_TYPE 0x0003
+#define IPX_SERVICE_FILTER_INFO_TYPE 0x0004
+#define IPX_ROUTE_FILTER_INFO_TYPE 0x0005
+#define IPX_IN_TRAFFIC_FILTER_INFO_TYPE 0x0006
+#define IPX_ADAPTER_INFO_TYPE 0x0007
+#define IPXWAN_INTERFACE_INFO_TYPE 0x0008
+#define IPX_GLOBAL_INFO_TYPE 0x0009
+#define IPX_STATIC_NETBIOS_NAME_INFO_TYPE 0x000A
+#define IPX_IN_TRAFFIC_FILTER_GLOBAL_INFO_TYPE 0x000B
+#define IPX_OUT_TRAFFIC_FILTER_INFO_TYPE 0x000C
+#define IPX_OUT_TRAFFIC_FILTER_GLOBAL_INFO_TYPE 0x000D
+/*--- Router Management Reference - Router Information Structures - IPX Information Structures */
 #if (_WIN32_WINNT >= 0x0500)
 typedef struct _IPX_IF_INFO {
 	ULONG AdminState;
