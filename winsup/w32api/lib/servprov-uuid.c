e73b03adcca4543c0ecde8603d9f51dbd5b1f5de
Chris Sutcliffe <ir0nh34d@users.sourceforge.net>
2006-06-04 15:37:31 +0000

    2006-06-04      Chris Sutcliffe <ir0nh34d@users.sourceforge.net>
            * lib/Makefile.in: Add unknwn-uuid and servprov-uuid as sources and
            objects.
            * lib/uuid.c (IID_IUnknown, IID_IClassFactory, IID_IServiceProvider):
            Remove.  Moved to unknwn-uuid and servprov-uuid.
            * lib/unknwn-uuid.c: New file.
            * lib/servprov-uuid.c: New file.

---
+++ winsup/w32api/lib/servprov-uuid.c
@@ -0,0 +1,9 @@
+/* servprov-uuid.c */
+/* Generate GUIDs for ServiceProvider interfaces */
+
+/* All IIDs defined in this file were extracted from
+ * HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Interface\ */
+
+#define INITGUID
+#include <basetyps.h>
+DEFINE_GUID(IID_IServiceProvider,0x6d5140c1,0x7436,0x11ce,0x80,0x34,0,0xaa,0,0x60,0x9,0xfa);
---
+++ winsup/w32api/lib/unknwn-uuid.c
@@ -0,0 +1,10 @@
+/* unknwn-uuid.c */
+/* Generate GUIDs for Unknown interfaces */
+
+/* All IIDs defined in this file were extracted from
+ * HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Interface\ */
+
+#define INITGUID
+#include <basetyps.h>
+DEFINE_OLEGUID(IID_IUnknown,0,0,0);
+DEFINE_OLEGUID(IID_IClassFactory,0x1,0,0);
