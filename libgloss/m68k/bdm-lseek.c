2cbb410debb42ca3fd29fe208522f0124d48c42c
Nathan Sidwell <nathan@codesourcery.com>
2006-06-05 14:22:26 +0000

    * libgloss/m68k/Makefile.in (BDM_SYSOBJS): Remove bdm-inbyte.o. 	Add bdm-time.o. 	* libgloss/m68k/bdm-close.c: Add copyright notice. 	* libgloss/m68k/bdm-fstat.c: Add copyright notice. 	* libgloss/m68k/bdm-gettimeofday.c: Add copyright notice. Fix &/| 	thinko. 	* libgloss/m68k/bdm-inbyte.c: Remove. 	* libgloss/m68k/bdm-isatty.c: Add copyright notice. 	* libgloss/m68k/bdm-lseek.c: Add copyright notice. 	* libgloss/m68k/bdm-open.c: Add copyright notice. 	* libgloss/m68k/bdm-read.c: Add copyright notice. 	* libgloss/m68k/bdm-rename.c: Add copyright notice. 	* libgloss/m68k/bdm-stat.c: Add copyright notice. 	* libgloss/m68k/bdm-system.c: Add copyright notice. 	* libgloss/m68k/bdm-time.c: New. 	* libgloss/m68k/bdm-unlink.c: Add copyright notice. 	* libgloss/m68k/bdm-write.c: Add copyright notice.

--- libgloss/m68k/bdm-lseek.c
+++ libgloss/m68k/bdm-lseek.c
@@ -1,3 +1,19 @@
+/*
+ * bdm-lseek.c -- 
+ *
+ * Copyright (c) 2006 CodeSourcery Inc
+ *
+ * The authors hereby grant permission to use, copy, modify, distribute,
+ * and license this software and its documentation for any purpose, provided
+ * that existing copyright notices are retained in all copies and that this
+ * notice is included verbatim in any distributions. No written agreement,
+ * license, or royalty fee is required for any of the authorized uses.
+ * Modifications to this software may be copyrighted by their authors
+ * and need not follow the licensing terms described here, provided that
+ * the new terms are clearly indicated on the first page of each file where
+ * they apply.
+ */
+
 #include "bdm-semihost.h"
 #include "bdm-gdb.h"
 #include <sys/types.h>
@@ -26,5 +42,5 @@ off_t lseek (int fd, off_t offset, int whence)
   parameters[3] = convert_to_gdb_lseek_flags (whence);
   BDM_TRAP (BDM_LSEEK, (uint32_t)parameters);
   errno = convert_from_gdb_errno (parameters[2]);
-  return ((uint64_t)parameters[0] << 32) & ((uint64_t)parameters[1]);
+  return ((uint64_t)parameters[0] << 32) | ((uint64_t)parameters[1]);
 }
