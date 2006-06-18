5ade5bb0ea4c6579a62d063193bd552b678e9064
Danny Smith <dannysmith@users.sourceforge.net>
2006-06-18 08:16:55 +0000

    * include/sys/time.h: Add header guard.  Add extern "C" bracketing 	for __cplusplus. 	(gettimeofday): Add prototype. 	* mingwex/gettimeofday.c: New file. 	* mingwex/makefile.in: Add gettimeofday source and object.

--- winsup/mingw/include/sys/time.h
+++ winsup/mingw/include/sys/time.h
@@ -1,5 +1,10 @@
-
+#ifndef _SYS_TIME_H_
+#define _SYS_TIME_H_
 #include <time.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
 
 #ifndef _TIMEVAL_DEFINED /* also in winsock[2].h */
 #define _TIMEVAL_DEFINED
@@ -14,3 +19,20 @@ struct timeval {
 	((tvp)->tv_usec cmp (uvp)->tv_usec))
 #define timerclear(tvp)	 (tvp)->tv_sec = (tvp)->tv_usec = 0
 #endif /* _TIMEVAL_DEFINED */
+
+/*
+   Implementation as per:
+   The Open Group Base Specifications, Issue 6
+   IEEE Std 1003.1, 2004 Edition
+
+   The timezone pointer arg is ignored.  Errors are ignored.
+*/ 
+int __cdecl gettimeofday(struct timeval *__restrict__,
+			 void *__restrict__  /*	tzp (unused) */);
+
+#ifdef	__cplusplus
+}
+#endif
+
+
+#endif /* _SYS_TIME_H_ */
