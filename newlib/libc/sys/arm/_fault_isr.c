83cc5b75d985be0f77fafa46a13a74db6b453a12
Paul Brook <paul@codesourcery.com>
2006-08-29 22:05:40 +0000

    2006-08-29  Paul Brook  <paul@codesourcery.com>
    	newlib/
    	* libc/sys/arm/_fault_isr.c (_fault_isr): Compile unconditionally.
    	* libc/sys/arm/_nmi_isr.c (_nmi_isr): Ditto.

--- newlib/libc/sys/arm/_fault_isr.c
+++ newlib/libc/sys/arm/_fault_isr.c
@@ -1,16 +1,12 @@
 #include "newlib.h"
 
-#ifdef _ARM_V7M
-
 /* Called when a hardware fault occurs.  Users can replace this
    function.  */ 
 
-void 
+void
 _fault_isr() 
 { 
   /* Sit an endless loop so that the user can analyze the situation
      from the debugger.  */
   while (1);
 }
-
-#endif
--- newlib/libc/sys/arm/_nmi_isr.c
+++ newlib/libc/sys/arm/_nmi_isr.c
@@ -1,16 +1,12 @@
 #include "newlib.h"
 
-#ifdef _ARM_V7M
-
 /* Called when a non-maskable interrupt occurs.  Users can replace this
    function.  */ 
 
-void 
+void
 _nmi_isr() 
 { 
   /* Sit an endless loop so that the user can analyze the situation
      from the debugger.  */
   while (1);
 }
-
-#endif
