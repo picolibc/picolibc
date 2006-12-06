017b276a7623b6bdd1d3d0310c83d208114fb0d1
Nick Clifton <nickc@redhat.com>
2006-12-06 14:36:41 +0000

    Define ATTRIBUTE_PACKED and use as necessary.

--- include/coff/external.h
+++ include/coff/external.h
@@ -50,7 +50,7 @@ typedef struct external_aouthdr
     char entry[4];	/* entry pt.				*/
     char text_start[4];	/* base of text used for this file 	*/
     char data_start[4];	/* base of data used for this file 	*/
-  }
+  } ATTRIBUTE_PACKED
 AOUTHDR;
 
 #define AOUTHDRSZ 28
@@ -164,7 +164,7 @@ struct external_syment
   char e_type[2];
   char e_sclass[1];
   char e_numaux[1];
-};
+} ATTRIBUTE_PACKED ;
 
 #define	SYMENT	struct external_syment
 #define	SYMESZ	18	
@@ -256,7 +256,7 @@ union external_auxent
     char x_tvlen[2];	/* length of .tv */
     char x_tvran[2][2];	/* tv range */
   } x_tv;		/* info about .tv section (in auxent of symbol .tv)) */
-};
+} ATTRIBUTE_PACKED ;
 
 #define	AUXENT	union external_auxent
 #define	AUXESZ	18
