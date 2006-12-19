4a48752eab833facf92577c93b9340e1c0dfb0e9
Corinna Vinschen <corinna@vinschen.de>
2006-12-19 10:17:49 +0000

    * include/cygwin/version.h: Bump DLL minor version number to 24.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -43,7 +43,7 @@ details. */
 	 changes to the DLL and is mainly informative in nature. */
 
 #define CYGWIN_VERSION_DLL_MAJOR 1005
-#define CYGWIN_VERSION_DLL_MINOR 23
+#define CYGWIN_VERSION_DLL_MINOR 24
 
       /* Major numbers before CYGWIN_VERSION_DLL_EPOCH are
 	 incompatible. */
