d7e4c7a807a5f1f633a184ea73be2599210499fb
Corinna Vinschen <corinna@vinschen.de>
2007-02-08 13:34:20 +0000

    * libc/include/sys/features.h: Add _POSIX_SHARED_MEMORY_OBJECTS for 	Cygwin.  Fix value of _POSIX_SEMAPHORES.  Fix formatting.

--- newlib/libc/include/sys/features.h
+++ newlib/libc/include/sys/features.h
@@ -108,15 +108,15 @@ extern "C" {
 #define _POSIX_REALTIME_SIGNALS			200112L
 #define _POSIX_REGEXP				     1
 #define _POSIX_SAVED_IDS			     1
-#define _POSIX_SEMAPHORES			     1
-/* #define _POSIX_SHARED_MEMORY_OBJECTS		    -1 */
+#define _POSIX_SEMAPHORES			200112L
+#define _POSIX_SHARED_MEMORY_OBJECTS		200112L 
 #define _POSIX_SHELL				     1
 /* #define _POSIX_SPAWN				    -1 */
 /* #define _POSIX_SPIN_LOCKS			    -1 */
 /* #define _POSIX_SPORADIC_SERVER		    -1 */
 #define _POSIX_SYNCHRONIZED_IO			200112L
 /* #define _POSIX_THREAD_ATTR_STACKADDR		    -1 */
-#define _POSIX_THREAD_ATTR_STACKSIZE	200112L
+#define _POSIX_THREAD_ATTR_STACKSIZE		200112L
 /* #define _POSIX_THREAD_CPUTIME		    -1 */
 /* #define _POSIX_THREAD_PRIO_INHERIT		    -1 */
 /* #define _POSIX_THREAD_PRIO_PROTECT		    -1 */
