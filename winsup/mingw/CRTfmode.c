b338158ee6c4035bccf02b9d79e75fca0090fc6c
Danny Smith <dannysmith@users.sourceforge.net>
2007-06-13 08:26:13 +0000

    * Makefile.in (CRT0S): Revert last change. 	* CRTfmode.c. Correct comment.

--- winsup/mingw/CRTfmode.c
+++ winsup/mingw/CRTfmode.c
@@ -4,13 +4,16 @@
  * This file is a part of the mingw-runtime package.
  * No warranty is given; refer to the file DISCLAIMER within the package.
  *
- * Include this object to set _CRT_fmode to a state that will cause 
+ * This libmingw.a object sets  _CRT_fmode to a state that will cause 
  * _mingw32_init_fmode to leave all file modes in their default state 
  * (basically text mode).
+ * 	
+ * To override the default, add, eg:
  *
- * To use this object include the object file in your link command:
- * gcc -o foo.exe foo.o CRTfmode.o
+ * #include <fcntl.h>	/*  _O_BINARY */
+ * int _CRT_fmode = _O_BINARY;
  *
+ * to your app.	
  */
 
 int _CRT_fmode = 0;
