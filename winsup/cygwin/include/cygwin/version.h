3e96da0b5322f2665b1d3613b07891b2c9d6d83a
Corinna Vinschen <corinna@vinschen.de>
2007-06-28 17:55:01 +0000

    * cygwin.din: Export stpcpy, stpncpy. 	* posix.sgml: Add stpcpy and stpncpy to list of implemented 	GNU extensions. 	* include/cygwin/version.h: Bump API minor number.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -314,12 +314,13 @@ details. */
            fopencookie.
       172: Export getifaddrs, freeifaddrs.
       173: Export __assert_func.
+      174: Export stpcpy, stpncpy.
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 173
+#define CYGWIN_VERSION_API_MINOR 174
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
