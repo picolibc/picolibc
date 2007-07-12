0986f3e2087b2f3b7d44f3dfbb29719d05581b60
Danny Smith <dannysmith@users.sourceforge.net>
2007-07-12 07:54:51 +0000

    * include/winuser.h: (PBT_APMQUERYSUSPEND, PBT_APMQUERYSTANDBY, 	PBT_APMQUERYSUSPENDFAILED, PBT_APMQUERYSTANDBYFAILED, 	PBT_APMSUSPEND, PBT_APMSTANDBY, PBT_APMRESUMECRITICAL, 	PBT_APMRESUMESUSPEND, PBT_APMRESUMESTANDBY, PBT_APMBATTERYLOW, 	PBT_APMPOWERSTATUSCHANGE, PBT_APMOEMEVENT, PBT_APMRESUMEAUTOMATIC): 	Sync with include/pbt.h.

--- winsup/w32api/include/pbt.h
+++ winsup/w32api/include/pbt.h
@@ -1,9 +1,10 @@
 #ifndef _PBT_H
 #define _PBT_H
-#if __GNUC__ >=3
+#if __GNUC__ >= 3
 #pragma GCC system_header
 #endif
 
+/* Also in winuser.h  */
 #define PBT_APMQUERYSUSPEND 0
 #define PBT_APMQUERYSTANDBY 1
 #define PBT_APMQUERYSUSPENDFAILED 2
@@ -13,8 +14,11 @@
 #define PBT_APMRESUMECRITICAL 6
 #define PBT_APMRESUMESUSPEND 7
 #define PBT_APMRESUMESTANDBY 8
-#define PBTF_APMRESUMEFROMFAILURE 1
 #define PBT_APMBATTERYLOW 9
 #define PBT_APMPOWERSTATUSCHANGE 10
 #define PBT_APMOEMEVENT 11
+#define PBT_APMRESUMEAUTOMATIC 18
+
+#define PBTF_APMRESUMEFROMFAILURE 1
+
 #endif
