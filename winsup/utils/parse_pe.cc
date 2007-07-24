dd67e696347313eb1c135942a2540249021ff598
Corinna Vinschen <corinna@vinschen.de>
2007-07-24 19:08:23 +0000

    * dumper.cc: Change license to plain GPLv2 + later. 	* dumper.h: Ditto. 	* parse_pe.cc: Ditto.

--- winsup/utils/parse_pe.cc
+++ winsup/utils/parse_pe.cc
@@ -6,9 +6,19 @@
 
    This file is part of Cygwin.
 
-   This software is a copyrighted work licensed under the terms of the
-   Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
-   details. */
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License (file COPYING.dumper) for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
 
 #include <bfd.h>
 #include <stdio.h>
