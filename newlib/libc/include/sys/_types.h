e5ae72300ff7b25649ae537917d35668cad50037
Jeff Johnston <jjohnstn@redhat.com>
2007-09-07 21:24:55 +0000

    2007-09-07  Jeff Johnston  <jjohnstn@redhat.com>
            * libc/include/sys/_types.h: Protect all types with flag
            checks to allow definition in machine/_types.h.

--- newlib/libc/include/sys/_types.h
+++ newlib/libc/include/sys/_types.h
@@ -12,8 +12,13 @@
 #include <machine/_types.h>
 #include <sys/lock.h>
 
+#ifndef __OFF_T_DEFINED
 typedef long _off_t;
+#endif
+
+#ifndef __OFF64_T_DEFINED
 __extension__ typedef long long _off64_t;
+#endif
 
 /*
  * We need fpos_t for the following, but it doesn't have a leading "_",
@@ -30,15 +35,18 @@ typedef _off64_t _fpos64_t;
 #endif
 #endif
 
+#ifndef __SSIZE_T_DEFINED
 #if defined(__INT_MAX__) && __INT_MAX__ == 2147483647
 typedef int _ssize_t;
 #else
 typedef long _ssize_t;
 #endif
+#endif
 
 #define __need_wint_t
 #include <stddef.h>
 
+#ifndef __MBSTATE_T_DEFINED
 /* Conversion state information.  */
 typedef struct
 {
@@ -49,10 +57,15 @@ typedef struct
     unsigned char __wchb[4];
   } __value;		/* Value so far.  */
 } _mbstate_t;
+#endif
 
+#ifndef __FLOCK_T_DEFINED
 typedef _LOCK_RECURSIVE_T _flock_t;
+#endif
 
+#ifndef __ICONV_T_DEFINED
 /* Iconv descriptor type */
 typedef void *_iconv_t;
+#endif
 
 #endif	/* _SYS__TYPES_H */
