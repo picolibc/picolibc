90c9a0de5f63520d75ebeb5a77939e1256930893
Corinna Vinschen <corinna@vinschen.de>
2008-03-06 10:30:50 +0000

    * child_info.h (CURR_CHILD_INFO_MAGIC): Reset.

--- winsup/cygwin/child_info.h
+++ winsup/cygwin/child_info.h
@@ -37,7 +37,7 @@ enum child_status
 #define EXEC_MAGIC_SIZE sizeof(child_info)
 
 /* Change this value if you get a message indicating that it is out-of-sync. */
-#define CURR_CHILD_INFO_MAGIC 0x3ebda16aU
+#define CURR_CHILD_INFO_MAGIC 0x7901d74eU
 
 /* NOTE: Do not make gratuitous changes to the names or organization of the
    below class.  The layout is checksummed to determine compatibility between
