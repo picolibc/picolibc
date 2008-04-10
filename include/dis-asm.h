1d0a4e1490bd16469c07c7da139e6078e7c2aa48
Andreas Krebbel <Andreas.Krebbel@de.ibm.com>
2008-04-10 13:36:43 +0000

    2008-04-10  Andreas Krebbel  <krebbel1@de.ibm.com>
    	* s390-dis.c (init_disasm): Evaluate disassembler_options.
    	(print_s390_disassembler_options): New function.
    	* disassemble.c (disassembler_usage): Invoke
    	print_s390_disassembler_options.
    
    2008-04-10  Andreas Krebbel  <krebbel1@de.ibm.com>
    
    	* dis-asm.h (print_s390_disassembler_options):
    	Prototype added.

--- include/dis-asm.h
+++ include/dis-asm.h
@@ -291,6 +291,7 @@ extern void print_mips_disassembler_options (FILE *);
 extern void print_ppc_disassembler_options (FILE *);
 extern void print_arm_disassembler_options (FILE *);
 extern void parse_arm_disassembler_option (char *);
+extern void print_s390_disassembler_options (FILE *);
 extern int  get_arm_regname_num_options (void);
 extern int  set_arm_regname_option (int);
 extern int  get_arm_regnames (int, const char **, const char **, const char *const **);
