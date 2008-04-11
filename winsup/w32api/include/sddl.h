3a3992087e58dd8f513c11f661c00db0ddc7d338
Corinna Vinschen <corinna@vinschen.de>
2008-04-11 11:36:41 +0000

    * include/sddl.h (ConvertStringSidToSidA): Define. 	(ConvertStringSidToSidW): Define. 	(ConvertStringSidToSid): Define.

--- winsup/w32api/include/sddl.h
+++ winsup/w32api/include/sddl.h
@@ -15,12 +15,16 @@ extern "C" {
 #if (WINVER >= 0x0500)
 WINADVAPI BOOL WINAPI ConvertSidToStringSidA(PSID Sid, LPSTR* StringSid);
 WINADVAPI BOOL WINAPI ConvertSidToStringSidW(PSID Sid, LPWSTR* StringSid);
+WINADVAPI BOOL WINAPI ConvertStringSidToSidA(LPSTR StringSid, PSID *Sid);
+WINADVAPI BOOL WINAPI ConvertStringSidToSidW(LPWSTR StringSid, PSID *Sid);
 #endif
 
 #ifdef UNICODE
 #define ConvertSidToStringSid ConvertSidToStringSidW
+#define ConvertStringSidToSid ConvertStringSidToSidW
 #else
 #define ConvertSidToStringSid ConvertSidToStringSidA
+#define ConvertStringSidToSid ConvertStringSidToSidA
 #endif
 
 #ifdef __cplusplus
