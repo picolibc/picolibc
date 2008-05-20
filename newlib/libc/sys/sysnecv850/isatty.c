a9309ff5b6845b33ef54aadead2ac25cb6e1b01b
Jeff Johnston <jjohnstn@redhat.com>
2008-05-20 18:00:12 +0000

    2008-05-20  DJ Delorie  <dj@redhat.com>
    	* libc/sys/sysnecv850/isatty.c (_isatty): Renamed from isatty.

--- newlib/libc/sys/sysnecv850/isatty.c
+++ newlib/libc/sys/sysnecv850/isatty.c
@@ -9,7 +9,7 @@ int __trap0 (int function, int p1, int p2, int p3);
 
 #define TRAP0(f, p1, p2, p3) __trap0(f, (int)(p1), (int)(p2), (int)(p3))
 
-isatty (fd)
+_isatty (fd)
      int fd;
 {
   return 1;
