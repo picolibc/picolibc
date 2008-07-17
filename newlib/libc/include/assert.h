d2af9effc5b998a46c5819a269807976465a00b4
Jeff Johnston <jjohnstn@redhat.com>
2008-07-17 20:56:51 +0000

    2008-07-17  Jeff Johnston  <jjohnstn@redhat.com>
            * libc/include/assert.h (__ASSERT_FUNC): Define to __FUNCTION__ when
            __STDC_VERSION__ < 199901L and __GNUC__ >= 2.

--- newlib/libc/include/assert.h
+++ newlib/libc/include/assert.h
@@ -21,9 +21,13 @@ extern "C" {
 #  if defined __cplusplus && defined __GNUC__
 #   define __ASSERT_FUNC __PRETTY_FUNCTION__
 
-  /* C99 requires the use of __func__, gcc also supports it.  */
-#  elif defined __GNUC__ || __STDC_VERSION__ >= 199901L
+  /* C99 requires the use of __func__.  */
+#  elif __STDC_VERSION__ >= 199901L
 #   define __ASSERT_FUNC __func__
+
+  /* Older versions of gcc don't have __func__ but can use __FUNCTION__.  */
+#  elif __GNUC__ >= 2
+#   define __ASSERT_FUNC __FUNCTION__
 
   /* failed to detect __func__ support.  */
 #  else
