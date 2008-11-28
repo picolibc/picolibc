6e40cdfc8619a2fee39f4c43037ff39025fab2ab
Thiemo Seufer <ths@networkno.de>
2008-11-28 18:02:17 +0000

    * aoutx.h (NAME): Add case statements for bfd_mach_mips14000, 	bfd_mach_mips16000. 	* archures.c (bfd_architecture): Add .#defines for bfd_mach_mips14000, 	bfd_mach_mips16000. 	* bfd-in2.h: Regenerate. 	* cpu-mips.c: Add enums I_mips14000, I_mips16000. 	(arch_info_struct): Add refs to R14000, R16000. 	* elfxx-mips.c (mips_set_isa_flags): Handle bfd_mach_mips14000, 	bfd_mach_mips16000. 	(mips_mach_extensions): Map R14000, R16000 to R10000.
    	* config/tc-mips.c (hilo_interlocks): Handle CPU_R14000, CPU_R16000.
    	(mips_cpu_info_table): Add r14000, r16000.
    	* doc/c-mips.texi: Add entries for 14000, 16000.
    
    	* mips-dis.c (mips_arch_choices): Add r14000, r16000.
    
    	* mips.h: Define CPU_R14000, CPU_R16000.
            (OPCODE_IS_MEMBER): Include R14000, R16000 in test.

--- include/opcode/mips.h
+++ include/opcode/mips.h
@@ -632,6 +632,8 @@ static const unsigned int mips_isa_table[] =
 #define CPU_RM9000	9000
 #define CPU_R10000	10000
 #define CPU_R12000	12000
+#define CPU_R14000	14000
+#define CPU_R16000	16000
 #define CPU_MIPS16	16
 #define CPU_MIPS32	32
 #define CPU_MIPS32R2	33
@@ -661,7 +663,8 @@ static const unsigned int mips_isa_table[] =
      || (cpu == CPU_R4010 && ((insn)->membership & INSN_4010) != 0)	\
      || (cpu == CPU_VR4100 && ((insn)->membership & INSN_4100) != 0)	\
      || (cpu == CPU_R3900 && ((insn)->membership & INSN_3900) != 0)	\
-     || ((cpu == CPU_R10000 || cpu == CPU_R12000)			\
+     || ((cpu == CPU_R10000 || cpu == CPU_R12000 || cpu == CPU_R14000	\
+	  || cpu == CPU_R16000)						\
 	 && ((insn)->membership & INSN_10000) != 0)			\
      || (cpu == CPU_SB1 && ((insn)->membership & INSN_SB1) != 0)	\
      || (cpu == CPU_R4111 && ((insn)->membership & INSN_4111) != 0)	\
