45e20e47bab7375b818f493a5b428c7bda7724fb
Corinna Vinschen <corinna@vinschen.de>
2009-03-06 10:09:55 +0000

    * cygwin.din: Export wprintf, fwprintf, swprintf, vwprintf, vfwprintf, 	vswprintf.  Sort. 	* posix.sgml: Move fwprintf, swprintf, vwprintf, vfwprintf, vswprintf 	to SUSv4 list.  Sort SUSv4 list. 	* include/cygwin/version.h: Bump API minor number.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -349,12 +349,13 @@ details. */
       198: Export reallocf.
       199: Export open_wmemstream.
       200: Export mbsnrtowcs, wcsnrtombs.
+      201: Export wprintf, fwprintf, swprintf, vwprintf, vfwprintf, vswprintf.
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 200
+#define CYGWIN_VERSION_API_MINOR 201
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
