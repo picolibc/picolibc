7b011fb1927a9b0e2a33007a422d94929928f28a
Corinna Vinschen <corinna@vinschen.de>
2009-03-15 13:29:04 +0000

    * include/inttypes.h: Remove "l" size specifier from all 16 and 32 bit 	definitions.

--- winsup/cygwin/include/inttypes.h
+++ winsup/cygwin/include/inttypes.h
@@ -19,17 +19,17 @@ details. */
 
 #define PRId8 "d"
 #define PRId16 "d"
-#define PRId32 "ld"
+#define PRId32 "d"
 #define PRId64 "lld"
 
 #define PRIdLEAST8 "d"
 #define PRIdLEAST16 "d"
-#define PRIdLEAST32 "ld"
+#define PRIdLEAST32 "d"
 #define PRIdLEAST64 "lld"
 
 #define PRIdFAST8 "d"
-#define PRIdFAST16 "ld"
-#define PRIdFAST32 "ld"
+#define PRIdFAST16 "d"
+#define PRIdFAST32 "d"
 #define PRIdFAST64 "lld"
 
 #define PRIdMAX "lld"
@@ -37,17 +37,17 @@ details. */
 
 #define PRIi8 "i"
 #define PRIi16 "i"
-#define PRIi32 "li"
+#define PRIi32 "i"
 #define PRIi64 "lli"
 
 #define PRIiLEAST8 "i"
 #define PRIiLEAST16 "i"
-#define PRIiLEAST32 "li"
+#define PRIiLEAST32 "i"
 #define PRIiLEAST64 "lli"
 
 #define PRIiFAST8 "i"
-#define PRIiFAST16 "li"
-#define PRIiFAST32 "li"
+#define PRIiFAST16 "i"
+#define PRIiFAST32 "i"
 #define PRIiFAST64 "lli"
 
 #define PRIiMAX "lli"
@@ -57,17 +57,17 @@ details. */
 
 #define PRIo8 "o"
 #define PRIo16 "o"
-#define PRIo32 "lo"
+#define PRIo32 "o"
 #define PRIo64 "llo"
 
 #define PRIoLEAST8 "o"
 #define PRIoLEAST16 "o"
-#define PRIoLEAST32 "lo"
+#define PRIoLEAST32 "o"
 #define PRIoLEAST64 "llo"
 
 #define PRIoFAST8 "o"
-#define PRIoFAST16 "lo"
-#define PRIoFAST32 "lo"
+#define PRIoFAST16 "o"
+#define PRIoFAST32 "o"
 #define PRIoFAST64 "llo"
 
 #define PRIoMAX "llo"
@@ -75,17 +75,17 @@ details. */
 
 #define PRIu8 "u"
 #define PRIu16 "u"
-#define PRIu32 "lu"
+#define PRIu32 "u"
 #define PRIu64 "llu"
 
 #define PRIuLEAST8 "u"
 #define PRIuLEAST16 "u"
-#define PRIuLEAST32 "lu"
+#define PRIuLEAST32 "u"
 #define PRIuLEAST64 "llu"
 
 #define PRIuFAST8 "u"
-#define PRIuFAST16 "lu"
-#define PRIuFAST32 "lu"
+#define PRIuFAST16 "u"
+#define PRIuFAST32 "u"
 #define PRIuFAST64 "llu"
 
 #define PRIuMAX "llu"
@@ -93,17 +93,17 @@ details. */
 
 #define PRIx8 "x"
 #define PRIx16 "x"
-#define PRIx32 "lx"
+#define PRIx32 "x"
 #define PRIx64 "llx"
 
 #define PRIxLEAST8 "x"
 #define PRIxLEAST16 "x"
-#define PRIxLEAST32 "lx"
+#define PRIxLEAST32 "x"
 #define PRIxLEAST64 "llx"
 
 #define PRIxFAST8 "x"
-#define PRIxFAST16 "lx"
-#define PRIxFAST32 "lx"
+#define PRIxFAST16 "x"
+#define PRIxFAST32 "x"
 #define PRIxFAST64 "llx"
 
 #define PRIxMAX "llx"
@@ -111,17 +111,17 @@ details. */
 
 #define PRIX8 "X"
 #define PRIX16 "X"
-#define PRIX32 "lX"
+#define PRIX32 "X"
 #define PRIX64 "llX"
 
 #define PRIXLEAST8 "X"
 #define PRIXLEAST16 "X"
-#define PRIXLEAST32 "lX"
+#define PRIXLEAST32 "X"
 #define PRIXLEAST64 "llX"
 
 #define PRIXFAST8 "X"
-#define PRIXFAST16 "lX"
-#define PRIXFAST32 "lX"
+#define PRIXFAST16 "X"
+#define PRIXFAST32 "X"
 #define PRIXFAST64 "llX"
 
 #define PRIXMAX "llX"
@@ -131,17 +131,17 @@ details. */
 
 #define SCNd8 "hhd"
 #define SCNd16 "hd"
-#define SCNd32 "ld"
+#define SCNd32 "d"
 #define SCNd64 "lld"
 
 #define SCNdLEAST8 "hhd"
 #define SCNdLEAST16 "hd"
-#define SCNdLEAST32 "ld"
+#define SCNdLEAST32 "d"
 #define SCNdLEAST64 "lld"
 
 #define SCNdFAST8 "hhd"
-#define SCNdFAST16 "ld"
-#define SCNdFAST32 "ld"
+#define SCNdFAST16 "d"
+#define SCNdFAST32 "d"
 #define SCNdFAST64 "lld"
 
 #define SCNdMAX "lld"
@@ -149,17 +149,17 @@ details. */
 
 #define SCNi8 "hhi"
 #define SCNi16 "hi"
-#define SCNi32 "li"
+#define SCNi32 "i"
 #define SCNi64 "lli"
 
 #define SCNiLEAST8 "hhi"
 #define SCNiLEAST16 "hi"
-#define SCNiLEAST32 "li"
+#define SCNiLEAST32 "i"
 #define SCNiLEAST64 "lli"
 
 #define SCNiFAST8 "hhi"
-#define SCNiFAST16 "li"
-#define SCNiFAST32 "li"
+#define SCNiFAST16 "i"
+#define SCNiFAST32 "i"
 #define SCNiFAST64 "lli"
 
 #define SCNiMAX "lli"
@@ -169,17 +169,17 @@ details. */
 
 #define SCNo8 "hho"
 #define SCNo16 "ho"
-#define SCNo32 "lo"
+#define SCNo32 "o"
 #define SCNo64 "llo"
 
 #define SCNoLEAST8 "hho"
 #define SCNoLEAST16 "ho"
-#define SCNoLEAST32 "lo"
+#define SCNoLEAST32 "o"
 #define SCNoLEAST64 "llo"
 
 #define SCNoFAST8 "hho"
-#define SCNoFAST16 "lo"
-#define SCNoFAST32 "lo"
+#define SCNoFAST16 "o"
+#define SCNoFAST32 "o"
 #define SCNoFAST64 "llo"
 
 #define SCNoMAX "llo"
@@ -187,17 +187,17 @@ details. */
 
 #define SCNu8 "hhu"
 #define SCNu16 "hu"
-#define SCNu32 "lu"
+#define SCNu32 "u"
 #define SCNu64 "llu"
 
 #define SCNuLEAST8 "hhu"
 #define SCNuLEAST16 "hu"
-#define SCNuLEAST32 "lu"
+#define SCNuLEAST32 "u"
 #define SCNuLEAST64 "llu"
 
 #define SCNuFAST8 "hhu"
-#define SCNuFAST16 "lu"
-#define SCNuFAST32 "lu"
+#define SCNuFAST16 "u"
+#define SCNuFAST32 "u"
 #define SCNuFAST64 "llu"
 
 #define SCNuMAX "llu"
@@ -205,17 +205,17 @@ details. */
 
 #define SCNx8 "hhx"
 #define SCNx16 "hx"
-#define SCNx32 "lx"
+#define SCNx32 "x"
 #define SCNx64 "llx"
 
 #define SCNxLEAST8 "hhx"
 #define SCNxLEAST16 "hx"
-#define SCNxLEAST32 "lx"
+#define SCNxLEAST32 "x"
 #define SCNxLEAST64 "llx"
 
 #define SCNxFAST8 "hhx"
-#define SCNxFAST16 "lx"
-#define SCNxFAST32 "lx"
+#define SCNxFAST16 "x"
+#define SCNxFAST32 "x"
 #define SCNxFAST64 "llx"
 
 #define SCNxMAX "llx"
