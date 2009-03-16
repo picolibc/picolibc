fe5c0c73f3fca9b6fc0526b1e7af726b255d0f84
Jan Kratochvil <jan.kratochvil@redhat.com>
2009-03-16 09:45:33 +0000

    include/elf/ 	* common.h (AT_RANDOM): Define.

--- include/elf/common.h
+++ include/elf/common.h
@@ -803,6 +803,7 @@
 #define	AT_SECURE	23		/* Boolean, was exec setuid-like?  */
 #define AT_BASE_PLATFORM 24		/* String identifying real platform,
 					   may differ from AT_PLATFORM.  */
+#define AT_RANDOM	25		/* Address of 16 random bytes.  */
 #define AT_EXECFN	31		/* Filename of executable.  */
 /* Pointer to the global system page used for system calls and other
    nice things.  */
