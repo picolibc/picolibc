a9a520b62b1b2f30cd068dd3e5c1eaedb1a3fd99
Corinna Vinschen <corinna@vinschen.de>
2009-03-19 09:22:01 +0000

    * cygwin.din: Export log2, log2f as functions. 	* posix.sgml: Add them to SUSv4 list. 	* include/cygwin/version.h: Bump API minor number.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -356,12 +356,13 @@ details. */
       205: Export wscanf, fwscanf, swscanf, vwscanf, vfwscanf, vswscanf.
       206: Export wcscasecmp, wcsncasecmp.
       207: Export wcsdup.
+      208: Export log2, log2f.
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 207
+#define CYGWIN_VERSION_API_MINOR 208
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
