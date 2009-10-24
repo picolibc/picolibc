4c038a6101747075c53ad1de8d4eb8e79f8c8742
Doug Evans <dje@google.com>
2009-10-24 00:17:08 +0000

    cpu/ 	* m32c.opc (opc.h): cgen-types.h -> cgen/basic-modes.h. 	cgen-ops.h -> cgen/basic-ops.h.
    	include/opcode/
    	* cgen-bitset.h: Delete, moved to ../cgen/bitset.h.
    	* cgen.h: Update.  Improve multi-inclusion macro name.
    
    	include/cgen/
    	* basic-modes.h: New file.  Moved here from opcodes/cgen-types.h.
    	* basic-ops.h: New file.  Moved here from opcodes/cgen-ops.h.
    	* bitset.h: New file.  Moved here from ../opcode/cgen-bitset.h.
    	Update license to GPL v3.
    
    	opcodes/
    	* cgen-ops.h: Delete, moved to ../include/cgen/basic-ops.h.
    	* cgen-types.h: Delete, moved to ../include/cgen/basic-modes.h.
    	* cgen-bitset.c: Update.
    	* fr30-desc.h: Regenerate.
    	* frv-desc.h: Regenerate.
    	* ip2k-desc.h: Regenerate.
    	* iq2000-desc.h: Regenerate.
    	* lm32-desc.h: Regenerate.
    	* m32c-desc.h: Regenerate.
    	* m32c-opc.h: Regenerate.
    	* m32r-desc.h: Regenerate.
    	* mep-desc.h: Regenerate.
    	* mt-desc.h: Regenerate.
    	* openrisc-desc.h: Regenerate.
    	* xc16x-desc.h: Regenerate.
    	* xstormy16-desc.h: Regenerate.

--- include/cgen/bitset.h
+++ include/cgen/bitset.h
@@ -1,22 +1,23 @@
 /* Header file the type CGEN_BITSET.
+   Copyright 2002, 2005, 2009 Free Software Foundation, Inc.
 
-Copyright 2002, 2005 Free Software Foundation, Inc.
+   This file is part of the GNU opcodes library.
 
-This file is part of GDB, the GNU debugger, and the GNU Binutils.
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+   It is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this library; see the file COPYING3.  If not, write to the
+   Free Software Foundation, 51 Franklin Street - Fifth Floor, Boston, MA
+   02110-1301, USA.  */
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #ifndef CGEN_BITSET_H
 #define CGEN_BITSET_H
 
--- include/opcode/cgen.h
+++ include/opcode/cgen.h
@@ -1,6 +1,6 @@
 /* Header file for targets using CGEN: Cpu tools GENerator.
 
-Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2005
+Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2005, 2009
 Free Software Foundation, Inc.
 
 This file is part of GDB, the GNU debugger, and the GNU Binutils.
@@ -19,11 +19,12 @@ You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
 
-#ifndef CGEN_H
-#define CGEN_H
+#ifndef OPCODE_CGEN_H
+#define OPCODE_CGEN_H
 
 #include "symcat.h"
-#include "cgen-bitset.h"
+#include "cgen/bitset.h"
+
 /* ??? This file requires bfd.h but only to get bfd_vma.
    Seems like an awful lot to require just to get such a fundamental type.
    Perhaps the definition of bfd_vma can be moved outside of bfd.h.
@@ -1470,4 +1471,4 @@ extern void cgen_clear_signed_overflow_ok (CGEN_CPU_DESC);
 /* Will an error message be generated if a signed field in an instruction overflows ? */
 extern unsigned int cgen_signed_overflow_ok_p (CGEN_CPU_DESC);
 
-#endif /* CGEN_H */
+#endif /* OPCODE_CGEN_H */
