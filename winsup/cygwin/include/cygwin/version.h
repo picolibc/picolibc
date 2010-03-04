a9fd74242880c6ec7d6f4697a722842e549b865d
Corinna Vinschen <corinna@vinschen.de>
2010-03-04 09:37:52 +0000

    * cygwin.din: Export __xdr functions. 	* include/cygwin/version.h: Bump version. 	* posix.sgml: Add a few more XDR functions to list 	of implemented Solaris functions.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -378,12 +378,13 @@ details. */
       222: CW_INT_SETLOCALE added.
       223: SIGPWR added.
       224: Export xdr* functions.
+      225: Export __xdr* functions.
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 224
+#define CYGWIN_VERSION_API_MINOR 225
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
