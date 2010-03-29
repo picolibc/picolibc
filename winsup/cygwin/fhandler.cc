ce3d89938b36b9d55cbfa40814347b8c10910db8
Corinna Vinschen <corinna@vinschen.de>
2010-03-29 15:02:50 +0000

    * fhandler.cc (fhandler_base::fstat): Keep st_ctime and st_birthtime 	fixed to 2006-12-01 00:00 UTC.

--- winsup/cygwin/fhandler.cc
+++ winsup/cygwin/fhandler.cc
@@ -1100,10 +1100,15 @@ fhandler_base::fstat (struct __stat64 *buf)
   buf->st_gid = getegid32 ();
   buf->st_nlink = 1;
   buf->st_blksize = PREFERRED_IO_BLKSIZE;
-  buf->st_ctim.tv_sec = time (NULL);	/* Arbitrary value: current time,
-					   like Linux */
+
+  buf->st_ctim.tv_sec = 1164931200L;	/* Arbitrary value: 2006-12-01 */
   buf->st_ctim.tv_nsec = 0L;
-  buf->st_atim = buf->st_mtim = buf->st_birthtim = buf->st_ctim;
+  buf->st_birthtim = buf->st_ctim;
+  buf->st_mtim.tv_sec = time (NULL);	/* Arbitrary value: current time,
+					   like Linux */
+  buf->st_mtim.tv_nsec = 0L;
+  buf->st_atim = buf->st_mtim;
+
   return 0;
 }
 
