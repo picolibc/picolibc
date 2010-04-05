26f5e38ec0f2640359a65c2840fd219ea3c0a660
DJ Delorie <dj@redhat.com>
2010-04-05 20:13:18 +0000

    merge from gcc

--- include/dwarf2.h
+++ include/dwarf2.h
@@ -66,7 +66,6 @@ typedef struct
   unsigned short li_version;
   unsigned int   li_prologue_length;
   unsigned char  li_min_insn_length;
-  unsigned char  li_max_ops_per_insn;
   unsigned char  li_default_is_stmt;
   int            li_line_base;
   unsigned char  li_line_range;
