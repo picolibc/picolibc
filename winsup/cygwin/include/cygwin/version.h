8f47a15cef72e29e9249f4b49167830264a2d6b4
Corinna Vinschen <corinna@vinschen.de>
2010-04-28 10:20:34 +0000

    * include/cygwin/version.h: Bump API minor version.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -379,12 +379,13 @@ details. */
       223: SIGPWR added.
       224: Export xdr* functions.
       225: Export __xdr* functions.
+      226: Export __locale_mb_cur_max.
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 225
+#define CYGWIN_VERSION_API_MINOR 226
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
