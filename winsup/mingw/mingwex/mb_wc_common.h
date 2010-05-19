5f7eee9ec93d8caf090d06aa45571940ee831565
Chris Sutcliffe <ir0nh34d@users.sourceforge.net>
2010-05-19 02:59:46 +0000

    2010-04-27 Danny Smith <dannysmith@users.sourceforge.net>
            * mingwex/mb_wc_common.h (get_codepage): Revert change of 2006-09-19.

--- winsup/mingw/mingwex/mb_wc_common.h
+++ winsup/mingw/mingwex/mb_wc_common.h
@@ -1,7 +1,13 @@
-__declspec(dllimport) unsigned int __lc_codepage;
+#include <locale.h>
+#include <string.h>
+#include <stdlib.h>
 
 static inline
 unsigned int get_codepage (void)
 {
-  return __lc_codepage;
+  /* locale :: "lang[_country[.code_page]]" | ".code_page"  */
+  char * cp_string;
+  if ((cp_string = strchr (setlocale(LC_CTYPE, NULL), '.')))
+    return  ((unsigned) atoi (cp_string + 1));
+  return 0;
 }
