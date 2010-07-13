3f45333072dcd5f588bbb18d358d97c79cbc102b
Corinna Vinschen <corinna@vinschen.de>
2010-07-13 11:18:55 +0000

    * libc/include/signal.h (sighandler_t): Only define if _POSIX_SOURCE 	is undefined.

--- newlib/libc/include/signal.h
+++ newlib/libc/include/signal.h
@@ -7,7 +7,9 @@
 _BEGIN_STD_C
 
 typedef int	sig_atomic_t;		/* Atomic entity type (ANSI) */
+#ifndef _POSIX_SOURCE
 typedef _sig_func_ptr sighandler_t;	/* glibc naming */
+#endif /* !_POSIX_SOURCE */
 
 #define SIG_DFL ((_sig_func_ptr)0)	/* Default action */
 #define SIG_IGN ((_sig_func_ptr)1)	/* Ignore action */
