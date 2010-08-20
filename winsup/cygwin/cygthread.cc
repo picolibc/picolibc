ff7fca61df0c4b2f68d7534d389bd5b2f2d467bf
Christopher Faylor <me@cgf.cx>
2010-08-20 15:28:28 +0000

    * cygthread.cc: Update copyright.

--- winsup/cygwin/cygthread.cc
+++ winsup/cygwin/cygthread.cc
@@ -1,7 +1,7 @@
 /* cygthread.cc
 
-   Copyright 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2008, 2009
-   Red Hat, Inc.
+   Copyright 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2008,
+   2009 2010 Red Hat, Inc.
 
 This software is a copyrighted work licensed under the terms of the
 Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
