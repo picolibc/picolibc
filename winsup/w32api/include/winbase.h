c7a5ff6c03aa104c60daff6e4aa11926de28d232
Chris Sutcliffe <ir0nh34d@users.sourceforge.net>
2010-08-24 22:25:44 +0000

    2010-08-24  Vinky  <rabbit_vinky@users.sourceforge.net>
            * include/winbase.h (OVERLAPPED): Correct definition.

--- winsup/w32api/include/winbase.h
+++ winsup/w32api/include/winbase.h
@@ -825,8 +825,13 @@ typedef struct _DEBUG_EVENT {
 typedef struct _OVERLAPPED {
 	ULONG_PTR Internal;
 	ULONG_PTR InternalHigh;
+	union {
+		struct {
 	DWORD Offset;
 	DWORD OffsetHigh;
+	};
+	PVOID Pointer;
+	};
 	HANDLE hEvent;
 } OVERLAPPED,*POVERLAPPED,*LPOVERLAPPED;
 typedef struct _STARTUPINFOA {
