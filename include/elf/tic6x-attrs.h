ffb7cf8e7ca4857b30ddcf656162718255adbe91
cvs2svn <>
2010-11-03 14:27:12 +0000

    This commit was manufactured by cvs2svn to create tag 'sid- snapshot-20101101'.
    Sprout from binutils-2_21-branch 2010-11-03 14:27:07 UTC cvs2svn 'This commit was manufactured by cvs2svn to create branch 'binutils-'
    Cherrypick from master 2010-10-29 16:47:54 UTC Joseph Myers <joseph@codesourcery.com> 'bfd:':
        ChangeLog
        configure
        configure.ac
        include/ChangeLog
        include/elf/ChangeLog
        include/elf/tic6x-attrs.h
    Delete:
        include/simple-object.h
        texinfo/texinfo.tex

--- include/elf/tic6x-attrs.h
+++ include/elf/tic6x-attrs.h
@@ -23,13 +23,5 @@
    name and value.  */
 
 TAG(Tag_ISA, 4)
-TAG(Tag_ABI_wchar_t, 6)
-TAG(Tag_ABI_stack_align_needed, 8)
-TAG(Tag_ABI_stack_align_preserved, 10)
 TAG(Tag_ABI_DSBT, 12)
-TAG(Tag_ABI_PID, 14)
-TAG(Tag_ABI_PIC, 16)
-TAG(Tag_ABI_array_object_alignment, 18)
-TAG(Tag_ABI_array_object_align_expected, 20)
 TAG(Tag_ABI_compatibility, 32)
-TAG(Tag_ABI_conformance, 67)
