8eb8d2727457ece6bac9d69750baf7cb1251cef8
Chris Sutcliffe <ir0nh34d@users.sourceforge.net>
2010-12-27 20:39:42 +0000

    2010-12-27  Markus Koenig  <basilo@users.sourceforge.net>
            * include/gdiplus/gdipluspixelformats.h (PixelFormatIndexed,
            PixelFormatGDI, PixelFormatAlpha, PixelFormatPAlpha,
            PixelFormatExtended, PixelFormatCanonical, PixelFormatUndefined,
            PixelFormatDontCare, PixelFormat1bppIndexed, PixelFormat4bppIndexed,
            PixelFormat8bppIndexed, PixelFormat16bppGrayScale,
            PixelFormat16bppRGB555, PixelFormat16bppRGB565, PixelFormat16bpp1555,
            PixelFormat24bppRGB, PixelFormat32bppRGB, PixelFormat32bppARGB,
            PixelFormat32bppPARGB, PixelFormat48bppRGB, PixelFormat64bppARGB,
            PixelFormat64bppPARGB, PixelFormatMax): Fix definition.

--- winsup/w32api/include/gdiplus/gdipluspixelformats.h
+++ winsup/w32api/include/gdiplus/gdipluspixelformats.h
@@ -29,43 +29,43 @@
 typedef DWORD ARGB;
 typedef INT PixelFormat;
 
-#define PixelFormatIndexed ((PixelFormat) 0x00010000)
-#define PixelFormatGDI ((PixelFormat) 0x00020000)
-#define PixelFormatAlpha ((PixelFormat) 0x00040000)
-#define PixelFormatPAlpha ((PixelFormat) 0x00080000)
-#define PixelFormatExtended ((PixelFormat) 0x00100000)
-#define PixelFormatCanonical ((PixelFormat) 0x00200000)
-#define PixelFormatUndefined ((PixelFormat) 0)
-#define PixelFormatDontCare ((PixelFormat) 0)
-#define PixelFormat1bppIndexed ((PixelFormat) \
+#define PixelFormatIndexed ((INT) 0x00010000)
+#define PixelFormatGDI ((INT) 0x00020000)
+#define PixelFormatAlpha ((INT) 0x00040000)
+#define PixelFormatPAlpha ((INT) 0x00080000)
+#define PixelFormatExtended ((INT) 0x00100000)
+#define PixelFormatCanonical ((INT) 0x00200000)
+#define PixelFormatUndefined ((INT) 0)
+#define PixelFormatDontCare ((INT) 0)
+#define PixelFormat1bppIndexed ((INT) \
 	(1 | (1<<8) | PixelFormatIndexed | PixelFormatGDI))
-#define PixelFormat4bppIndexed ((PixelFormat) \
+#define PixelFormat4bppIndexed ((INT) \
 	(2 | (4<<8) | PixelFormatIndexed | PixelFormatGDI))
-#define PixelFormat8bppIndexed ((PixelFormat) \
+#define PixelFormat8bppIndexed ((INT) \
 	(3 | (8<<8) | PixelFormatIndexed | PixelFormatGDI))
-#define PixelFormat16bppGrayScale ((PixelFormat) \
+#define PixelFormat16bppGrayScale ((INT) \
 	(4 | (16<<8) | PixelFormatExtended))
-#define PixelFormat16bppRGB555 ((PixelFormat) \
+#define PixelFormat16bppRGB555 ((INT) \
 	(5 | (16<<8) | PixelFormatGDI))
-#define PixelFormat16bppRGB565 ((PixelFormat) \
+#define PixelFormat16bppRGB565 ((INT) \
 	(6 | (16<<8) | PixelFormatGDI))
-#define PixelFormat16bppARGB1555 ((PixelFormat) \
+#define PixelFormat16bppARGB1555 ((INT) \
 	(7 | (16<<8) | PixelFormatAlpha | PixelFormatGDI))
-#define PixelFormat24bppRGB ((PixelFormat) \
+#define PixelFormat24bppRGB ((INT) \
 	(8 | (24<<8) | PixelFormatGDI))
-#define PixelFormat32bppRGB ((PixelFormat) \
+#define PixelFormat32bppRGB ((INT) \
 	(9 | (32<<8) | PixelFormatGDI))
-#define PixelFormat32bppARGB ((PixelFormat) \
+#define PixelFormat32bppARGB ((INT) \
 	(10 | (32<<8) | PixelFormatAlpha | PixelFormatGDI | PixelFormatCanonical))
-#define PixelFormat32bppPARGB ((PixelFormat) \
+#define PixelFormat32bppPARGB ((INT) \
 	(11 | (32<<8) | PixelFormatAlpha | PixelFormatPAlpha | PixelFormatGDI))
-#define PixelFormat48bppRGB ((PixelFormat) \
+#define PixelFormat48bppRGB ((INT) \
 	(12 | (48<<8) | PixelFormatExtended))
-#define PixelFormat64bppARGB ((PixelFormat) \
+#define PixelFormat64bppARGB ((INT) \
 	(13 | (64<<8) | PixelFormatAlpha | PixelFormatCanonical | PixelFormatExtended))
-#define PixelFormat64bppPARGB ((PixelFormat) \
+#define PixelFormat64bppPARGB ((INT) \
 	(14 | (64<<8) | PixelFormatAlpha | PixelFormatPAlpha | PixelFormatExtended))
-#define PixelFormatMax ((PixelFormat) 15)
+#define PixelFormatMax ((INT) 15)
 
 typedef enum PaletteFlags {
 	PaletteFlagsHasAlpha = 1,
