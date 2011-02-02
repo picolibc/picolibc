0cda941fa18e868e79d56a1699edb61091bf44fb
Corinna Vinschen <corinna@vinschen.de>
2011-02-02 10:16:30 +0000

    * include/err.h (err): Add noreturn attribute. 	(errx): Ditto. 	(verr): Ditto. 	(verrx): Ditto.

--- winsup/cygwin/include/err.h
+++ winsup/cygwin/include/err.h
@@ -1,6 +1,6 @@
 /* err.h
 
-   Copyright 2003 Red Hat, Inc.
+   Copyright 2003, 2011 Red Hat, Inc.
 
 This file is part of Cygwin.
 
@@ -16,17 +16,17 @@ details. */
 
 __BEGIN_DECLS
 
-extern void warn (const char *fmt, ...);
-extern void warnx (const char *fmt, ...);
+extern void warn (const char *fmt, ...)
+extern void warnx (const char *fmt, ...)
 
-extern void err (int eval, const char *fmt, ...);
-extern void errx (int eval, const char *fmt, ...);
+extern void err (int eval, const char *fmt, ...) __attribute__ ((__noreturn__));
+extern void errx (int eval, const char *fmt, ...) __attribute__ ((__noreturn__));
 
 extern void vwarn (const char *fmt, va_list ap);
 extern void vwarnx (const char *fmt, va_list ap);
 
-extern void verr (int eval, const char *fmt, va_list ap);
-extern void verrx (int eval, const char *fmt, va_list ap);
+extern void verr (int eval, const char *fmt, va_list ap) __attribute__ ((__noreturn__));
+extern void verrx (int eval, const char *fmt, va_list ap) __attribute__ ((__noreturn__));
 
 __END_DECLS
 
