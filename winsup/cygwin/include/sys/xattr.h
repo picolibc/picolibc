c9c50e7aa4694cd7cb94600c89b28fdea751c9a2
Corinna Vinschen <corinna@vinschen.de>
2011-03-29 08:03:35 +0000

    * include/sys/xattr.h: New file.

---
+++ winsup/cygwin/include/sys/xattr.h
@@ -0,0 +1,18 @@
+/* sys/xattr.h
+
+   Copyright 2011 Red Hat, Inc.
+
+This file is part of Cygwin.
+
+This software is a copyrighted work licensed under the terms of the
+Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
+details. */
+
+/* sys/xattr.h header file for Cygwin.  */
+
+#ifndef _SYS_XATTR_H
+#define _SYS_XATTR_H
+
+#include <attr/xattr.h>
+
+#endif /* _SYS_XATTR_H */
