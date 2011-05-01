c60d0bbe688ae9ad37e56ab8ba73124e8b6c73b7
Corinna Vinschen <corinna@vinschen.de>
2011-05-01 08:14:40 +0000

    * net.cc (fdsock): Drop setting uninterruptible_io to true.

--- winsup/cygwin/net.cc
+++ winsup/cygwin/net.cc
@@ -554,7 +554,6 @@ fdsock (cygheap_fdmanip& fd, const device *dev, SOCKET soc)
   if (fixup)
     ((fhandler_socket *) fd)->init_fixup_before ();
   fd->set_flags (O_RDWR | O_BINARY);
-  fd->uninterruptible_io (true);
   debug_printf ("fd %d, name '%s', soc %p", (int) fd, dev->name, soc);
 
   /* Raise default buffer sizes (instead of WinSock default 8K).
