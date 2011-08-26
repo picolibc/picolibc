477215fe7db5277f44fe35c5a600597bbf957fa0
Corinna Vinschen <corinna@vinschen.de>
2011-08-26 07:23:42 +0000

    * libc/time/mktm_r.c (_mktm_r): Fix previous fix.

--- newlib/libc/time/mktm_r.c
+++ newlib/libc/time/mktm_r.c
@@ -182,7 +182,7 @@ _DEFUN (_mktm_r, (tim_p, res, is_gmtime),
 		{
 		  res->tm_mon = 11;
 		  res->tm_year -= 1;
-		  res->tm_yday = 364 + isleap(res->tm_year);
+		  res->tm_yday = 364 + isleap(res->tm_year + 1900);
 		}
 	      res->tm_mday = ip[res->tm_mon];
 	    }
@@ -254,4 +254,3 @@ _DEFUN (__tzcalc_limits, (year),
 
   return 1;
 }
-
