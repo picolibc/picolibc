7bbfea993a12c146054142631135738782a57932
Corinna Vinschen <corinna@vinschen.de>
2011-10-07 08:14:25 +0000

    Fix copyright.

--- winsup/cygwin/sigproc.h
+++ winsup/cygwin/sigproc.h
@@ -1,6 +1,7 @@
 /* sigproc.h
 
-   Copyright 1997, 1998, 2000, 2001, 2002, 2003, 2004, 2005, 2006 Red Hat, Inc.
+   Copyright 1997, 1998, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
+   2011 Red Hat, Inc.
 
 This file is part of Cygwin.
 
