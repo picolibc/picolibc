4c28f4392a52852d34acd04cc698047bc21375ea
Nick Clifton <nickc@redhat.com>
2011-10-25 11:18:10 +0000

    bfd: 	* Makefile.am (ALL_MACHINES): Add cpu-epiphany.lo . 	(ALL_MACHINES_CFILES): Add cpu-epiphany.c . 	(BFD32_BACKENDS): Add elf32-epiphany.lo . 	(BFD32_BACKENDS_CFILES): Add elf32-epiphany.c . 	* Makefile.in, bfd-in2.h, configure, libbfd.h: Regenerate. 	* archures.c (bfd_arch_epiphany): Add. 	(bfd_mach_epiphany16, bfd_mach_epiphany32): Define. 	(bfd_epiphany_arch): Declare. 	(bfd_archures_list): Add &bfd_epiphany_arch. 	* config.bfd (epiphany-*-elf): New target case. 	* configure.in (bfd_elf32_epiphany_vec): New target vector case. 	* reloc.c (BFD_RELOC_EPIPHANY_SIMM8): New relocation. 	(BFD_RELOC_EPIPHANY_SIMM24, BFD_RELOC_EPIPHANY_HIGH): Likewise. 	(BFD_RELOC_EPIPHANY_LOW, BFD_RELOC_EPIPHANY_SIMM11): Likewise. 	(BFD_RELOC_EPIPHANY_IMM11, BFD_RELOC_EPIPHANY_IMM8): Likewise. 	* targets.c (bfd_elf32_epiphany_vec): Declare. 	(_bfd_target_vector): Add bfd_elf32_epiphany_vec. 	* po/SRC-POTFILES.in, po/bfd.pot: Regenerate. 	* cpu-epiphany.c, elf32-epiphany.c: New files. binutils: 	* readelf.c (include "elf/epiphany.h") 	(guess_is_rela, dump_relocation): Handle EM_ADAPTEVA_EPIPHANY. 	(get_machine_name, is_32bit_abs_reloc, is_32bit_pcrel_reloc): Likewise. 	(is_16bit_abs_reloc, is_none_reloc): Likewise. 	* po/binutils.pot: Regenerate. cpu: 	* cpu/epiphany.cpu, cpu/epiphany.opc: New files. gas: 	* NEWS: Mention addition of Adapteva Epiphany support. 	* config/tc-epiphany.c, config/tc-epiphany.h: New files. 	* Makefile.am (TARGET_CPU_CFILES): Add config/tc-epiphany.c . 	(TARGET_CPU_HFILES): Add config/tc-epiphany.h . 	* Makefile.in, configure, doc/Makefile.in, po/POTFILES.in: Regenerate. 	* configure.in: Also set using_cgen for epiphany. 	* configure.tgt: Handle epiphany. 	* doc/Makefile.am (CPU_DOCS): Add c-epiphany.texi . 	* doc/all.texi: Set EPIPHANY. 	* doc/as.texinfo: Add EPIPHANY-specific text. 	* doc/c-epiphany.texi: New file. 	* po/gas.pot: Regenerate. gas/testsuite: 	* gas/epiphany: New directory. include: 	* dis-asm.h (print_insn_epiphany): Declare. 	* elf/epiphany.h: New file. 	* elf/common.h (EM_ADAPTEVA_EPIPHANY): Define. ld: 	* NEWS: Mention addition of Adapteva Epiphany support. 	* Makefile.am (ALL_EMULATION_SOURCES): Add eelf32epiphany.c . 	(eelf32epiphany.c): New rule. 	* Makefile.in: Regenerate. 	* configure.tgt: Handle epiphany-*-elf. 	* po/ld.pot: Regenerate. 	* testsuite/ld-srec/srec.exp: xfail epiphany. 	* emulparams/elf32epiphany.sh: New file. opcodes: 	* Makefile.am (HFILES): Add epiphany-desc.h and epiphany-opc.h . 	(TARGET_LIBOPCODES_CFILES): Add  epiphany-asm.c, epiphany-desc.c, 	epiphany-dis.c, epiphany-ibld.c and epiphany-opc.c . 	(CLEANFILES): Add stamp-epiphany. 	(EPIPHANY_DEPS): Set.  Make CGEN-generated Epiphany files depend on it. 	(stamp-epiphany): New rule. 	* Makefile.in, configure, po/POTFILES.in, po/opcodes.pot: Regenerate. 	* configure.in: Handle bfd_epiphany_arch. 	* disassemble.c (ARCH_epiphany): Define. 	(disassembler): Handle bfd_arch_epiphany. 	* epiphany-asm.c, epiphany-desc.c, epiphany-desc.h: New files. 	* epiphany-dis.c, epiphany-ibld.c, epiphany-opc.c: Likewise. 	* epiphany-opc.h: Likewise.

--- include/dis-asm.h
+++ include/dis-asm.h
@@ -233,6 +233,7 @@ extern int print_insn_crx               (bfd_vma, disassemble_info *);
 extern int print_insn_d10v		(bfd_vma, disassemble_info *);
 extern int print_insn_d30v		(bfd_vma, disassemble_info *);
 extern int print_insn_dlx 		(bfd_vma, disassemble_info *);
+extern int print_insn_epiphany		(bfd_vma, disassemble_info *);
 extern int print_insn_fr30		(bfd_vma, disassemble_info *);
 extern int print_insn_frv		(bfd_vma, disassemble_info *);
 extern int print_insn_h8300		(bfd_vma, disassemble_info *);
