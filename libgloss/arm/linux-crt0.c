c3a549f6878359c9f5b950b58e16a1a2beed3290
Corinna Vinschen <corinna@vinschen.de>
2012-01-17 10:03:40 +0000

    * arm/linux-crt0.c: Use ldr instead of adr to get address of 	_start_thumb.

--- libgloss/arm/linux-crt0.c
+++ libgloss/arm/linux-crt0.c
@@ -18,8 +18,10 @@ asm("\n"
 	".global _start\n"
 	".type _start, %function\n"
 	"_start:\n"
-	"\tadr r0, _start_thumb+1\n"
+	"\tldr r0, .LC0\n"
 	"\tbx r0\n"
+	".LC0:\n"
+	"\t.word _start_thumb\n"
 	".size _start, .-_start\n");
 
 __attribute__((naked, used))
