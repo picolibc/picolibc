2196614e3df4433c43c44eb44b75c65af021089d
Corinna Vinschen <corinna@vinschen.de>
2012-02-17 17:34:01 +0000

    * timer.cc (getitimer): Don't create another local ret variable in 	timer_gettime calling branch.  Simplify conditional since timer_gettime 	always returns 0 or -1 anyway.

--- winsup/cygwin/timer.cc
+++ winsup/cygwin/timer.cc
@@ -423,10 +423,8 @@ getitimer (int which, struct itimerval *ovalue)
       else
 	{
 	  struct itimerspec spec_ovalue;
-	  int ret = timer_gettime ((timer_t) &ttstart, &spec_ovalue);
-	  if (ret)
-	    ret = -1;
-	  else
+	  ret = timer_gettime ((timer_t) &ttstart, &spec_ovalue);
+	  if (!ret)
 	    {
 	      ovalue->it_interval.tv_sec = spec_ovalue.it_interval.tv_sec;
 	      ovalue->it_interval.tv_usec = spec_ovalue.it_interval.tv_nsec / 1000;
