d8a7ac6d189ae26347c06f2858b71529e4e8b9e2
Christopher Faylor <me@cgf.cx>
2012-03-21 17:23:57 +0000

    * child_info.h: Reset magic number.

--- winsup/cygwin/child_info.h
+++ winsup/cygwin/child_info.h
@@ -35,7 +35,7 @@ enum child_status
 #define EXEC_MAGIC_SIZE sizeof(child_info)
 
 /* Change this value if you get a message indicating that it is out-of-sync. */
-#define CURR_CHILD_INFO_MAGIC 0x10f103a4U
+#define CURR_CHILD_INFO_MAGIC 0xca644850U
 
 #define NPROCS	256
 
