1c46670094d3fb8c10bc4663f3efb587c47cb39d
Nick Clifton <nickc@redhat.com>
2012-05-03 13:12:06 +0000

    Add support for Motorola XGATE embedded CPU

--- include/dis-asm.h
+++ include/dis-asm.h
@@ -293,6 +293,7 @@ extern int print_insn_v850		(bfd_vma, disassemble_info *);
 extern int print_insn_vax		(bfd_vma, disassemble_info *);
 extern int print_insn_w65		(bfd_vma, disassemble_info *);
 extern int print_insn_xc16x		(bfd_vma, disassemble_info *);
+extern int print_insn_xgate             (bfd_vma, disassemble_info *);
 extern int print_insn_xstormy16		(bfd_vma, disassemble_info *);
 extern int print_insn_xtensa		(bfd_vma, disassemble_info *);
 extern int print_insn_z80		(bfd_vma, disassemble_info *);
