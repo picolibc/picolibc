8f55341ea1bf9811aac237bf5defb0f102c62be6
Christopher Faylor <me@cgf.cx>
2012-05-14 23:29:03 +0000

    * child_info.h (CURR_CHILD_INFO_MAGIC): Update.

--- winsup/cygwin/child_info.h
+++ winsup/cygwin/child_info.h
@@ -35,7 +35,7 @@ enum child_status
 #define EXEC_MAGIC_SIZE sizeof(child_info)
 
 /* Change this value if you get a message indicating that it is out-of-sync. */
-#define CURR_CHILD_INFO_MAGIC 0xca644850U
+#define CURR_CHILD_INFO_MAGIC 0xf1378eabU
 
 #define NPROCS	256
 
