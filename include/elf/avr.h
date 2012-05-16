53b7c465ed504059aeb570d612fd98fd8565094f
Nick Clifton <nickc@redhat.com>
2012-05-16 14:52:16 +0000

    PR 13503 	* reloc.c: Rename BFD_RELOC_AVR_8_HHI to BFD_RELOC_AVR_8_HLO. 	* bfd-in2.h: Regenerate. 	* libbfd.h: Regenrate. 	* elf32-avr.c (elf_avr_howto_table): Rename R_AVR_8_HHI8 to 	R_AVR_8_HLO8. 	(avr_reloc_map): Ditto.
    	* config/tc-avr.c (avr_cons_fix_new): Rename R_AVR_8_HHI8 to
    	R_AVR_8_HLO8.
    	(exp_mod_data) Ditto. And replace "hhi8" with "hlo8".
    	(md_apply_fix): Rename BFD_RELOC_AVR_8_HHI to BFD_RELOC_AVR_8_HLO.
    
    	* avr.h (RELOC_NUMBERS): Rename R_AVR_8_HHI8 to R_AVR_8_HLO8.

--- include/elf/avr.h
+++ include/elf/avr.h
@@ -80,7 +80,7 @@ START_RELOC_NUMBERS (elf_avr_reloc_type)
      RELOC_NUMBER (R_AVR_8, 		       26)
      RELOC_NUMBER (R_AVR_8_LO8,                27)
      RELOC_NUMBER (R_AVR_8_HI8,                28)
-     RELOC_NUMBER (R_AVR_8_HHI8,               29)
+     RELOC_NUMBER (R_AVR_8_HLO8,               29)
 END_RELOC_NUMBERS (R_AVR_max)
 
 #endif /* _ELF_AVR_H */
