53bbfeed98c82b018bdfa5e719abd31ac1beba40
Corinna Vinschen <corinna@vinschen.de>
2012-05-30 14:42:52 +0000

    * libc/stdio/fgetws.c (_fgetws_r): Call _mbsnrtowcs_r rather than 	_mbsrtowcs_r and restrict number of wide chars to n - 1.

--- newlib/libc/stdio/fgetws.c
+++ newlib/libc/stdio/fgetws.c
@@ -110,9 +110,13 @@ _DEFUN(_fgetws_r, (ptr, ws, n, fp),
     {
       src = (char *) fp->_p;
       nl = memchr (fp->_p, '\n', fp->_r);
-      nconv = _mbsrtowcs_r (ptr, wsp, &src,
-			    nl != NULL ? (nl - fp->_p + 1) : fp->_r,
-			    &fp->_mbstate);
+      nconv = _mbsnrtowcs_r (ptr, wsp, &src,
+			     /* Read all bytes up to the next NL, or up to the
+				end of the buffer if there is no NL. */
+			     nl != NULL ? (nl - fp->_p + 1) : fp->_r,
+			     /* But never more than n - 1 wide chars. */
+			     n - 1,
+			     &fp->_mbstate);
       if (nconv == (size_t) -1)
 	/* Conversion error */
 	goto error;
