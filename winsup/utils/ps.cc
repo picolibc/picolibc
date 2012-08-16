2e22f9b144c06f9d20b0e97e7ea00d2a62a19f0a
Corinna Vinschen <corinna@vinschen.de>
2012-08-16 16:27:41 +0000

    * ps.cc (main): Define uid correctly as uid_t.

--- winsup/utils/ps.cc
+++ winsup/utils/ps.cc
@@ -149,7 +149,8 @@ int
 main (int argc, char *argv[])
 {
   external_pinfo *p;
-  int aflag, lflag, fflag, sflag, uid, proc_id;
+  int aflag, lflag, fflag, sflag, proc_id;
+  uid_t uid;
   bool found_proc_id = true;
   DWORD proc_access = PROCESS_QUERY_LIMITED_INFORMATION;
   cygwin_getinfo_types query = CW_GETPINFO;
