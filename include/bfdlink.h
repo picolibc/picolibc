3dfe1f6c694a92cd2d74c0bd9cf341f8a054250b
Alan Modra <modra@gmail.com>
2012-10-22 13:33:48 +0000

    include/ 	PR ld/14426 	* bfdlink.h (bfd_link_info): Add ignore_hash. ld/ 	PR ld/14426 	* ldlex.h (option_values): Add OPTION_IGNORE_UNRESOLVED_SYMBOL. 	* lexsup.c (parse_args): Likewise. 	(ld_options): Describe --ignore-unresolved-symbol. 	* ldmain.h (add_ignoresym): Declare. 	* ldmain.c (add_ignoresym): New function, extracted from.. 	(undefined_symbol): ..here.  Return if the symbol is in ignore_hash. 	(constructor_callback): Don't use global link_info here. 	(reloc_overflow): Likewise.

--- include/bfdlink.h
+++ include/bfdlink.h
@@ -435,6 +435,10 @@ struct bfd_link_info
      option).  If this is NULL, no symbols are being wrapped.  */
   struct bfd_hash_table *wrap_hash;
 
+  /* Hash table of symbols which may be left unresolved during
+     a link.  If this is NULL, no symbols can be left unresolved.  */
+  struct bfd_hash_table *ignore_hash;
+
   /* The output BFD.  */
   bfd *output_bfd;
 
