09707415eb6110268da579fa0641c3a451f138ca
Corinna Vinschen <corinna@vinschen.de>
2013-01-11 11:04:50 +0000

    * fhandler.h (class dev_console): Flag for expanded control sequence. 	* fhandler_console.cc (char_command): Supporting cursor style modes.

--- winsup/cygwin/fhandler.h
+++ winsup/cygwin/fhandler.h
@@ -1,7 +1,7 @@
 /* fhandler.h
 
    Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
-   2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 Red Hat, Inc.
+   2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 Red Hat, Inc.
 
 This file is part of Cygwin.
 
@@ -1246,6 +1246,7 @@ class dev_console
   unsigned rarg;
   bool saw_question_mark;
   bool saw_greater_than_sign;
+  bool saw_space;
   bool vt100_graphics_mode_G0;
   bool vt100_graphics_mode_G1;
   bool iso_2022_G1;
