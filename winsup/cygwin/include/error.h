f73889ef24fd86ae14b5e6ea07d0d5b91b18a56d
Corinna Vinschen <corinna@vinschen.de>
2013-02-21 11:26:31 +0000

    Pull in changes from HEAD

--- winsup/cygwin/include/error.h
+++ winsup/cygwin/include/error.h
@@ -19,9 +19,15 @@ extern "C"
 void error (int, int, const char *, ...);
 void error_at_line (int, int, const char *, unsigned int, const char *, ...);
 
+#ifdef  __INSIDE_CYGWIN__
 extern unsigned int error_message_count;
 extern int error_one_per_line;
 extern void (*error_print_progname) (void);
+#else
+extern __declspec(dllimport) unsigned int error_message_count;
+extern __declspec(dllimport) int error_one_per_line;
+extern __declspec(dllimport) void (*error_print_progname) (void);
+#endif
 
 #ifdef __cplusplus
 }
