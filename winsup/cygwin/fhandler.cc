27f8495dd59a4d5827040ab599ef457ee9e21af6
Corinna Vinschen <corinna@vinschen.de>
2013-10-23 09:58:12 +0000

    * fhandler.cc (fhandler_base::fstat): Drop FH_FULL case to align 	/dev/full permissions to Linux.

--- winsup/cygwin/fhandler.cc
+++ winsup/cygwin/fhandler.cc
@@ -1250,9 +1250,6 @@ fhandler_base::fstat (struct stat *buf)
     case FH_PIPER:
       buf->st_mode = S_IFIFO | S_IRUSR;
       break;
-    case FH_FULL:
-      buf->st_mode = S_IFCHR | S_IRUSR | S_IWUSR | S_IWGRP | S_IWOTH;
-      break;
     default:
       buf->st_mode = S_IFCHR | STD_RBITS | STD_WBITS | S_IWGRP | S_IWOTH;
       break;
