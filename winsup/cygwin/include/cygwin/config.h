158508011cc53b01c9b6b0ded3b0f4c7c3a187eb
Corinna Vinschen <corinna@vinschen.de>
2013-10-23 10:06:17 +0000

    * include /cygwin/config.h (_READ_WRITE_BUFSIZE_TYPE): Define.

--- winsup/cygwin/include/cygwin/config.h
+++ winsup/cygwin/include/cygwin/config.h
@@ -69,6 +69,7 @@ extern char *_tlsbase __asm__ ("%fs:4");
    Cygwin.  Changing them in applications has no or not the desired effect.
    Just leave them alone. */
 #define _READ_WRITE_RETURN_TYPE _ssize_t
+#define _READ_WRITE_BUFSIZE_TYPE size_t
 #define __LARGE64_FILES 1
 #define __USE_INTERNAL_STAT64 1
 #define __LINUX_ERRNO_EXTENSIONS__ 1
