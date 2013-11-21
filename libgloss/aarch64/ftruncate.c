37d3b04f8803d335c1262ae08ca7f4a91cdd445f
Marcus Shawcroft <marcus.shawcroft@arm.com>
2013-11-21 15:53:52 +0000

    [AArch64] Set errno in ftruncate() and truncate().

--- libgloss/aarch64/ftruncate.c
+++ libgloss/aarch64/ftruncate.c
@@ -23,10 +23,12 @@
  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
+#include <errno.h>
 #include <sys/types.h>
 
 int
 ftruncate (int file, off_t length)
 {
+  errno = ENOSYS;
   return -1;
 }
--- libgloss/aarch64/truncate.c
+++ libgloss/aarch64/truncate.c
@@ -23,10 +23,12 @@
  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
+#include <errno.h>
 #include <sys/types.h>
 
 int
 truncate (const char *path, off_t length)
 {
+  errno = ENOSYS;
   return -1;
 }
