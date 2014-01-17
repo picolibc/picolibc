92a8ea56e590421dab6ad28cdcbc73720af4e905
Corinna Vinschen <corinna@vinschen.de>
2014-01-17 11:01:46 +0000

    * include/sys/file.h (LOCK_SH): Drop definition in favor of new 	definitions in newlib. 	(LOCK_EX): Ditto. 	(LOCK_NB): Ditto. 	(LOCK_UN): Ditto. 	(flock): Ditto. 	(F_OK): Drop.  Already correctly defined in unistd.h. 	(X_OK): Ditto. 	(W_OK): Ditto. 	(R_OK): Ditto.

--- winsup/cygwin/include/sys/file.h
+++ winsup/cygwin/include/sys/file.h
@@ -23,7 +23,6 @@
 #ifndef _FILE_H_
 #define _FILE_H_
 
-#include <sys/cdefs.h>
 #include <fcntl.h>
 
 /* Whence values for lseek(); renamed by POSIX 1003.1 */
@@ -31,24 +30,5 @@
 #define L_CURR		SEEK_CUR
 #define L_INCR		SEEK_CUR
 #define L_XTND		SEEK_END
-
-/* Operations for flock() function */
-#define	LOCK_SH		1	/* Shared lock. */
-#define	LOCK_EX		2	/* Exclusive lock. */
-#define	LOCK_NB		4	/* Don't block when locking. */
-#define	LOCK_UN		8	/* Unlock. */
-
-/* Operations for access function */
-#define	F_OK		0	/* does file exist */
-#define X_OK		1	/* is it executable or searchable by caller */
-#define	W_OK		2	/* is it writable by caller */
-#define	R_OK		4	/* is it readable by caller */
-
-/* Apply or remove an advisory lock on the file fd refers to. */
-__BEGIN_DECLS
-
-int	_EXFUN(flock, (int, int));
-
-__END_DECLS
 
 #endif
