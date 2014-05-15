2d0cb1acc7f46e49595d9a36bf4db6c2e490585f
Corinna Vinschen <corinna@vinschen.de>
2014-05-15 11:24:42 +0000

    * include/cygwin/version.h (CYGWIN_VERSION_API_MINOR): Bump to 273.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -446,12 +446,15 @@ details. */
            as number of partitions on tape.
       271: Export posix_spawn, posix_spawnp, and helper functions.
       272: Export tm_gmtoff and tm_zone members.
+      273: Introduce account mapping from WIndows account DBs.  Add CW_SETENT,
+	   CW_GETENT, CW_ENDENT, CW_GETNSSSEP, CW_GETPWSID, CW_GETGRSID,
+	   CW_CYGNAME_FROM_WINNAME.
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 272
+#define CYGWIN_VERSION_API_MINOR 273
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
