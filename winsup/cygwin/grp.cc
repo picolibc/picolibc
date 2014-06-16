6681d11fd3464c7ba9a74e7f594dcbd3eb96688a
Corinna Vinschen <corinna@vinschen.de>
2014-06-16 09:12:29 +0000

    * grp.cc (getgrouplist): Fix setting ngroups to make sure to return 	the right value.

--- winsup/cygwin/grp.cc
+++ winsup/cygwin/grp.cc
@@ -656,11 +656,11 @@ getgrouplist (const char *user, gid_t gid, gid_t *groups, int *ngroups)
 	  groups[cnt] = grp->gr_gid;
 	++cnt;
       }
-  *ngroups = cnt;
   if (cnt > *ngroups)
     ret = -1;
   else
     ret = cnt;
+  *ngroups = cnt;
 
   syscall_printf ( "%d = getgrouplist(%s, %u, %p, %d)",
 		  ret, user, gid, groups, *ngroups);
