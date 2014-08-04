a4a96eb436fc89b3277aaa0deb1fdef7a9498b08
Jon TURNEY <jon.turney@dronecode.org.uk>
2014-08-04 21:32:37 +0000

    * libc/include/math.h: Also define M_PI etc. if _XOPEN_SOURCE is 	defined appropriately.

--- newlib/libc/include/math.h
+++ newlib/libc/include/math.h
@@ -542,7 +542,11 @@ extern int matherr _PARAMS((struct exception *e));
 #define TLOSS 5
 #define PLOSS 6
 
+#endif /* ! defined (__STRICT_ANSI__) */
+
 /* Useful constants.  */
+
+#if !defined(__STRICT_ANSI__) || ((_XOPEN_SOURCE - 0) >= 500)
 
 #define MAXFLOAT	3.40282347e+38F
 
@@ -552,16 +556,21 @@ extern int matherr _PARAMS((struct exception *e));
 #define M_LN2		_M_LN2
 #define M_LN10		2.30258509299404568402
 #define M_PI		3.14159265358979323846
-#define M_TWOPI         (M_PI * 2.0)
 #define M_PI_2		1.57079632679489661923
 #define M_PI_4		0.78539816339744830962
-#define M_3PI_4		2.3561944901923448370E0
-#define M_SQRTPI        1.77245385090551602792981
 #define M_1_PI		0.31830988618379067154
 #define M_2_PI		0.63661977236758134308
 #define M_2_SQRTPI	1.12837916709551257390
 #define M_SQRT2		1.41421356237309504880
 #define M_SQRT1_2	0.70710678118654752440
+
+#endif
+
+#ifndef __STRICT_ANSI__
+
+#define M_TWOPI         (M_PI * 2.0)
+#define M_3PI_4		2.3561944901923448370E0
+#define M_SQRTPI        1.77245385090551602792981
 #define M_LN2LO         1.9082149292705877000E-10
 #define M_LN2HI         6.9314718036912381649E-1
 #define M_SQRT3	1.73205080756887719000
