7d0b0134ee443539f04a18b0ef7f01e92af149b3
Corinna Vinschen <corinna@vinschen.de>
2015-01-12 16:38:02 +0000

    * minires-os-if.c (cygwin_query): Change questions into answers.

--- winsup/cygwin/libc/minires-os-if.c
+++ winsup/cygwin/libc/minires-os-if.c
@@ -249,6 +249,13 @@ static int cygwin_query(res_state statp, const char * DomName, int Class, int Ty
   rr = pQueryResultsSet;
   section = 0;
   while (rr) {
+    /* Some Windows versions return questions when providing locally generated
+       answers, for example for "localhost" or for the computer name. */
+    if (((rr->Flags.DW & 0x3) == DnsSectionQuestion) &&
+       (rr->wDataLength > 0)) {
+      DPRINTF(debug, "Changing record below from question to answer\n");
+      rr->Flags.DW ^= DnsSectionQuestion ^ DnsSectionAnswer;
+    }
     if (!counts[0] && (rr->Flags.DW & 0x3)) {
       /* No question. Adopt the first name as the name in the question */
       if ((len = dn_comp(rr->pName, ptr, AnsLength - 4,
