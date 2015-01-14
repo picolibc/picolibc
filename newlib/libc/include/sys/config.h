29193dd04c58979f5cae643d1d17935ff846cc0d
Corinna Vinschen <corinna@vinschen.de>
2015-01-14 09:25:16 +0000

    * configure.host: Add extra system for OpenRISC baremetal 	* libc/include/sys/config.h: Dynamic reentrancy for or1k sys targets 	* libc/sys/or1k/: New system for or1k baremetal 	* libc/sys/or1k/Makefile.am: New file 	* libc/sys/or1k/Makefile.in: New file 	* libc/sys/or1k/aclocal.m4: New file 	* libc/sys/or1k/configure.in: New file 	* libc/sys/or1k/configure: New file 	* libc/sys/or1k/getreent.S: New file 	* libc/sys/or1k/mlock.S: New file 	* libc/sys/or1k/or1k-asm.S: New file

--- newlib/libc/include/sys/config.h
+++ newlib/libc/include/sys/config.h
@@ -185,6 +185,10 @@
 #define __CUSTOM_FILE_IO__
 #endif
 
+#if defined(__or1k__) || defined(__or1knd__)
+#define __DYNAMIC_REENT__
+#endif
+
 /* This block should be kept in sync with GCC's limits.h.  The point
    of having these definitions here is to not include limits.h, which
    would pollute the user namespace, while still using types of the
