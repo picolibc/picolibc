f1b0fac03c2b94795e4111ff4a5e24e6e6e2fd02
Corinna Vinschen <corinna@vinschen.de>
2015-02-06 17:09:50 +0000

    * common.din: Export cabsl, cimagl, creall, finitel, hypotl, sqrtl. 	* include/cygwin/version.h (CYGWIN_VERSION_API_MINOR): Bump.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -467,12 +467,13 @@ details. */
            putwc_unlocked, putwchar_unlocked.
       284: Export sockatmark.
       285: Export wcstold.
+      285: Export cabsl, cimagl, creall, finitel, hypotl, sqrtl.
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 285
+#define CYGWIN_VERSION_API_MINOR 286
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
