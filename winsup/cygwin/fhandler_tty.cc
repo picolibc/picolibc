9d5309bf8911fbfee2270132da8446e81527f2fb
Corinna Vinschen <corinna@vinschen.de>
2015-02-25 16:46:57 +0000

    * fhandler_tty.cc (fhandler_pty_slave::read): Having no input is not an 	error condition for tcflush.

--- winsup/cygwin/fhandler_tty.cc
+++ winsup/cygwin/fhandler_tty.cc
@@ -1,7 +1,7 @@
 /* fhandler_tty.cc
 
    Copyright 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
-   2008, 2009, 2010, 2011, 2012, 2013 Red Hat, Inc.
+   2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015 Red Hat, Inc.
 
 This file is part of Cygwin.
 
@@ -755,7 +755,8 @@ fhandler_pty_slave::read (void *ptr, size_t& len)
 	  /*NOTREACHED*/
 	case WAIT_TIMEOUT:
 	  termios_printf ("wait timed out, time_to_wait %u", time_to_wait);
-	  if (!totalread)
+	  /* No error condition when called from tcflush. */
+	  if (!totalread && ptr)
 	    {
 	      set_sig_errno (EAGAIN);
 	      totalread = -1;
