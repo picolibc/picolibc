b3e27da5a5fda36aade790a50f94a430b2971a05
Corinna Vinschen <corinna@vinschen.de>
2015-10-22 16:21:17 +0200

    Export aligned_alloc, at_quick_exit, quick_exit.
    	* common.din (aligned_alloc): Export.
    	(at_quick_exit): Export.
    	(quick_exit): Export.
    
    	* posix.xml (std-iso): New section.
    	(std-deprec): Rearrange title text.
    	* new-features.xml (ov-new2.3): Document aligned_alloc, at_quick_exit,
    
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -472,13 +472,14 @@ details. */
       288: Export getcontext, makecontext, setcontext, swapcontext.
       289: Export sigsetjmp, siglongjmp.
       290: Add sysconf cache handling.
+      291: Export aligned_alloc, at_quick_exit, quick_exit.
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull,
 	sigaltstack, sethostname. */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 290
+#define CYGWIN_VERSION_API_MINOR 291
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible
