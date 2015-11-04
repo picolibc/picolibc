57d93bebb2d940f4f2338a5e156f9ddcd7d1fbc7
Corinna Vinschen <corinna@vinschen.de>
2015-11-04 14:47:25 +0100

    Accommodate trailing NUL in PrlSF filesystem name.
    	* globals.cc (ro_u_prlfs): Add trailing NUL.  Explain why.
    	* mount.cc (fs_info::update): Add a comment to explain PrlSF.
    	* path.h (path_conv::fs_is_prlfs): Add for symmetry.
    
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

--- winsup/cygwin/path.h
+++ winsup/cygwin/path.h
@@ -376,6 +376,7 @@ class path_conv
   bool fs_is_nwfs () const {return fs.is_nwfs ();}
   bool fs_is_ncfsd () const {return fs.is_ncfsd ();}
   bool fs_is_afs () const {return fs.is_afs ();}
+  bool fs_is_prlfs () const {return fs.is_prlfs ();}
   fs_info_type fs_type () const {return fs.what_fs ();}
   ULONG fs_serial_number () const {return fs.serial_number ();}
   inline const char *set_path (const char *p)
