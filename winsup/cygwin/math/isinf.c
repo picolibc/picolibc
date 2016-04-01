929be8005ceed50182388ba14dc20317ddeeb7bd
Yaakov Selkowitz <yselkowi@redhat.com>
2016-04-01 17:38:17 -0500

    cygwin/math: make isinf functions signed
    glibc returns -1 for negative infinity:
    
    http://man7.org/linux/man-pages/man3/isinfl.3.html
    https://sourceware.org/bugzilla/show_bug.cgi?id=15367
    
    Signed-off-by: Yaakov Selkowitz <yselkowi@redhat.com>

--- winsup/cygwin/math/isinf.c
+++ winsup/cygwin/math/isinf.c
@@ -1,18 +1,18 @@
 int
 isinf (double x)
 {
-  return __builtin_isinf (x);
+  return __builtin_isinf_sign (x);
 }
 
 int
 isinff (float x)
 {
-  return __builtin_isinf (x);
+  return __builtin_isinf_sign (x);
 }
 
 int
 isinfl (long double x)
 {
-  return __builtin_isinf (x);
+  return __builtin_isinf_sign (x);
 }
 
