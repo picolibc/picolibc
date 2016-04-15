770e94f3fdae6b49964739c43127ff52284ac0e3
Christian Mauderer <Christian.Mauderer@embedded-brains.de>
2016-04-15 13:44:25 +0200

    Add BSD guards for fixed-size integer types
    Signed-off-by: Christian Mauderer <christian.mauderer@embedded-brains.de>

--- newlib/libc/include/sys/_stdint.h
+++ newlib/libc/include/sys/_stdint.h
@@ -16,31 +16,62 @@ extern "C" {
 #endif
 
 #ifdef ___int8_t_defined
+#ifndef _INT8_T_DECLARED
 typedef __int8_t int8_t ;
-typedef __uint8_t uint8_t ;
-#define __int8_t_defined 1
+#define _INT8_T_DECLARED
 #endif
+#ifndef _UINT8_T_DECLARED
+typedef __uint8_t uint8_t ;
+#define _UINT8_T_DECLARED
+#endif
+#define __int8_t_defined 1
+#endif /* ___int8_t_defined */
 
 #ifdef ___int16_t_defined
+#ifndef _INT16_T_DECLARED
 typedef __int16_t int16_t ;
-typedef __uint16_t uint16_t ;
-#define __int16_t_defined 1
+#define _INT16_T_DECLARED
 #endif
+#ifndef _UINT16_T_DECLARED
+typedef __uint16_t uint16_t ;
+#define _UINT16_T_DECLARED
+#endif
+#define __int16_t_defined 1
+#endif /* ___int16_t_defined */
 
 #ifdef ___int32_t_defined
+#ifndef _INT32_T_DECLARED
 typedef __int32_t int32_t ;
-typedef __uint32_t uint32_t ;
-#define __int32_t_defined 1
+#define _INT32_T_DECLARED
 #endif
+#ifndef _UINT32_T_DECLARED
+typedef __uint32_t uint32_t ;
+#define _UINT32_T_DECLARED
+#endif
+#define __int32_t_defined 1
+#endif /* ___int32_t_defined */
 
 #ifdef ___int64_t_defined
+#ifndef _INT64_T_DECLARED
 typedef __int64_t int64_t ;
+#define _INT64_T_DECLARED
+#endif
+#ifndef _UINT64_T_DECLARED
 typedef __uint64_t uint64_t ;
+#define _UINT64_T_DECLARED
+#endif
 #define __int64_t_defined 1
+#endif /* ___int64_t_defined */
+
+#ifndef INTPTR_T_DECLARED
+typedef __intptr_t intptr_t;
+#define _INTPTR_T_DECLARED
 #endif
 
-typedef __intptr_t intptr_t;
+#ifndef _UINTPTR_T_DECLARED
 typedef __uintptr_t uintptr_t;
+#define _UINTPTR_T_DECLARED
+#endif
 
 #ifdef __cplusplus
 }
