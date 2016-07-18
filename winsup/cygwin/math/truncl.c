71df3bfd51cd5ee71f52a85470dc267be05d3f46
Corinna Vinschen <corinna@vinschen.de>
2016-07-18 22:00:17 +0200

    truncl: Fix setting rounding bits in FPU control word
    Mingw-w64, which is the source of this code, uses different
    definitions of the rounding bits FE_TONEAREST and friends.
    They immediately reflect the bit values in the FPU control word,
    while on Cygwin they are shifted down to become the values 0-3.
    
    Fix the bit computing expression to account for the difference.
    
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

--- winsup/cygwin/math/truncl.c
+++ winsup/cygwin/math/truncl.c
@@ -6,6 +6,10 @@
 #include <fenv.h>
 #include <math.h>
 
+/* Mask and shift amount for rounding bits on x86/x86_64. */
+#define FE_CW_ROUND_MASK	(0x0c00)
+#define FE_CW_ROUND_SHIFT	(10)
+
 long double
 truncl (long double _x)
 {
@@ -16,8 +20,8 @@ truncl (long double _x)
   unsigned short saved_cw;
   unsigned short tmp_cw;
   __asm__ __volatile__ ("fnstcw %0;" : "=m" (saved_cw)); /* save FPU control word */
-  tmp_cw = (saved_cw & ~(FE_TONEAREST | FE_DOWNWARD | FE_UPWARD | FE_TOWARDZERO))
-	    | FE_TOWARDZERO;
+  tmp_cw = (saved_cw & ~FE_CW_ROUND_MASK)
+	   | (FE_TOWARDZERO << FE_CW_ROUND_SHIFT);
   __asm__ __volatile__ ("fldcw %0;" : : "m" (tmp_cw));
   __asm__ __volatile__ ("frndint;" : "=t" (retval)  : "0" (_x)); /* round towards zero */
   __asm__ __volatile__ ("fldcw %0;" : : "m" (saved_cw) ); /* restore saved control word */
