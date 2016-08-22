113abc27a6df3325f024aa2c5aff30126cfeb247
David Wohlferd <dw@LimeGreenSocks.com>
2016-08-22 00:51:12 -0700

    Avoid truncating from long double to double in sinhl().
    This routine makes a call to fabs instead of fabsl(), causing truncation.
    
    Clang complains (warning: absolute value function 'fabs' given an argument of type 'long double' but has parameter of type 'double' which may cause truncation of value).
    
    Signed-off-by: David Wohlferd <dw@LimeGreenSocks.com>

--- winsup/cygwin/math/sinhl.c
+++ winsup/cygwin/math/sinhl.c
@@ -67,7 +67,7 @@ long double sinhl(long double x)
   if (x_class == FP_ZERO)
     return x;
   if (x_class == FP_INFINITE ||
-      (fabs (x) > (MAXLOGL + LOGE2L)))
+      (fabsl (x) > (MAXLOGL + LOGE2L)))
   {
     errno = ERANGE;
 #ifdef INFINITIES
