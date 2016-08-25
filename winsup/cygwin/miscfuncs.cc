988629da1d299cfef7023e07ad900dd047b9fc63
Corinna Vinschen <corinna@vinschen.de>
2016-08-25 12:06:17 +0200

    Fix formatting
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

--- winsup/cygwin/miscfuncs.cc
+++ winsup/cygwin/miscfuncs.cc
@@ -1134,10 +1134,11 @@ SetThreadName(DWORD dwThreadID, const char* threadName)
 #endif
     };
 
-  __try {
-    RaiseException (MS_VC_EXCEPTION, 0, sizeof (info)/sizeof (ULONG_PTR), info);
-  }
-  __except (NO_ERROR) {
-  }
+  __try
+    {
+      RaiseException (MS_VC_EXCEPTION, 0, sizeof (info) / sizeof (ULONG_PTR),
+		      info);
+    }
+  __except (NO_ERROR)
   __endtry
 }
