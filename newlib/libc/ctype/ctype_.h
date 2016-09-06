8394e47d73dbeb298a138b88c4d3718e8672cd55
Thomas Preud'homme <thomas.preudhomme@arm.com>
2016-09-06 11:26:55 +0100

    Make ctype_.c and ctype_.h agree on _ctype_b type
    _ctype_b is defined in ctype_.c as a const char array for non cygwin
    targets allowing negative ctype index but as a char array for the same
    targets in ctype_.h, giving type conflict at compile time.  This is
    because the cygwin targets are not treated specially in the latter file.
    This patch adds the necessary logic for cygwin targets in ctype_.h.

--- newlib/libc/ctype/ctype_.h
+++ newlib/libc/ctype/ctype_.h
@@ -6,7 +6,11 @@
 
 #ifdef ALLOW_NEGATIVE_CTYPE_INDEX
 
+#ifndef __CYGWIN__
+  extern _CONST char _ctype_b[];
+#else
   extern char _ctype_b[];
+#endif
 # define DEFAULT_CTYPE_PTR	((char *) _ctype_b + 127)
 
 #else	/* !ALLOW_NEGATIVE_CTYPE_INDEX */
