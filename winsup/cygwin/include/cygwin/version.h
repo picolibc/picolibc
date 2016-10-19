ba2851eecacd79d6ec4083bdf1d38b2a53cabdd7
Corinna Vinschen <corinna@vinschen.de>
2016-10-19 12:52:06 +0200

    Bump Cygwin version to 2.6.1
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -11,7 +11,7 @@ details. */
    changes to the DLL and is mainly informative in nature. */
 
 #define CYGWIN_VERSION_DLL_MAJOR 2006
-#define CYGWIN_VERSION_DLL_MINOR 0
+#define CYGWIN_VERSION_DLL_MINOR 1
 
 /* Major numbers before CYGWIN_VERSION_DLL_EPOCH are incompatible. */
 
