ae3419884801cc4ef7344d93e262a2505186b5c3
Alan Modra <amodra@gmail.com>
2017-01-04 23:49:58 +1030

    update-copyright.py for binutils
    This is a modified form of gcc's contrib/update-copyright.py.
    
    	* update-copyright.py: New file.
    	* add-log.el: Update copyright year range.
    	* texi2pod.pl: Likewise.

--- etc/add-log.el
+++ etc/add-log.el
@@ -25,7 +25,7 @@
 
 ;;; add-log.el --- change log maintenance commands for Emacs
 
-;; Copyright (C) 1985, 1986, 1988, 1993, 1994 Free Software Foundation, Inc.
+;; Copyright (C) 1985-2017 Free Software Foundation, Inc.
 
 ;; Keywords: maint
 
--- etc/texi2pod.pl
+++ etc/texi2pod.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl -w
 
-#   Copyright (C) 1999, 2000, 2001, 2003 Free Software Foundation, Inc.
+#   Copyright (C) 1999-2017 Free Software Foundation, Inc.
 
 # This file is part of GCC.
 
