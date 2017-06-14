35cd6863fb3a970507b5eaf587eda59abcefb241
Aditya Upadhyay <aadit0402@gmail.com>
2017-06-14 10:36:38 +0200

    ccoshl: Align formatting to upstream
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

--- newlib/libm/complex/ccoshl.c
+++ newlib/libm/complex/ccoshl.c
@@ -35,11 +35,11 @@
 long double complex
 ccoshl(long double complex z)
 {
-  long double complex w;
-  long double x, y;
+	long double complex w;
+	long double x, y;
 
-  x = creall(z);
-  y = cimagl(z);
-  w = coshl(x) * cosl(y) + (sinhl(x) * sinl(y)) * I;
-  return w;
+	x = creall(z);
+	y = cimagl(z);
+	w = coshl(x) * cosl(y) + (sinhl(x) * sinl(y)) * I;
+	return w;
 }
