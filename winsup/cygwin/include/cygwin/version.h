2c83bc950fec04feb76a018a994598deb3787256
Yaakov Selkowitz <yselkowi@redhat.com>
2017-08-02 01:09:49 -0500

    cygwin: Export explicit_bzero
    This was added to newlib together with timingsafe_*cmp but never exported.
    
    Signed-off-by: Yaakov Selkowitz <yselkowi@redhat.com>

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -478,12 +478,13 @@ details. */
   311: Export __xpg_sigpause.
   312: Export strverscmp, versionsort.
   313: Export fls, flsl, flsll.
+  314: Export explicit_bzero.
 
   Note that we forgot to bump the api for ualarm, strtoll, strtoull,
   sigaltstack, sethostname. */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 313
+#define CYGWIN_VERSION_API_MINOR 314
 
 /* There is also a compatibity version number associated with the shared memory
    regions.  It is incremented when incompatible changes are made to the shared
