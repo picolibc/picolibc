picolibc/picolibc

commit 603ef545bdbdbf7495e1a0bbabffb8741fc2a5bb

* fhandler.cc (fhandler_base::open): Never open files with 	FILE_OVERWITE/FILE_OVERWRITE_IF.  Set file size to 0 explicitely if 	regular, existing file has been opened for writing with O_TRUNC flag 	set.  Explain why.

winsup/cygwin/fhandler.cc
@@ -540,20 +540,12 @@ fhandler_base::open (int flags, mode_t mode)
 	break;
     }
 
-  if ((flags & O_TRUNC) && ((flags & O_ACCMODE) != O_RDONLY))
-    {
-      if (flags & O_CREAT)
-	create_disposition = FILE_OVERWRITE_IF;
-      else
-	create_disposition = FILE_OVERWRITE;
-    }
-  else if (flags & O_CREAT)
-    create_disposition = FILE_OPEN_IF;
-  else
-    create_disposition = FILE_OPEN;
-
+  /* Don't use the FILE_OVERWRITE{_IF} flags here.  See below for an
+     explanation, why that's not such a good idea. */
   if ((flags & O_EXCL) && (flags & O_CREAT))
     create_disposition = FILE_CREATE;
+  else
+    create_disposition = (flags & O_CREAT) ? FILE_OPEN_IF : FILE_OPEN;
 
   if (get_device () == FH_FS)
     {
@@ -663,6 +655,32 @@ fhandler_base::open (int flags, mode_t mode)
      http://lists.samba.org/archive/samba-technical/2008-July/060247.html */
   if (io.Information == FILE_CREATED && has_acls ())
     set_file_attribute (fh, pc, ILLEGAL_UID, ILLEGAL_GID, S_JUSTCREATED | mode);
+
+  /* If you O_TRUNC a file on Linux, the data is truncated, but the EAs are
+     preserved.  If you open a file on Windows with FILE_OVERWRITE{_IF} or
+     FILE_SUPERSEDE, all streams are truncated, including the EAs.  So we don't
+     use the FILE_OVERWRITE{_IF} flags, but instead just open the file and set
+     the size of the data stream explicitely to 0.  Apart from being more Linux
+     compatible, this implementation has the pleasant side-effect to be more
+     than 5% faster than using FILE_OVERWRITE{_IF} (tested on W7 32 bit). */
+  if ((flags & O_TRUNC)
+      && (flags & O_ACCMODE) != O_RDONLY
+      && io.Information != FILE_CREATED
+      && get_device () == FH_FS)
+    {
+      FILE_END_OF_FILE_INFORMATION feofi = { EndOfFile:{ QuadPart:0 } };
+      status = NtSetInformationFile (fh, &io, &feofi, sizeof feofi,
+				     FileEndOfFileInformation);
+      /* In theory, truncating the file should never fail, since the opened
+	 handle has FILE_READ_DATA permissions, which is all you need to
+	 be allowed to truncate a file.  Better safe than sorry. */
+      if (!NT_SUCCESS (status))
+	{
+	  __seterrno_from_nt_status (status);
+	  NtClose (fh);
+	  goto done;
+	}
+    }
 
   set_io_handle (fh);
   set_flags (flags, pc.binmode ());

commit 0d57ef9de5e2e5b12694169ab0a3c4203e1aa605

Cygwin: open: Remove unused code to handle HIDDEN and SYSTEM files
Commit 603ef545bdbdbf7495e1a0bbabffb8741fc2a5bb broke this snippet and
commit 5b312b4747cc4acda39c187369c02fcea456513b didn't help at all since
FILE_CREATE is exactly *not* the situation the test was originally
supposed to handle.

In fact, none of the open flags used by fhandler_base::open actually
hits this problem anymore, so just drop the code.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/fhandler.cc
@@ -626,14 +626,6 @@ fhandler_base::open (int flags, mode_t mode)
 	    }
 	}
 
-      /* Trying to overwrite an already existing file with FILE_ATTRIBUTE_HIDDEN
-	 and/or FILE_ATTRIBUTE_SYSTEM attribute set, NtCreateFile fails with
-	 STATUS_ACCESS_DENIED.  Per MSDN you have to create the file with the
-	 same attributes as already specified for the file. */
-      if (create_disposition == FILE_CREATE
-	  && has_attribute (FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM))
-	file_attributes |= pc.file_attributes ();
-
       if (flags & (O_CREAT | O_TMPFILE))
 	{
 	  file_attributes |= FILE_ATTRIBUTE_NORMAL;