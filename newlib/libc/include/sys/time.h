3266b2dd5e7036149413b96d558053b99df95c48
pfg <pfg@FreeBSD.org>
2017-11-20 19:43:44 +0000

    sys: further adoption of SPDX licensing ID tags.
    Mainly focus on files that use BSD 3-Clause license.
    
    The Software Package Data Exchange (SPDX) group provides a specification
    to make it easier for automated tools to detect and summarize well known
    opensource licenses. We are gradually adopting the specification, noting
    that the tags are considered only advisory and do not, in any way,
    superceed or replace the license texts.
    
    Special thanks to Wind River for providing access to "The Duke of
    Highlander" tool: an older (2014) run over FreeBSD tree was useful as a
    starting point.

--- newlib/libc/include/sys/time.h
+++ newlib/libc/include/sys/time.h
@@ -3,6 +3,8 @@
    Public domain; no rights reserved. */
 
 /*-
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
  * Copyright (c) 1982, 1986, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
