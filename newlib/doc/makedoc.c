7bfa24c49563717426d1ee237f29d1ccfc4b51db
Yaakov Selkowitz <yselkowi@redhat.com>
2017-12-04 03:25:26 -0600

    ansification: fix makedoc for ANSI C
    Signed-off-by: Yaakov Selkowitz <yselkowi@redhat.com>

--- newlib/doc/makedoc.c
+++ newlib/doc/makedoc.c
@@ -1287,13 +1287,13 @@ DEFUN(compile, (string),
 		     /* Got a number, embedd the magic push number
 			function */
 		     add_to_definition(ptr, push_number);
-		     add_to_definition(ptr, atol(word));
+		     add_to_definition(ptr, (stinst_type)atol(word));
 		     break;
 		   default:
 		     add_to_definition(ptr, call);
 		     lookup = lookup_word(word);
 		     if (!lookup) ret++;
-		     add_to_definition(ptr, lookup);
+		     add_to_definition(ptr, (stinst_type)lookup);
 		 }
 
 		string = nextword(string, &word);		     
