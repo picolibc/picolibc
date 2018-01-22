8810f929fc05e1794e06c80b2313728998de4ea5
Yaakov Selkowitz <yselkowi@redhat.com>
2018-01-21 23:51:51 -0600

    cygwin: Declare pthread_rwlock_timedrdlock, pthread_rwlock_timedwrlock
    These were added in commit 8128f5482f2b1889e2336488e9d45a33c9972d11 but
    without their public declarations.
    
    Signed-off-by: Yaakov Selkowitz <yselkowi@redhat.com>

--- winsup/cygwin/include/pthread.h
+++ winsup/cygwin/include/pthread.h
@@ -191,8 +191,12 @@ int pthread_spin_unlock (pthread_spinlock_t *);
 int pthread_rwlock_destroy (pthread_rwlock_t *rwlock);
 int pthread_rwlock_init (pthread_rwlock_t *rwlock, const pthread_rwlockattr_t *attr);
 int pthread_rwlock_rdlock (pthread_rwlock_t *rwlock);
+int pthread_rwlock_timedrdlock (pthread_rwlock_t *rwlock,
+				const struct timespec *abstime);
 int pthread_rwlock_tryrdlock (pthread_rwlock_t *rwlock);
 int pthread_rwlock_wrlock (pthread_rwlock_t *rwlock);
+int pthread_rwlock_timedwrlock (pthread_rwlock_t *rwlock,
+				const struct timespec *abstime);
 int pthread_rwlock_trywrlock (pthread_rwlock_t *rwlock);
 int pthread_rwlock_unlock (pthread_rwlock_t *rwlock);
 int pthread_rwlockattr_init (pthread_rwlockattr_t *rwlockattr);
