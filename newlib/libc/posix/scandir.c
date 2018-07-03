0e7db0c356c0c47e34bd2cac1d3b83386540a7f7
jhb <jhb@FreeBSD.org>
2018-07-03 17:31:45 +0000

    Clean up the vcs ID strings
    in libc's gen/ directory.
    
    - Move CSRG IDs into __SCCSID().
    - When a file has been copied, consistently use 'From: <tag>' for strings
      referencing the version of the source file copied from in the license
      block comment.
    - Some of the 'From:' tags were using $FreeBSD$ that was being expanded on
      each checkout.  Fix those to hardcode the FreeBSD tag from the file that
      was copied at the time of the copy.
    - When multiple strings are present list them in "chronological" order,
      so CSRG (__SCCSID) before FreeBSD (__FBSDID).  If a file came from
      OtherBSD and contains a CSRG ID from the OtherBSD file, use the order
      CSRG -> OtherBSD -> FreeBSD.
    
    Reviewed by:	imp
    Differential Revision:	https://reviews.freebsd.org/D15831

--- newlib/libc/posix/scandir.c
+++ newlib/libc/posix/scandir.c
@@ -31,9 +31,8 @@
  * SUCH DAMAGE.
  */
 
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)scandir.c	5.10 (Berkeley) 2/23/91";
-#endif /* LIBC_SCCS and not lint */
+#include <sys/cdefs.h>
+__SCCSID("@(#)scandir.c	8.3 (Berkeley) 1/2/94");
 
 /*
  * Scan the directory dirname calling select to make a list of selected
