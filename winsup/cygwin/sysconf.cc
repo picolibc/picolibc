f6417be2aa98e9da0aca22fbbe2a2a7c39a6f45b
Yaakov Selkowitz <yselkowi@redhat.com>
2018-07-25 10:48:55 -0500

    Cygwin: define _POSIX_ASYNCHRONOUS_IO
    This feature is now available as of the recent AIO commits.
    
    Signed-off-by: Yaakov Selkowitz <yselkowi@redhat.com>

--- winsup/cygwin/sysconf.cc
+++ winsup/cygwin/sysconf.cc
@@ -528,7 +528,7 @@ static struct
   {cons, {c:SIGQUEUE_MAX}},		/*  18, _SC_SIGQUEUE_MAX */
   {cons, {c:TIMER_MAX}},		/*  19, _SC_TIMER_MAX */
   {nsup, {c:0}},			/*  20, _SC_TZNAME_MAX */
-  {cons, {c:-1L}},			/*  21, _SC_ASYNCHRONOUS_IO */
+  {cons, {c:_POSIX_ASYNCHRONOUS_IO}},	/*  21, _SC_ASYNCHRONOUS_IO */
   {cons, {c:_POSIX_FSYNC}},		/*  22, _SC_FSYNC */
   {cons, {c:_POSIX_MAPPED_FILES}},	/*  23, _SC_MAPPED_FILES */
   {cons, {c:-1L}},			/*  24, _SC_MEMLOCK */
