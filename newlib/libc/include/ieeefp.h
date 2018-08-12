c1200386e80cffaccb08c4a0b0cde307db9d5b1f
Keith Packard <keithp@keithp.com>
2018-08-12 12:43:45 -0700

    ieeefp.c: Auto-detect _LDBL_EQ_DBL
    Make configuring the library a bit simpler
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/include/ieeefp.h
+++ newlib/libc/include/ieeefp.h
@@ -143,6 +143,11 @@ typedef union
 
 #endif /* __IEEE_LITTLE_ENDIAN */
 
+#if LDBL_MANT_DIG == DBL_MANT_DIG && LDBL_MIN_EXP == DBL_MIN_EXP && \
+    LDBL_MAX_EXP == DBL_MAX_EXP
+#define _LDBL_EQ_DBL
+#endif
+
 #ifndef _LDBL_EQ_DBL
 
 #ifndef LDBL_MANT_DIG
