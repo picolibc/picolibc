d8ccbcdaccff12ca9509358d31114515acd20590
Andy Koppe <andy.koppe@gmail.com>
2018-08-29 15:37:13 +0100

    Drop non-reserved parameter names from sys/signal.h
    Standard headers shouldn't use non-reserved identifiers as parameter
    names in function declarations, because programs could in theory
    define macros with such names before including a header.

--- newlib/libc/include/sys/signal.h
+++ newlib/libc/include/sys/signal.h
@@ -161,11 +161,11 @@ typedef struct sigaltstack {
 #define SIG_BLOCK 1	/* set of signals to block */
 #define SIG_UNBLOCK 2	/* set of signals to, well, unblock */
 
-int sigprocmask (int how, const sigset_t *set, sigset_t *oset);
+int sigprocmask (int, const sigset_t *, sigset_t *);
 #endif
 
 #if __POSIX_VISIBLE >= 199506
-int pthread_sigmask (int how, const sigset_t *set, sigset_t *oset);
+int pthread_sigmask (int, const sigset_t *, sigset_t *);
 #endif
 
 #ifdef _COMPILING_NEWLIB
@@ -188,7 +188,7 @@ int sigfillset (sigset_t *);
 int sigemptyset (sigset_t *);
 int sigpending (sigset_t *);
 int sigsuspend (const sigset_t *);
-int sigwait (const sigset_t *set, int *sig);
+int sigwait (const sigset_t *, int *);
 
 #if !defined(__CYGWIN__) && !defined(__rtems__)
 /* These depend upon the type of sigset_t, which right now 
@@ -223,7 +223,7 @@ int sigaltstack (const stack_t *__restrict, stack_t *__restrict);
 #endif
 
 #if __POSIX_VISIBLE >= 199506
-int pthread_kill (pthread_t thread, int sig);
+int pthread_kill (pthread_t, int);
 #endif
 
 #if __POSIX_VISIBLE >= 199309
@@ -231,11 +231,10 @@ int pthread_kill (pthread_t thread, int sig);
 /*  3.3.8 Synchronously Accept a Signal, P1003.1b-1993, p. 76
     NOTE: P1003.1c/D10, p. 39 adds sigwait().  */
 
-int sigwaitinfo (const sigset_t *set, siginfo_t *info);
-int sigtimedwait (const sigset_t *set, siginfo_t *info,
-		  const struct timespec  *timeout);
+int sigwaitinfo (const sigset_t *, siginfo_t *);
+int sigtimedwait (const sigset_t *, siginfo_t *, const struct timespec *);
 /*  3.3.9 Queue a Signal to a Process, P1003.1b-1993, p. 78 */
-int sigqueue (pid_t pid, int signo, const union sigval value);
+int sigqueue (pid_t, int, const union sigval);
 
 #endif /* __POSIX_VISIBLE >= 199309 */
 
