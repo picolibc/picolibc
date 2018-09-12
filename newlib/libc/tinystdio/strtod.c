7dba31cb8edcb8723cce2e5e7cc3b2e084e312ad
Keith Packard <keithp@keithp.com>
2018-09-12 16:08:07 -0700

    tinystdio: Support double in strtod
    Instead of using strtof and only handling floats
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/tinystdio/strtod.c
+++ newlib/libc/tinystdio/strtod.c
@@ -38,15 +38,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* Only GCC 4.2 calls the library function to convert an unsigned long
-   to float.  Other GCC-es (including 4.3) use a signed long to float
-   conversion along with a large inline code to correct the result.	*/
-extern double __floatunsisf (unsigned long);
-
-static const float pwr_p10 [6] = {
+static const double pwr_p10 [6] = {
     1e+1, 1e+2, 1e+4, 1e+8, 1e+16, 1e+32
 };
-static const float pwr_m10 [6] = {
+static const double pwr_m10 [6] = {
     1e-1, 1e-2, 1e-4, 1e-8, 1e-16, 1e-32
 };
 
@@ -84,5 +79,131 @@ static const char pstr_nan[] = {'N','A','N'};
 double
 strtod (const char * nptr, char ** endptr)
 {
-    return (double) strtof(nptr, endptr);
+    uint64_t u64;
+    double flt;
+    unsigned char c;
+    int exp;
+
+    unsigned char flag;
+#define FL_MINUS    0x01	/* number is negative	*/
+#define FL_ANY	    0x02	/* any digit was readed	*/
+#define FL_OVFL	    0x04	/* overflow was		*/
+#define FL_DOT	    0x08	/* decimal '.' was	*/
+#define FL_MEXP	    0x10	/* exponent 'e' is neg.	*/
+
+    if (endptr)
+	*endptr = (char *)nptr;
+
+    do {
+	c = *nptr++;
+    } while (isspace (c));
+
+    flag = 0;
+    if (c == '-') {
+	flag = FL_MINUS;
+	c = *nptr++;
+    } else if (c == '+') {
+	c = *nptr++;
+    }
+
+    if (!strncmp (nptr - 1, pstr_inf, 3)) {
+	nptr += 2;
+	if (!strncmp (nptr, pstr_inity, 5))
+	    nptr += 5;
+	if (endptr)
+	    *endptr = (char *)nptr;
+	return flag & FL_MINUS ? -INFINITY : +INFINITY;
+    }
+
+    /* NAN() construction is not realised.
+       Length would be 3 characters only.	*/
+    if (!strncmp (nptr - 1, pstr_nan, 3)) {
+	if (endptr)
+	    *endptr = (char *)nptr + 2;
+	return NAN;
+    }
+
+    u64 = 0;
+    exp = 0;
+    while (1) {
+
+	c -= '0';
+
+	if (c <= 9) {
+	    flag |= FL_ANY;
+	    if (flag & FL_OVFL) {
+		if (!(flag & FL_DOT))
+		    exp += 1;
+	    } else {
+		if (flag & FL_DOT)
+		    exp -= 1;
+		u64 = u64 * 10 + c;
+		if (u64 >= (0xffffffffffffffffULL - 9) / 10)
+		    flag |= FL_OVFL;
+	    }
+
+	} else if (c == (('.'-'0') & 0xff)  &&  !(flag & FL_DOT)) {
+	    flag |= FL_DOT;
+	} else {
+	    break;
+	}
+	c = *nptr++;
+    }
+
+    if (c == (('e'-'0') & 0xff) || c == (('E'-'0') & 0xff))
+    {
+	int i;
+	c = *nptr++;
+	i = 2;
+	if (c == '-') {
+	    flag |= FL_MEXP;
+	    c = *nptr++;
+	} else if (c == '+') {
+	    c = *nptr++;
+	} else {
+	    i = 1;
+	}
+	c -= '0';
+	if (c > 9) {
+	    nptr -= i;
+	} else {
+	    i = 0;
+	    do {
+		if (i < 3200)
+		    i = (((i << 2) + i) << 1) + c;	/* i = 10*i + c	*/
+		c = *nptr++ - '0';
+	    } while (c <= 9);
+	    if (flag & FL_MEXP)
+		i = -i;
+	    exp += i;
+	}
+    }
+
+    if ((flag & FL_ANY) && endptr)
+	*endptr = (char *)nptr - 1;
+
+    flt = (double) (u64);		/* manually	*/
+    if ((flag & FL_MINUS) && (flag & FL_ANY))
+	flt = -flt;
+
+    if (flt != 0) {
+	int pwr;
+	const double *pptr;
+	if (exp < 0) {
+	    pptr = (pwr_m10 + 5);
+	    exp = -exp;
+	} else {
+	    pptr = (pwr_p10 + 5);
+	}
+	for (pwr = 32; pwr; pwr >>= 1) {
+	    for (; exp >= pwr; exp -= pwr) {
+		flt *= *pptr;
+	    }
+	    pptr--;
+	}
+//	if (!isfinite(flt) || flt == 0)
+//	    errno = ERANGE;
+    }
+
+    return flt;
 }
