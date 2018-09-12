e6b39dc1b65f82a4ac832cf896cd7797bc34cb65
Keith Packard <keithp@keithp.com>
2018-09-11 18:16:59 -0700

    tinystdio: Use avr version of strtod, just as with strtof
    This actually just uses strtof as avr doesn't have doubles. Someday...
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/tinystdio/strtod.c
+++ newlib/libc/tinystdio/strtod.c
@@ -28,15 +28,20 @@
    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
    POSSIBILITY OF SUCH DAMAGE. */
 
-/* $Id: strtof.c 2191 2010-11-05 13:45:57Z arcanum $ */
+/* $Id: strtod.c 2191 2010-11-05 13:45:57Z arcanum $ */
+
 
 #include <ctype.h>
 #include <errno.h>
 #include <limits.h>
 #include <math.h>		/* INFINITY, NAN		*/
-#include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
+
+/* Only GCC 4.2 calls the library function to convert an unsigned long
+   to float.  Other GCC-es (including 4.3) use a signed long to float
+   conversion along with a large inline code to correct the result.	*/
+extern double __floatunsisf (unsigned long);
 
 static const float pwr_p10 [6] = {
     1e+1, 1e+2, 1e+4, 1e+8, 1e+16, 1e+32
@@ -50,8 +55,8 @@ static const char pstr_inf[] = {'I','N','F'};
 static const char pstr_inity[] = {'I','N','I','T','Y'};
 static const char pstr_nan[] = {'N','A','N'};
 
-/**  The strtof() function converts the initial portion of the string pointed
-     to by \a nptr to float representation.
+/**  The strtod() function converts the initial portion of the string pointed
+     to by \a nptr to double representation.
 
      The expected form of the string is an optional plus ( \c '+' ) or minus
      sign ( \c '-' ) followed by a sequence of digits optionally containing
@@ -61,7 +66,7 @@ static const char pstr_nan[] = {'N','A','N'};
 
      Leading white-space characters in the string are skipped.
 
-     The strtof() function returns the converted value, if any.
+     The strtod() function returns the converted value, if any.
 
      If \a endptr is not \c NULL, a pointer to the character after the last
      character used in the conversion is stored in the location referenced by
@@ -76,135 +81,8 @@ static const char pstr_nan[] = {'N','A','N'};
      returned and \c ERANGE is stored in \c errno.
  */
 
-float
-strtof (const char * nptr, char ** endptr)
+double
+strtod (const char * nptr, char ** endptr)
 {
-    uint32_t u32;
-    float flt;
-    unsigned char c;
-    int exp;
-
-    unsigned char flag;
-#define FL_MINUS    0x01	/* number is negative	*/
-#define FL_ANY	    0x02	/* any digit was readed	*/
-#define FL_OVFL	    0x04	/* overflow was		*/
-#define FL_DOT	    0x08	/* decimal '.' was	*/
-#define FL_MEXP	    0x10	/* exponent 'e' is neg.	*/
-
-    if (endptr)
-	*endptr = (char *)nptr;
-
-    do {
-	c = *nptr++;
-    } while (isspace (c));
-
-    flag = 0;
-    if (c == '-') {
-	flag = FL_MINUS;
-	c = *nptr++;
-    } else if (c == '+') {
-	c = *nptr++;
-    }
-
-    if (!strncmp (nptr - 1, pstr_inf, 3)) {
-	nptr += 2;
-	if (!strncmp (nptr, pstr_inity, 5))
-	    nptr += 5;
-	if (endptr)
-	    *endptr = (char *)nptr;
-	return flag & FL_MINUS ? -INFINITY : +INFINITY;
-    }
-
-    /* NAN() construction is not realised.
-       Length would be 3 characters only.	*/
-    if (!strncmp (nptr - 1, pstr_nan, 3)) {
-	if (endptr)
-	    *endptr = (char *)nptr + 2;
-	return NAN;
-    }
-
-    u32 = 0;
-    exp = 0;
-    while (1) {
-
-	c -= '0';
-
-	if (c <= 9) {
-	    flag |= FL_ANY;
-	    if (flag & FL_OVFL) {
-		if (!(flag & FL_DOT))
-		    exp += 1;
-	    } else {
-		if (flag & FL_DOT)
-		    exp -= 1;
-		/* u32 = u32 * 10 + c	*/
-		u32 = (((u32 << 2) + u32) << 1) + c;
-		if (u32 >= (0xffffffffUL - 9) / 10)
-		    flag |= FL_OVFL;
-	    }
-
-	} else if (c == (('.'-'0') & 0xff)  &&  !(flag & FL_DOT)) {
-	    flag |= FL_DOT;
-	} else {
-	    break;
-	}
-	c = *nptr++;
-    }
-
-    if (c == (('e'-'0') & 0xff) || c == (('E'-'0') & 0xff))
-    {
-	int i;
-	c = *nptr++;
-	i = 2;
-	if (c == '-') {
-	    flag |= FL_MEXP;
-	    c = *nptr++;
-	} else if (c == '+') {
-	    c = *nptr++;
-	} else {
-	    i = 1;
-	}
-	c -= '0';
-	if (c > 9) {
-	    nptr -= i;
-	} else {
-	    i = 0;
-	    do {
-		if (i < 3200)
-		    i = (((i << 2) + i) << 1) + c;	/* i = 10*i + c	*/
-		c = *nptr++ - '0';
-	    } while (c <= 9);
-	    if (flag & FL_MEXP)
-		i = -i;
-	    exp += i;
-	}
-    }
-
-    if ((flag & FL_ANY) && endptr)
-	*endptr = (char *)nptr - 1;
-
-    flt = (float) (u32);		/* manually	*/
-    if ((flag & FL_MINUS) && (flag & FL_ANY))
-	flt = -flt;
-
-    if (flt != 0) {
-	int pwr;
-	const float *pptr;
-	if (exp < 0) {
-	    pptr = (pwr_m10 + 5);
-	    exp = -exp;
-	} else {
-	    pptr = (pwr_p10 + 5);
-	}
-	for (pwr = 32; pwr; pwr >>= 1) {
-	    for (; exp >= pwr; exp -= pwr) {
-		flt *= *pptr;
-	    }
-	    pptr--;
-	}
-//	if (!isfinite(flt) || flt == 0)
-//	    errno = ERANGE;
-    }
-
-    return flt;
+    return (double) strtof(nptr, endptr);
 }
