62fb0614c6ceb06152ca347420baf9fba8821cd5
Sebastian Huber <sebastian.huber@embedded-brains.de>
2019-01-31 11:42:29 +0100

    scandir: Update copyright notice from FreeBSD
    Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

--- newlib/libc/posix/scandir.c
+++ newlib/libc/posix/scandir.c
@@ -1,10 +1,10 @@
 #ifndef HAVE_OPENDIR
 
-/*
+/*-
  * SPDX-License-Identifier: BSD-3-Clause
  *
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
