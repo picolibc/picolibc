b9ceb9ffdb7c5a150649896ba0b055054cd57da3
Anton Maklakov <anton@espressif.com>
2019-04-12 16:11:15 +0700

    libc: Remove ESP-IDF specific *_VISIBLE macros from xtensa config because we support a generic libc

--- newlib/libc/include/sys/features.h
+++ newlib/libc/include/sys/features.h
@@ -385,7 +385,7 @@ extern "C" {
 /* POSIX 1003.26-2003 defined device control method */
 #define _POSIX_26_VERSION			200312L
 
-#endif
+#endif /* __rtems__ */
 
 /* XMK loosely adheres to POSIX -- 1003.1 */
 #ifdef __XMK__
@@ -527,19 +527,17 @@ extern "C" {
 
 #endif /* __CYGWIN__ */
 
-/* ESP-IDF-specific: enable pthreads support */
+/* ESP-specific */
 #ifdef __XTENSA__
+
 #define _POSIX_THREADS                          1
 #define _POSIX_TIMEOUTS                         1
 #define _POSIX_TIMERS                           1
 #define _POSIX_MONOTONIC_CLOCK                  200112L
 #define _POSIX_CLOCK_SELECTION                  200112L
 #define _UNIX98_THREAD_MUTEX_ATTRIBUTES         1
-#undef __MISC_VISIBLE
-#define __MISC_VISIBLE                          1
-#undef __GNU_VISIBLE
-#define __GNU_VISIBLE                           1
-#endif
+
+#endif /* __XTENSA__ */
 
 #ifdef __cplusplus
 }
