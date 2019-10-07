70e834ea7c970dd9bf98167e0cf7d4d7c643a225
Brian Inglis <Brian.Inglis@SystematicSW.ab.ca>
2019-10-07 10:23:01 -0600

    fhandler_proc.cc(format_proc_cpuinfo): round cpu MHz
    Round cpu MHz to correct Windows and match Linux cpuinfo.

--- winsup/cygwin/fhandler_proc.cc
+++ winsup/cygwin/fhandler_proc.cc
@@ -695,6 +695,7 @@ format_proc_cpuinfo (void *, char *&destbuf)
 
       RtlQueryRegistryValues (RTL_REGISTRY_ABSOLUTE, cpu_key, tab,
 			      NULL, NULL);
+      cpu_mhz = ((cpu_mhz - 1) / 10 + 1) * 10;	/* round up to multiple of 10 */
       bufptr += __small_sprintf (bufptr, "processor\t: %d\n", cpu_number);
       uint32_t maxf, vendor_id[4], unused;
 
