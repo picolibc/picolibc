f09de59ce94ff67464dcac13cf2419342feb51a5
Keith Packard <keithp@keithp.com>
2019-10-08 13:29:34 -0700

    Don't use obsolete math implementations by default
    Enabling the old math code removes some useful functions from the
    library, uses some stale code and generally isn't a good plan. The new
    math code requires newer compiler features (post 1999).
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/include/machine/ieeefp.h
+++ newlib/libc/include/machine/ieeefp.h
@@ -461,8 +461,8 @@
 #endif
 
 #ifndef __OBSOLETE_MATH_DEFAULT
-/* Use old math code by default.  */
-#define __OBSOLETE_MATH_DEFAULT 1
+/* Use new math code by default.  */
+#define __OBSOLETE_MATH_DEFAULT 0
 #endif
 #ifndef __OBSOLETE_MATH
 #define __OBSOLETE_MATH __OBSOLETE_MATH_DEFAULT
