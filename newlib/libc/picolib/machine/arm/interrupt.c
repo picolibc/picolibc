5c09210209a49350d899cc55f895b0e2a25479dd
Keith Packard <keithp@keithp.com>
2019-10-21 21:03:28 -0700

    picolib: Add weakly bound interrupt vector for ARM builds
    This provides a basic interrupt vector so that ARM programs can
    run. It will be used if the application doesn't provide one of its
    own.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ newlib/libc/picolib/machine/arm/interrupt.c
@@ -0,0 +1,89 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright © 2019 Keith Packard
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+#include <stdint.h>
+
+#define STRINGIFY(x) #x
+
+/* Interrupt functions */
+
+void arm_halt_isr(void);
+
+void arm_halt_isr(void)
+{
+	for(;;);
+}
+
+void arm_ignore_isr(void);
+void arm_ignore_isr(void)
+{
+}
+
+#define isr(name) \
+	void __attribute__ ((weak)) arm_ ## name ## _isr(void); \
+	_Pragma(STRINGIFY(weak arm_ ## name ## _isr = arm_ignore_isr))
+
+#define isr_halt(name) \
+	void __attribute__ ((weak)) arm_ ## name ## _isr(void); \
+	_Pragma(STRINGIFY(weak arm_ ## name ## _isr = arm_halt_isr))
+
+isr(nmi);
+isr_halt(hardfault);
+isr_halt(memmanage);
+isr_halt(busfault);
+isr_halt(usagefault);
+isr(svc);
+isr(debugmon);
+isr(pendsv);
+isr(systick);
+
+void _start(void);
+extern uint8_t __stack[];
+
+#define i(addr,name)	[(addr)/4] = arm_ ## name ## _isr
+
+__section(".data.init.enter")
+const void *__weak_interrupt_vector[] __attribute((aligned(128))) = {
+	[0] = __stack,
+	[1] = _start,
+	i(0x08, nmi),
+	i(0x0c, hardfault),
+	i(0x2c, svc),
+	i(0x30, debugmon),
+	i(0x38, pendsv),
+	i(0x3c, systick),
+};
+__weak_reference(__weak_interrupt_vector, __interrupt_vector);
