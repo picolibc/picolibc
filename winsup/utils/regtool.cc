d3110717f0a39ec7d00921c048bfd3c8f8efe7c4
Brian Inglis <Brian.Inglis@SystematicSW.ab.ca>
2019-11-11 10:29:00 -0700

    regtool: allow /proc/registry{,32,64}/ registry path prefix
    The user can supply the registry path prefix /proc/registry{,32,64}/ to
    use path completion.

--- winsup/utils/regtool.cc
+++ winsup/utils/regtool.cc
@@ -166,11 +166,13 @@ usage (FILE *where = stderr)
       "  machine  HKLM  HKEY_LOCAL_MACHINE\n"
       "  users    HKU   HKEY_USERS\n"
       "\n"
-      "If the keyname starts with a forward slash ('/'), the forward slash is used\n"
-      "as separator and the backslash can be used as escape character.\n");
+      "You can use forward slash ('/') as a separator instead of backslash, in\n"
+      "that case backslash is treated as an escape character.\n"
+      "You can also supply the registry path prefix /proc/registry{,32,64}/ to\n"
+      "use path completion.\n");
       fprintf (where, ""
       "Example:\n"
-      "%s list '/machine/SOFTWARE/Classes/MIME/Database/Content Type/audio\\/wav'\n\n", prog_name);
+      "%s list '/HKLM/SOFTWARE/Classes/MIME/Database/Content Type/audio\\/wav'\n\n", prog_name);
     }
   if (where == stderr)
     fprintf (where,
@@ -349,6 +351,15 @@ find_key (int howmanyparts, REGSAM access, int option = 0)
 	*h = *e;
       *h = 0;
       n = e;
+    }
+  else if (strncmp ("\\proc\\registry", n, strlen ("\\proc\\registry")) == 0)
+    {
+      /* skip /proc/registry{,32,64}/ prefix */
+      n += strlen ("\\proc\\registry");
+      if (strncmp ("64", n, strlen ("64")) == 0)
+        n += strlen ("64");
+      else if (strncmp ("32", n, strlen ("32")) == 0)
+        n += strlen ("32");
     }
   while (*n != '\\')
     n++;
