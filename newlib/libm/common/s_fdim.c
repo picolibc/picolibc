18b4e0e5187f7e2076661e53747977f21dabedff
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2020-03-10 11:24:12 +0100

    Fix error in fdim/f for infinities
    The comparison c == FP_INFINITE causes the function to return +inf as it
    expects x = +inf to always be larger than y. This shortcut causes
    several issues as it also returns +inf for the following cases:
     - fdim(+inf, +inf), expected (as per C99): +0.0
     - fdim(-inf, any non NaN), expected: +0.0
    
    I don't see a reason to keep the comparison as all the infinity cases
    return the correct result using just the ternary operation.

--- newlib/libm/common/s_fdim.c
+++ newlib/libm/common/s_fdim.c
@@ -49,11 +49,8 @@ ANSI C, POSIX.
 	double y;
 #endif
 {
-  int c = __fpclassifyd(x);
-  if (c == FP_NAN)  return(x);
+  if (__fpclassifyd(x) == FP_NAN)  return(x);
   if (__fpclassifyd(y) == FP_NAN)  return(y);
-  if (c == FP_INFINITE)
-    return HUGE_VAL;
 
   return x > y ? x - y : 0.0;
 }
