2c9cdf9675775b5142ba991546e9d7ed7e7e48a1
Keith Packard <keithp@keithp.com>
2020-05-04 23:50:24 -0700

    tinystdio: Make 'fflush' into a real function
    Instead of a static inline.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/tinystdio/stdio.h
+++ newlib/libc/tinystdio/stdio.h
@@ -840,21 +840,12 @@ extern int	sscanf(const char *__buf, const char *__fmt, ...);
 extern int	__i_sscanf(const char *__buf, const char *__fmt, ...);
 extern int	__f_sscanf(const char *__buf, const char *__fmt, ...);
 
-#if defined(__DOXYGEN__)
 /**
    Flush \c stream.
 
    If the stream provides a flush hook, use that. Otherwise return 0.
  */
 extern int	fflush(FILE *stream);
-#else
-static __inline int fflush(FILE *stream)
-{
-	if (stream->flush)
-		return (stream->flush)(stream);
-	return 0;
-}
-#endif
 
 #ifndef __DOXYGEN__
 /* only mentioned for libstdc++ support, not implemented in library */
