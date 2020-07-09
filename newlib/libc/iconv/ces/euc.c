6c772f4547c5eea46fb814806509b78f08dfdfa1
Keith Packard via Newlib <newlib@sourceware.org>
2020-07-09 16:58:45 -0700

    libc/iconv: Detect CES handler loading failure
    Fix the code checking for character set loading failure so that
    it checks the return value from the init function.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/iconv/ces/euc.c
+++ newlib/libc/iconv/ces/euc.c
@@ -306,7 +306,7 @@ ok:
       data->data[i] = _iconv_to_ucs_ces_handlers_table.init (
                                                         rptr,
                                                         data->desc[i].csname);
-      if (data->data == NULL)
+      if (data->data[i] == NULL)
         goto error;
     } 
 
