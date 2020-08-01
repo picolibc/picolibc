587adb02b0f5b2e4521139c3e79299e79095444a
Keith Packard <keithp@keithp.com>
2020-07-31 17:13:55 -0700

    test: Disable asprintf tests for original stdio
    asprintf/vasprintf may not be included.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- test/printf-tests.c
+++ test/printf-tests.c
@@ -18,6 +18,7 @@ extern int _scanf_float();
 
 int (*_reference_printf_float)() = _printf_float;
 int (*_reference_scanf_float)() = _scanf_float;
+#define TEST_ASPRINTF
 #endif
 #endif
 #endif
@@ -38,9 +39,11 @@ static int test(int serial, char *expect, char *fmt, ...) {
     char *abuf = NULL;
     va_start(ap, fmt);
     int n;
+#ifdef TEST_ASPRINTF
     int an;
     va_list aap;
     va_copy(aap, ap);
+#endif
 #ifndef NO_FLOATING_POINT
     double dv;
     char *star;
@@ -62,27 +65,37 @@ static int test(int serial, char *expect, char *fmt, ...) {
 			    int iv2 = va_arg(ap, int);
 			    dv = va_arg(ap, double);
 			    n = snprintf(buf, 1024, fmt, iv1, iv2, printf_float(dv));
+#ifdef TEST_ASPRINTF
 			    an = asprintf(&abuf, fmt, iv1, iv2, printf_float(dv));
+#endif
 		    } else  {
 			    int iv = va_arg(ap, int);
 			    dv = va_arg(ap, double);
 			    n = snprintf(buf, 1024, fmt, iv, printf_float(dv));
+#ifdef TEST_ASPRINTF
 			    an = asprintf(&abuf, fmt, iv, printf_float(dv));
+#endif
 		    }
 	    } else {
 		    dv = va_arg(ap, double);
 		    n = snprintf(buf, 1024, fmt, printf_float(dv));
+#ifdef TEST_ASPRINTF
 		    an = asprintf(&abuf, fmt, printf_float(dv));
+#endif
 	    }
 	    break;
 #endif
     default:
 	    n = vsnprintf(buf, 1024, fmt, ap);
+#ifdef TEST_ASPRINTF
 	    an = vasprintf(&abuf, fmt, aap);
+#endif
 	    break;
     }
     va_end(ap);
+#ifdef TEST_ASPRINTF
     va_end(aap);
+#endif
     if (n >= 1024) {
         failmsg(serial, "buffer overflow");
 	free(abuf);
@@ -99,6 +112,7 @@ static int test(int serial, char *expect, char *fmt, ...) {
 	free(abuf);
         return 1;
     }
+#ifdef TEST_ASPRINTF
     if (an != n) {
 	failmsg(serial, "asprintf return %d sprintf return %d\n", an, n);
 	free(abuf);
@@ -110,6 +124,7 @@ static int test(int serial, char *expect, char *fmt, ...) {
 	return 1;
     }
     free(abuf);
+#endif
     return 0;
 }
 
