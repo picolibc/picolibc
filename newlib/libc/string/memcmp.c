c25d450784747ef1bdf6c53ec0d024c3df1335e6
Angus Gratton <angus@espressif.com>
2020-08-08 10:36:52 +1000

    memcmp: Also use the word-optimized version when possible
    This is disabled by default when building with -Os as well, but it's called so
    often that there should be a worthwhile performance gain from reading full words when
    possible.

--- newlib/libc/string/memcmp.c
+++ newlib/libc/string/memcmp.c
@@ -47,7 +47,7 @@ memcmp (const void *m1,
 	const void *m2,
 	size_t n)
 {
-#if defined(PREFER_SIZE_OVER_SPEED) || defined(__OPTIMIZE_SIZE__)
+#if defined(PREFER_SIZE_OVER_SPEED)
   unsigned char *s1 = (unsigned char *) m1;
   unsigned char *s2 = (unsigned char *) m2;
 
