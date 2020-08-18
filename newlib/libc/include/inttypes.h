bd4ea209f1b8f76e9d1eebe842949fca80169c87
Keith Packard <keithp@keithp.com>
2020-08-18 09:15:24 -0700

    libc/inttypes.h: Remove declarations of a few _r functions
    These are legacy newlib functions that used 'struct _reent' and are
    not part of picolibc.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/include/inttypes.h
+++ newlib/libc/include/inttypes.h
@@ -314,8 +314,6 @@ typedef struct {
   intmax_t	rem;
 } imaxdiv_t;
 
-struct _reent;
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -323,13 +321,9 @@ extern "C" {
 extern intmax_t  imaxabs(intmax_t j);
 extern imaxdiv_t imaxdiv(intmax_t numer, intmax_t denomer);
 extern intmax_t  strtoimax(const char *__restrict, char **__restrict, int);
-extern intmax_t  _strtoimax_r(struct _reent *, const char *__restrict, char **__restrict, int);
 extern uintmax_t strtoumax(const char *__restrict, char **__restrict, int);
-extern uintmax_t _strtoumax_r(struct _reent *, const char *__restrict, char **__restrict, int);
 extern intmax_t  wcstoimax(const wchar_t *__restrict, wchar_t **__restrict, int);
-extern intmax_t  _wcstoimax_r(struct _reent *, const wchar_t *__restrict, wchar_t **__restrict, int);
 extern uintmax_t wcstoumax(const wchar_t *__restrict, wchar_t **__restrict, int);
-extern uintmax_t _wcstoumax_r(struct _reent *, const wchar_t *__restrict, wchar_t **__restrict, int);
 
 #if __BSD_VISIBLE
 extern intmax_t  strtoimax_l(const char *__restrict, char **_restrict, int, locale_t);
