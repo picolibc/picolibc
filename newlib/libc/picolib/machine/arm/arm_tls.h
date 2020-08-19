e96b31f9d3c6319e1ce5766cea617dd362720464
Keith Packard <keithp@keithp.com>
2020-08-18 18:22:38 -0700

    libc/machine/arm: Use hardware TLS register where available
    On machines with the System Control coprocessor is available, use the
    Thread ID register to hold the TLS location as per the standard 'hard'
    tp ABI.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/picolib/machine/arm/arm_tls.h
+++ newlib/libc/picolib/machine/arm/arm_tls.h
@@ -32,35 +32,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#include "arm_tls.h"
-/*
- * This cannot be a C ABI function as the compiler assumes that it
- * does not modify anything other than r0 and lr.
- */
-	.syntax unified
-	.text
-	.align 4
-	.p2align 4,,15
-	.global __aeabi_read_tp
-	.type __aeabi_read_tp,%function
 
-__aeabi_read_tp:
-	.cfi_sections .debug_frame
-	.cfi_startproc
-#ifdef ARM_TLS_CP15
-	mrc 15, 0, r0, c13, c0, 3
-#else
-	/* Load the address of __tls */
-	ldr r0,1f
-	/* Dereference to get the value of __tls */
-	ldr r0,[r0]
-#endif
-	/* All done, return to caller */
-	bx lr
-	.cfi_endproc
-	
-	/* Holds the address of __tls */
-	.align 2
-#ifndef ARM_TLS_CP15
-1: .word __tls
+#if (__ARM_FEATURE_COPROC & 1) && __ARM_ARCH_PROFILE != 'M' && __ARM_ARCH >= 6
+#define ARM_TLS_CP15
 #endif
