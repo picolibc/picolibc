8b428c4b7a4ae4f06bd89051a93502acc1346581
Sebastian Meyer <meyer@absint.com>
2020-09-16 13:06:15 +0200

    Check for __INT_MAX__ & friends in build system
    Previous test did look at the GCC version. Instead, just check if the
    compiler has these defines.
    
    This is obviously done because CompCert has them, but fails the GCC
    version check.

--- newlib/libc/include/machine/_default_types.h
+++ newlib/libc/include/machine/_default_types.h
@@ -10,7 +10,7 @@
 /*
  * Guess on types by examining *_MIN / *_MAX defines.
  */
-#if __GNUC_PREREQ (3, 3)
+#ifdef HAVE_MAX_DEFINES
 /* GCC >= 3.3.0 has __<val>__ implicitly defined. */
 #define __EXP(x) __##x##__
 #else
