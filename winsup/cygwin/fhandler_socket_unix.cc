2031b48c93512e644d18d7ed71263fe27ca1eac6
Ken Brown <kbrown@cornell.edu>
2020-10-04 11:05:33 -0400

    Cygwin: AF_UNIX: open_pipe: call recv_peer_info
    If open_pipe is called with xchg_sock_info true, call recv_peer_info
    in addition to send_sock_info.

--- winsup/cygwin/fhandler_socket_unix.cc
+++ winsup/cygwin/fhandler_socket_unix.cc
@@ -941,7 +941,11 @@ fhandler_socket_unix::open_pipe (PUNICODE_STRING pipe_name, bool xchg_sock_info)
     {
       set_handle (ph);
       if (xchg_sock_info)
-	send_sock_info (false);
+	{
+	  /* FIXME: Should we check for errors? */
+	  send_sock_info (false);
+	  recv_peer_info ();
+	}
     }
   return status;
 }
