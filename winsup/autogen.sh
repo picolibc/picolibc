fccb9d7239649d66665a92a556c6ab2b065d5f64
Jon Turney <jon.turney@dronecode.org.uk>
2020-10-21 20:41:58 +0100

    Cygwin: Remove surplus autoconf auxiliary files
    Since we are now only configuring once, in winsup, with
    AC_CONFIG_AUX_DIR(..), the auxiliary files are taken from the top-level.
    
    (Previously we had a random assorment of AC_CONFIG_AUX_DIR(..) and
    AC_CONFIG_AUX_DIR(../..) in winsup subdirectories, so auxiliary files
    would be taken from winsup or the top-level.)

--- winsup/autogen.sh
+++ winsup/autogen.sh
@@ -1,12 +1,3 @@
-#!/bin/sh -e
-if ! /usr/bin/test -e config.guess; then
-    /usr/bin/wget -q -O config.guess 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
-    /bin/chmod a+x config.guess
-fi
-if ! /usr/bin/test -e config.sub; then
-    /usr/bin/wget -q -O config.sub 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
-    /bin/chmod a+x config.sub
-fi
 /usr/bin/aclocal --force
 /usr/bin/autoconf -f
 /bin/rm -rf autom4te.cache
