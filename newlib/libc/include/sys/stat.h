0c0f3df224c51769d3095769cc78005967495f85
Joel Sherrill <joel@rtems.org>
2021-05-19 13:43:25 -0500

    sys/stat.h: Enable UTIME_NOW and UTIME_OMIT for RTEMS

--- newlib/libc/include/sys/stat.h
+++ newlib/libc/include/sys/stat.h
@@ -128,7 +128,7 @@ struct	stat
 #define	S_ISLNK(m)	(((m)&_IFMT) == _IFLNK)
 #define	S_ISSOCK(m)	(((m)&_IFMT) == _IFSOCK)
 
-#if defined(__CYGWIN__)
+#if defined(__CYGWIN__) || defined(__rtems__)
 /* Special tv_nsec values for futimens(2) and utimensat(2). */
 #define UTIME_NOW	-2L
 #define UTIME_OMIT	-1L
