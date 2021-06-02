f205ae46ef6c6ef368f09955d577c9cde7b473fb
Keith Packard <keithp@keithp.com>
2021-06-02 16:31:48 -0700

    test: Add rounding-mode test
    This verifies that both library functions and simple arithmetic
    respect rounding mode settings.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ test/rounding-mode.c
@@ -0,0 +1,278 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright © 2021 Keith Packard
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <fenv.h>
+
+#ifndef PICOLIBC_DOUBLE_NOROUND
+static double
+do_round_int(double value, int mode)
+{
+	switch (mode) {
+#ifdef FE_TONEAREST
+	case FE_TONEAREST:
+		return round(value);
+#endif
+#ifdef FE_UPWARD
+	case FE_UPWARD:
+		return ceil(value);
+#endif
+#ifdef FE_DOWNWARD
+	case FE_DOWNWARD:
+		return floor(value);
+#endif
+#ifdef FE_TOWARDZERO
+	case FE_TOWARDZERO:
+		return trunc(value);
+#endif
+	default:
+		return value;
+	}
+}
+#endif
+
+#ifndef PICOLIBC_FLOAT_NOROUND
+static float
+do_roundf_int(float value, int mode)
+{
+	switch (mode) {
+#ifdef FE_TONEAREST
+	case FE_TONEAREST:
+		return roundf(value);
+#endif
+#ifdef FE_UPWARD
+	case FE_UPWARD:
+		return ceilf(value);
+#endif
+#ifdef FE_DOWNWARD
+	case FE_DOWNWARD:
+		return floorf(value);
+#endif
+#ifdef FE_TOWARDZERO
+	case FE_TOWARDZERO:
+		return truncf(value);
+#endif
+	default:
+		return value;
+	}
+}
+#endif
+
+double
+div(double a, double b);
+
+double
+mul(double a, double b);
+
+double
+sub(double a, double b);
+
+float
+div_f(float a, float b);
+
+float
+mul_f(float a, float b);
+
+float
+sub_f(float a, float b);
+
+double
+div_mul_sub(double a, double b, double c, double d)
+{
+	return sub(mul(div(a,b), c), d);
+}
+
+#define do_fancy(sign) div_mul_sub(sign 1.0, 3.0, 3.0, sign 1.0)
+
+float
+div_f_mul_sub(float a, float b, float c, float d)
+{
+	return sub_f(mul_f(div_f(a,b), c), d);
+}
+
+#define do_fancyf(sign) div_f_mul_sub(sign 1.0f, 3.0f, 3.0f, sign 1.0f)
+
+static int
+check(int mode, char *name, double value)
+{
+	int ret = 0;
+
+	printf("test %s for value %g\n", name, value);
+#ifndef PICOLIBC_DOUBLE_NOROUND
+	double want, got;
+	want = do_round_int(value, mode);
+	if (fesetround(mode) != 0) {
+		printf("ERROR fesetround %s failed\n", name);
+		ret++;
+	}
+	got = nearbyint(value);
+	if (want != got) {
+		printf("ERROR double %s: value %g want %g got %g\n", name, value, want, got);
+		ret++;
+	}
+
+	want = do_round_int(-value, mode);
+	if (fesetround(mode) != 0) {
+		printf("ERROR fesetround %s failed\n", name);
+		ret++;
+	}
+	got = nearbyint(-value);
+	if (want != got) {
+		printf("ERROR double %s: -value %g want %g got %g\n", name, value, want, got);
+		ret++;
+	}
+#endif
+#ifndef PICOLIBC_FLOAT_NOROUND
+	float valuef = value, wantf, gotf;
+
+	wantf = do_roundf_int(valuef, mode);
+	if (fesetround(mode) != 0) {
+		printf("ERROR fesetround %s failed\n", name);
+		ret++;
+	}
+	gotf = nearbyintf(valuef);
+	if (wantf != gotf) {
+		printf("ERROR float %s: value %g want %g got %g\n", name, (double) valuef, (double) wantf, (double) gotf);
+		ret++;
+	}
+
+	wantf = do_roundf_int(-valuef, mode);
+	if (fesetround(mode) != 0) {
+		printf("ERROR fesetround %s failed\n", name);
+		ret++;
+	}
+	gotf = nearbyintf(-valuef);
+	if (wantf != gotf) {
+		printf("ERROR float %s: -value %g want %g got %g\n", name, (double) valuef, (double) wantf, (double) gotf);
+		ret++;
+	}
+#endif
+	if (ret)
+		printf("ERROR %s failed\n", name);
+	return ret;
+}
+
+static double my_values[] = {
+	1.0,
+	1.0 / 3.0,
+	3.0 / 2.0,	/* let either round out or round even work */
+	2.0 / 3.0,
+};
+
+#define NUM_VALUE (sizeof(my_values)/sizeof(my_values[0]))
+
+int main(void)
+{
+	int i;
+	int ret = 0;
+
+	for (i = 0; i < NUM_VALUE; i++) {
+#ifdef FE_TONEAREST
+		ret += check(FE_TONEAREST, "FE_TONEAREST", my_values[i]);
+#endif
+#ifdef FE_UPWARD
+		ret += check(FE_UPWARD, "FE_UPWARD", my_values[i]);
+#endif
+#ifdef FE_DOWNWARD
+		ret += check(FE_DOWNWARD, "FE_DOWNWARD", my_values[i]);
+#endif
+#ifdef FE_TOWARDZERO
+		ret += check(FE_TOWARDZERO, "FE_TOWARDZERO", my_values[i]);
+#endif
+	}
+#if defined(FE_UPWARD) && defined(FE_DOWNWARD) && defined(FE_TOWARDZERO)
+
+#define check_func(big, small, name) do {				\
+		printf("testing %s\n", name); \
+		if (big <= small) {					\
+			printf("ERROR %s: %g is not > %g\n", name, (double) big, (double) small); \
+			ret++;						\
+		}							\
+		if (big < 0) {						\
+			printf("ERROR %s: %g is not >= 0\n", name, (double) big); \
+			ret++;						\
+		}							\
+		if (small > 0) {					\
+			printf("ERROR %s: %g is not <= 0\n", name, (double) small); \
+			ret++;						\
+		}							\
+	} while(0)
+
+
+#ifndef PICOLIBC_DOUBLE_NOROUND
+	double up_plus, toward_plus, down_plus, up_minus, toward_minus, down_minus;
+
+	fesetround(FE_UPWARD);
+	up_plus = do_fancy();
+	up_minus = do_fancy(-);
+
+	fesetround(FE_DOWNWARD);
+	down_plus = do_fancy();
+	down_minus = do_fancy(-);
+
+	fesetround(FE_TOWARDZERO);
+	toward_plus = do_fancy();
+	toward_minus = do_fancy(-);
+
+	check_func(up_plus, down_plus, "up/down");
+	check_func(up_plus, toward_plus, "up/toward");
+	check_func(up_minus, down_minus, "-up/-down");
+	check_func(toward_minus, down_minus, "-toward/-down");
+#endif
+#ifndef PICOLIBC_FLOAT_NOROUND
+	float fup_plus, ftoward_plus, fdown_plus, fup_minus, ftoward_minus, fdown_minus;
+
+	fesetround(FE_UPWARD);
+	fup_plus = do_fancyf();
+	fup_minus = do_fancyf(-);
+
+	fesetround(FE_DOWNWARD);
+	fdown_plus = do_fancyf();
+	fdown_minus = do_fancyf(-);
+
+	fesetround(FE_TOWARDZERO);
+	ftoward_plus = do_fancyf();
+	ftoward_minus = do_fancyf(-);
+
+	check_func(fup_plus, fdown_plus, "fup/fdown");
+	check_func(fup_plus, ftoward_plus, "fup/ftoward");
+	check_func(fup_minus, fdown_minus, "-fup/-fdown");
+	check_func(ftoward_minus, fdown_minus, "-ftoward/-fdown");
+#endif
+#endif
+
+	return ret;
+}
