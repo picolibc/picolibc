0cb40f415b7d4f11f819b03320770737fa072be8
Christoph Muellner <cmuellner@linux.com>
2021-08-02 18:46:16 +0200

    RISC-V: Reliably initialize t0 in _times()
    The current implementation does not reliably initialize t0 once.
    Additionally the initialization requires two calls to _gettimeofday().
    Let's sacrifice a byte to keep the initialization status
    and reduce the maximum number of calls to _gettimeofday().
    
    This has caused issues in an application that invokes clock().
    The problematic situation is as follows:
    
    1) The program calls clock() which calls _times().
    2) _gettimeofday(&t0, 0) puts 0 in t0.tv_usec (because less than 1 us has
       elapsed since the beginning of time).
    3) _gettimeofday(&t, 0) puts 1 in t.tv_usec (since now more than 1 us has
       elapsed since the beginning of time).
    4) That call to clock() returns 1 (the value from step 3 minus the value in
       step 2).
    5) The program does a second call to clock().
    6) The code above still sees 0 in t0 so it tries to update t0 again and
       _gettimeofday(&t0, 0) puts 1 in t0.tv_usec.
    7) The _gettimeofday(&t, 0) puts 1 in t.tv_usec (since less than 1us has
       elapsed since step 3).
    8) clock() returns 0 (step 7 minus step 6) and indicates that time is
       moving backwards.
    
    Signed-off-by: Christoph Muellner <cmuellner@gcc.gnu.org>

--- libgloss/riscv/sys_times.c
+++ libgloss/riscv/sys_times.c
@@ -21,13 +21,18 @@ extern int _gettimeofday(struct timeval *, void *);
 clock_t
 _times(struct tms *buf)
 {
-  // when called for the first time, initialize t0
+  static char initialized;
   static struct timeval t0;
-  if (t0.tv_sec == 0 && t0.tv_usec == 0)
-    _gettimeofday (&t0, 0);
-
   struct timeval t;
+
   _gettimeofday (&t, 0);
+
+  // when called for the first time, initialize t0
+  if (!initialized) {
+    t0.tv_sec = t.tv_sec;
+    t0.tv_usec = t.tv_usec;
+    initialized = 1;
+  }
 
   long long utime = (t.tv_sec - t0.tv_sec) * 1000000 + (t.tv_usec - t0.tv_usec);
   buf->tms_utime = utime * CLOCKS_PER_SEC / 1000000;
