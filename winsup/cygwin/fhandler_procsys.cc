picolibc/picolibc

commit 43f65cdd7dae0bbeb5b5f26b008c8645d524869d

* Makefile.in (DLL_OFILES): Add fhandler_procsys.o. 	* devices.h (enum fh_devices): Add FH_PROCSYS. 	* devices.in (dev_procsys_storage): New device. 	* devices.cc: Regenerate. 	* dtable.cc (build_fh_pc): Add code to allocate fhandler_procsys. 	* fhandler.h (proc_len): Convert to size_t. 	(procsys): Declare. 	(procsys_len): Declare. 	(enum virtual_ftype_t): Move here from fhandler_virtual.h. 	Add members supported by fhandler_procsys. 	(fhandler_virtual::exists): Return virtual_ftype_t.  Change 	in all derived classes. 	(class fhandler_procsys): New class. 	(fhandler_union): Add fhandler_procnet and fhandler_procsys members. 	* fhandler_disk_file.cc (__DIR_mounts::check_missing_mount): Use 	ro_u_proc. 	(fhandler_base::fstat_by_handle): Don't copy attributes if file is an 	NT device. 	(fhandler_base::fstat_by_name): Ditto. 	* fhandler_netdrive.cc (fhandler_netdrive::exists): Return 	virtual_ftype_t. 	* fhandler_proc.cc (proc_tab): Sort alphabetically.  Use _VN macro 	to store length. 	(proc_len): Change to size_t. 	(proc_tab_cmp): New static function. 	(virt_tab_search): New function to search entry in virt_tab_t 	arrays.  Use throughout in /proc and sibling classes instead of 	loop. 	(fhandler_proc::exists): Return virtual_ftype_t. 	* fhandler_process.cc (process_tab): Sort alphabetically.  Use _VN 	macro to store length. 	(fhandler_process::exists): Return virtual_ftype_t. 	(fhandler_process::open): Simplify code. 	* fhandler_procnet.cc (procnet_tab): Sort alphabetically.  Use _VN 	macro to store length. 	(fhandler_procnet::exists): Return virtual_ftype_t. 	(fhandler_procnet::open): Simplify. 	* fhandler_procsys.cc: New file. 	* fhandler_registry.cc (fhandler_registry::exists): Return 	virtual_ftype_t. 	* fhandler_virtual.cc (fhandler_virtual::exists): Ditto. 	* fhandler_virtual.h (enum virtual_ftype_t): Move to fhandler.h. 	(virt_tab_t): Add name_len member. 	(_VN): New macro. 	(virt_tab_search): Declare. 	* mount.cc (mount_info::conv_to_win32_path): Fix comment.

winsup/cygwin/fhandler_procsys.cc
@@ -1,0 +1,422 @@
+/* fhandler_procsys.cc: fhandler for native NT namespace.
+
+   Copyright 2010 Red Hat, Inc.
+
+This file is part of Cygwin.
+
+This software is a copyrighted work licensed under the terms of the
+Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
+details. */
+
+#include "winsup.h"
+#include <stdlib.h>
+#include "cygerrno.h"
+#include "security.h"
+#include "path.h"
+#include "fhandler.h"
+#include "dtable.h"
+#include "cygheap.h"
+#include <winioctl.h>
+#include "ntdll.h"
+#include "tls_pbuf.h"
+
+#include <dirent.h>
+
+/* Path of the /proc/sys filesystem */
+const char procsys[] = "/proc/sys";
+const size_t procsys_len = sizeof (procsys) - 1;
+
+#define mk_unicode_path(p) \
+	WCHAR namebuf[strlen (get_name ()) + 1]; \
+	{ \
+	  const char *from; \
+	  PWCHAR to; \
+	  for (to = namebuf, from = get_name () + procsys_len; *from; \
+	       to++, from++) \
+	    /* The NT device namespace is ASCII only. */ \
+	    *to = (*from == '/') ? L'\\' : (WCHAR) *from; \
+	  if (to == namebuf) \
+	    *to++ = L'\\'; \
+	  *to = L'\0'; \
+	  RtlInitUnicodeString ((p), namebuf); \
+	}
+
+/* Returns 0 if path doesn't exist, >0 if path is a directory,
+   -1 if path is a file, -2 if it's a symlink.  */
+virtual_ftype_t
+fhandler_procsys::exists (struct __stat64 *buf)
+{
+  UNICODE_STRING path; \
+  OBJECT_ATTRIBUTES attr;
+  IO_STATUS_BLOCK io;
+  NTSTATUS status;
+  HANDLE h;
+  FILE_BASIC_INFORMATION fbi;
+  virtual_ftype_t file_type = virt_chr;
+
+  if (strlen (get_name ()) == procsys_len)
+    return virt_rootdir;
+  mk_unicode_path (&path);
+  /* First try to open as file/device to get more info. */
+  InitializeObjectAttributes (&attr, &path, OBJ_CASE_INSENSITIVE, NULL, NULL);
+  status = NtOpenFile (&h, READ_CONTROL | FILE_READ_ATTRIBUTES, &attr, &io,
+		       FILE_SHARE_VALID_FLAGS, FILE_OPEN_FOR_BACKUP_INTENT);
+  if (status == STATUS_OBJECT_PATH_NOT_FOUND)
+    return virt_none;
+  /* If the name isn't found, or we get this dreaded sharing violation, let
+     the caller try again as normal file. */
+  if (status == STATUS_OBJECT_NAME_NOT_FOUND
+      || status == STATUS_NO_MEDIA_IN_DEVICE
+      || status == STATUS_SHARING_VIOLATION)
+    return virt_fsfile;	/* Just try again as normal file. */
+  /* Check for pipe errors, which make a good hint... */
+  if (status >= STATUS_PIPE_NOT_AVAILABLE && status <= STATUS_PIPE_BUSY)
+    file_type = virt_pipe;
+  else if (status == STATUS_ACCESS_DENIED)
+    {
+      /* Check if this is just some file or dir on a real FS to circumvent
+         most permission problems. */
+      status = NtQueryAttributesFile (&attr, &fbi);
+      if (NT_SUCCESS (status))
+	return (fbi.FileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+	       ? virt_fsdir : virt_fsfile;
+    }
+  else if (NT_SUCCESS (status))
+    {
+      NTSTATUS dev_stat;
+      FILE_FS_DEVICE_INFORMATION ffdi;
+
+      /* If requested, check permissions. */
+      if (buf)
+      	get_object_attribute (h, &buf->st_uid, &buf->st_gid, &buf->st_mode);
+      /* Check for the device type. */
+      dev_stat = NtQueryVolumeInformationFile (h, &io, &ffdi, sizeof ffdi,
+					       FileFsDeviceInformation);
+      /* And check for file attributes.  If we get them, we peeked into
+	 a real FS through /proc/sys. */
+      status = NtQueryInformationFile (h, &io, &fbi, sizeof fbi,
+				       FileBasicInformation);
+      NtClose (h);
+      if (NT_SUCCESS (dev_stat))
+	{
+	  if (ffdi.DeviceType == FILE_DEVICE_NAMED_PIPE)
+	    file_type = NT_SUCCESS (status) ? virt_pipe : virt_blk;
+	  else if (NT_SUCCESS (status))
+	    file_type = (fbi.FileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+			? virt_fsdir : virt_fsfile;
+	  else if (ffdi.DeviceType == FILE_DEVICE_DISK
+		   || ffdi.DeviceType == FILE_DEVICE_CD_ROM
+		   || ffdi.DeviceType == FILE_DEVICE_DFS
+		   || ffdi.DeviceType == FILE_DEVICE_VIRTUAL_DISK)
+	    file_type = virt_blk;
+	}
+    }
+  /* Then check if it's a symlink. */
+  status = NtOpenSymbolicLinkObject (&h, READ_CONTROL | SYMBOLIC_LINK_QUERY,
+				     &attr);
+  if (NT_SUCCESS (status))
+    {
+      /* If requested, check permissions. */
+      if (buf)
+      	get_object_attribute (h, &buf->st_uid, &buf->st_gid, &buf->st_mode);
+      NtClose (h);
+      return virt_symlink;
+    }
+  /* Eventually, test if it's an object directory. */
+  status = NtOpenDirectoryObject (&h, READ_CONTROL | DIRECTORY_QUERY, &attr);
+  if (NT_SUCCESS (status))
+    {
+      /* If requested, check permissions. */
+      if (buf)
+      	get_object_attribute (h, &buf->st_uid, &buf->st_gid, &buf->st_mode);
+      NtClose (h);
+      return virt_directory;
+    }
+  else if (status == STATUS_ACCESS_DENIED)
+    return virt_directory;
+  /* Give up.  Just treat as character device. */
+  return file_type;
+}
+
+virtual_ftype_t
+fhandler_procsys::exists ()
+{
+  return exists (NULL);
+}
+
+fhandler_procsys::fhandler_procsys ():
+  fhandler_virtual ()
+{
+}
+
+bool
+fhandler_procsys::fill_filebuf ()
+{
+  /* The NT device namespace is ASCII only. */
+  char *fnamep;
+  UNICODE_STRING path, target;
+  OBJECT_ATTRIBUTES attr;
+  NTSTATUS status;
+  HANDLE h;
+  tmp_pathbuf tp;
+
+  mk_unicode_path (&path);
+  if (path.Buffer[path.Length / sizeof (WCHAR) - 1] == L'\\')
+    path.Length -= sizeof (WCHAR);
+  InitializeObjectAttributes (&attr, &path, OBJ_CASE_INSENSITIVE, NULL, NULL);
+  status = NtOpenSymbolicLinkObject (&h, SYMBOLIC_LINK_QUERY, &attr);
+  if (!NT_SUCCESS (status))
+    return false;
+  RtlInitEmptyUnicodeString (&target, tp.w_get (),
+			     (NT_MAX_PATH - 1) * sizeof (WCHAR));
+  status = NtQuerySymbolicLinkObject (h, &target, NULL);
+  NtClose (h);
+  if (!NT_SUCCESS (status))
+    return false;
+  size_t len = sys_wcstombs (NULL, 0, target.Buffer,
+			     target.Length / sizeof (WCHAR));
+  filebuf = (char *) crealloc_abort (filebuf, procsys_len + len + 1);
+  sys_wcstombs (fnamep = stpcpy (filebuf, procsys), len + 1, target.Buffer,
+		target.Length / sizeof (WCHAR));
+  while ((fnamep = strchr (fnamep, '\\')))
+    *fnamep = '/';
+  return true;
+}
+
+int
+fhandler_procsys::fstat (struct __stat64 *buf)
+{
+  const char *path = get_name ();
+  debug_printf ("fstat (%s)", path);
+
+  fhandler_base::fstat (buf);
+  /* Best bet. */
+  buf->st_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP;
+  buf->st_uid = 544;
+  buf->st_gid = 18;
+  buf->st_dev = buf->st_rdev = dev ().devn;
+  buf->st_ino = get_ino ();
+  switch (exists (buf))
+    {
+    case virt_directory:
+    case virt_rootdir:
+    case virt_fsdir:
+      buf->st_mode |= S_IFDIR;
+      if (buf->st_mode & S_IRUSR)
+	buf->st_mode |= S_IXUSR;
+      if (buf->st_mode & S_IRGRP)
+	buf->st_mode |= S_IXGRP;
+      if (buf->st_mode & S_IROTH)
+	buf->st_mode |= S_IXOTH;
+      break;
+    case virt_file:
+    case virt_fsfile:
+      buf->st_mode |= S_IFREG;
+      break;
+    case virt_symlink:
+      buf->st_mode |= S_IFLNK;
+      break;
+    case virt_pipe:
+      buf->st_mode |= S_IFIFO;
+      break;
+    case virt_socket:
+      buf->st_mode |= S_IFSOCK;
+      break;
+    case virt_chr:
+      buf->st_mode |= S_IFCHR;
+      break;
+    case virt_blk:
+      buf->st_mode |= S_IFBLK;
+      break;
+    default:
+      set_errno (ENOENT);
+      return -1;
+    }
+  return 0;
+}
+
+DIR *
+fhandler_procsys::opendir (int fd)
+{
+  UNICODE_STRING path;
+  OBJECT_ATTRIBUTES attr;
+  NTSTATUS status;
+  HANDLE h;
+  DIR *dir = fhandler_virtual::opendir (fd);
+
+  mk_unicode_path (&path);
+  InitializeObjectAttributes (&attr, &path, OBJ_CASE_INSENSITIVE, NULL, NULL);
+  status = NtOpenDirectoryObject (&h, DIRECTORY_QUERY, &attr);
+  if (!NT_SUCCESS (status))
+    {
+      free (dir);
+      __seterrno_from_nt_status (status);
+      return NULL;
+    }
+  dir->__handle = h;
+  return dir;
+}
+
+int
+fhandler_procsys::readdir (DIR *dir, dirent *de)
+{
+  NTSTATUS status;
+  struct fdbi
+  {
+    DIRECTORY_BASIC_INFORMATION dbi;
+    WCHAR buf[2][NAME_MAX + 1];
+  } f;
+  int res = EBADF;
+
+  if (dir->__handle != INVALID_HANDLE_VALUE)
+    {
+      BOOLEAN restart = dir->__d_position ? FALSE : TRUE;
+      status = NtQueryDirectoryObject (dir->__handle, &f, sizeof f, TRUE,
+				       restart, (PULONG) &dir->__d_position,
+				       NULL);
+      if (!NT_SUCCESS (status))
+	res = ENMFILE;
+      else
+	{
+	  sys_wcstombs (de->d_name, NAME_MAX + 1, f.dbi.ObjectName.Buffer,
+			f.dbi.ObjectName.Length / sizeof (WCHAR));
+	  de->d_ino = hash_path_name (get_ino (), de->d_name);
+	  de->d_type = 0;
+	  res = 0;
+	}
+    }
+  syscall_printf ("%d = readdir (%p, %p)", res, dir, de);
+  return res;
+}
+
+long
+fhandler_procsys::telldir (DIR *dir)
+{
+  return dir->__d_position;
+}
+
+void
+fhandler_procsys::seekdir (DIR *dir, long pos)
+{
+  dir->__d_position = pos;
+}
+
+int
+fhandler_procsys::closedir (DIR *dir)
+{
+  if (dir->__handle != INVALID_HANDLE_VALUE)
+    {
+      NtClose (dir->__handle);
+      dir->__handle = INVALID_HANDLE_VALUE;
+    }
+  return fhandler_virtual::closedir (dir);
+}
+
+void __stdcall
+fhandler_procsys::read (void *ptr, size_t& len)
+{
+  NTSTATUS status;
+  IO_STATUS_BLOCK io;
+  LARGE_INTEGER off = { QuadPart:0LL };
+
+  status = NtReadFile (get_handle (), NULL, NULL, NULL, &io, ptr, len,
+		       &off, NULL);
+  if (!NT_SUCCESS (status))
+    {
+      __seterrno_from_nt_status (status);
+      len = -1;
+    }
+  else
+    len = io.Information;
+}
+
+ssize_t __stdcall
+fhandler_procsys::write (const void *ptr, size_t len)
+{
+  return fhandler_base::raw_write (ptr, len);
+}
+
+int
+fhandler_procsys::open (int flags, mode_t mode)
+{
+  UNICODE_STRING path;
+  OBJECT_ATTRIBUTES attr;
+  IO_STATUS_BLOCK io;
+  NTSTATUS status;
+  HANDLE h;
+  ULONG access;
+  ULONG options = FILE_OPEN_FOR_BACKUP_INTENT;
+
+
+  int res = fhandler_virtual::open (flags, mode);
+  if (!res)
+    goto out;
+
+  if ((flags & (O_CREAT | O_EXCL)) == (O_CREAT | O_EXCL) || (flags & O_TRUNC))
+    {
+      set_errno (EINVAL);
+      res = 0;
+      goto out;
+    }
+  mk_unicode_path (&path);
+  InitializeObjectAttributes (&attr, &path, OBJ_INHERIT | OBJ_CASE_INSENSITIVE,
+			      NULL, NULL);
+  switch (exists ())
+    {
+    case virt_directory:
+    case virt_rootdir:
+      if ((flags & O_ACCMODE) != O_RDONLY)
+	{
+	  set_errno (EISDIR);
+	  res = 0;
+	  goto out;
+	}
+      nohandle (true);
+      res = 1;
+      goto out;
+    default:
+      break;
+    }
+  if ((flags & O_ACCMODE) == O_RDONLY)
+    access = GENERIC_READ;
+  else if ((flags & O_ACCMODE) == O_WRONLY)
+    access = GENERIC_WRITE | READ_CONTROL | FILE_READ_ATTRIBUTES;
+  else
+    access = GENERIC_READ | GENERIC_WRITE;
+  if (flags & O_SYNC)
+    options |= FILE_WRITE_THROUGH;
+  if (flags & O_DIRECT)
+    options |= FILE_NO_INTERMEDIATE_BUFFERING;
+  if (!(flags & O_NONBLOCK))
+    {
+      access |= SYNCHRONIZE;
+      options |= FILE_SYNCHRONOUS_IO_NONALERT;
+    }
+  status = NtOpenFile (&h, access, &attr, &io, FILE_SHARE_VALID_FLAGS, options);
+  if (!NT_SUCCESS (status))
+    {
+      __seterrno_from_nt_status (status);
+      res = 0;
+      goto out;
+    }
+  set_io_handle (h);
+  set_open_status ();
+  res = 1;
+out:
+  syscall_printf ("%d = fhandler_procsys::open (%p, %d)", res, flags, mode);
+  return res;
+}
+
+int
+fhandler_procsys::close ()
+{
+  if (!nohandle ())
+    NtClose (get_handle ());
+  return fhandler_virtual::close ();
+}
+#if 0
+int
+fhandler_procsys::ioctl (unsigned int cmd, void *)
+{
+}
+#endif

winsup/cygwin/Makefile.in
@@ -141,13 +141,14 @@ DLL_OFILES:=assert.o autoload.o bsdlib.o ctype.o cxx.o cygheap.o cygthread.o \
 	fhandler.o fhandler_clipboard.o fhandler_console.o fhandler_disk_file.o \
 	fhandler_dsp.o fhandler_fifo.o fhandler_floppy.o fhandler_mailslot.o \
 	fhandler_mem.o fhandler_netdrive.o fhandler_nodevice.o fhandler_proc.o \
-	fhandler_process.o fhandler_procnet.o fhandler_random.o fhandler_raw.o \
-	fhandler_registry.o fhandler_serial.o fhandler_socket.o fhandler_tape.o \
-	fhandler_termios.o fhandler_tty.o fhandler_virtual.o fhandler_windows.o \
-	fhandler_zero.o flock.o fnmatch.o fork.o fts.o ftw.o getopt.o glob.o \
-	glob_pattern_p.o globals.o grp.o heap.o hookapi.o inet_addr.o inet_network.o \
-	init.o ioctl.o ipc.o kernel32.o libstdcxx_wrapper.o localtime.o lsearch.o \
-	malloc_wrapper.o minires-os-if.o minires.o miscfuncs.o mktemp.o mmap.o msg.o \
+	fhandler_process.o fhandler_procnet.o fhandler_procsys.o fhandler_random.o \
+	fhandler_raw.o fhandler_registry.o fhandler_serial.o fhandler_socket.o \
+	fhandler_tape.o fhandler_termios.o fhandler_tty.o fhandler_virtual.o \
+	fhandler_windows.o fhandler_zero.o flock.o fnmatch.o fork.o fts.o ftw.o \
+	getopt.o glob.o glob_pattern_p.o globals.o grp.o heap.o hookapi.o \
+	inet_addr.o inet_network.o init.o ioctl.o ipc.o kernel32.o \
+	libstdcxx_wrapper.o localtime.o lsearch.o malloc_wrapper.o \
+	minires-os-if.o minires.o miscfuncs.o mktemp.o mmap.o msg.o \
 	mount.o net.o netdb.o nfs.o nftw.o nlsfuncs.o ntea.o passwd.o path.o \
 	pinfo.o pipe.o poll.o posix_ipc.o pseudo-reloc.o pthread.o random.o \
 	regcomp.o regerror.o regexec.o regfree.o registry.o resource.o rexec.o \

winsup/cygwin/devices.cc
@@ -24,6 +24,9 @@ const device dev_proc_storage =
 const device dev_procnet_storage =
   {"", {FH_PROCNET}, ""};
 
+const device dev_procsys_storage =
+  {"", {FH_PROCSYS}, ""};
+
 const device dev_netdrive_storage =
   {"", {FH_NETDRIVE}, ""};
 

winsup/cygwin/devices.h
@@ -1,6 +1,6 @@
 /* devices.h
 
-   Copyright 2002, 2003, 2004, 2005 Red Hat, Inc.
+   Copyright 2002, 2003, 2004, 2005, 2007, 2009, 2010 Red Hat, Inc.
 
 This file is part of Cygwin.
 
@@ -53,6 +53,7 @@ enum fh_devices
   FH_DEV     = FHDEV (0, 245),
   FH_PROCNET = FHDEV (0, 244),
   FH_PROCESSFD = FHDEV (0, 243),
+  FH_PROCSYS = FHDEV (0, 242),
 
   DEV_FLOPPY_MAJOR = 2,
   FH_FLOPPY  = FHDEV (DEV_FLOPPY_MAJOR, 0),

winsup/cygwin/devices.in
@@ -20,6 +20,9 @@ const device dev_proc_storage =
 const device dev_procnet_storage =
   {"", {FH_PROCNET}, ""};
 
+const device dev_procsys_storage =
+  {"", {FH_PROCSYS}, ""};
+
 const device dev_netdrive_storage =
   {"", {FH_NETDRIVE}, ""};
 

winsup/cygwin/dtable.cc
@@ -540,6 +540,9 @@ build_fh_pc (path_conv& pc, bool set_name)
 	case FH_PROCNET:
 	  fh = cnew (fhandler_procnet) ();
 	  break;
+	case FH_PROCSYS:
+	  fh = cnew (fhandler_procsys) ();
+	  break;
 	case FH_NETDRIVE:
 	  fh = cnew (fhandler_netdrive) ();
 	  break;

winsup/cygwin/fhandler.h
@@ -34,7 +34,9 @@ extern const char *windows_device_names[];
 extern struct __cygwin_perfile *perfile_table;
 #define __fmode (*(user_data->fmode_ptr))
 extern const char proc[];
-extern const int proc_len;
+extern const size_t proc_len;
+extern const char procsys[];
+extern const size_t procsys_len;
 
 class select_record;
 class select_stuff;
@@ -97,6 +99,20 @@ enum del_lock_called_from {
   on_close,
   after_fork,
   after_exec
+};
+
+enum virtual_ftype_t {
+  virt_blk = -7,	/* Block special */
+  virt_chr = -6,	/* Character special */
+  virt_fsfile = -5,	/* FS-based file via /proc/sys */
+  virt_socket = -4,	/* Socket */
+  virt_pipe = -3,	/* Pipe */
+  virt_symlink = -2,	/* Symlink */
+  virt_file = -1,	/* Regular file */
+  virt_none = 0,	/* Invalid, Error */
+  virt_directory = 1,	/* Directory */
+  virt_rootdir = 2,	/* Root directory of virtual FS */
+  virt_fsdir = 3,	/* FS-based directory via /proc/sys */
 };
 
 class fhandler_base
@@ -1331,7 +1347,7 @@ class fhandler_virtual : public fhandler_base
   fhandler_virtual ();
   virtual ~fhandler_virtual();
 
-  virtual int exists();
+  virtual virtual_ftype_t exists();
   DIR *opendir (int fd) __attribute__ ((regparm (2)));
   long telldir (DIR *);
   void seekdir (DIR *, long);
@@ -1357,7 +1373,7 @@ class fhandler_proc: public fhandler_virtual
 {
  public:
   fhandler_proc ();
-  int exists();
+  virtual_ftype_t exists();
   int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
   static DWORD get_proc_fhandler(const char *path);
 
@@ -1366,11 +1382,30 @@ class fhandler_proc: public fhandler_virtual
   bool fill_filebuf ();
 };
 
+class fhandler_procsys: public fhandler_virtual
+{
+ public:
+  fhandler_procsys ();
+  virtual_ftype_t exists(struct __stat64 *buf) __attribute__ ((regparm (2)));
+  virtual_ftype_t exists();
+  DIR *opendir (int fd) __attribute__ ((regparm (2)));
+  int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
+  long telldir (DIR *);
+  void seekdir (DIR *, long);
+  int closedir (DIR *);
+  int open (int flags, mode_t mode = 0);
+  int close ();
+  void __stdcall read (void *ptr, size_t& len) __attribute__ ((regparm (3)));
+  ssize_t __stdcall write (const void *ptr, size_t len);
+  int __stdcall fstat (struct __stat64 *buf) __attribute__ ((regparm (2)));
+  bool fill_filebuf ();
+};
+
 class fhandler_netdrive: public fhandler_virtual
 {
  public:
   fhandler_netdrive ();
-  int exists();
+  virtual_ftype_t exists();
   int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
   void seekdir (DIR *, long);
   void rewinddir (DIR *);
@@ -1388,7 +1423,7 @@ class fhandler_registry: public fhandler_proc
  public:
   fhandler_registry ();
   void set_name (path_conv &pc);
-  int exists();
+  virtual_ftype_t exists();
   int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
   long telldir (DIR *);
   void seekdir (DIR *, long);
@@ -1408,7 +1443,7 @@ class fhandler_process: public fhandler_proc
   pid_t pid;
  public:
   fhandler_process ();
-  int exists();
+  virtual_ftype_t exists();
   DIR *opendir (int fd) __attribute__ ((regparm (2)));
   int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
   int open (int flags, mode_t mode = 0);
@@ -1421,7 +1456,7 @@ class fhandler_procnet: public fhandler_proc
   pid_t pid;
  public:
   fhandler_procnet ();
-  int exists();
+  virtual_ftype_t exists();
   int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
   int open (int flags, mode_t mode = 0);
   int __stdcall fstat (struct __stat64 *buf) __attribute__ ((regparm (2)));
@@ -1462,6 +1497,8 @@ typedef union
   char __pipe[sizeof (fhandler_pipe)];
   char __proc[sizeof (fhandler_proc)];
   char __process[sizeof (fhandler_process)];
+  char __procnet[sizeof (fhandler_procnet)];
+  char __procsys[sizeof (fhandler_procsys)];
   char __pty_master[sizeof (fhandler_pty_master)];
   char __registry[sizeof (fhandler_registry)];
   char __serial[sizeof (fhandler_serial)];

winsup/cygwin/fhandler_disk_file.cc
@@ -118,7 +118,7 @@ public:
 	    {
 	      found[__DIR_PROC] = true;
 	      if (retname)
-		RtlInitUnicodeString (retname, L"proc");
+		*retname = ro_u_proc;
 	      return 2;
 	    }
 	  if (!found[__DIR_CYGDRIVE])
@@ -407,7 +407,9 @@ fhandler_base::fstat_by_handle (struct __stat64 *buf)
      entry, as in other calls to fstat_helper. */
   if (pc.is_rep_symlink ())
     fi.fbi.FileAttributes &= ~FILE_ATTRIBUTE_DIRECTORY;
-  pc.file_attributes (fi.fbi.FileAttributes);
+  /* Only copy attributes if not a device root dir. */
+  if (!(pc.file_attributes () & FILE_ATTRIBUTE_DEVICE))
+    pc.file_attributes (fi.fbi.FileAttributes);
   return fstat_helper (buf,
 		       fi.fbi.ChangeTime.QuadPart ? &fi.fbi.ChangeTime
 						  : &fi.fbi.LastWriteTime,
@@ -474,7 +476,9 @@ fhandler_base::fstat_by_name (struct __stat64 *buf)
      entry, as in other calls to fstat_helper. */
   if (pc.is_rep_symlink ())
     fdi_buf.fdi.FileAttributes &= ~FILE_ATTRIBUTE_DIRECTORY;
-  pc.file_attributes (fdi_buf.fdi.FileAttributes);
+  /* Only copy attributes if not a device root dir. */
+  if (!(pc.file_attributes () & FILE_ATTRIBUTE_DEVICE))
+    pc.file_attributes (fdi_buf.fdi.FileAttributes);
   return fstat_helper (buf,
 		       fdi_buf.fdi.ChangeTime.QuadPart
 		       ? &fdi_buf.fdi.ChangeTime : &fdi_buf.fdi.LastWriteTime,

commit 5036d447c54c41968ccd19a6294f69e4085d0143

Cygwin: fhandler_procsys::readdir: fix NtQueryDirectoryObject usage
As outlined in the previous patch, the non-atomicity of iterating
over a directory in the NT namespace via NtQueryDirectoryObject
one entry each, results in potential duplication of directory entries.

Fix this for fhandler_procsys::readdir as well by fetching the entire
dir inside fhandler_procsys::opendir, storing it in a buffer, and just
return buffer content from fhandler_procsys::readdir.

Fixes: 43f65cdd7dae ("fhandler_procsys.cc: New file.")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/fhandler_procsys.cc
@@ -309,59 +309,88 @@ fhandler_procsys::opendir (int fd)
   UNICODE_STRING path;
   OBJECT_ATTRIBUTES attr;
   NTSTATUS status;
-  HANDLE h;
+  HANDLE dir_hdl;
   DIR *dir;
 
   mk_unicode_path (&path);
   InitializeObjectAttributes (&attr, &path, OBJ_CASE_INSENSITIVE, NULL, NULL);
-  status = NtOpenDirectoryObject (&h, DIRECTORY_QUERY, &attr);
+  status = NtOpenDirectoryObject (&dir_hdl, DIRECTORY_QUERY, &attr);
   if (!NT_SUCCESS (status))
     {
       __seterrno_from_nt_status (status);
       return NULL;
     }
+
+  void *dbi_buf = NULL;
+  ULONG size = 65536;
+  ULONG context = 0;
+  int iter;
+  for (iter = 0; iter <= 3; ++iter)	/* Allows for a 512K buffer */
+    {
+      void *new_buf = realloc (dbi_buf, size);
+      if (!new_buf)
+	goto err;
+      dbi_buf = new_buf;
+      status = NtQueryDirectoryObject (dir_hdl, dbi_buf, size, FALSE, TRUE,
+				       &context, NULL);
+      if (!NT_SUCCESS (status))
+	{
+	  __seterrno_from_nt_status (status);
+	  goto err;
+	}
+      if (status != STATUS_MORE_ENTRIES)
+	break;
+      size <<= 1;
+    }
+  if (iter > 3)
+    {
+      __seterrno_from_nt_status (STATUS_INSUFFICIENT_RESOURCES);
+      goto err;
+    }
   if (!(dir = fhandler_virtual::opendir (fd)))
-    NtClose (h);
-  else
-    dir->__handle = h;
+    goto err;
+  /* Note that dir->__handle points to the buffer, it does NOT contain an
+     actual handle! */
+  dir->__handle = dbi_buf;
+  /* dir->__d_internal contains the number of objects returned in the buffer. */
+  dir->__d_internal = context;
   return dir;
+
+err:
+  NtClose (dir_hdl);
+  free (dbi_buf);
+  return NULL;
 }
 
 int
 fhandler_procsys::readdir (DIR *dir, dirent *de)
 {
-  NTSTATUS status;
-  struct fdbi
-  {
-    DIRECTORY_BASIC_INFORMATION dbi;
-    WCHAR buf[2][NAME_MAX + 1];
-  } f;
+  PDIRECTORY_BASIC_INFORMATION dbi;
   int res = EBADF;
 
   if (dir->__handle != INVALID_HANDLE_VALUE)
     {
-      BOOLEAN restart = dir->__d_position ? FALSE : TRUE;
-      status = NtQueryDirectoryObject (dir->__handle, &f, sizeof f, TRUE,
-				       restart, (PULONG) &dir->__d_position,
-				       NULL);
-      if (!NT_SUCCESS (status))
+      dbi = ((PDIRECTORY_BASIC_INFORMATION) dir->__handle);
+      dbi += dir->__d_position;
+      if (dir->__d_position >= (__int32_t) dir->__d_internal
+	  || dbi->ObjectName.Length == 0)
 	res = ENMFILE;
       else
 	{
-	  sys_wcstombs (de->d_name, NAME_MAX + 1, f.dbi.ObjectName.Buffer,
-			f.dbi.ObjectName.Length / sizeof (WCHAR));
+	  sys_wcstombs (de->d_name, NAME_MAX + 1, dbi->ObjectName.Buffer,
+			dbi->ObjectName.Length / sizeof (WCHAR));
 	  de->d_ino = hash_path_name (get_ino (), de->d_name);
-	  if (RtlEqualUnicodeString (&f.dbi.ObjectTypeName, &ro_u_natdir,
-				     FALSE))
+	  if (RtlEqualUnicodeString (&dbi->ObjectTypeName, &ro_u_natdir, FALSE))
 	    de->d_type = DT_DIR;
-	  else if (RtlEqualUnicodeString (&f.dbi.ObjectTypeName, &ro_u_natsyml,
+	  else if (RtlEqualUnicodeString (&dbi->ObjectTypeName, &ro_u_natsyml,
 					  FALSE))
 	    de->d_type = DT_LNK;
-	  else if (!RtlEqualUnicodeString (&f.dbi.ObjectTypeName, &ro_u_natdev,
+	  else if (!RtlEqualUnicodeString (&dbi->ObjectTypeName, &ro_u_natdev,
 					   FALSE))
 	    de->d_type = DT_CHR;
 	  else /* Can't nail down "Device" objects without further testing. */
 	    de->d_type = DT_UNKNOWN;
+	  ++dir->__d_position;
 	  res = 0;
 	}
     }
@@ -378,7 +407,12 @@ fhandler_procsys::telldir (DIR *dir)
 void
 fhandler_procsys::seekdir (DIR *dir, long pos)
 {
-  dir->__d_position = pos;
+  if (pos < 0)
+    dir->__d_position = 0;
+  else if (pos > (__int32_t) dir->__d_internal)
+    dir->__d_position = (__int32_t) dir->__d_internal;
+  else
+    dir->__d_position = pos;
 }
 
 int
@@ -386,7 +420,7 @@ fhandler_procsys::closedir (DIR *dir)
 {
   if (dir->__handle != INVALID_HANDLE_VALUE)
     {
-      NtClose (dir->__handle);
+      free (dir->__handle);
       dir->__handle = INVALID_HANDLE_VALUE;
     }
   return fhandler_virtual::closedir (dir);