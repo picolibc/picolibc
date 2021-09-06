picolibc/picolibc

commit bdb7991db38b7bc802f4ef498a16ee850a837f44

Cygwin: workaround a g++ 11.2 initialization bug
trying to use aggregate initialization syntax on a member of a
nameless union member failes in g++ 11.2.

Workaround this by using explicit initialization.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/miscfuncs.cc
@@ -511,15 +511,16 @@ class thread_allocator
   PVOID (thread_allocator::*alloc_func) (SIZE_T);
   PVOID _alloc (SIZE_T size)
   {
-    MEM_ADDRESS_REQUIREMENTS thread_req = {
+    static const MEM_ADDRESS_REQUIREMENTS thread_req = {
       (PVOID) THREAD_STORAGE_LOW,
       (PVOID) (THREAD_STORAGE_HIGH - 1),
       THREAD_STACK_SLOT
     };
-    MEM_EXTENDED_PARAMETER thread_ext = {
-      .Type = MemExtendedParameterAddressRequirements,
-      .Pointer = (PVOID) &thread_req
-    };
+    /* g++ 11.2 workaround: don't use initializer */
+    MEM_EXTENDED_PARAMETER thread_ext;
+    thread_ext.Type = MemExtendedParameterAddressRequirements;
+    thread_ext.Pointer = (PVOID) &thread_req;
+
     SIZE_T real_size = roundup2 (size, THREAD_STACK_SLOT);
     PVOID real_stackaddr = NULL;
 
@@ -531,15 +532,16 @@ class thread_allocator
        monster stack, fulfill request from mmap area. */
     if (!real_stackaddr)
       {
-	MEM_ADDRESS_REQUIREMENTS mmap_req = {
+	static const MEM_ADDRESS_REQUIREMENTS mmap_req = {
 	  (PVOID) MMAP_STORAGE_LOW,
 	  (PVOID) (MMAP_STORAGE_HIGH - 1),
 	  THREAD_STACK_SLOT
 	};
-	MEM_EXTENDED_PARAMETER mmap_ext = {
-	  .Type = MemExtendedParameterAddressRequirements,
-	  .Pointer = (PVOID) &mmap_req
-	};
+	/* g++ 11.2 workaround: don't use initializer */
+	MEM_EXTENDED_PARAMETER mmap_ext;
+	mmap_ext.Type = MemExtendedParameterAddressRequirements;
+	mmap_ext.Pointer = (PVOID) &mmap_req;
+
 	real_stackaddr = VirtualAlloc2 (GetCurrentProcess(), NULL, real_size,
 					MEM_RESERVE | MEM_TOP_DOWN,
 					PAGE_READWRITE, &mmap_ext, 1);

winsup/cygwin/mmap.cc
@@ -201,15 +201,15 @@ MapView (HANDLE h, void *addr, size_t len, DWORD openflags,
      a function under loader lock (STATUS_DLL_INIT_FAILED). */
   if (!from_fixup_after_fork && wincap.has_extended_mem_api ())
     {
-      MEM_ADDRESS_REQUIREMENTS mmap_req = {
+      static const MEM_ADDRESS_REQUIREMENTS mmap_req = {
 	(PVOID) MMAP_STORAGE_LOW,
 	(PVOID) (MMAP_STORAGE_HIGH - 1),
 	0
       };
-      MEM_EXTENDED_PARAMETER mmap_ext = {
-	.Type = MemExtendedParameterAddressRequirements,
-	.Pointer = (PVOID) &mmap_req
-      };
+      /* g++ 11.2 workaround: don't use initializer */
+      MEM_EXTENDED_PARAMETER mmap_ext;
+      mmap_ext.Type = MemExtendedParameterAddressRequirements;
+      mmap_ext.Pointer = (PVOID) &mmap_req;
 
       alloc_type |= attached (prot) ? MEM_RESERVE : 0;
       status = NtMapViewOfSectionEx (h, NtCurrentProcess (), &base, &offset,
@@ -1621,15 +1621,15 @@ fhandler_dev_zero::mmap (caddr_t *addr, size_t len, int prot,
 #ifdef __x86_64__
       if (wincap.has_extended_mem_api ())
 	{
-	  MEM_ADDRESS_REQUIREMENTS mmap_req = {
+	  static const MEM_ADDRESS_REQUIREMENTS mmap_req = {
 	    (PVOID) MMAP_STORAGE_LOW,
 	    (PVOID) (MMAP_STORAGE_HIGH - 1),
 	    0
 	  };
-	  MEM_EXTENDED_PARAMETER mmap_ext = {
-	    .Type = MemExtendedParameterAddressRequirements,
-	    .Pointer = (PVOID) &mmap_req
-	  };
+	  /* g++ 11.2 workaround: don't use initializer */
+	  MEM_EXTENDED_PARAMETER mmap_ext;
+	  mmap_ext.Type = MemExtendedParameterAddressRequirements;
+	  mmap_ext.Pointer = (PVOID) &mmap_req;
 
 	  base = VirtualAlloc2 (GetCurrentProcess(), *addr, len, alloc_type,
 				protect, *addr ? NULL : &mmap_ext,

commit 3d322ac930ca6d2516680fc78e3d3018cc737483

Cygwin: fix initializing MEM_EXTENDED_PARAMETER
MEM_EXTENDED_PARAMETER consists of a 64 bit bitfield which contains
the Type and the Reserved members.  The former usage of designated
initializer lists initialized Type, but not Reserved.  Since that's
not possible anymore due to a g++ 11.2 bug, Cygwin initializes the
MEM_EXTENDED_PARAMETER structs explicitely.  This results in a
random value in Reserved, which at least VirtualAlloc2 chokes on
(ERROR_INVALID_PARAMETER).

Set Reserved explicitely to 0 for a fix.

Fixes: bdb7991db38b ("Cygwin: workaround a g++ 11.2 initialization bug")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/miscfuncs.cc
@@ -519,6 +519,7 @@ class thread_allocator
     /* g++ 11.2 workaround: don't use initializer */
     MEM_EXTENDED_PARAMETER thread_ext;
     thread_ext.Type = MemExtendedParameterAddressRequirements;
+    thread_ext.Reserved = 0;
     thread_ext.Pointer = (PVOID) &thread_req;
 
     SIZE_T real_size = roundup2 (size, THREAD_STACK_SLOT);
@@ -540,6 +541,7 @@ class thread_allocator
 	/* g++ 11.2 workaround: don't use initializer */
 	MEM_EXTENDED_PARAMETER mmap_ext;
 	mmap_ext.Type = MemExtendedParameterAddressRequirements;
+	mmap_ext.Reserved = 0;
 	mmap_ext.Pointer = (PVOID) &mmap_req;
 
 	real_stackaddr = VirtualAlloc2 (GetCurrentProcess(), NULL, real_size,

winsup/cygwin/mmap.cc
@@ -209,6 +209,7 @@ MapView (HANDLE h, void *addr, size_t len, DWORD openflags,
       /* g++ 11.2 workaround: don't use initializer */
       MEM_EXTENDED_PARAMETER mmap_ext;
       mmap_ext.Type = MemExtendedParameterAddressRequirements;
+      mmap_ext.Reserved = 0;
       mmap_ext.Pointer = (PVOID) &mmap_req;
 
       alloc_type |= attached (prot) ? MEM_RESERVE : 0;
@@ -1629,6 +1630,7 @@ fhandler_dev_zero::mmap (caddr_t *addr, size_t len, int prot,
 	  /* g++ 11.2 workaround: don't use initializer */
 	  MEM_EXTENDED_PARAMETER mmap_ext;
 	  mmap_ext.Type = MemExtendedParameterAddressRequirements;
+	  mmap_ext.Reserved = 0;
 	  mmap_ext.Pointer = (PVOID) &mmap_req;
 
 	  base = VirtualAlloc2 (GetCurrentProcess(), *addr, len, alloc_type,