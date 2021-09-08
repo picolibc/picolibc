picolibc/picolibc

commit 3d322ac930ca6d2516680fc78e3d3018cc737483

Cygwin: fix initializing MEM_EXTENDED_PARAMETER
MEM_EXTENDED_PARAMETER consists of a 64 bit bitfield which contains
the Type and the Reserved members.  The former usage of designated
initializer lists initialized Type, but not Reserved.  Since that's
not possible anymore due to a g++ 11.2 bug, Cygwin initializes the
MEM_EXTENDED_PARAMETER structs explicitely.  This results in a
random value in Reserved, which at least VirtualAlloc2 chokes on
(ERROR_INVALID_PARAMETER).

Set Reserved explicitely to 0 for a fix.

Fixes: bdb7991db38b ("Cygwin: workaround a g++ 11.2 initialization bug")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/miscfuncs.cc
@@ -519,6 +519,7 @@ class thread_allocator
     /* g++ 11.2 workaround: don't use initializer */
     MEM_EXTENDED_PARAMETER thread_ext;
     thread_ext.Type = MemExtendedParameterAddressRequirements;
+    thread_ext.Reserved = 0;
     thread_ext.Pointer = (PVOID) &thread_req;
 
     SIZE_T real_size = roundup2 (size, THREAD_STACK_SLOT);
@@ -540,6 +541,7 @@ class thread_allocator
 	/* g++ 11.2 workaround: don't use initializer */
 	MEM_EXTENDED_PARAMETER mmap_ext;
 	mmap_ext.Type = MemExtendedParameterAddressRequirements;
+	mmap_ext.Reserved = 0;
 	mmap_ext.Pointer = (PVOID) &mmap_req;
 
 	real_stackaddr = VirtualAlloc2 (GetCurrentProcess(), NULL, real_size,

winsup/cygwin/mmap.cc
@@ -209,6 +209,7 @@ MapView (HANDLE h, void *addr, size_t len, DWORD openflags,
       /* g++ 11.2 workaround: don't use initializer */
       MEM_EXTENDED_PARAMETER mmap_ext;
       mmap_ext.Type = MemExtendedParameterAddressRequirements;
+      mmap_ext.Reserved = 0;
       mmap_ext.Pointer = (PVOID) &mmap_req;
 
       alloc_type |= attached (prot) ? MEM_RESERVE : 0;
@@ -1629,6 +1630,7 @@ fhandler_dev_zero::mmap (caddr_t *addr, size_t len, int prot,
 	  /* g++ 11.2 workaround: don't use initializer */
 	  MEM_EXTENDED_PARAMETER mmap_ext;
 	  mmap_ext.Type = MemExtendedParameterAddressRequirements;
+	  mmap_ext.Reserved = 0;
 	  mmap_ext.Pointer = (PVOID) &mmap_req;
 
 	  base = VirtualAlloc2 (GetCurrentProcess(), *addr, len, alloc_type,

commit 8fba45f0ccc3b936edfb93671141bf3fe0b0ee5e

Cygwin: fix initializing MEM_EXTENDED_PARAMETER, take 2
In https://cygwin.com/pipermail/cygwin/2021-September/249361.html
Brian pointed out that initializing the structure would be more
future proof, should the developers at Microsoft ever decide to
split the Reserved field and use some bits of the struct for
other purposes.

Fixes: 3d322ac930ca ("Cygwin: fix initializing MEM_EXTENDED_PARAMETER")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/miscfuncs.cc
@@ -517,9 +517,8 @@ class thread_allocator
       THREAD_STACK_SLOT
     };
     /* g++ 11.2 workaround: don't use initializer */
-    MEM_EXTENDED_PARAMETER thread_ext;
+    MEM_EXTENDED_PARAMETER thread_ext = { 0 };
     thread_ext.Type = MemExtendedParameterAddressRequirements;
-    thread_ext.Reserved = 0;
     thread_ext.Pointer = (PVOID) &thread_req;
 
     SIZE_T real_size = roundup2 (size, THREAD_STACK_SLOT);
@@ -539,9 +538,8 @@ class thread_allocator
 	  THREAD_STACK_SLOT
 	};
 	/* g++ 11.2 workaround: don't use initializer */
-	MEM_EXTENDED_PARAMETER mmap_ext;
+	MEM_EXTENDED_PARAMETER mmap_ext = { 0 };
 	mmap_ext.Type = MemExtendedParameterAddressRequirements;
-	mmap_ext.Reserved = 0;
 	mmap_ext.Pointer = (PVOID) &mmap_req;
 
 	real_stackaddr = VirtualAlloc2 (GetCurrentProcess(), NULL, real_size,

winsup/cygwin/mmap.cc
@@ -207,9 +207,8 @@ MapView (HANDLE h, void *addr, size_t len, DWORD openflags,
 	0
       };
       /* g++ 11.2 workaround: don't use initializer */
-      MEM_EXTENDED_PARAMETER mmap_ext;
+      MEM_EXTENDED_PARAMETER mmap_ext = { 0 };
       mmap_ext.Type = MemExtendedParameterAddressRequirements;
-      mmap_ext.Reserved = 0;
       mmap_ext.Pointer = (PVOID) &mmap_req;
 
       alloc_type |= attached (prot) ? MEM_RESERVE : 0;
@@ -1628,9 +1627,8 @@ fhandler_dev_zero::mmap (caddr_t *addr, size_t len, int prot,
 	    0
 	  };
 	  /* g++ 11.2 workaround: don't use initializer */
-	  MEM_EXTENDED_PARAMETER mmap_ext;
+	  MEM_EXTENDED_PARAMETER mmap_ext = { 0 };
 	  mmap_ext.Type = MemExtendedParameterAddressRequirements;
-	  mmap_ext.Reserved = 0;
 	  mmap_ext.Pointer = (PVOID) &mmap_req;
 
 	  base = VirtualAlloc2 (GetCurrentProcess(), *addr, len, alloc_type,