26894de91d296af502b53c243e71f88c06ea1a3d
Christian Biesinger via Newlib <newlib@sourceware.org>
2021-10-27 12:41:22 -0400

    Unconditionally declare strsignal
    Currently, newlib does not declare strsignal if DEFS_H is defined,
    ostensibly to work around a gdb bug. However, gdb itself compiles
    even with this ifndef removed, and this makes sim (another part of
    gdb) fail to compile.
    
    Since it is not clear exactly what issue this was working around,
    this patch just replaces that ifdef with the correct check,
    i.e. __POSIX_VISIBLE >= 200809.

--- newlib/libc/include/string.h
+++ newlib/libc/include/string.h
@@ -130,7 +130,7 @@ char	*strlwr (char *);
 char	*strupr (char *);
 #endif
 
-#ifndef DEFS_H	/* Kludge to work around problem compiling in gdb */
+#if __POSIX_VISIBLE >= 200809
 char	*strsignal (int __signo);
 #endif
 
