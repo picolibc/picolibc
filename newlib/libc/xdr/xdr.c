c078c27985cdc8617096cd04f683f7c2e19f4b64
Keith Packard <keithp@keithp.com>
2021-11-03 21:56:46 -0700

    xdr: Silence compiler warning about uninitialized variable
    'size' will be set in the ENCODE path, but the compiler can't tell, so
    just initialize it always.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/xdr/xdr.c
+++ newlib/libc/xdr/xdr.c
@@ -767,7 +767,7 @@ xdr_string (XDR * xdrs,
         u_int maxsize)
 {
   char *sp = *cpp;              /* sp is the actual string pointer */
-  u_int size;
+  u_int size = 0;
   u_int nodesize;
 
   /*
