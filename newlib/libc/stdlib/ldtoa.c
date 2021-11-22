picolibc/picolibc

commit 4d90e5335914551862831de3e02f6c102b78435b

ldtoa: fix dropping too many digits from output
ldtoa cuts the number of digits it returns based on a computation of
number of supported bits (144) divide by log10(2).  Not only is the
integer approximation of log10(2) ~= 8/27 missing a digit here, it
also fails to take really small double and long double values into
account.

Allow for the full potential precision of long double values.  At the
same time, change the local string array allocation to request only as
much bytes as necessary to support the caller-requested number of
digits, to keep the stack size low on small targets.

In the long run a better fix would be to switch to gdtoa, as the BSD
variants, as well as Mingw64 do.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

newlib/libc/stdlib/ldtoa.c
@@ -34,9 +34,9 @@ void _IO_ldtostr (long double *, char *, int, int, char);
 #define NBITS ((NI-4)*16)
 
  /* Maximum number of decimal digits in ASCII conversion
-  * = NBITS*log10(2)
+  * Take full possible size of output into account
   */
-#define NDEC (NBITS*8/27)
+#define NDEC 1023
 
  /* The exponent of 1.0 */
 #define EXONE (0x3fff)
@@ -2794,7 +2794,6 @@ _ldtoa_r (struct _reent *ptr, long double d, int mode, int ndigits,
   LDPARMS rnd;
   LDPARMS *ldp = &rnd;
   char *outstr;
-  char outbuf[NDEC + MAX_EXP_DIGITS + 10];
   union uconv du;
   du.d = d;
 
@@ -2840,6 +2839,8 @@ _ldtoa_r (struct _reent *ptr, long double d, int mode, int ndigits,
    keep straight the returned value of outexpon.  */
   if (ndigits > NDEC)
     ndigits = NDEC;
+
+  char outbuf[ndigits + MAX_EXP_DIGITS + 10];
 
   etoasc (e, outbuf, ndigits, mode, ldp);
   s = outbuf;
@@ -3111,6 +3112,8 @@ tnzro:
       else
 	{
 	  emovi (y, w);
+	  /* Note that this loop does not access the incoming string array,
+	   * which may be shorter than NDEC + 1 bytes! */
 	  for (i = 0; i < NDEC + 1; i++)
 	    {
 	      if ((w[NI - 1] & 0x7) != 0)

commit 68faeef4be712df60443c6bb5764b1e9ddad20e2

ldtoa: don't restrict outbuf size to ndigits
https://cygwin.com/pipermail/cygwin/2021-November/249930.html
reported a regression introduce by using a dynamically sized local
char array in favor of a statically sized array.

Fix this by reverting to a statically sized array, using a small
buffer on the stack for a reasonable number of requested digits, a
big mallocated buffer otherwise.  This should work for small targets
as well, given that malloc is used in printf anyway right now.

This is *still* hopefully just a temporary measure, unless somebody
actually provides a new ldtoa.

Fixes: 4d90e53359145 ("ldtoa: fix dropping too many digits from output")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

newlib/libc/stdlib/ldtoa.c
@@ -33,10 +33,10 @@ void _IO_ldtostr (long double *, char *, int, int, char);
  /* Number of bits of precision */
 #define NBITS ((NI-4)*16)
 
- /* Maximum number of decimal digits in ASCII conversion
-  * Take full possible size of output into account
-  */
+ /* Maximum number of decimal digits in ASCII conversion */
 #define NDEC 1023
+ /* Use static stack buffer for up to 44 digits */
+#define NDEC_SML 44
 
  /* The exponent of 1.0 */
 #define EXONE (0x3fff)
@@ -2794,6 +2794,8 @@ _ldtoa_r (struct _reent *ptr, long double d, int mode, int ndigits,
   LDPARMS rnd;
   LDPARMS *ldp = &rnd;
   char *outstr;
+  char outbuf_sml[NDEC_SML + MAX_EXP_DIGITS + 10];
+  char *outbuf = outbuf_sml;
   union uconv du;
   du.d = d;
 
@@ -2836,11 +2838,22 @@ _ldtoa_r (struct _reent *ptr, long double d, int mode, int ndigits,
     ndigits = 20;
 
 /* This sanity limit must agree with the corresponding one in etoasc, to
-   keep straight the returned value of outexpon.  */
+   keep straight the returned value of outexpon.  Note that we use a dynamic
+   limit now, either NDEC or NDEC_SML, depending on ndigits.  See the usage
+   of "my_NDEC" in etoasc. */
   if (ndigits > NDEC)
     ndigits = NDEC;
 
-  char outbuf[ndigits + MAX_EXP_DIGITS + 10];
+  /* Allocate buffer if more than NDEC_SML digits are requested. */
+  if (ndigits > NDEC_SML)
+    {
+      outbuf = (char *) _malloc_r (ptr, NDEC + MAX_EXP_DIGITS + 10);
+      if (!outbuf)
+	{
+	  ndigits = NDEC_SML;
+	  outbuf = outbuf_sml;
+	}
+    }
 
   etoasc (e, outbuf, ndigits, mode, ldp);
   s = outbuf;
@@ -2933,6 +2946,9 @@ stripspaces:
   if (rve)
     *rve = outstr + (s - outbuf);
 
+  if (outbuf != outbuf_sml)
+    _free_r (ptr, outbuf);
+
   return outstr;
 }
 
@@ -2987,6 +3003,7 @@ etoasc (short unsigned int *x, char *string, int ndigits, int outformat,
   char *s, *ss;
   unsigned short m;
   unsigned short *equot = ldp->equot;
+  int my_NDEC = (ndigits > NDEC_SML) ? NDEC : NDEC_SML;
 
   ndigs = ndigits;
   rndsav = ldp->rndprc;
@@ -3112,9 +3129,7 @@ tnzro:
       else
 	{
 	  emovi (y, w);
-	  /* Note that this loop does not access the incoming string array,
-	   * which may be shorter than NDEC + 1 bytes! */
-	  for (i = 0; i < NDEC + 1; i++)
+	  for (i = 0; i < my_NDEC + 1; i++)
 	    {
 	      if ((w[NI - 1] & 0x7) != 0)
 		break;
@@ -3190,8 +3205,8 @@ isone:
 else if( ndigs < 0 )
         ndigs = 0;
 */
-  if (ndigs > NDEC)
-    ndigs = NDEC;
+  if (ndigs > my_NDEC)
+    ndigs = my_NDEC;
   if (digit == 10)
     {
       *s++ = '1';

winsup/cygwin/release/3.3.3
@@ -34,3 +34,6 @@ Bug Fixes
 
 - Fix two bugs in raise(2).
   Addresses: https://cygwin.com/pipermail/cygwin/2021-November/249973.html
+
+- Fix regression in printf introduced with Cygwin 3.3.2.
+  Addresses: https://cygwin.com/pipermail/cygwin/2021-November/249930.html