fd5878021f10ae75e235d5e2bd09340835e748f1
Takashi Yano <takashi.yano@nifty.ne.jp>
2021-12-14 11:58:26 +0900

    Cygwin: pty: Fix conditions for input transfer.
    - The recent commit "Cygwin: pty: Add missing input transfer when
      switch_to_pcon_in state." causes regression that rlwrap cannot
      work with cmd.exe. This patch fixes the issue.

--- winsup/cygwin/fhandler.h
+++ winsup/cygwin/fhandler.h
@@ -2280,6 +2280,7 @@ class fhandler_pty_common: public fhandler_termios
   static DWORD get_console_process_id (DWORD pid, bool match,
 				       bool cygwin = false,
 				       bool stub_only = false);
+  bool to_be_read_from_pcon (void);
 
  protected:
   static BOOL process_opost_output (HANDLE h, const void *ptr, ssize_t& len,
@@ -2446,7 +2447,6 @@ public:
     fh->copy_from (this);
     return fh;
   }
-  bool to_be_read_from_pcon (void);
   void get_master_thread_param (master_thread_param_t *p);
   void get_master_fwd_thread_param (master_fwd_thread_param_t *p);
   void set_mask_flusho (bool m) { get_ttyp ()->mask_flusho = m; }
