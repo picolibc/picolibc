picolibc/picolibc

commit a2bfe7cae61a8bac162bdb06f57d1dd15df3f4b9

Cygwin: ACLs: ignore *_INHERIT flags in file ACLs
get_posix_access() creates DEF_*_OBJ aclent_t entries from Windows ACEs
with INHERIT flags set, independent of the file type.  These flags only
make sense on directory objects, but certain Windows functions don't
check the file type and allow INHERIT ACE flags even on non-directories.

As a fix, make sure to ignore the INHERIT flags on non-directory ACLs
and don't propagate the matching DEF_*_OBJ aclent_t entries to callers.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/sec_acl.cc
@@ -811,7 +811,8 @@ get_posix_access (PSECURITY_DESCRIPTOR psd,
 			  class_perm = lacl[pos].a_perm;
 			}
 		    }
-		  if (ace->Header.AceFlags & SUB_CONTAINERS_AND_OBJECTS_INHERIT)
+		  if (S_ISDIR (attr)
+		      && (ace->Header.AceFlags & SUB_CONTAINERS_AND_OBJECTS_INHERIT) != 0)
 		    {
 		      if ((pos = searchace (lacl, MAX_ACL_ENTRIES,
 					    DEF_CLASS_OBJ)) >= 0)
@@ -952,7 +953,8 @@ get_posix_access (PSECURITY_DESCRIPTOR psd,
 		standard_ACEs_only = false;
 	    }
 	}
-      if ((ace->Header.AceFlags & SUB_CONTAINERS_AND_OBJECTS_INHERIT))
+      if (S_ISDIR (attr)
+	  && (ace->Header.AceFlags & SUB_CONTAINERS_AND_OBJECTS_INHERIT) != 0)
 	{
 	  if (type == USER_OBJ)
 	    {
@@ -1197,10 +1199,11 @@ int
 getacl (HANDLE handle, path_conv &pc, int nentries, aclent_t *aclbufp)
 {
   security_descriptor sd;
+  mode_t attr = pc.isdir () ? S_IFDIR : 0;
 
   if (get_file_sd (handle, pc, sd, false))
     return -1;
-  int pos = get_posix_access (sd, NULL, NULL, NULL, aclbufp, nentries);
+  int pos = get_posix_access (sd, &attr, NULL, NULL, aclbufp, nentries);
   syscall_printf ("%R = getacl(%S)", pos, pc.get_nt_native_path ());
   return pos;
 }

winsup/cygwin/release/3.3.4
@@ -17,3 +17,6 @@ Bug Fixes
 
 - Avoid a crash when NtQueryInformationProcess returns invalid handle data.
   Addresses: https://cygwin.com/pipermail/cygwin-patches/2021q4/011611.html
+
+- Ignore INHERIT ACEs when reading the DACL of non-directory files.
+  Addresses: https://cygwin.com/pipermail/cygwin/2022-January/250363.html

commit f467a1da58e86bed7730c2d20fb4c2c0444750bb

Cygwin: ACLs: don't set indicator for default ACEs prematurely
Commit a2bfe7cae61a introduced a change ignoring default ACEs on files
to come up with a valid POSIX ACL, reflecting the Windows ACEs actually
making sense on files.

However, if CREATOR OWNER or CREATOR GROUP ACEs - both only making sense
as default ACEs - are found in the ACL, a value indicating the presence
of default ACEs gets set, even on files.  This in turn breaks a
subsequent integrity check and get_posix_access returns EINVAL.

The code path handling default ACEs on directories sets this indicator
anyway, so don't set it just because one of the above SIDs are found.

Fixes: a2bfe7cae61a ("Cygwin: ACLs: ignore *_INHERIT flags in file ACLs")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/sec_acl.cc
@@ -850,14 +850,12 @@ get_posix_access (PSECURITY_DESCRIPTOR psd,
       else if (ace_sid == well_known_creator_owner_sid)
 	{
 	  type = DEF_USER_OBJ;
-	  types_def |= type;
 	  id = ACL_UNDEFINED_ID;
 	  saw_def_user_obj = true;
 	}
       else if (ace_sid == well_known_creator_group_sid)
 	{
 	  type = DEF_GROUP_OBJ;
-	  types_def |= type;
 	  id = ACL_UNDEFINED_ID;
 	  saw_def_group_obj = true;
 	}