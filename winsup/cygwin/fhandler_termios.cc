c05c36a7c8af5e97fa379abb7571ed35a0088e98
Takashi Yano <takashi.yano@nifty.ne.jp>
2022-02-10 16:53:47 +0900

    Cygwin: pty, console: Fix Ctrl-C handling for non-cygwin apps.
    - Currently, if cat is started from cmd.exe which is started in cygwin
      console, Ctrl-C terminates not only cat but also cmd.exe. This also
      happens in pty in which pseudo console is disabled. This patch fixes
      the issue.

--- winsup/cygwin/fhandler_termios.cc
+++ winsup/cygwin/fhandler_termios.cc
@@ -147,6 +147,8 @@ tty_min::kill_pgrp (int sig)
       _pinfo *p = pids[i];
       if (!p || !p->exists () || p->ctty != ntty || p->pgid != pgid)
 	continue;
+      if (p->process_state & PID_NOTCYGWIN)
+	continue;
       if (p == myself)
 	killself = sig != __SIGSETPGRP && !exit_state;
       else
@@ -326,6 +328,42 @@ fhandler_termios::line_edit (const char *rptr, size_t nread, termios& ti,
 
       if (ti.c_iflag & ISTRIP)
 	c &= 0x7f;
+      winpids pids ((DWORD) 0);
+      if (get_ttyp ()->pcon_input_state_eq (tty::to_nat))
+	{
+	  bool need_discard_input = false;
+	  for (unsigned i = 0; i < pids.npids; i++)
+	    {
+	      _pinfo *p = pids[i];
+	      if (c == '\003' && p && p->ctty == tc ()->ntty
+		  && p->pgid == tc ()->getpgid ()
+		  && (p->process_state & PID_NOTCYGWIN))
+		{
+		  /* CTRL_C_EVENT does not work for the process started with
+		     CREATE_NEW_PROCESS_GROUP flag, so send CTRL_BREAK_EVENT
+		     instead. */
+		  if (p->process_state & PID_NEW_PG)
+		    GenerateConsoleCtrlEvent (CTRL_BREAK_EVENT,
+					      p->dwProcessId);
+		  else
+		    GenerateConsoleCtrlEvent (CTRL_C_EVENT, 0);
+		  need_discard_input = true;
+		}
+	    }
+	  if (need_discard_input
+	      && !CCEQ (ti.c_cc[VINTR], c)
+	      && !CCEQ (ti.c_cc[VQUIT], c)
+	      && !CCEQ (ti.c_cc[VSUSP], c))
+	    {
+	      if (!(ti.c_lflag & NOFLSH))
+		{
+		  eat_readahead (-1);
+		  discard_input ();
+		}
+	      ti.c_lflag &= ~FLUSHO;
+	      continue;
+	    }
+	}
       if (ti.c_lflag & ISIG)
 	{
 	  int sig;
