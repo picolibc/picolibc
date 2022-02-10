5c21b3721475c0d848b567837b4459cffcf0ca13
Mike Frysinger <vapier@gentoo.org>
2022-02-09 23:26:19 -0500

    newlib: fix mkdoc dependencies
    Make sure we depend on the right name of mkdoc all the time, and that
    the rules that need it (e.g. .def files) depend on it.
    
    Reported-by: Jon Turney <jon.turney@dronecode.org.uk>

--- newlib/doc/local.mk
+++ newlib/doc/local.mk
@@ -1,7 +1,7 @@
-# We can't use noinst_PROGRAMS, because automake will add $(EXEEXT).
-noinst_DATA += doc/makedoc
-
 MKDOC = doc/makedoc$(EXEEXT_FOR_BUILD)
+
+# We can't use noinst_PROGRAMS, because automake will add $(EXEEXT).
+noinst_DATA += $(MKDOC)
 
 # We don't use CFLAGS with CC_FOR_BUILD because here CFLAGS will
 # actually be CFLAGS_FOR_TARGET, and in some cases that will include
