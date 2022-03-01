8c383e9ae7d34124e359299c6dc2e917a7a19527
Mike Frysinger <vapier@gentoo.org>
2022-03-01 01:02:40 -0500

    newlib: xstormy16: break up mallocr stubs
    Move the multiplex logic out of the build and into source files to
    make the build rules a lot simpler.

--- newlib/libc/machine/xstormy16/mallocr.c
+++ newlib/libc/machine/xstormy16/mallocr.c
@@ -1,33 +1,7 @@
 #include <malloc.h>
 
-#ifdef DEFINE_MALLOC
 void *
 _malloc_r (struct _reent *r, size_t sz)
 {
   return malloc (sz);
 }
-#endif
-
-#ifdef DEFINE_CALLOC
-void *
-_calloc_r (struct _reent *r, size_t a, size_t b)
-{
-  return calloc (a, b);
-}
-#endif
-
-#ifdef DEFINE_FREE
-void
-_free_r (struct _reent *r, void *x)
-{
-  free (x);
-}
-#endif
-
-#ifdef DEFINE_REALLOC
-void *
-_realloc_r (struct _reent *r, void *x, size_t sz)
-{
-  return realloc (x, sz);
-}
-#endif
