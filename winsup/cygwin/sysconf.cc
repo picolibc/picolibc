picolibc/picolibc

commit 59e3b6ca7dc10603d1e692928a236fe5a0523200

* cygmalloc.h (MALLOC_FAILURE_ACTION): Define empty. 	* cygwin.din (posix_madvise): Export. 	(posix_memalign): Export. 	* fhandler.cc (fhandler_base::fpathconf): Return useful values in 	_PC_VDISABLE, _PC_SYNC_IO and _PC_SYMLINK_MAX cases. 	* malloc_wrapper.cc (malloc): Set errno here since it's not set in 	dlmalloc.c anymore. 	(realloc): Ditto. 	(calloc): Ditto. 	(memalign): Ditto. 	(valloc): Ditto. 	(posix_memalign): New function. 	* mmap.cc (posix_madvise): New function. 	* sysconf.cc (get_open_max): New function. 	(get_page_size): Ditto. 	(get_nproc_values): Ditto. 	(get_avphys): Ditto. 	(sc_type): New type. 	(sca): New array to map _SC_xxx options to sysconf return values. 	(sysconf): Reimplement using sca array. 	* include/limits.h: Add all missing values as defined by SUSv3. 	* include/pthread.h (PTHREAD_DESTRUCTOR_ITERATIONS): Move definition 	to sys/limits.h. 	(PTHREAD_KEYS_MAX): Ditto. 	* include/semaphore.h (SEM_VALUE_MAX): Ditto. 	* include/cygwin/stdlib.h (posix_memalign): Declare. 	* include/cygwin/version.h: Bump API minor number. 	* include/sys/mman.h: Add posix_madvise flags. 	(posix_madvise): Declare. 	* include/sys/termios.h (_POSIX_VDISABLE): Move definition to 	sys/limits.h.

winsup/cygwin/sysconf.cc
@@ -1,7 +1,7 @@
 /* sysconf.cc
 
    Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
-   2006 Red Hat, Inc.
+   2006, 2007 Red Hat, Inc.
 
 This file is part of Cygwin.
 
@@ -22,131 +22,248 @@ details. */
 #include "cygheap.h"
 #include "ntdll.h"
 
+static long
+get_open_max (int in)
+{
+  long max = getdtablesize ();
+  if (max < OPEN_MAX)
+    max = OPEN_MAX;
+  return max;
+}
+
+static long
+get_page_size (int in)
+{
+  return getpagesize ();
+}
+
+static long
+get_nproc_values (int in)
+{
+  switch (in)
+    {
+    case _SC_NPROCESSORS_CONF:
+    case _SC_NPROCESSORS_ONLN:
+      if (!wincap.supports_smp ())
+	return 1;
+      /*FALLTHRU*/
+    case _SC_PHYS_PAGES:
+      if (wincap.supports_smp ())
+	{
+	  NTSTATUS ret;
+	  SYSTEM_BASIC_INFORMATION sbi;
+	  if ((ret = NtQuerySystemInformation (SystemBasicInformation,
+						 (PVOID) &sbi,
+					       sizeof sbi, NULL))
+		!= STATUS_SUCCESS)
+	    {
+	      __seterrno_from_nt_status (ret);
+	      debug_printf ("NtQuerySystemInformation: ret %d, Dos(ret) %E",
+			    ret);
+	      return -1;
+	    }
+	  switch (in)
+	    {
+	    case _SC_NPROCESSORS_CONF:
+	     return sbi.NumberProcessors;
+	    case _SC_NPROCESSORS_ONLN:
+	     {
+	       int i = 0;
+	       do
+		 if (sbi.ActiveProcessors & 1)
+		   i++;
+	       while (sbi.ActiveProcessors >>= 1);
+	       return i;
+	     }
+	    case _SC_PHYS_PAGES:
+	      return sbi.NumberOfPhysicalPages
+		     / (getpagesize () / getsystempagesize ());
+	    }
+	}
+    }
+  return -1;
+}
+
+static long
+get_avphys (int in)
+{
+  if (wincap.supports_smp ())
+    {
+      NTSTATUS ret;
+      SYSTEM_PERFORMANCE_INFORMATION spi;
+      if ((ret = NtQuerySystemInformation (SystemPerformanceInformation,
+					     (PVOID) &spi,
+					   sizeof spi, NULL))
+	    != STATUS_SUCCESS)
+	{
+	  __seterrno_from_nt_status (ret);
+	  debug_printf ("NtQuerySystemInformation: ret %d, Dos(ret) %E",
+			ret);
+	  return -1;
+	}
+      return spi.AvailablePages / (getpagesize () / getsystempagesize ());
+    }
+  return -1;
+}
+
+enum sc_type { nsup, cons, func };
+
+static struct
+{
+  sc_type type;
+  union
+    {
+      long c;
+      long (*f)(int);
+    };
+} sca[] =
+{
+  {cons, {c:ARG_MAX}},			/*   0, _SC_ARG_MAX */
+  {cons, {c:CHILD_MAX}},		/*   1, _SC_CHILD_MAX */
+  {cons, {c:CLOCKS_PER_SEC}},		/*   2, _SC_CLK_TCK */
+  {cons, {c:NGROUPS_MAX}},		/*   3, _SC_NGROUPS_MAX */
+  {func, {f:get_open_max}},		/*   4, _SC_OPEN_MAX */
+  {cons, {c:_POSIX_JOB_CONTROL}},	/*   5, _SC_JOB_CONTROL */
+  {cons, {c:_POSIX_SAVED_IDS}},		/*   6, _SC_SAVED_IDS */
+  {cons, {c:_POSIX_VERSION}},		/*   7, _SC_VERSION */
+  {func, {f:get_page_size}},		/*   8, _SC_PAGESIZE */
+  {func, {f:get_nproc_values}},		/*   9, _SC_NPROCESSORS_CONF */
+  {func, {f:get_nproc_values}},		/*  10, _SC_NPROCESSORS_ONLN */
+  {func, {f:get_nproc_values}},		/*  11, _SC_PHYS_PAGES */
+  {func, {f:get_avphys}},		/*  12, _SC_AVPHYS_PAGES */
+  {nsup, {c:0}},			/*  13, _SC_MQ_OPEN_MAX */
+  {nsup, {c:0}},			/*  14, _SC_MQ_PRIO_MAX */
+  {cons, {c:RTSIG_MAX}},		/*  15, _SC_RTSIG_MAX */
+  {cons, {c:-1L}},			/*  16, _SC_SEM_NSEMS_MAX */
+  {cons, {c:SEM_VALUE_MAX}},		/*  17, _SC_SEM_VALUE_MAX */
+  {cons, {c:SIGQUEUE_MAX}},		/*  18, _SC_SIGQUEUE_MAX */
+  {cons, {c:TIMER_MAX}},		/*  19, _SC_TIMER_MAX */
+  {nsup, {c:0}},			/*  20, _SC_TZNAME_MAX */
+  {cons, {c:-1L}},			/*  21, _SC_ASYNCHRONOUS_IO */
+  {cons, {c:_POSIX_FSYNC}},		/*  22, _SC_FSYNC */
+  {cons, {c:_POSIX_MAPPED_FILES}},	/*  23, _SC_MAPPED_FILES */
+  {cons, {c:-1L}},			/*  24, _SC_MEMLOCK */
+  {cons, {c:_POSIX_MEMLOCK_RANGE}},	/*  25, _SC_MEMLOCK_RANGE */
+  {cons, {c:_POSIX_MEMORY_PROTECTION}},	/*  26, _SC_MEMORY_PROTECTION */
+  {cons, {c:-1L}},			/*  27, _SC_MESSAGE_PASSING */
+  {cons, {c:-1L}},			/*  28, _SC_PRIORITIZED_IO */
+  {cons, {c:_POSIX_REALTIME_SIGNALS}},	/*  29, _SC_REALTIME_SIGNALS */
+  {cons, {c:_POSIX_SEMAPHORES}},	/*  30, _SC_SEMAPHORES */
+  {cons, {c:-1L}},			/*  31, _SC_SHARED_MEMORY_OBJECTS */
+  {cons, {c:_POSIX_SYNCHRONIZED_IO}},	/*  32, _SC_SYNCHRONIZED_IO */
+  {cons, {c:_POSIX_TIMERS}},		/*  33, _SC_TIMERS */
+  {nsup, {c:0}},			/*  34, _SC_AIO_LISTIO_MAX */
+  {nsup, {c:0}},			/*  35, _SC_AIO_MAX */
+  {nsup, {c:0}},			/*  36, _SC_AIO_PRIO_DELTA_MAX */
+  {nsup, {c:0}},			/*  37, _SC_DELAYTIMER_MAX */
+  {cons, {c:PTHREAD_KEYS_MAX}},		/*  38, _SC_THREAD_KEYS_MAX */
+  {cons, {c:PTHREAD_STACK_MIN}},	/*  39, _SC_THREAD_STACK_MIN */
+  {cons, {c:-1L}},			/*  40, _SC_THREAD_THREADS_MAX */
+  {cons, {c:TTY_NAME_MAX}},		/*  41, _SC_TTY_NAME_MAX */
+  {cons, {c:_POSIX_THREADS}},		/*  42, _SC_THREADS */
+  {cons, {c:-1L}},			/*  43, _SC_THREAD_ATTR_STACKADDR */
+  {cons, {c:_POSIX_THREAD_ATTR_STACKSIZE}},	/*  44, _SC_THREAD_ATTR_STACKSIZE */
+  {cons, {c:_POSIX_THREAD_PRIORITY_SCHEDULING}},	/*  45, _SC_THREAD_PRIORITY_SCHEDULING */
+  {cons, {c:-1L}},			/*  46, _SC_THREAD_PRIO_INHERIT */
+  {cons, {c:-1L}},			/*  47, _SC_THREAD_PRIO_PROTECT */
+  {cons, {c:_POSIX_THREAD_PROCESS_SHARED}},	/*  48, _SC_THREAD_PROCESS_SHARED */
+  {cons, {c:_POSIX_THREAD_SAFE_FUNCTIONS}},	/*  49, _SC_THREAD_SAFE_FUNCTIONS */
+  {cons, {c:16384L}},			/*  50, _SC_GETGR_R_SIZE_MAX */
+  {cons, {c:16384L}},			/*  51, _SC_GETPW_R_SIZE_MAX */
+  {cons, {c:LOGIN_NAME_MAX}},		/*  52, _SC_LOGIN_NAME_MAX */
+  {cons, {c:PTHREAD_DESTRUCTOR_ITERATIONS}},	/*  53, _SC_THREAD_DESTRUCTOR_ITERATIONS */
+  {cons, {c:_POSIX_ADVISORY_INFO}},	/*  54, _SC_ADVISORY_INFO */
+  {cons, {c:ATEXIT_MAX}},		/*  55, _SC_ATEXIT_MAX */
+  {cons, {c:-1L}},			/*  56, _SC_BARRIERS */
+  {cons, {c:BC_BASE_MAX}},		/*  57, _SC_BC_BASE_MAX */
+  {cons, {c:BC_DIM_MAX}},		/*  58, _SC_BC_DIM_MAX */
+  {cons, {c:BC_SCALE_MAX}},		/*  59, _SC_BC_SCALE_MAX */
+  {cons, {c:BC_STRING_MAX}},		/*  60, _SC_BC_STRING_MAX */
+  {cons, {c:-1L}},			/*  61, _SC_CLOCK_SELECTION */
+  {nsup, {c:0}},			/*  62, _SC_COLL_WEIGHTS_MAX */
+  {cons, {c:-1L}},			/*  63, _SC_CPUTIME */
+  {cons, {c:EXPR_NEST_MAX}},		/*  64, _SC_EXPR_NEST_MAX */
+  {cons, {c:HOST_NAME_MAX}},		/*  65, _SC_HOST_NAME_MAX */
+  {cons, {c:IOV_MAX}},			/*  66, _SC_IOV_MAX */
+  {cons, {c:_POSIX_IPV6}},		/*  67, _SC_IPV6 */
+  {cons, {c:LINE_MAX}},			/*  68, _SC_LINE_MAX */
+  {cons, {c:-1L}},			/*  69, _SC_MONOTONIC_CLOCK */
+  {cons, {c:_POSIX_RAW_SOCKETS}},	/*  70, _SC_RAW_SOCKETS */
+  {cons, {c:_POSIX_READER_WRITER_LOCKS}},	/*  71, _SC_READER_WRITER_LOCKS */
+  {cons, {c:_POSIX_REGEXP}},		/*  72, _SC_REGEXP */
+  {cons, {c:RE_DUP_MAX}},		/*  73, _SC_RE_DUP_MAX */
+  {cons, {c:_POSIX_SHELL}},		/*  74, _SC_SHELL */
+  {cons, {c:-1L}},			/*  75, _SC_SPAWN */
+  {cons, {c:-1L}},			/*  76, _SC_SPIN_LOCKS */
+  {cons, {c:-1L}},			/*  77, _SC_SPORADIC_SERVER */
+  {nsup, {c:0}},			/*  78, _SC_SS_REPL_MAX */
+  {cons, {c:SYMLOOP_MAX}},		/*  79, _SC_SYMLOOP_MAX */
+  {cons, {c:-1L}},			/*  80, _SC_THREAD_CPUTIME */
+  {cons, {c:-1L}},			/*  81, _SC_THREAD_SPORADIC_SERVER */
+  {cons, {c:-1L}},			/*  82, _SC_TIMEOUTS */
+  {cons, {c:-1L}},			/*  83, _SC_TRACE */
+  {cons, {c:-1L}},			/*  84, _SC_TRACE_EVENT_FILTER */
+  {nsup, {c:0}},			/*  85, _SC_TRACE_EVENT_NAME_MAX */
+  {cons, {c:-1L}},			/*  86, _SC_TRACE_INHERIT */
+  {cons, {c:-1L}},			/*  87, _SC_TRACE_LOG */
+  {nsup, {c:0}},			/*  88, _SC_TRACE_NAME_MAX */
+  {nsup, {c:0}},			/*  89, _SC_TRACE_SYS_MAX */
+  {nsup, {c:0}},			/*  90, _SC_TRACE_USER_EVENT_MAX */
+  {cons, {c:-1L}},			/*  91, _SC_TYPED_MEMORY_OBJECTS */
+  {cons, {c:-1L}},			/*  92, _SC_V6_ILP32_OFF32 */
+  {cons, {c:_POSIX_V6_ILP32_OFFBIG}},	/*  93, _SC_V6_ILP32_OFFBIG */
+  {cons, {c:-1L}},			/*  94, _SC_V6_LP64_OFF64 */
+  {cons, {c:-1L}},			/*  95, _SC_V6_LPBIG_OFFBIG */
+  {cons, {c:_XOPEN_CRYPT}},		/*  96, _SC_XOPEN_CRYPT */
+  {cons, {c:_XOPEN_ENH_I18N}},		/*  97, _SC_XOPEN_ENH_I18N */
+  {cons, {c:-1L}},			/*  98, _SC_XOPEN_LEGACY */
+  {cons, {c:-1L}},			/*  99, _SC_XOPEN_REALTIME */
+  {cons, {c:STREAM_MAX}},		/* 100, _SC_STREAM_MAX */
+  {cons, {c:_POSIX_PRIORITY_SCHEDULING}},	/* 101, _SC_PRIORITY_SCHEDULING */
+  {cons, {c:-1L}},			/* 102, _SC_XOPEN_REALTIME_THREADS */
+  {cons, {c:_XOPEN_SHM}},		/* 103, _SC_XOPEN_SHM */
+  {cons, {c:-1L}},			/* 104, _SC_XOPEN_STREAMS */
+  {cons, {c:-1L}},			/* 105, _SC_XOPEN_UNIX */
+  {cons, {c:_XOPEN_VERSION}},		/* 106, _SC_XOPEN_VERSION */
+  {cons, {c:_POSIX2_CHAR_TERM}},	/* 107, _SC_2_CHAR_TERM */
+  {cons, {c:_POSIX2_C_BIND}},		/* 108, _SC_2_C_BIND */
+  {cons, {c:_POSIX2_C_BIND}},		/* 109, _SC_2_C_DEV */
+  {cons, {c:-1L}},			/* 110, _SC_2_FORT_DEV */
+  {cons, {c:-1L}},			/* 111, _SC_2_FORT_RUN */
+  {cons, {c:-1L}},			/* 112, _SC_2_LOCALEDEF */
+  {cons, {c:-1L}},			/* 113, _SC_2_PBS */
+  {cons, {c:-1L}},			/* 114, _SC_2_PBS_ACCOUNTING */
+  {cons, {c:-1L}},			/* 115, _SC_2_PBS_CHECKPOINT */
+  {cons, {c:-1L}},			/* 116, _SC_2_PBS_LOCATE */
+  {cons, {c:-1L}},			/* 117, _SC_2_PBS_MESSAGE */
+  {cons, {c:-1L}},			/* 118, _SC_2_PBS_TRACK */
+  {cons, {c:_POSIX2_SW_DEV}},		/* 119, _SC_2_SW_DEV */
+  {cons, {c:_POSIX2_UPE}},		/* 120, _SC_2_UPE */
+  {cons, {c:_POSIX2_VERSION}},		/* 121, _SC_2_VERSION */
+};
+
+#define SC_MIN _SC_ARG_MAX
+#define SC_MAX _SC_2_VERSION
+
 /* sysconf: POSIX 4.8.1.1 */
 /* Allows a portable app to determine quantities of resources or
    presence of an option at execution time. */
 long int
 sysconf (int in)
 {
-  switch (in)
+  if (in >= SC_MIN && in <= SC_MAX)
     {
-      /* Keep order as in sys/unistd.h */
-      case _SC_ARG_MAX:
-	/* FIXME: what's the right value?  _POSIX_ARG_MAX is only 4K.
-	   FIXME: Wouldn't it be more correct to return ARG_MAX here? */
-	return 1048576;
-      case _SC_CHILD_MAX:
-	return CHILD_MAX;
-      case _SC_CLK_TCK:
-	return CLOCKS_PER_SEC;
-      case _SC_NGROUPS_MAX:
-	return NGROUPS_MAX;
-      case _SC_OPEN_MAX:
+      switch (sca[in].type)
 	{
-	  long max = getdtablesize ();
-	  if (max < OPEN_MAX)
-	    max = OPEN_MAX;
-	  return max;
+	case nsup:
+	  break;
+	case cons:
+	  return sca[in].c;
+	case func:
+	  return sca[in].f (in);
 	}
-      case _SC_JOB_CONTROL:
-	return _POSIX_JOB_CONTROL;
-      case _SC_SAVED_IDS:
-	return _POSIX_SAVED_IDS;
-      case _SC_VERSION:
-	return _POSIX_VERSION;
-      case _SC_PAGESIZE:
-	return getpagesize ();
-      case _SC_NPROCESSORS_CONF:
-      case _SC_NPROCESSORS_ONLN:
-	if (!wincap.supports_smp ())
-	  return 1;
-	/*FALLTHRU*/
-      case _SC_PHYS_PAGES:
-	if (wincap.supports_smp ())
-	  {
-	    NTSTATUS ret;
-	    SYSTEM_BASIC_INFORMATION sbi;
-	    if ((ret = NtQuerySystemInformation (SystemBasicInformation,
-						   (PVOID) &sbi,
-						 sizeof sbi, NULL))
-		  != STATUS_SUCCESS)
-	      {
-		__seterrno_from_nt_status (ret);
-		debug_printf ("NtQuerySystemInformation: ret %d, Dos(ret) %E",
-			      ret);
-		return -1;
-	      }
-	    switch (in)
-	      {
-	      case _SC_NPROCESSORS_CONF:
-	       return sbi.NumberProcessors;
-	      case _SC_NPROCESSORS_ONLN:
-	       {
-		 int i = 0;
-		 do
-		   if (sbi.ActiveProcessors & 1)
-		     i++;
-		 while (sbi.ActiveProcessors >>= 1);
-		 return i;
-	       }
-	      case _SC_PHYS_PAGES:
-		return sbi.NumberOfPhysicalPages;
-	      }
-	  }
-	break;
-      case _SC_AVPHYS_PAGES:
-	if (wincap.supports_smp ())
-	  {
-	    NTSTATUS ret;
-	    SYSTEM_PERFORMANCE_INFORMATION spi;
-	    if ((ret = NtQuerySystemInformation (SystemPerformanceInformation,
-						   (PVOID) &spi,
-						 sizeof spi, NULL))
-		  != STATUS_SUCCESS)
-	      {
-		__seterrno_from_nt_status (ret);
-		debug_printf ("NtQuerySystemInformation: ret %d, Dos(ret) %E",
-			      ret);
-		return -1;
-	      }
-	    return spi.AvailablePages;
-	  }
-      case _SC_RTSIG_MAX:
-	return RTSIG_MAX;
-      case _SC_TIMER_MAX:
-	return TIMER_MAX;
-#if 0	/* FIXME -- unimplemented */
-      case _SC_TZNAME_MAX:
-        return _POSIX_TZNAME_MAX;
-#endif
-      case _SC_MEMLOCK_RANGE:
-	return _POSIX_MEMLOCK_RANGE;
-      case _SC_SEMAPHORES:
-        return _POSIX_SEMAPHORES;
-      case _SC_TIMERS:
-	return _POSIX_TIMERS;
-      case _SC_TTY_NAME_MAX:
-	return TTY_NAME_MAX;
-      case _SC_THREADS:
-	return _POSIX_THREADS;
-      case _SC_THREAD_ATTR_STACKSIZE:
-	return _POSIX_THREAD_ATTR_STACKSIZE;
-      case _SC_THREAD_PRIORITY_SCHEDULING:
-	return _POSIX_THREAD_PRIORITY_SCHEDULING;
-      case _SC_THREAD_PROCESS_SHARED:
-	return _POSIX_THREAD_PROCESS_SHARED;
-      case _SC_THREAD_SAFE_FUNCTIONS:
-	return _POSIX_THREAD_SAFE_FUNCTIONS;
-      case _SC_GETPW_R_SIZE_MAX:
-      case _SC_GETGR_R_SIZE_MAX:
-	return 16*1024;
-      case _SC_LOGIN_NAME_MAX:
-	return LOGIN_NAME_MAX;
-      case _SC_STREAM_MAX:
-	return STREAM_MAX;
     }
-
-  /* Invalid input or unimplemented sysconf name */
+  /* Unimplemented sysconf name or invalid option value. */
   set_errno (EINVAL);
-  return -1;
+  return -1L;
 }

winsup/cygwin/cygmalloc.h
@@ -1,6 +1,6 @@
 /* cygmalloc.h: cygwin DLL malloc stuff
 
-   Copyright 2002, 2003, 2004, 2005 Red Hat, Inc.
+   Copyright 2002, 2003, 2004, 2005, 2007 Red Hat, Inc.
 
 This file is part of Cygwin.
 
@@ -25,6 +25,7 @@ void dlmalloc_stats ();
 #ifndef __INSIDE_CYGWIN__
 # define USE_DL_PREFIX 1
 #else
+#define MALLOC_FAILURE_ACTION
 # define __malloc_lock() mallock.acquire ()
 # define __malloc_unlock() mallock.release ()
 extern muto mallock;

winsup/cygwin/cygwin.din
@@ -988,6 +988,8 @@ popen SIGFE
 _popen = popen SIGFE
 posix_fadvise SIGFE
 posix_fallocate SIGFE
+posix_madvise SIGFE
+posix_memalign SIGFE
 posix_openpt SIGFE
 posix_regcomp SIGFE
 posix_regerror SIGFE

winsup/cygwin/fhandler.cc
@@ -1852,19 +1852,21 @@ fhandler_base::fpathconf (int v)
     case _PC_NO_TRUNC:
       return 1;
     case _PC_VDISABLE:
-      if (!is_tty ())
-        set_errno (EINVAL);
+      if (is_tty ())
+        return _POSIX_VDISABLE;
+      set_errno (EINVAL);
       break;
     case _PC_ASYNC_IO:
     case _PC_PRIO_IO:
-    case _PC_SYNC_IO:
       break;
+    case _PC_SYNC_IO:
+      return 1;
     case _PC_FILESIZEBITS:
       return FILESIZEBITS;
     case _PC_2_SYMLINKS:
       return 1;
     case _PC_SYMLINK_MAX:
-      break;
+      return SYMLINK_MAX;
     case _PC_POSIX_PERMISSIONS:
     case _PC_POSIX_SECURITY:
       if (get_device () == FH_FS)

winsup/cygwin/include/cygwin/stdlib.h
@@ -1,6 +1,6 @@
 /* stdlib.h
 
-   Copyright 2005, 2006 Red Hat Inc.
+   Copyright 2005, 2006, 2007 Red Hat Inc.
 
 This file is part of Cygwin.
 
@@ -32,6 +32,7 @@ int unlockpt (int);
 #endif /*__STRICT_ANSI__*/
 
 int posix_openpt (int);
+int posix_memalign (void **, size_t, size_t);
 
 #ifdef _COMPILING_NEWLIB
 #define unsetenv UNUSED_unsetenv

winsup/cygwin/include/cygwin/version.h
@@ -1,7 +1,7 @@
 /* version.h -- Cygwin version numbers and accompanying documentation.
 
    Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
-   2005, 2006 Red Hat, Inc.
+   2005, 2006, 2007 Red Hat, Inc.
 
 This file is part of Cygwin.
 
@@ -301,12 +301,13 @@ details. */
       161: Export resolver functions.
       162: New struct ifreq.  Export if_nametoindex, if_indextoname,
 	   if_nameindex, if_freenameindex.
+      163: Export posix_madvise, posix_memalign.
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 162
+#define CYGWIN_VERSION_API_MINOR 163
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible

commit fcec4830abf0aa4821ba1a5dd1d9fb35d671cb85

Cygwin: sysconf: don't set errno for unsupported options
We return -1 with errno set to EINVAL for sysconf options for
values required by POSIX, but not implemented on Cygwin.
This is incorrect.  Return -1, but don't set errno for these options.
Drop the "nsup" enum to indicate unsupported values, it's not
required anymore.

Fixes: 59e3b6ca7dc10 (CVS import)
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/sysconf.cc
@@ -473,7 +473,7 @@ get_cpu_cache (int in)
   return 0;
 }
 
-enum sc_type { nsup, cons, func };
+enum sc_type { cons, func };
 
 static struct
 {
@@ -547,7 +547,7 @@ static struct
   {cons, {c:BC_SCALE_MAX}},		/*  59, _SC_BC_SCALE_MAX */
   {cons, {c:BC_STRING_MAX}},		/*  60, _SC_BC_STRING_MAX */
   {cons, {c:_POSIX_CLOCK_SELECTION}},	/*  61, _SC_CLOCK_SELECTION */
-  {nsup, {c:0}},			/*  62, _SC_COLL_WEIGHTS_MAX */
+  {cons, {c:-1L}},			/*  62, _SC_COLL_WEIGHTS_MAX */
   {cons, {c:_POSIX_CPUTIME}},		/*  63, _SC_CPUTIME */
   {cons, {c:EXPR_NEST_MAX}},		/*  64, _SC_EXPR_NEST_MAX */
   {cons, {c:HOST_NAME_MAX}},		/*  65, _SC_HOST_NAME_MAX */
@@ -563,19 +563,19 @@ static struct
   {cons, {c:_POSIX_SPAWN}},		/*  75, _SC_SPAWN */
   {cons, {c:_POSIX_SPIN_LOCKS}},	/*  76, _SC_SPIN_LOCKS */
   {cons, {c:-1L}},			/*  77, _SC_SPORADIC_SERVER */
-  {nsup, {c:0}},			/*  78, _SC_SS_REPL_MAX */
+  {cons, {c:-1L}},			/*  78, _SC_SS_REPL_MAX */
   {cons, {c:SYMLOOP_MAX}},		/*  79, _SC_SYMLOOP_MAX */
   {cons, {c:_POSIX_THREAD_CPUTIME}},	/*  80, _SC_THREAD_CPUTIME */
   {cons, {c:-1L}},			/*  81, _SC_THREAD_SPORADIC_SERVER */
   {cons, {c:_POSIX_TIMEOUTS}},		/*  82, _SC_TIMEOUTS */
   {cons, {c:-1L}},			/*  83, _SC_TRACE */
   {cons, {c:-1L}},			/*  84, _SC_TRACE_EVENT_FILTER */
-  {nsup, {c:0}},			/*  85, _SC_TRACE_EVENT_NAME_MAX */
+  {cons, {c:-1}},			/*  85, _SC_TRACE_EVENT_NAME_MAX */
   {cons, {c:-1L}},			/*  86, _SC_TRACE_INHERIT */
   {cons, {c:-1L}},			/*  87, _SC_TRACE_LOG */
-  {nsup, {c:0}},			/*  88, _SC_TRACE_NAME_MAX */
-  {nsup, {c:0}},			/*  89, _SC_TRACE_SYS_MAX */
-  {nsup, {c:0}},			/*  90, _SC_TRACE_USER_EVENT_MAX */
+  {cons, {c:-1L}},			/*  88, _SC_TRACE_NAME_MAX */
+  {cons, {c:-1L}},			/*  89, _SC_TRACE_SYS_MAX */
+  {cons, {c:-1L}},			/*  90, _SC_TRACE_USER_EVENT_MAX */
   {cons, {c:-1L}},			/*  91, _SC_TYPED_MEMORY_OBJECTS */
   {cons, {c:_POSIX_V6_ILP32_OFF32}},	/*  92, _SC_V6_ILP32_OFF32 */
   {cons, {c:_POSIX_V6_ILP32_OFFBIG}},	/*  93, _SC_V6_ILP32_OFFBIG */
@@ -640,8 +640,6 @@ sysconf (int in)
     {
       switch (sca[in].type)
 	{
-	case nsup:
-	  break;
 	case cons:
 	  return sca[in].c;
 	case func: