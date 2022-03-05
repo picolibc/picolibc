4ef5deb2d96b1c466872bc3ae75e90c937e5d185
Keith Packard <keithp@keithp.com>
2022-03-04 22:28:11 -0800

    math: Fix fmax/fmin exceptions with nans
    sNaN raises FE_INVALID and returns qNaN.
    qNaN returns other param.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libm/common/sf_fmin.c
+++ newlib/libm/common/sf_fmin.c
@@ -6,33 +6,25 @@
 
 #include "fdlibm.h"
 
-#ifdef __STDC__
-	float fminf(float x, float y)
-#else
-	float fminf(x,y)
-	float x;
-	float y;
-#endif
+float fminf(float x, float y)
 {
-  if (__fpclassifyf(x) == FP_NAN)
-    return y;
-  if (__fpclassifyf(y) == FP_NAN)
-    return x;
-  
-  return x < y ? x : y;
+    if (issignaling(x) || issignaling(y))
+        return x + y;
+
+    if (isnan(x))
+        return y;
+
+    if (isnan(y))
+        return x;
+
+    return x < y ? x : y;
 }
 
 #ifdef _DOUBLE_IS_32BITS
 
-#ifdef __STDC__
-	double fmin(double x, double y)
-#else
-	double fmin(x,y)
-	double x;
-	double y;
-#endif
+double fmin(double x, double y)
 {
-  return (double) fminf((float) x, (float) y);
+    return (double) fminf((float) x, (float) y);
 }
 
 #endif /* defined(_DOUBLE_IS_32BITS) */
