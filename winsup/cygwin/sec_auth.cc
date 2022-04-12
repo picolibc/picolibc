05827d2df8312e19d564b06af9f8f1a97cf8c1b9
Jon Turney <jon.turney@dronecode.org.uk>
2022-04-12 11:48:54 +0100

    Cygwin: Fix typo KERB_S4U_LOGON_FLAG_IDENTITY -> IDENTIFY
    (cherry picked from commit 01c734b0d7c1ce0f848f7efc091856c58a6ef55e)

--- winsup/cygwin/sec_auth.cc
+++ winsup/cygwin/sec_auth.cc
@@ -1490,7 +1490,7 @@ typedef struct _MSV1_0_S4U_LOGON
 } MSV1_0_S4U_LOGON, *PMSV1_0_S4U_LOGON;
 
 /* Missing in Mingw-w64 */
-#define KERB_S4U_LOGON_FLAG_IDENTITY 0x08
+#define KERB_S4U_LOGON_FLAG_IDENTIFY 0x08
 
 #endif
 
@@ -1611,7 +1611,7 @@ s4uauth (bool logon, PCWSTR domain, PCWSTR user, NTSTATUS &ret_status)
       RtlSecureZeroMemory (authinf, authinf_size);
       s4u_logon = (KERB_S4U_LOGON *) authinf;
       s4u_logon->MessageType = KerbS4ULogon;
-      s4u_logon->Flags = logon ? 0 : KERB_S4U_LOGON_FLAG_IDENTITY;
+      s4u_logon->Flags = logon ? 0 : KERB_S4U_LOGON_FLAG_IDENTIFY;
       /* Append user to login info */
       RtlInitEmptyUnicodeString (&s4u_logon->ClientUpn,
 				 (PWCHAR) (s4u_logon + 1),
