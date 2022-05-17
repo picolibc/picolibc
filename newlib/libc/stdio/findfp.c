picolibc/picolibc

commit 26747c47bc0a1137e02e0377306d721cc3478855

Add stdio_exit_handler()
Add a dedicated stdio exit handler to avoid using _GLOBAL_REENT in exit().

newlib/libc/stdio/findfp.c
@@ -26,6 +26,8 @@
 #include <sys/lock.h>
 #include "local.h"
 
+void (*__stdio_exit_handler) (void);
+
 #if defined(_REENT_SMALL) && !defined(_REENT_GLOBAL_STDIO_STREAMS)
 const struct __sFILE_fake __sf_fake_stdin =
     {_NULL, 0, 0, 0, 0, {_NULL, 0}, 0, _NULL};
@@ -153,6 +155,12 @@ sfmoreglue (struct _reent *d, int n)
   return &g->glue;
 }
 
+static void
+stdio_exit_handler (void)
+{
+  (void) _fwalk_reent (_GLOBAL_REENT, CLEANUP_FILE);
+}
+
 /*
  * Find a free FILE for fopen et al.
  */
@@ -166,12 +174,13 @@ __sfp (struct _reent *d)
 
   _newlib_sfp_lock_start ();
 
-  if (_GLOBAL_REENT->__cleanup == NULL) {
+  if (__stdio_exit_handler == NULL) {
 #ifdef _REENT_GLOBAL_STDIO_STREAMS
     _GLOBAL_REENT->__sglue._niobs = 3;
     _GLOBAL_REENT->__sglue._iobs = &__sf[0];
 #endif
     __sinit (_GLOBAL_REENT);
+    __stdio_exit_handler = stdio_exit_handler;
   }
 
   for (g = &_GLOBAL_REENT->__sglue;; g = g->_next)

newlib/libc/include/sys/reent.h
@@ -837,6 +837,8 @@ extern struct _reent *_impure_ptr __ATTRIBUTE_IMPURE_PTR__;
 
 extern struct _reent _impure_data __ATTRIBUTE_IMPURE_DATA__;
 
+extern void (*__stdio_exit_handler) (void);
+
 void _reclaim_reent (struct _reent *);
 
 /* #define _REENT_ONLY define this to get only reentrant routines */

newlib/libc/stdlib/exit.c
@@ -59,7 +59,8 @@ exit (int code)
 #endif
     __call_exitprocs (code, NULL);
 
-  if (_GLOBAL_REENT->__cleanup)
-    (*_GLOBAL_REENT->__cleanup) (_GLOBAL_REENT);
+  if (__stdio_exit_handler != NULL)
+    (*__stdio_exit_handler) ();
+
   _exit (code);
 }

winsup/cygwin/signal.cc
@@ -14,6 +14,7 @@ details. */
 #include <unistd.h>
 #include <sys/cygwin.h>
 #include <sys/signalfd.h>
+#include <sys/reent.h> /* needed for __stdio_exit_handler declaration */
 #include "pinfo.h"
 #include "sigproc.h"
 #include "cygtls.h"
@@ -409,8 +410,8 @@ abort (void)
   _my_tls.call_signal_handler (); /* Call any signal handler */
 
   /* Flush all streams as per SUSv2.  */
-  if (_GLOBAL_REENT->__cleanup)
-    _GLOBAL_REENT->__cleanup (_GLOBAL_REENT);
+  if (__stdio_exit_handler)
+    (*__stdio_exit_handler) ();
   do_exit (SIGABRT);	/* signal handler didn't exit.  Goodbye. */
 }
 

commit e826fbb2ae884257479165476acbc8d8be7d0081

Fix stdio exit handling
Make sure that the stdio exit handler is set in all stdio initialization paths.

The bug was introduced by commit 26747c47bc0a1137e02e0377306d721cc3478855.

newlib/libc/stdio/findfp.c
@@ -168,6 +168,19 @@ stdio_exit_handler (void)
   (void) _fwalk_sglue (_GLOBAL_REENT, CLEANUP_FILE, &__sglue);
 }
 
+static void
+global_stdio_init (void)
+{
+  if (__stdio_exit_handler == NULL) {
+    __stdio_exit_handler = stdio_exit_handler;
+#ifdef _REENT_GLOBAL_STDIO_STREAMS
+    stdin_init (&__sf[0]);
+    stdout_init (&__sf[1]);
+    stderr_init (&__sf[2]);
+#endif
+  }
+}
+
 /*
  * Find a free FILE for fopen et al.
  */
@@ -180,11 +193,7 @@ __sfp (struct _reent *d)
   struct _glue *g;
 
   _newlib_sfp_lock_start ();
-
-  if (__stdio_exit_handler == NULL) {
-    __sinit (_GLOBAL_REENT);
-    __stdio_exit_handler = stdio_exit_handler;
-  }
+  global_stdio_init ();
 
   for (g = &__sglue;; g = g->_next)
     {
@@ -273,13 +282,9 @@ __sinit (struct _reent *s)
 # endif /* _REENT_GLOBAL_STDIO_STREAMS */
 #endif
 
-#ifdef _REENT_GLOBAL_STDIO_STREAMS
-  if (__sf[0]._cookie == NULL) {
-    stdin_init (&__sf[0]);
-    stdout_init (&__sf[1]);
-    stderr_init (&__sf[2]);
-  }
-#else /* _REENT_GLOBAL_STDIO_STREAMS */
+  global_stdio_init ();
+
+#ifndef _REENT_GLOBAL_STDIO_STREAMS
   stdin_init (s->_stdin);
   stdout_init (s->_stdout);
   stderr_init (s->_stderr);