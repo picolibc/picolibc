59b80070ceba5f9e2ea4ae75395910e185c3a9ee
Keith Packard <keithp@keithp.com>
2022-05-17 11:30:52 -0700

    cmake: Get semihost arm tests running
    Build picocrt and semihost, add TLS to picolib, get compile
    passed to test linking, add linking script.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- cmake/TC-arm-none-eabi.cmake
+++ cmake/TC-arm-none-eabi.cmake
@@ -4,6 +4,7 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 # which compilers to use for C
+set(TARGET_COMPILE_OPTIONS -mthumb -march=armv7-m -mfloat-abi=soft)
 set(CMAKE_C_COMPILER arm-none-eabi-gcc -nostdlib)
 
 # where is the target environment located
@@ -16,3 +17,11 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # search headers and libraries in the target environment
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(_HAVE_PICOLIBC_TLS_API TRUE)
+
+set(FORMAT_DEFAULT_DOUBLE 1)
+set(FORMAT_DEFAULT_FLOAT 0)
+set(FORMAT_DEFAULT_INTEGER 0)
+
+set(PICOLIBC_LINK_FLAGS -nostartfiles -T ${CMAKE_CURRENT_SOURCE_DIR}/cmake/TC-arm-none-eabi.ld -lgcc)
