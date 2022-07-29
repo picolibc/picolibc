20adacdca2fe2bd3d41a7287f77d42b4bf22f423
Corinna Vinschen <corinna@vinschen.de>
2022-07-29 12:17:43 +0200

    Cygwin: profile.c: drop i686 register access
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

--- winsup/cygwin/profil.c
+++ winsup/cygwin/profil.c
@@ -42,10 +42,10 @@ get_thrpc (HANDLE thr)
   ctx.ContextFlags = CONTEXT_CONTROL | CONTEXT_INTEGER;
   pc = (size_t) - 1;
   if (GetThreadContext (thr, &ctx)) {
-#ifndef _WIN64
-    pc = ctx.Eip;
-#else
+#ifdef __x86_64__
     pc = ctx.Rip;
+#else
+#error unimplemented for this target
 #endif
   }
   ResumeThread (thr);
