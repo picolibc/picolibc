1556b96b1b3b03112f271dfe30e043ed538354fd
Corinna Vinschen <corinna@vinschen.de>
2022-08-08 18:16:09 +0200

    Cygwin: stop exporting _alloca
    This is a remnant from 32 bit times, mindlessly copied into
    the 64 bit export table.
    
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -477,12 +477,13 @@ details. */
        sem_clockwait, sig2str, str2sig.
   342: Remove cleanup_glue.
   343: Change FD_SETSIZE and NOFILE.
+  344: Remove _alloca.
 
   Note that we forgot to bump the api for ualarm, strtoll, strtoull,
   sigaltstack, sethostname. */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 343
+#define CYGWIN_VERSION_API_MINOR 344
 
 /* There is also a compatibity version number associated with the shared memory
    regions.  It is incremented when incompatible changes are made to the shared
