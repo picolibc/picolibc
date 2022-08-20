c1f8a7b502d96fcd9ce4adb59e210399417fb3b3
Corinna Vinschen <corinna@vinschen.de>
2022-08-20 20:27:37 +0200

    Cygwin: wchar.h: Fix comment
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

--- winsup/cygwin/local_includes/wchar.h
+++ winsup/cygwin/local_includes/wchar.h
@@ -106,7 +106,7 @@ static inline size_t
 sys_mbstouni_alloc (PUNICODE_STRING dst, int type, const char *src,
 		    size_t nms = (size_t) -1)
 {
-  /* sys_mbstowcs returns length *including* trailing \0 */
+  /* sys_mbstowcs_alloc returns length *including* trailing \0 */
   size_t len = sys_mbstowcs_alloc (&dst->Buffer, type, src, nms);
   dst->MaximumLength = len * sizeof (WCHAR);
   dst->Length = dst->MaximumLength - sizeof (WCHAR);
