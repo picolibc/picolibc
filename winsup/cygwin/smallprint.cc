picolibc/picolibc

commit 07ec40170a32cc614818ec8fa13cacef0a8252a0

Cygwin: smallprint.cc: Convert tmpbuf to lockless
The old technique was from a time when we had to reduce stack pressure
by moving 64K buffers elsewhere.  It was implemented using a static
global buffer, guarded by a muto. However, that adds a lock which may
unnecessarily serialize threads.

Use Windows heap buffers per invocation instead.  HeapAlloc/HeapFree are
pretty fast, scale nicely in multithreaded scenarios and don't serialize
threads unnecessarily.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/smallprint.cc
@@ -56,31 +56,25 @@ static const char hex_str_lower[] = "0123456789abcdef";
 
 class tmpbuf
 {
-  static WCHAR buf[NT_MAX_PATH];
-  static muto lock;
-  bool locked;
+  PWCHAR buf;
+
 public:
   operator WCHAR * ()
   {
-    if (!locked)
-      {
-	lock.init ("smallprint_buf")->acquire ();
-	locked = true;
-      }
+    if (!buf)
+      buf = (PWCHAR) HeapAlloc (GetProcessHeap (), HEAP_ZERO_MEMORY,
+				NT_MAX_PATH * sizeof (WCHAR));
     return buf;
   }
-  operator char * () {return (char *) ((WCHAR *) *this);}
+  operator char * ()  { return (char *) ((WCHAR *) *this); }
 
-  tmpbuf (): locked (false) {};
+  tmpbuf () : buf (NULL) {}
   ~tmpbuf ()
   {
-    if (locked)
-      lock.release ();
+    if (buf)
+      HeapFree (GetProcessHeap (), 0, buf);
   }
 };
-
-WCHAR tmpbuf::buf[NT_MAX_PATH];
-NO_COPY muto tmpbuf::lock;
 
 static char *
 __rn (char *dst, int base, int dosign, long long val, int len, int pad, unsigned long long mask)

commit c3e92052bbaa861d35646e7f9ceb813c8be26a64

Cygwin: smallprint.cc: Drop HEAP_ZERO_MEMORY
Leftover from testing.

Reported-by: Noel Grandin <noelgrandin@gmail.com>
Fixes: 07ec40170a32 ("Cygwin: smallprint.cc: Convert tmpbuf to lockless")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/smallprint.cc
@@ -62,7 +62,7 @@ public:
   operator WCHAR * ()
   {
     if (!buf)
-      buf = (PWCHAR) HeapAlloc (GetProcessHeap (), HEAP_ZERO_MEMORY,
+      buf = (PWCHAR) HeapAlloc (GetProcessHeap (), 0,
 				NT_MAX_PATH * sizeof (WCHAR));
     return buf;
   }