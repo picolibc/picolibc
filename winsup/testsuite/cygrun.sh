a1ee8a0f0508b5975bf52b5fa018a4434817cfe4
Jon Turney <jon.turney@dronecode.org.uk>
2022-08-24 18:21:18 +0100

    Cygwin: testsuite: Drop using DejaGnu to run tests
    A more sophisticated (and modern) test harness would probably be useful,
    but switching to Automake's built-in test harness gets us parallel test
    execution, colourization of failures, simplifies matters, seems adequate
    for the current testuite, and means we don't need to write any icky Tcl.
    
    Signed-off-by: Jon Turney <jon.turney@dronecode.org.uk>

---
+++ winsup/testsuite/cygrun.sh
@@ -0,0 +1,17 @@
+#!/bin/dash
+#
+# test driver to run $1 in the appropriate environment
+#
+
+# $1 = test executable to run
+exe=$1
+
+export PATH="$runtime_root:${PATH}"
+
+if [ "$1" = "./mingw/cygload" ]
+then
+    windows_runtime_root=$(cygpath -m $runtime_root)
+    $exe -v -cygwin $windows_runtime_root/cygwin1.dll
+else
+    cygdrop $cygrun $exe
+fi
