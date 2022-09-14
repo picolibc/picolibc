2d9b2e354f03dfb1fea9fcbaa1369dbdc6efb240
Keith Packard <keithp@keithp.com>
2022-09-14 15:29:06 +0100

    ctype: Use uint32_t to hold ucs-4 values
    Replace use of wint_t with explicit uint32_t in case wint_t
    is not large enough to hold ucs-4 but wchar_t is.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/ctype/categories.c
+++ newlib/libc/ctype/categories.c
@@ -22,17 +22,17 @@ static const struct _category categories[] = {
 };
 
 static enum category
-bisearch_cat(wint_t ucs, const struct _category *table, int max)
+bisearch_cat(uint32_t ucs, const struct _category *table, int max)
 {
   int min = 0;
   int mid;
 
-  if (ucs < table[0].first || ucs > (wint_t) (table[max].first + table[max].delta))
+  if (ucs < table[0].first || ucs > (uint32_t) (table[max].first + table[max].delta))
     return CAT_error;
   while (max >= min)
     {
       mid = (min + max) / 2;
-      if (ucs > (wint_t) (table[mid].first + table[mid].delta))
+      if (ucs > (uint32_t) (table[mid].first + table[mid].delta))
 	min = mid + 1;
       else if (ucs < table[mid].first)
 	max = mid - 1;
@@ -42,7 +42,7 @@ bisearch_cat(wint_t ucs, const struct _category *table, int max)
   return CAT_error;
 }
 
-enum category category(wint_t ucs)
+enum category category(uint32_t ucs)
 {
   return bisearch_cat(ucs, categories,
 		      sizeof(categories) / sizeof(*categories) - 1);
--- newlib/libc/ctype/categories.h
+++ newlib/libc/ctype/categories.h
@@ -3,9 +3,11 @@ Copyright (c) 2017 Thomas Wolff towo@towo.net
  */
 /* category data */
 
+#include <stdint.h>
+
 enum category {
 #include "categories.cat"
     CAT_error = -1,
 };
 
-extern enum category category(wint_t ucs);
+extern enum category category(uint32_t ucs);
