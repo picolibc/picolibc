0382b383f8765c6459ffe8bc2b9962025c8409c1
Alex Richardson <alexrichardson@google.com>
2022-11-16 14:51:35 +0000

    arm/aarch64: handle TLS alignment in _set_tls()
    If the TLS region contains overaligned variables, we have to add padding
    after the tcb region to ensure that it has the same alignment as the actual
    TLS data. Without this change, the newly-extended tls.c test was failing
    since the addresses of the TLS variables were incorrect since the
    R_ARM_TLS_LE32 relocation evaluated to an offset of 0x80 (due to the 128
    byte alignment) for the first TLS variable but _set_tp() was only
    subtracting 8. I noticed this due to the ARM CI failures, but I also
    changed AArch64 since they use the same TLS scheme (Variant I).
    
    To obtain the TLS segment alignment I added a new __tls_align variable to
    the linker script. This should be more efficient than looking at the PT_TLS
    PHDR (which we currently can't accesses anyway).

--- newlib/libc/picolib/machine/arm/tls.c
+++ newlib/libc/picolib/machine/arm/tls.c
@@ -49,15 +49,19 @@ void *__tls;
  * TLS relocations are generated relative to
  * a location this far *before* the first thread
  * variable (!)
+ * NB: The actual size before tp also includes padding
+ * to align up to the alignment of .tdata/.tbss.
  */
 #define TCB_SIZE	8
+extern char __arm32_tls_tcb_offset;
+#define TP_OFFSET ((size_t)&__arm32_tls_tcb_offset)
 
 void
 _set_tls(void *tls)
 {
 #ifdef ARM_TLS_CP15
-	__asm__("mcr 15, 0, %0, cr13, cr0, 3" : : "r" (tls - TCB_SIZE));
+	__asm__("mcr 15, 0, %0, cr13, cr0, 3" : : "r" (tls - TP_OFFSET));
 #else
-	__tls = (uint8_t *) tls - TCB_SIZE;
+	__tls = (uint8_t *) tls - TP_OFFSET;
 #endif
 }
