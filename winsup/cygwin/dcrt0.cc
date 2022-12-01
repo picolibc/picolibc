picolibc/picolibc

commit 3de7be4c1debaa8fcd27f8fc15509179fdb37b9f

* DevNotes: Add entry cgf-000007. * child_info.h (child_info_spawn::parent_winpid): Declare new field. (child_info_spawn::get_parent_handle): Declare new function. * dcrt0.cc (child_info_spawn::get_parent_handle): Define new function. (child_info_spawn::handle_spawn): Recreate parent handle if possible when dynamically loaded.  Don't mess with parent handle if it's NULL. * spawn.cc (child_info_spawn::worker): Set parent_winpid appropriately.

winsup/cygwin/dcrt0.cc
@@ -625,13 +625,24 @@ child_info_fork::handle_fork ()
     api_fatal ("recreate_mmaps_after_fork_failed");
 }
 
+bool
+child_info_spawn::get_parent_handle ()
+{
+  parent = OpenProcess (PROCESS_VM_READ, false, parent_winpid);
+  moreinfo->myself_pinfo = NULL;
+  return !!parent;
+}
+
 void
 child_info_spawn::handle_spawn ()
 {
   extern void fixup_lockf_after_exec ();
   HANDLE h;
-  cygheap_fixup_in_child (true);
-  memory_init (false);
+  if (!dynamically_loaded || get_parent_handle ())
+      {
+	cygheap_fixup_in_child (true);
+	memory_init (false);
+      }
   if (!moreinfo->myself_pinfo ||
       !DuplicateHandle (GetCurrentProcess (), moreinfo->myself_pinfo,
 			GetCurrentProcess (), &h, 0,
@@ -669,7 +680,7 @@ child_info_spawn::handle_spawn ()
      Otherwise, we no longer need this handle so close it.
      Need to do this after debug_fixup_after_fork_exec or DEBUGGING handling of
      handles might get confused. */
-  if (type != _CH_EXEC)
+  if (type != _CH_EXEC && child_proc_info->parent)
     {
       CloseHandle (child_proc_info->parent);
       child_proc_info->parent = NULL;

winsup/cygwin/DevNotes
@@ -1,3 +1,26 @@
+2012-05-14  cgf-000007
+
+<1.7.16>
+- Fix invocation of strace from a cygwin process.  Fixes:
+  http://cygwin.com/ml/cygwin/2012-05/msg00292.html
+</1.7.16>
+
+The change in cgf-000004 introduced a problem for processes which load
+cygwin1.dll dynamically.  strace.exe is the most prominent example of
+this.
+
+Since the parent handle is now closed for "non-Cygwin" processes, when
+strace.exe tried to dynamically load cygwin1.dll, the handle was invalid
+and child_info_spawn::handle_spawn couldn't use retrieve information
+from the parent.  This eventually led to a strace_printf error due to an
+attempt to dereference an unavailable cygheap.  Probably have to fix
+this someday.  You shouldn't use the cygheap while attempting to print
+an error about the inavailability of said cygheap.
+
+This was fixed by saving the parent pid in child_info_spawn and calling
+OpenProcess for the parent pid and using that handle iff a process is
+dynamically loaded.
+
 2012-05-12  cgf-000006
 
 <1.7.16>

winsup/cygwin/child_info.h
@@ -66,6 +66,7 @@ public:
   HANDLE subproc_ready;	// used for synchronization with parent
   HANDLE user_h;
   HANDLE parent;
+  DWORD parent_winpid;
   DWORD cygheap_reserve_sz;
   unsigned fhandler_union_cb;
   DWORD exit_code;	// process exit code
@@ -175,6 +176,7 @@ public:
     lock->release ();
     return !!hExeced;
   }
+  bool get_parent_handle ();
   bool has_execed_cygwin () const { return iscygwin () && has_execed (); }
   operator HANDLE& () {return hExeced;}
   int worker (const char *, const char *const *, const char *const [], int,

winsup/cygwin/release/1.7.16
@@ -12,3 +12,5 @@ fifo.  Fixes: http://cygwin.com/ml/cygwin/2012-05/msg00253.html
 - Fix hang when calling pthread_testcancel in a canceled thread.
   Fixes some of: http://cygwin.com/ml/cygwin/2012-05/msg00186.html
 
+- Fix invocation of strace from a cygwin process.  Fixes:
+  http://cygwin.com/ml/cygwin/2012-05/msg00292.html

winsup/cygwin/spawn.cc
@@ -614,6 +614,7 @@ child_info_spawn::worker (const char *prog_arg, const char *const *argv,
       SetHandleInformation (wr_proc_pipe, HANDLE_FLAG_INHERIT, 0);
       SetHandleInformation (parent, HANDLE_FLAG_INHERIT, 0);
     }
+  parent_winpid = GetCurrentProcessId ();
 
   /* When ruid != euid we create the new process under the current original
      account and impersonate in child, this way maintaining the different

commit 30add3e6b3e3211b3e2d4a093f45bee5c6e24b8b

Cygwin: exec: don't access cygheap before it's initialized
This is a long-standing thinko.

When you exec a process, dll_crt0_0 in the child process calls
child_info_spawn::handle_spawn().  handle_spawn() initialises the
cygheap.

Now consider calling strace.  Strace is a non-Cygwin process dynamically
loading cygwin1.dll via LoadLibrary.  This in turn initializes the DLL:

- dll_crt0_0 finds that the process it attaches to has been exec'd, so
  child_info_spawn::handle_spawn() is called.

- If the DLL is being dynamically loaded, handle_spawn() calls
  child_info_spawn::get_parent_handle().  This in turn tries to set
  the moreinfo->myself_pinfo value inside the cygheap to NULL.

- However, at this time, the cygheap has not yet been initialized.  This
  only occurs in the cygheap_fixup_in_child() call after get_parent_handle()
  returns.

--> SEGV

This thinko never had a negative side effect, because the cygheap was
pre-allocated at DLL load time until commit 2f9b8ff00cce ("Cygwin:
decouple cygheap from Cygwin DLL").  With 2f9b8ff00cce, the cygheap
actually doesn't exist until after the call to cygheap_fixup_in_child().

Fix this problem by moving the assignment after the call to
cygheap_fixup_in_child().

Fixes: 3de7be4c1deb ("* DevNotes: Add entry cgf-000007. [...]")
Fixes: 2f9b8ff00cce ("Cygwin: decouple cygheap from Cygwin DLL")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/dcrt0.cc
@@ -620,7 +620,6 @@ bool
 child_info_spawn::get_parent_handle ()
 {
   parent = OpenProcess (PROCESS_VM_READ, FALSE, parent_winpid);
-  moreinfo->myself_pinfo = NULL;
   return !!parent;
 }
 
@@ -632,6 +631,8 @@ child_info_spawn::handle_spawn ()
   if (!dynamically_loaded || get_parent_handle ())
       {
 	cygheap_fixup_in_child (true);
+	if (dynamically_loaded)
+	  moreinfo->myself_pinfo = NULL;
 	memory_init ();
       }
 