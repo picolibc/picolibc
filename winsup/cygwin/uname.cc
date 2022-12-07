picolibc/picolibc

commit 97eb64b909bc0a47b3159ae7f7ef2060e4bb8d54

Cygwin: uname: generate default release string from git as well
When building a release with cygport, we get uname version info
from cygport, which in turn gets version info from `git describe'.

During development, the release info for local builds was not
that helpful yet.  Fix that, by creating version info from
`git describe' if CYGPORT_RELEASE_INFO isn't given.  Make sure to
always force rebuild of the version info to pick up source file
changes as well as git actions.

Rearrange code slightly to generate machine info first, release info
after that.  Use snprintf to generate release string safely.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/Makefile.am
@@ -365,7 +365,8 @@ GMON_FILES= \
 	profil.c
 
 GENERATED_FILES= \
-	sigfe.s
+	sigfe.s \
+	uname_version.c
 
 liblib_a_SOURCES= \
 	$(LIB_FILES)
@@ -417,6 +418,11 @@ BUILT_SOURCES = \
 dirs = $(srcdir) $(srcdir)/fhandler $(srcdir)/lib $(srcdir)/libc $(srcdir)/math $(srcdir)/mm $(srcdir)/regex $(srcdir)/sec $(srcdir)/tzcode
 find_src_files = $(wildcard $(dir)/*.[chS]) $(wildcard $(dir)/*.cc)
 src_files := $(foreach dir,$(dirs),$(find_src_files))
+
+uname_version.c: .FORCE
+	$(AM_V_GEN)cd $(srcdir) && \
+	echo "const char *uname_dev_version = \"$$(git describe --dirty | sed -e 's/cygwin-//')\";" > $(abs_builddir)/uname_version.c
+.FORCE:
 
 # mkvers.sh creates version.cc in the first place, winver.o always
 # second, so version.cc is always older than winver.o

winsup/cygwin/uname.cc
@@ -9,6 +9,7 @@ Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
 details. */
 
 #include "winsup.h"
+#include <stdio.h>
 #include <sys/utsname.h>
 #include <netdb.h>
 #include "cygwin_version.h"
@@ -41,29 +42,31 @@ uname_x (struct utsname *name)
       memset (buf, 0, sizeof buf);
       cygwin_gethostname (buf, sizeof buf - 1);
       strncat (name->nodename, buf, sizeof (name->nodename) - 1);
-      /* release */
-#ifdef CYGPORT_RELEASE_INFO
-      stpcpy (name->release, __XSTRING (CYGPORT_RELEASE_INFO));
-#else
-      __small_sprintf (name->release, "%d.%d.%d-0.%d.local.",
-		       cygwin_version.dll_major / 1000,
-		       cygwin_version.dll_major % 1000,
-		       cygwin_version.dll_minor,
-		       cygwin_version.api_minor);
-#endif
-      /* version */
-      stpcpy (name->version, cygwin_version.dll_build_date);
-      strcat (name->version, " UTC");
       /* machine */
       switch (wincap.cpu_arch ())
 	{
 	  case PROCESSOR_ARCHITECTURE_AMD64:
-	    strcat (name->release, strcpy (name->machine, "x86_64"));
+	    strcpy (name->machine, "x86_64");
 	    break;
 	  default:
 	    strcpy (name->machine, "unknown");
 	    break;
 	}
+      /* release */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-truncation="
+#ifdef CYGPORT_RELEASE_INFO
+      snprintf (name->release, _UTSNAME_LENGTH, "%s.%s",
+		__XSTRING (CYGPORT_RELEASE_INFO), name->machine);
+#else
+      extern const char *uname_dev_version;
+      snprintf (name->release, _UTSNAME_LENGTH, "%s.%s",
+		uname_dev_version, name->machine);
+#endif
+#pragma GCC diagnostic pop
+      /* version */
+      stpcpy (name->version, cygwin_version.dll_build_date);
+      strcat (name->version, " UTC");
       /* domainame */
       memset (buf, 0, sizeof buf);
       getdomainname (buf, sizeof buf - 1);

commit 4949a82cde98886a79a962bf583cdbe9a2186078

Cygwin: uname: fix building in non-git source dir
commit 97eb64b909bc broke building outside of a git dir, because
the git describe command would fail.  Fix this by checking if
we're in a git tree at all and just generate an empty string
as version string.  Use this in uname_x to generate a fallback
version.

Fixes: 97eb64b909bc ("Cygwin: uname: generate default release
string from git as well"
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/Makefile.am
@@ -421,7 +421,10 @@ src_files := $(foreach dir,$(dirs),$(find_src_files))
 
 uname_version.c: .FORCE
 	$(AM_V_GEN)cd $(srcdir) && \
-	echo "const char *uname_dev_version = \"$$(git describe --dirty | sed -e 's/cygwin-//')\";" > $(abs_builddir)/uname_version.c
+	echo "const char *uname_dev_version = \
+	\"$$(git rev-parse --git-dir >/dev/null 2>&1 && \
+	     git describe --dirty | sed -e 's/cygwin-//')\";" \
+	     > $(abs_builddir)/uname_version.c
 .FORCE:
 
 # mkvers.sh creates version.cc in the first place, winver.o always

winsup/cygwin/uname.cc
@@ -60,8 +60,16 @@ uname_x (struct utsname *name)
 		__XSTRING (CYGPORT_RELEASE_INFO), name->machine);
 #else
       extern const char *uname_dev_version;
-      snprintf (name->release, _UTSNAME_LENGTH, "%s.%s",
-		uname_dev_version, name->machine);
+      if (uname_dev_version && uname_dev_version[0])
+	snprintf (name->release, _UTSNAME_LENGTH, "%s.%s",
+		  uname_dev_version, name->machine);
+      else
+	__small_sprintf (name->release, "%d.%d.%d-api-%d.%s",
+			 cygwin_version.dll_major / 1000,
+			 cygwin_version.dll_major % 1000,
+			 cygwin_version.dll_minor,
+			 cygwin_version.api_minor,
+			 name->machine);
 #endif
 #pragma GCC diagnostic pop
       /* version */