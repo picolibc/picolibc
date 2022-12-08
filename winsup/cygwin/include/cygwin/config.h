picolibc/picolibc

commit 2902b3a09e0a45c4971b59f78ab175b71b3c755e

Cygwin: drop requirement to build newlib's stdio64
Given that 64 bit Cygwin defines all file access types (off_t,
fpos_t, and derived types) as 64 bit anyway, there's no reason
left to rely on the stdio64 part of newlib.  Use base functions
and base types.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/include/cygwin/config.h
@@ -54,8 +54,6 @@ extern inline struct _reent *__getreent (void)
    Just leave them alone. */
 #define _READ_WRITE_RETURN_TYPE _ssize_t
 #define _READ_WRITE_BUFSIZE_TYPE size_t
-#define __LARGE64_FILES 1
-#define __USE_INTERNAL_STAT64 1
 #define __LINUX_ERRNO_EXTENSIONS__ 1
 #define _MB_EXTENDED_CHARSETS_ALL 1
 #define __HAVE_LOCALE_INFO__ 1

newlib/configure.host
@@ -420,7 +420,6 @@ fi
 case "${host}" in
   *-*-cygwin*)
 	posix_dir=posix
-	stdio64_dir=stdio64
 	xdr_dir=xdr
 	;;
   *-*-netware*)

newlib/libc/include/stdio.h
@@ -67,14 +67,10 @@ typedef __FILE FILE;
 # define __FILE_defined
 #endif
 
-#ifdef __CYGWIN__
-typedef _fpos64_t fpos_t;
-#else
 typedef _fpos_t fpos_t;
 #ifdef __LARGE64_FILES
 typedef _fpos64_t fpos64_t;
 #endif
-#endif /* !__CYGWIN__ */
 
 #ifndef _OFF_T_DECLARED
 typedef __off_t off_t;

winsup/cygwin/syscalls.cc
@@ -4191,7 +4191,7 @@ gethostid (void)
 
 #define ETC_SHELLS "/etc/shells"
 static int shell_index;
-static struct __sFILE64 *shell_fp;
+static FILE *shell_fp;
 
 extern "C" char *
 getusershell ()
@@ -4210,7 +4210,7 @@ getusershell ()
   static char buf[PATH_MAX];
   int ch, buf_idx;
 
-  if (!shell_fp && !(shell_fp = fopen64 (ETC_SHELLS, "rt")))
+  if (!shell_fp && !(shell_fp = fopen (ETC_SHELLS, "rt")))
     {
       if (def_shells[shell_index])
 	return strcpy (buf, def_shells[shell_index++]);

commit 0f376ae22036bac2fbc7863264909b599e42616c

Cygwin: rename __sFILE to __sFILE64 for backward compatibility
Until Cygwin 3.3.6, we define __LARGE64_FILES unconditionally, so we
were using the type __sFILE64 even for 64 bit.  That was lazy and wrong.
so commit 2902b3a09e0a ("Cygwin: drop requirement to build newlib's
stdio64") tried to fix that.

Unfortunately this patch forgot to take the exposure of the typename
__sFILE64 in userspace into account.  This leads to trouble in C++ due
to name mangling.

Fix this by redefining __sFILE to __sFILE64.  The type name is very much
internal, so it doesn't really matter, except for the fact that it needs
to stay backward compatible so as not to break building against C++ libs
built under older versions of Cygwin.

Fixes: 2902b3a09e0a ("Cygwin: drop requirement to build newlib's stdio64")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/include/cygwin/config.h
@@ -49,6 +49,12 @@ extern inline struct _reent *__getreent (void)
 
 #define __FILENAME_MAX__ 4096	/* Keep in sync with PATH_MAX in limits.h. */
 
+/* Unfortunately we defined __LARGE64_FILES until Cygwin 3.3.6, so
+   FILE was based on `struct __sFILE64'.  The name is exposed into
+   userspace and consequentially used in C++ name mangling.  We must
+   redefine __sFILE as __sFILE64 to stay backward compatible. */
+#define __sFILE	__sFILE64
+
 /* The following block of macros is required to build newlib correctly for
    Cygwin.  Changing them in applications has no or not the desired effect.
    Just leave them alone. */