picolibc/picolibc

commit 195169186bfd58e45aaa31236575fb9d5957adad

Cygwin: wait_sig: allow to compute process-wide mask of pending signals
Add a signal __SIGPENDINGALL to allow computing the mask of all
currently pending signals.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/sigproc.cc
@@ -1371,6 +1371,26 @@ wait_sig (VOID *)
 	case __SIGSTRACE:
 	  strace.activate (false);
 	  break;
+	case __SIGPENDINGALL:
+	  {
+	    unsigned bit;
+	    bool issig_wait;
+
+	    *pack.mask = 0;
+	    while ((q = q->next))
+	      {
+		if (q->sigtls->sigmask & (bit = SIGTOMASK (q->si.si_signo)))
+		  {
+		    tl_entry = cygheap->find_tls (q->si.si_signo, issig_wait);
+		    if (tl_entry)
+		      {
+			*pack.mask |= bit;
+			cygheap->unlock_tls (tl_entry);
+		      }
+		  }
+	      }
+	  }
+	  break;
 	case __SIGPENDING:
 	  {
 	    unsigned bit;

winsup/cygwin/sigproc.h
@@ -22,7 +22,8 @@ enum
   __SIGHOLD	    = -(_NSIG + 7),
   __SIGNOHOLD	    = -(_NSIG + 8),
   __SIGSETPGRP	    = -(_NSIG + 9),
-  __SIGTHREADEXIT   = -(_NSIG + 10)
+  __SIGTHREADEXIT   = -(_NSIG + 10),
+  __SIGPENDINGALL   = -(_NSIG + 11),
 };
 #endif
 

commit 7c14e5a10add3ad3dc53ed6af1ad2044d205e844

Cygwin: /proc/<PID>/status: avoid crash computing signal info
The code computing the mask of pending signals used the per-queued
signal TLS pointer without checking it for NULL.  Fix this by using
the process-wide signal mask in that case.

Fixes: 195169186bfd5 ("Cygwin: wait_sig: allow to compute process-wide mask of pending signals")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/release/3.4.4
@@ -10,3 +10,6 @@ Bug Fixes
 
 - Reinstantiate exporting _alloca.
   Addresses: https://cygwin.com/pipermail/cygwin/2023-January/252797.html
+
+- Avoid hangs when reading /proc/<PID>/status.
+  Addresses: https://cygwin.com/pipermail/cygwin/2022-December/252756.html

winsup/cygwin/sigproc.cc
@@ -1375,7 +1375,8 @@ wait_sig (VOID *)
 	    *pack.mask = 0;
 	    while ((q = q->next))
 	      {
-		if (q->sigtls->sigmask & (bit = SIGTOMASK (q->si.si_signo)))
+		_cygtls *sigtls = q->sigtls ?: _main_tls;
+		if (sigtls->sigmask & (bit = SIGTOMASK (q->si.si_signo)))
 		  {
 		    tl_entry = cygheap->find_tls (q->si.si_signo, issig_wait);
 		    if (tl_entry)