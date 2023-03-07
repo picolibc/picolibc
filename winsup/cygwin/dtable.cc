picolibc/picolibc

commit c38a2d83730311cad68d3c2d4759dfc18a3c64f3

* syscalls.cc (setsid): On second thought, in the spirit of keeping things kludgy, set ctty to -2 here as a special flag, and... (open): ...only eschew setting O_NOCTTY when that case is detected.

winsup/cygwin/syscalls.cc
@@ -1015,7 +1015,7 @@ setsid (void)
     syscall_printf ("hmm.  pgid %d pid %d", myself->pgid, myself->pid);
   else
     {
-      myself->ctty = -1;
+      myself->ctty = -2;
       myself->sid = getpid ();
       myself->pgid = getpid ();
       if (cygheap->ctty)
@@ -1275,7 +1275,7 @@ open (const char *unix_path, int flags, ...)
 	     tty for the process.  */
 	  int opt = PC_OPEN | ((flags & (O_NOFOLLOW | O_EXCL))
 			       ?  PC_SYM_NOFOLLOW : PC_SYM_FOLLOW);
-	  if (!(flags & O_NOCTTY) && fd > 2 && myself->ctty > 0)
+	  if (!(flags & O_NOCTTY) && fd > 2 && myself->ctty != -2)
 	    {
 	      flags |= O_NOCTTY;
 	      opt |= PC_CTTY;	/* flag that, if opened, this fhandler could

commit 93f70d7849b865f2081596bbeabdd6fca6101553

Cygwin: ctty: Remove old 'kludge' code.
Remove old 'kludge' code which does not seem necessary anymore. The
comment of the 'kludge' is as follows.

  * syscalls.cc (setsid): On second thought, in the spirit of keeping
    things kludgy, set ctty to -2 here as a special flag, and...
    (open): ...only eschew setting O_NOCTTY when that case is detected.

Fixes: c38a2d837303
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/dtable.cc
@@ -764,13 +764,6 @@ dtable::dup3 (int oldfd, int newfd, int flags)
       return -1;
     }
 
-  /* This is a temporary kludge until all utilities can catch up with
-     a change in behavior that implements linux functionality:  opening
-     a tty should not automatically cause it to become the controlling
-     tty for the process.  */
-  if (newfd > 2)
-    flags |= O_NOCTTY;
-
   if ((newfh = dup_worker (fds[oldfd], flags)) == NULL)
     {
       res = -1;

winsup/cygwin/syscalls.cc
@@ -1450,17 +1450,6 @@ open (const char *unix_path, int flags, ...)
       int opt = PC_OPEN | PC_SYM_NOFOLLOW_PROCFD;
       opt |= (flags & (O_NOFOLLOW | O_EXCL)) ? PC_SYM_NOFOLLOW
 					     : PC_SYM_FOLLOW;
-      /* This is a temporary kludge until all utilities can catch up
-	 with a change in behavior that implements linux functionality:
-	 opening a tty should not automatically cause it to become the
-	 controlling tty for the process.  */
-      if (!(flags & O_NOCTTY) && fd > 2 && myself->ctty != -2)
-	{
-	  flags |= O_NOCTTY;
-	  /* flag that, if opened, this fhandler could later be capable
-	     of being a controlling terminal if /dev/tty is opened. */
-	  opt |= PC_CTTY;
-	}
 
       /* If we're opening a FIFO, we will call device_access_denied
 	 below.  This leads to a call to fstat, which can use the