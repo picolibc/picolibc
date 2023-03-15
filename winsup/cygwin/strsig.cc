picolibc/picolibc

commit 61522196c71593da09572fce9af9e0d7dad61bc3

* Merge in cygwin-64bit-branch.

winsup/cygwin/strsig.cc
@@ -1,6 +1,6 @@
 /* strsig.cc
 
-   Copyright 2004, 2007, 2008, 2010, 2011 Red Hat, Inc.
+   Copyright 2004, 2007, 2008, 2010, 2011, 2013 Red Hat, Inc.
 
 This file is part of Cygwin.
 
@@ -20,7 +20,7 @@ struct sigdesc
   const char *str;
 };
 
-#define __signals \
+#define __signals_common \
   _s(SIGHUP, "Hangup"),				/*  1 */ \
   _s(SIGINT, "Interrupt"),			/*  2 */ \
   _s(SIGQUIT, "Quit"),				/*  3 */ \
@@ -54,13 +54,54 @@ struct sigdesc
   _s2(SIGPWR, "Power failure",			/* 29 */ \
       SIGLOST, "Resource lost"),			 \
   _s(SIGUSR1, "User defined signal 1"),		/* 30 */ \
-  _s(SIGUSR2, "User defined signal 2"),		/* 31 */ \
+  _s(SIGUSR2, "User defined signal 2"),		/* 31 */
+
+#ifdef __x86_64__
+# define __signals \
+  __signals_common \
+  _s(SIGRTMIN, "Real-time signal 0"),		/* 32 */ \
+  _s(SIGRTMIN + 1, "Real-time signal 1"),	/* 33 */ \
+  _s(SIGRTMIN + 2, "Real-time signal 2"),	/* 34 */ \
+  _s(SIGRTMIN + 3, "Real-time signal 3"),	/* 35 */ \
+  _s(SIGRTMIN + 4, "Real-time signal 4"),	/* 36 */ \
+  _s(SIGRTMIN + 5, "Real-time signal 5"),	/* 37 */ \
+  _s(SIGRTMIN + 6, "Real-time signal 6"),	/* 38 */ \
+  _s(SIGRTMIN + 7, "Real-time signal 7"),	/* 39 */ \
+  _s(SIGRTMIN + 8, "Real-time signal 8"),	/* 40 */ \
+  _s(SIGRTMIN + 9, "Real-time signal 9"),	/* 41 */ \
+  _s(SIGRTMIN + 10, "Real-time signal 10"),	/* 42 */ \
+  _s(SIGRTMIN + 11, "Real-time signal 11"),	/* 43 */ \
+  _s(SIGRTMIN + 12, "Real-time signal 12"),	/* 44 */ \
+  _s(SIGRTMIN + 13, "Real-time signal 13"),	/* 45 */ \
+  _s(SIGRTMIN + 14, "Real-time signal 14"),	/* 46 */ \
+  _s(SIGRTMIN + 15, "Real-time signal 15"),	/* 47 */ \
+  _s(SIGRTMIN + 16, "Real-time signal 16"),	/* 48 */ \
+  _s(SIGRTMIN + 17, "Real-time signal 17"),	/* 49 */ \
+  _s(SIGRTMIN + 18, "Real-time signal 18"),	/* 50 */ \
+  _s(SIGRTMIN + 19, "Real-time signal 19"),	/* 51 */ \
+  _s(SIGRTMIN + 20, "Real-time signal 20"),	/* 52 */ \
+  _s(SIGRTMIN + 21, "Real-time signal 21"),	/* 53 */ \
+  _s(SIGRTMIN + 22, "Real-time signal 22"),	/* 54 */ \
+  _s(SIGRTMIN + 23, "Real-time signal 23"),	/* 55 */ \
+  _s(SIGRTMIN + 24, "Real-time signal 24"),	/* 56 */ \
+  _s(SIGRTMIN + 25, "Real-time signal 25"),	/* 57 */ \
+  _s(SIGRTMIN + 26, "Real-time signal 26"),	/* 58 */ \
+  _s(SIGRTMIN + 27, "Real-time signal 27"),	/* 59 */ \
+  _s(SIGRTMIN + 28, "Real-time signal 28"),	/* 60 */ \
+  _s(SIGRTMIN + 29, "Real-time signal 29"),	/* 61 */ \
+  _s(SIGRTMIN + 30, "Real-time signal 30"),	/* 62 */ \
+  _s(SIGRTMIN + 31, "Real-time signal 31"),	/* 63 */ \
+  _s(SIGRTMAX, "Real-time signal 32")		/* 64 */
+#else
+# define __signals \
+  __signals_common \
   _s2(SIGRTMIN, "Real-time signal 0",		/* 32 */ \
       SIGRTMAX, "Real-time signal 0")
+#endif
 
 #define _s(n, s) #n
 #define _s2(n, s, n1, s1) #n
-const char *sys_sigabbrev[] NO_COPY_INIT =
+const char *sys_sigabbrev[] =
 {
   NULL,
   __signals
@@ -70,7 +111,7 @@ const char *sys_sigabbrev[] NO_COPY_INIT =
 #undef _s2
 #define _s(n, s) s
 #define _s2(n, s, n1, s1) s
-const char *sys_siglist[] NO_COPY_INIT =
+const char *sys_siglist[] =
 {
   NULL,
   __signals
@@ -128,7 +169,7 @@ psiginfo (const siginfo_t *info, const char *s)
 	    fprintf (stderr, " (%d [%p])", info->si_code, info->si_addr);
 	    break;
 	  case SIGCHLD:
-	    fprintf (stderr, " (%d %d %d %ld)", info->si_code, info->si_pid,
+	    fprintf (stderr, " (%d %d %d %u)", info->si_code, info->si_pid,
 		     info->si_status, info->si_uid);
 	    break;
 /* FIXME: implement si_band
@@ -137,7 +178,7 @@ psiginfo (const siginfo_t *info, const char *s)
 	    break;
 */
 	  default:
-	    fprintf (stderr, " (%d %d %ld)", info->si_code, info->si_pid, info->si_uid);
+	    fprintf (stderr, " (%d %d %u)", info->si_code, info->si_pid, info->si_uid);
 	}
     }
 

commit 07b5d3726fb9548d474751bed1a3aa00dba263e8

Cygwin: signals: drop accidental spaces from real-time signal names
The signal names for the real-time signals contain spaces from
the beginning of their availability.  Fix that.

Fixes: 61522196c715 ("* Merge in cygwin-64bit-branch.")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/strsig.cc
@@ -56,37 +56,37 @@ struct sigdesc
   _s(SIGUSR1, "User defined signal 1"),		/* 30 */ \
   _s(SIGUSR2, "User defined signal 2"),		/* 31 */ \
   _s(SIGRTMIN, "Real-time signal 0"),		/* 32 */ \
-  _s(SIGRTMIN + 1, "Real-time signal 1"),	/* 33 */ \
-  _s(SIGRTMIN + 2, "Real-time signal 2"),	/* 34 */ \
-  _s(SIGRTMIN + 3, "Real-time signal 3"),	/* 35 */ \
-  _s(SIGRTMIN + 4, "Real-time signal 4"),	/* 36 */ \
-  _s(SIGRTMIN + 5, "Real-time signal 5"),	/* 37 */ \
-  _s(SIGRTMIN + 6, "Real-time signal 6"),	/* 38 */ \
-  _s(SIGRTMIN + 7, "Real-time signal 7"),	/* 39 */ \
-  _s(SIGRTMIN + 8, "Real-time signal 8"),	/* 40 */ \
-  _s(SIGRTMIN + 9, "Real-time signal 9"),	/* 41 */ \
-  _s(SIGRTMIN + 10, "Real-time signal 10"),	/* 42 */ \
-  _s(SIGRTMIN + 11, "Real-time signal 11"),	/* 43 */ \
-  _s(SIGRTMIN + 12, "Real-time signal 12"),	/* 44 */ \
-  _s(SIGRTMIN + 13, "Real-time signal 13"),	/* 45 */ \
-  _s(SIGRTMIN + 14, "Real-time signal 14"),	/* 46 */ \
-  _s(SIGRTMIN + 15, "Real-time signal 15"),	/* 47 */ \
-  _s(SIGRTMIN + 16, "Real-time signal 16"),	/* 48 */ \
-  _s(SIGRTMIN + 17, "Real-time signal 17"),	/* 49 */ \
-  _s(SIGRTMIN + 18, "Real-time signal 18"),	/* 50 */ \
-  _s(SIGRTMIN + 19, "Real-time signal 19"),	/* 51 */ \
-  _s(SIGRTMIN + 20, "Real-time signal 20"),	/* 52 */ \
-  _s(SIGRTMIN + 21, "Real-time signal 21"),	/* 53 */ \
-  _s(SIGRTMIN + 22, "Real-time signal 22"),	/* 54 */ \
-  _s(SIGRTMIN + 23, "Real-time signal 23"),	/* 55 */ \
-  _s(SIGRTMIN + 24, "Real-time signal 24"),	/* 56 */ \
-  _s(SIGRTMIN + 25, "Real-time signal 25"),	/* 57 */ \
-  _s(SIGRTMIN + 26, "Real-time signal 26"),	/* 58 */ \
-  _s(SIGRTMIN + 27, "Real-time signal 27"),	/* 59 */ \
-  _s(SIGRTMIN + 28, "Real-time signal 28"),	/* 60 */ \
-  _s(SIGRTMIN + 29, "Real-time signal 29"),	/* 61 */ \
-  _s(SIGRTMIN + 30, "Real-time signal 30"),	/* 62 */ \
-  _s(SIGRTMIN + 31, "Real-time signal 31"),	/* 63 */ \
+  _s(SIGRTMIN+1, "Real-time signal 1"),	/* 33 */ \
+  _s(SIGRTMIN+2, "Real-time signal 2"),	/* 34 */ \
+  _s(SIGRTMIN+3, "Real-time signal 3"),	/* 35 */ \
+  _s(SIGRTMIN+4, "Real-time signal 4"),	/* 36 */ \
+  _s(SIGRTMIN+5, "Real-time signal 5"),	/* 37 */ \
+  _s(SIGRTMIN+6, "Real-time signal 6"),	/* 38 */ \
+  _s(SIGRTMIN+7, "Real-time signal 7"),	/* 39 */ \
+  _s(SIGRTMIN+8, "Real-time signal 8"),	/* 40 */ \
+  _s(SIGRTMIN+9, "Real-time signal 9"),	/* 41 */ \
+  _s(SIGRTMIN+10, "Real-time signal 10"),	/* 42 */ \
+  _s(SIGRTMIN+11, "Real-time signal 11"),	/* 43 */ \
+  _s(SIGRTMIN+12, "Real-time signal 12"),	/* 44 */ \
+  _s(SIGRTMIN+13, "Real-time signal 13"),	/* 45 */ \
+  _s(SIGRTMIN+14, "Real-time signal 14"),	/* 46 */ \
+  _s(SIGRTMIN+15, "Real-time signal 15"),	/* 47 */ \
+  _s(SIGRTMIN+16, "Real-time signal 16"),	/* 48 */ \
+  _s(SIGRTMIN+17, "Real-time signal 17"),	/* 49 */ \
+  _s(SIGRTMIN+18, "Real-time signal 18"),	/* 50 */ \
+  _s(SIGRTMIN+19, "Real-time signal 19"),	/* 51 */ \
+  _s(SIGRTMIN+20, "Real-time signal 20"),	/* 52 */ \
+  _s(SIGRTMIN+21, "Real-time signal 21"),	/* 53 */ \
+  _s(SIGRTMIN+22, "Real-time signal 22"),	/* 54 */ \
+  _s(SIGRTMIN+23, "Real-time signal 23"),	/* 55 */ \
+  _s(SIGRTMIN+24, "Real-time signal 24"),	/* 56 */ \
+  _s(SIGRTMIN+25, "Real-time signal 25"),	/* 57 */ \
+  _s(SIGRTMIN+26, "Real-time signal 26"),	/* 58 */ \
+  _s(SIGRTMIN+27, "Real-time signal 27"),	/* 59 */ \
+  _s(SIGRTMIN+28, "Real-time signal 28"),	/* 60 */ \
+  _s(SIGRTMIN+29, "Real-time signal 29"),	/* 61 */ \
+  _s(SIGRTMIN+30, "Real-time signal 30"),	/* 62 */ \
+  _s(SIGRTMIN+31, "Real-time signal 31"),	/* 63 */ \
   _s(SIGRTMAX, "Real-time signal 32")		/* 64 */
 
 #define _s(n, s) #n