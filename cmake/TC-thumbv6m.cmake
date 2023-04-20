fa0ea90ba1d27218a4f33185665d663b6273ac53
Keith Packard <keithp@keithp.com>
2023-04-20 11:59:26 -0700

    scripts: Add sample cmake build for thumbv6m targets
    Provide another sample arm target which is slightly different than
    thumbv7m that can test the atomic operation fallbacks (thumbv6m
    doesn't have atomic operations).
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ cmake/TC-thumbv6m.cmake
@@ -0,0 +1,27 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Generic)
+
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+# which compilers to use for C
+set(TARGET_COMPILE_OPTIONS -mthumb -march=armv6-m -mfloat-abi=soft)
+set(CMAKE_C_COMPILER arm-none-eabi-gcc -nostdlib)
+
+# where is the target environment located
+set(CMAKE_FIND_ROOT_PATH  /usr/bin)
+
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(_HAVE_PICOLIBC_TLS_API TRUE)
+
+set(FORMAT_DEFAULT_DOUBLE 1)
+set(FORMAT_DEFAULT_FLOAT 0)
+set(FORMAT_DEFAULT_INTEGER 0)
+
+set(PICOLIBC_LINK_FLAGS -nostartfiles -T ${CMAKE_CURRENT_SOURCE_DIR}/cmake/TC-arm-none-eabi.ld -lgcc)
