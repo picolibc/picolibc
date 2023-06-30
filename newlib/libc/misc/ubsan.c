fa0bd947d7ae1df6cf9f6c46564a55fc0e4748c4
Keith Packard <keithp@keithp.com>
2023-06-30 13:44:38 -0700

    libc/misc: Add __ubsan_handle_out_of_bounds implementation
    This function is called when the compiler is checking for array bounds
    and not trapping on error.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>
    Cc: Kees Cook <keescook@chromium.org>

---
+++ newlib/libc/misc/ubsan.c
@@ -0,0 +1,59 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright © 2022 Keith Packard
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdlib.h>
+
+struct source_location {
+    const char *filename;
+    uint32_t line;
+    uint32_t column;
+};
+
+struct out_of_bounds_data {
+    struct source_location location;
+    const void *array_type;
+    const void *index_type;
+};
+
+void
+__ubsan_handle_out_of_bounds(struct out_of_bounds_data *data, void *id)
+{
+    (void) id;
+    printf("out of bounds access at %s:%" PRIu32 ":%" PRIu32"\n",
+           data->location.filename, data->location.line, data->location.column);
+    abort();
+}
