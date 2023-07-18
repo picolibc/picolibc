picolibc/picolibc

commit 42faed412857d4a0069d982d5c8cfe138b7a0197

* thread.h (class pthread): Add bool member canceled. 	* thread.cc (pthread::pthread): Initialize canceled to false. 	(pthread::cancel): Set canceled before setting cancel_event. 	(pthread::testcancel): Check for canceled.  Only wait for cancel_event 	if canceled is true.  Explain why. 	(pthread::_fixup_after_fork): Set canceled to false.

winsup/cygwin/thread.cc
@@ -378,7 +378,7 @@ List<pthread> pthread::threads;
 
 /* member methods */
 pthread::pthread ():verifyable_object (PTHREAD_MAGIC), win32_obj_id (0),
-		    valid (false), suspended (false),
+		    valid (false), suspended (false), canceled (false),
 		    cancelstate (0), canceltype (0), cancel_event (0),
 		    joiner (NULL), next (NULL), cleanup_stack (NULL)
 {
@@ -538,6 +538,7 @@ pthread::cancel ()
     {
       // cancel deferred
       mutex.unlock ();
+      canceled = true;
       SetEvent (cancel_event);
       return 0;
     }
@@ -871,8 +872,16 @@ pthread::testcancel ()
   if (cancelstate == PTHREAD_CANCEL_DISABLE)
     return;
 
-  if (IsEventSignalled (cancel_event))
-    cancel_self ();
+  /* We check for the canceled flag first.  This allows to use the
+     pthread_testcancel function a lot without adding the overhead of
+     an OS call.  Only if the thread is marked as canceled, we wait for
+     cancel_event being really set, on the off-chance that pthread_cancel
+     gets interrupted before calling SetEvent. */
+  if (canceled)
+    {
+      WaitForSingleObject (cancel_event, INFINITE);
+      cancel_self ();
+    }
 }
 
 void
@@ -1039,6 +1048,7 @@ pthread::_fixup_after_fork ()
       magic = 0;
       valid = false;
       win32_obj_id = NULL;
+      canceled = false;
       cancel_event = NULL;
     }
 }

winsup/cygwin/thread.h
@@ -366,6 +366,7 @@ public:
   void *return_ptr;
   bool valid;
   bool suspended;
+  bool canceled;
   int cancelstate, canceltype;
   _cygtls *cygtls;
   HANDLE cancel_event;

commit a02144808c5bc4e9753bfeabf06313c6413e1cd3

Cygwin: don't wait infinitely on a pthread cancel event
Starting with commit 42faed412857 ("* thread.h (class pthread): Add bool
member canceled."), pthread::testcancel waits infinitely on cancel_event
after it checked if the canceled variable is set.  However, this might
introduce a deadlock, if the thread calling pthread_cancel is terminated
after setting canceled to true, but before calling SetEvent on cancel_event.

In fact, it's not at all necessary to wait infinitely.  By definition,
the thread is only canceled if cancel_event is set.  The canceled
variable is just a helper to speed up code.  We can safely assume that
the thread hasn't been canceled yet, if canceled is set, but cancel_event
isn't.

Fixes: 42faed412857 ("* thread.h (class pthread): Add bool member canceled.")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/thread.cc
@@ -962,12 +962,9 @@ pthread::testcancel ()
      pthread_testcancel function a lot without adding the overhead of
      an OS call.  Only if the thread is marked as canceled, we wait for
      cancel_event being really set, on the off-chance that pthread_cancel
-     gets interrupted before calling SetEvent. */
-  if (canceled)
-    {
-      WaitForSingleObject (cancel_event, INFINITE);
-      cancel_self ();
-    }
+     gets interrupted or terminated before calling SetEvent. */
+  if (canceled && IsEventSignalled (cancel_event))
+    cancel_self ();
 }
 
 /* Return cancel event handle if it exists *and* cancel is not disabled.