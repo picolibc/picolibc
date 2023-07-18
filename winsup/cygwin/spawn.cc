picolibc/picolibc

commit 3cb9da14617c58c2821c80d48f0bd80a2deb5fdf

* spawn.cc (system_call_cleanup): Rename from pthread_cleanup.  Extend functionality. (system_call_cleanup::system_call_cleanup): Set up signals like system() requires.  Unblock previously-blocked signal handling. (system_call_cleanup::~system_call_cleanup): Restore signal handling after system(). (child_info_spawn::worker): Put signals on hold and use system_call_cleanup class to set and restore signals rather than doing it prior to to running the program.  Remove the ill-conceived pthread_cleanup stuff.

winsup/cygwin/spawn.cc
@@ -234,26 +234,37 @@ iscmd (const char *argv0, const char *what)
 	 (n == 0 || isdirsep (argv0[n - 1]));
 }
 
-struct pthread_cleanup
+struct system_call_cleanup
 {
   _sig_func_ptr oldint;
   _sig_func_ptr oldquit;
   sigset_t oldmask;
-  pthread_cleanup (): oldint (NULL), oldquit (NULL), oldmask ((sigset_t) -1) {}
-};
-
-static void
-do_cleanup (void *args)
-{
-# define cleanup ((pthread_cleanup *) args)
-  if (cleanup->oldmask != (sigset_t) -1)
-    {
-      signal (SIGINT, cleanup->oldint);
-      signal (SIGQUIT, cleanup->oldquit);
-      sigprocmask (SIG_SETMASK, &(cleanup->oldmask), NULL);
-    }
+  system_call_cleanup (bool issystem)
+  {
+    if (!issystem)
+      oldint = (_sig_func_ptr) -1;
+    else
+      {
+	sigset_t child_block;
+	oldint = signal (SIGINT,  SIG_IGN);
+	oldquit = signal (SIGQUIT, SIG_IGN);
+	sigemptyset (&child_block);
+	sigaddset (&child_block, SIGCHLD);
+	sigprocmask (SIG_BLOCK, &child_block, &oldmask);
+	sig_send (NULL, __SIGNOHOLD);
+      }
+  }
+  ~system_call_cleanup ()
+  {
+    if (oldmask != (sigset_t) -1)
+      {
+	signal (SIGINT, oldint);
+	signal (SIGQUIT, oldquit);
+	sigprocmask (SIG_SETMASK, &oldmask, NULL);
+      }
+  }
 # undef cleanup
-}
+};
 
 child_info_spawn NO_COPY ch_spawn;
 
@@ -295,17 +306,8 @@ child_info_spawn::worker (const char *prog_arg, const char *const *argv,
     }
 
   /* FIXME: There is a small race here and FIXME: not thread safe! */
-  pthread_cleanup cleanup;
   if (mode == _P_SYSTEM)
-    {
-      sigset_t child_block;
-      cleanup.oldint = signal (SIGINT, SIG_IGN);
-      cleanup.oldquit = signal (SIGQUIT, SIG_IGN);
-      sigemptyset (&child_block);
-      sigaddset (&child_block, SIGCHLD);
-      sigprocmask (SIG_BLOCK, &child_block, &cleanup.oldmask);
-    }
-  pthread_cleanup_push (do_cleanup, (void *) &cleanup);
+    sig_send (NULL, __SIGHOLD);
   av newargv;
   linebuf one_line;
   PWCHAR envblock = NULL;
@@ -739,6 +741,8 @@ loop:
       goto out;
     }
 
+  system_call_cleanup (mode == _P_SYSTEM);
+
   /* The CREATE_SUSPENDED case is handled below */
   if (iscygwin () && !(c_flags & CREATE_SUSPENDED))
     strace.write_childpid (pi.dwProcessId);
@@ -880,7 +884,6 @@ out:
   this->cleanup ();
   if (envblock)
     free (envblock);
-  pthread_cleanup_pop (1);
   return (int) res;
 }
 

commit 89e80d414b9d4ec71c6d43503704f7b3da3a2407

Cygwin: Restore signal handlers on thread cancellation during system()
Add back the restoration of signal handlers modified during system() on
thread cancellation.

Removed in 3cb9da14 which describes it as 'ill-conceived' (additional
context doesn't appear to be available).

We use the internal implementation helpers for the pthread cleanup
chain, so we can neatly tuck it inside the object, and keep the point
when we restore the signal handlers the same. (The
pthread_cleanup_push/pop() functions are implemented as macros which
must appear in the same lexical scope.)

Fixes: 3cb9da14617c ("Put signals on hold and use system_call_cleanup
class to set and restore signals rather than doing it prior to to
running the program.  Remove the ill-conceived pthread_cleanup stuff.")
Signed-off-by: Jon Turney <jon.turney@dronecode.org.uk>

winsup/cygwin/spawn.cc
@@ -228,6 +228,8 @@ struct system_call_handle
   _sig_func_ptr oldint;
   _sig_func_ptr oldquit;
   sigset_t oldmask;
+  __pthread_cleanup_handler cleanup_handler;
+
   bool is_system_call ()
   {
     return oldint != ILLEGAL_SIG_FUNC_PTR;
@@ -253,18 +255,27 @@ struct system_call_handle
 	sigaddset (&child_block, SIGCHLD);
 	sigprocmask (SIG_BLOCK, &child_block, &oldmask);
 	sig_send (NULL, __SIGNOHOLD);
+
+	cleanup_handler = { system_call_handle::cleanup, this, NULL };
+	_pthread_cleanup_push (&cleanup_handler);
       }
   }
   ~system_call_handle ()
   {
     if (is_system_call ())
+      _pthread_cleanup_pop (1);
+  }
+  static void cleanup (void *arg)
+  {
+# define this_ ((system_call_handle *) arg)
+    if (this_->is_system_call ())
       {
-	signal (SIGINT, oldint);
-	signal (SIGQUIT, oldquit);
-	sigprocmask (SIG_SETMASK, &oldmask, NULL);
+	signal (SIGINT, this_->oldint);
+	signal (SIGQUIT, this_->oldquit);
+	sigprocmask (SIG_SETMASK, &(this_->oldmask), NULL);
       }
   }
-# undef cleanup
+# undef this_
 };
 
 child_info_spawn NO_COPY ch_spawn;