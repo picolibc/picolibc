picolibc/picolibc

commit 73151c54d58148eed83117dfa69f9da404fe53ac

* syscalls.cc (stat_worker): Don't call build_fh_pc with invalid pc.

winsup/cygwin/syscalls.cc
@@ -1202,22 +1202,23 @@ int __stdcall
 stat_worker (path_conv &pc, struct __stat64 *buf)
 {
   int res = -1;
-  fhandler_base *fh = NULL;
 
   myfault efault;
   if (efault.faulted (EFAULT))
     goto error;
 
-  if (!(fh = build_fh_pc (pc)))
-    goto error;
-
-  if (fh->error ())
+  if (pc.error)
     {
-      debug_printf ("got %d error from build_fh_name", fh->error ());
-      set_errno (fh->error ());
+      debug_printf ("got %d error from build_fh_name", pc.error);
+      set_errno (pc.error);
     }
-  else if (fh->exists ())
+  else if (pc.exists ())
     {
+      fhandler_base *fh;
+
+      if (!(fh = build_fh_pc (pc)))
+	goto error;
+
       debug_printf ("(%s, %p, %p), file_attributes %d",
 		    pc.normalized_path, buf, fh, (DWORD) *fh);
       memset (buf, 0, sizeof (*buf));
@@ -1231,14 +1232,14 @@ stat_worker (path_conv &pc, struct __stat64 *buf)
 	  if (!buf->st_rdev)
 	    buf->st_rdev = buf->st_dev;
 	}
+      delete fh;
     }
   else
     set_errno (ENOENT);
 
-  delete fh;
  error:
   MALLOC_CHECK;
-  syscall_printf ("%d = (%s, %p)", res, pc.normalized_path, buf);
+  syscall_printf ("%d = (%s, %p)", res, pc.normalized_path ?: "", buf);
   return res;
 }
 

commit 42b44044b34d7c26abf84f8b24f820e04e1662f2

Cygwin: Fix Windows file handle leak in stat("file", -1)
Don't leak a Windows file handle if stat() is called with a valid
filename, but invalid stat buffer pointer.

We do not destroy fh (which closes a Windows handle it has opened) if an
exception happens in the __try block.

Avoid this by re-ordering things so that we don't construct the fhandler
object until after we've attempted to use the struct stat buffer.

Fixes: 73151c54d581 ("syscalls.cc (stat_worker): Don't call build_fh_pc with invalid pc.")
Signed-off-by: Jon Turney <jon.turney@dronecode.org.uk>

winsup/cygwin/syscalls.cc
@@ -1967,12 +1967,13 @@ stat_worker (path_conv &pc, struct stat *buf)
 	{
 	  fhandler_base *fh;
 
-	  if (!(fh = build_fh_pc (pc)))
-	    __leave;
-
 	  debug_printf ("(%S, %p, %p), file_attributes %d",
 			pc.get_nt_native_path (), buf, fh, (DWORD) *fh);
 	  memset (buf, 0, sizeof (*buf));
+
+	  if (!(fh = build_fh_pc (pc)))
+	    __leave;
+
 	  res = fh->fstat (buf);
 	  if (!res)
 	    fh->stat_fixup (buf);