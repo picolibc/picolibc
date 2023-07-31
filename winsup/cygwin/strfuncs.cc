picolibc/picolibc

commit 5da71b6059956a8f20a6be02e82867aa28aa3880

Cygwin: add support for GB18030 codeset
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/strfuncs.cc
@@ -245,7 +245,8 @@ mbsnrtowci(wint_t *dst, const char **src, size_t nms, size_t len, mbstate_t *ps)
    eucJP, the both most used Japanese charset encodings, this shouldn't
    be such a big problem. */
 
-/* GBK, eucKR, and Big5 conversions are not available so far in newlib. */
+/* GBK, GB18030, eucKR, and Big5 conversions are not available so far
+   in newlib. */
 
 static int
 __db_wctomb (struct _reent *r, char *s, wchar_t wchar, UINT cp)
@@ -323,6 +324,12 @@ extern "C" int
 __gbk_wctomb (struct _reent *r, char *s, wchar_t wchar, mbstate_t *state)
 {
   return __db_wctomb (r,s, wchar, 936);
+}
+
+extern "C" int
+__gb18030_wctomb (struct _reent *r, char *s, wchar_t wchar, mbstate_t *state)
+{
+  return __db_wctomb (r,s, wchar, 54936);
 }
 
 extern "C" int
@@ -480,6 +487,13 @@ __gbk_mbtowc (struct _reent *r, wchar_t *pwc, const char *s, size_t n,
 	      mbstate_t *state)
 {
   return __db_mbtowc (r, pwc, s, n, 936, state);
+}
+
+extern "C" int
+__gb18030_mbtowc (struct _reent *r, wchar_t *pwc, const char *s, size_t n,
+		  mbstate_t *state)
+{
+  return __db_mbtowc (r, pwc, s, n, 54936, state);
 }
 
 extern "C" int

newlib/libc/locale/locale.c
@@ -65,8 +65,8 @@ build with multibyte support and support for all ISO and Windows Codepage.
 Otherwise all singlebyte charsets are simply mapped to ASCII.  Right now,
 only newlib for Cygwin is built with full charset support by default.
 Under Cygwin, this implementation additionally supports the charsets
-<<"GBK">>, <<"GB2312">>, <<"eucCN">>, <<"eucKR">>, and <<"Big5">>.  Cygwin
-does not support <<"JIS">>.
+<<"GB18030">>, <<"GBK">>, <<"GB2312">>, <<"eucCN">>, <<"eucKR">>, and
+<<"Big5">>.  Cygwin does not support <<"JIS">>.
 
 Cygwin additionally supports locales from the file
 /usr/share/locale/locale.alias.
@@ -657,7 +657,7 @@ restart:
 	}
 #ifdef __CYGWIN__
       /* Newlib does neither provide EUC-KR nor EUC-CN, and Cygwin's
-      	 implementation requires Windows support. */
+	 implementation requires Windows support. */
       else if (!strcasecmp (c, "KR"))
 	{
 	  strcpy (charset, "EUCKR");
@@ -817,11 +817,18 @@ restart:
 	 requires Windows support. */
       if (!strcasecmp (charset, "GBK")
 	  || !strcasecmp (charset, "GB2312"))
-      	{
+	{
 	  strcpy (charset, charset[2] == '2' ? "GB2312" : "GBK");
 	  mbc_max = 2;
 	  l_wctomb = __gbk_wctomb;
 	  l_mbtowc = __gbk_mbtowc;
+	}
+      else if (!strcasecmp (charset, "GB18030"))
+	{
+	  strcpy (charset, "GB18030");
+	  mbc_max = 4;
+	  l_wctomb = __gb18030_wctomb;
+	  l_mbtowc = __gb18030_mbtowc;
 	}
       else
 #endif /* __CYGWIN__ */

newlib/libc/stdlib/local.h
@@ -24,6 +24,7 @@ wctomb_p __iso_wctomb (int val);
 wctomb_p __cp_wctomb (int val);
 #ifdef __CYGWIN__
 wctomb_f __gbk_wctomb;
+wctomb_f __gb18030_wctomb;
 wctomb_f __kr_wctomb;
 wctomb_f __big5_wctomb;
 #endif
@@ -45,6 +46,7 @@ mbtowc_p __iso_mbtowc (int val);
 mbtowc_p __cp_mbtowc (int val);
 #ifdef __CYGWIN__
 mbtowc_f __gbk_mbtowc;
+mbtowc_f __gb18030_mbtowc;
 mbtowc_f __kr_mbtowc;
 mbtowc_f __big5_mbtowc;
 #endif

winsup/cygwin/fhandler/proc.cc
@@ -2323,6 +2323,7 @@ format_proc_codesets (void *, char *&destbuf)
 			 "EUC-CN\n"
 			 "EUC-JP\n"
 			 "EUC-KR\n"
+			 "GB18030\n"
 			 "GB2312\n"
 			 "GBK\n"
 			 "GEORGIAN-PS\n"

winsup/cygwin/nlsfuncs.cc
@@ -1578,8 +1578,8 @@ __eval_codepage_from_internal_charset ()
 	  break;
 	}
       break;
-    case 'G': /* GBK/GB2312 */
-      codepage = 936;
+    case 'G': /* GBK/GB2312/GB18030 */
+      codepage = (charset[2] == '1') ? 54936 : 936;
       break;
     case 'I': /* ISO-8859-x */
       codepage = strtoul (charset + 9, NULL, 10) + 28590;

winsup/cygwin/release/3.5.0
@@ -21,3 +21,5 @@ What's new:
   supported codesets and locales for all interested parties.  Locale(1)
   opens these files and uses the info for printing locale info like any
   other process could do.
+
+- Add support for GB18030 codeset.

winsup/doc/new-features.xml
@@ -41,6 +41,10 @@ files and uses the info for printing locale info like any other process
 could do.
 </para></listitem>
 
+<listitem><para>
+Add support for GB18030 codeset.
+</para></listitem>
+
 </itemizedlist>
 
 </sect2>

commit 68bb3deabd0381465e18d41ae7f722cfe95ba3c8

Cygwin: fix GB18030 support
The changes to support GB18030 were insufficient and the underlying
Windows conversion functions just failed. Fix how the Windows functions
are called for GB18030.

Fixes: 5da71b605995 ("Cygwin: add support for GB18030 codeset")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/strfuncs.cc
@@ -331,7 +331,55 @@ __gbk_wctomb (struct _reent *r, char *s, wchar_t wchar, mbstate_t *state)
 extern "C" int
 __gb18030_wctomb (struct _reent *r, char *s, wchar_t wchar, mbstate_t *state)
 {
-  return __db_wctomb (r,s, wchar, 54936);
+  int ret;
+  wchar_t wres[2];
+
+  if (s == NULL)
+    return 0;
+
+  if (state->__count == 0)
+    {
+      if (wchar <= 0x7f)
+	{
+	  *s = (char) wchar;
+	  return 1;
+	}
+
+      if (wchar >= 0xd800 && wchar <= 0xdbff)
+	{
+	  /* First half of a surrogate pair */
+	  state->__count = 18030;
+	  state->__value.__wch = wchar;
+	  return 0;
+	}
+      ret = WideCharToMultiByte (54936, WC_ERR_INVALID_CHARS, &wchar, 1, s,
+				 4, NULL, NULL);
+      if (ret > 0)
+	return ret;
+      goto ilseq;
+    }
+  else if (state->__count == 18030 && state->__value.__wch >= 0xd800
+	   && state->__value.__wch <= 0xdbff)
+    {
+      if (wchar >= 0xdc00 && wchar <= 0xdfff)
+	{
+	  /* Create multibyte sequence from full surrogate pair. */
+	  wres[0] = state->__value.__wch;
+	  wres[1] = wchar;
+	  ret = WideCharToMultiByte (54936, WC_ERR_INVALID_CHARS, wres, 2, s, 4,
+				     NULL, NULL);
+	  if (ret > 0)
+	    {
+	      state->__count = 0;
+	      return ret;
+	    }
+	}
+ilseq:
+      _REENT_ERRNO(r) = EILSEQ;
+      return -1;
+    }
+  _REENT_ERRNO(r) = EINVAL;
+  return -1;
 }
 
 extern "C" int
@@ -495,7 +543,105 @@ extern "C" int
 __gb18030_mbtowc (struct _reent *r, wchar_t *pwc, const char *s, size_t n,
 		  mbstate_t *state)
 {
-  return __db_mbtowc (r, pwc, s, n, 54936, state);
+  wchar_t wres[2], dummy;
+  unsigned char ch;
+  int ret, len, ocount;
+  size_t ncopy;
+
+  if (state->__count < 0 || (state->__count > (int) sizeof state->__value.__wchb
+			     && state->__count != 18030))
+    {
+      errno = EINVAL;
+      return -1;
+    }
+
+  if (s == NULL)
+    {
+      s = "";
+      n = 1;
+      pwc = NULL;
+    }
+
+  if (state->__count == 18030)
+    {
+      /* Return second half of the surrogate pair */
+      *pwc = state->__value.__wch;
+      state->__count = 0;
+      return 1;
+    }
+
+  ncopy = MIN (MIN (n, MB_CUR_MAX),
+	       sizeof state->__value.__wchb - state->__count);
+  memcpy (state->__value.__wchb + state->__count, s, ncopy);
+  ocount = state->__count;
+  state->__count += ncopy;
+  s = (char *) state->__value.__wchb;
+  n = state->__count;
+
+  if (n == 0) /* Incomplete multibyte sequence */
+    return -2;
+
+  if (!pwc)
+    pwc = &dummy;
+
+  /* Check if input is a valid GB18030 char (per FreeBSD):
+   * Single byte:         [00-7f]
+   * Two byte:            [81-fe][40-7e,80-fe]
+   * Four byte:           [81-fe][30-39][81-fe][30-39]
+   */
+  ch = *(unsigned char *) s;
+  if (ch <= 0x7f)
+    {
+      *pwc = ch;
+      state->__count = 0;
+      return ch ? 1 : 0;
+    }
+  if (ch >= 0x81 && ch <= 0xfe)
+    {
+      if (n < 2)
+	return -2;
+      ch = (unsigned char) s[1];
+      if ((ch >= 0x40 && ch <= 0x7e) || (ch >= 0x80 && ch <= 0xfe))
+	len = 2;
+      else if (ch >= 0x30 && ch <= 0x39)
+	{
+	  if (n < 3)
+	    return -2;
+	  ch = (unsigned char) s[2];
+	  if (ch < 0x81 || ch > 0xfe)
+	    goto ilseq;
+	  if (n < 4)
+	    return -2;
+	  ch = (unsigned char) s[3];
+	  if (ch < 0x30 || ch > 0x39)
+	    goto ilseq;
+	  len = 4;
+	}
+      else
+	goto ilseq;
+    }
+  else
+    goto ilseq;
+  ret = MultiByteToWideChar (54936, MB_ERR_INVALID_CHARS, s, len, wres, 2);
+  if (ret)
+    {
+      *pwc = wres[0];
+      if (ret == 2)
+	{
+	  /* Surrogate pair. Store second half for later and return
+	     first half. Return real count - 1, return 1 when the second
+	     half of the pair is returned in the next run. */
+	  state->__count = 18030;
+	  state->__value.__wch = wres[1];
+	  --len;
+	}
+      else
+	state->__count = 0;
+      return len - ocount;
+    }
+ilseq:
+  _REENT_ERRNO(r) = EILSEQ;
+  return -1;
 }
 
 extern "C" int