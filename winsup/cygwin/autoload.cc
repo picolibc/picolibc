picolibc/picolibc

commit 70e476d27be8e49146c49e8d6e1319100b84d5eb

2006-07-25  Corinna Vinschen  <corinna@vinschen.de>
	* include/cygwin/version.h: Bump DLL version to 1.7.0.

2006-07-25  Corinna Vinschen  <corinna@vinschen.de>

	* select.h: Remove.
	* fhandler_socket.cc: Don't include select.h.
	* select.cc: Ditto.

2006-07-25  Corinna Vinschen  <corinna@vinschen.de>

	* cygtls.h: Drop socket related includes.
	(struct _local_storage): Remove exitsock and exitsock_sin. Add
	select_sockevt.
	* cygtls.cc: Accomodate above change throughout.
	* fhandler.h (class fhandler_socket): Make wsock_evt public.
	* fhandler_socket.cc (fhandler_socket::fhandler_socket): Accomodate
	reordering members.
	(fhandler_socket::evaluate_events): Drop FD_CONNECT event as soon as
	it gets read once.  Never remove FD_WRITE event here.
	(fhandler_socket::wait_for_events): Wait 50 ms instead of INFINITE for
	socket events.
	(fhandler_socket::accept): Fix conditional.  Set wsock_events members
	of accepted socket to useful start values.
	(fhandler_socket::recv_internal): Always drop FD_READ/FD_OOB events from
	wsock_events after the call to WSARecvFrom.
	(fhandler_socket::send_internal): Drop FD_WRITE event from wsock_events
	if the call to WSASendTo fails with WSAEWOULDBLOCK.  Fix return value
	condition.
	* select.cc (struct socketinf): Change to accomodate using socket event
	handling.
	(peek_socket): Use event handling for peeking socket.
	(thread_socket): Ditto.
	(start_thread_socket): Ditto.
	(socket_cleanup): Same here.
	* tlsoffsets.h: Regenerate.

2006-07-20  Corinna Vinschen  <corinna@vinschen.de>

	* fhandler.h (class fhandler_socket): Rearrange slightly to keep
	event handling methods and members together.  Drop owner status flag.
	Split wait method.  Rename event handling methods for readability.
	* fhandler_socket.cc (struct wsa_event): Add owner field.
	(LOCK_EVENTS): New macro.
	(UNLOCK_EVENTS): Ditto.
	(fhandler_socket::init_events): rename from prepare.
	(fhandler_socket::evaluate_events): First half of former wait method.

winsup/cygwin/autoload.cc
@@ -370,7 +370,10 @@ LoadDLLfunc (SetSecurityDescriptorOwner, 12, advapi32)
 LoadDLLfunc (SetTokenInformation, 16, advapi32)
 LoadDLLfunc (RegGetKeySecurity, 16, advapi32)
 
+/* 127 == ERROR_PROC_NOT_FOUND */
+LoadDLLfuncEx2 (DsGetDcNameA, 24, netapi32, 1, 127)
 LoadDLLfunc (NetApiBufferFree, 4, netapi32)
+LoadDLLfuncEx (NetGetAnyDCName, 12, netapi32, 1)
 LoadDLLfuncEx (NetGetDCName, 12, netapi32, 1)
 LoadDLLfunc (NetLocalGroupEnum, 28, netapi32)
 LoadDLLfunc (NetLocalGroupGetMembers, 32, netapi32)
@@ -455,8 +458,6 @@ LoadDLLfunc (SetClipboardData, 8, user32)
 LoadDLLfunc (SetProcessWindowStation, 4, user32)
 LoadDLLfunc (SetTimer, 16, user32)
 LoadDLLfunc (SetUserObjectSecurity, 12, user32)
-
-LoadDLLfunc (rcmd, 24, wsock32)
 
 LoadDLLfunc (accept, 12, ws2_32)
 LoadDLLfunc (bind, 12, ws2_32)

winsup/cygwin/select.cc
@@ -30,7 +30,6 @@ details. */
 #define USE_SYS_TYPES_FD_SET
 #include <winsock.h>
 #include "cygerrno.h"
-#include "select.h"
 #include "security.h"
 #include "path.h"
 #include "fhandler.h"
@@ -1260,105 +1259,69 @@ fhandler_base::select_except (select_record *s)
   return s;
 }
 
-struct socketinf
-  {
-    cygthread *thread;
-    winsock_fd_set readfds, writefds, exceptfds;
-    SOCKET exitsock;
-    select_record *start;
-  };
-
 static int
 peek_socket (select_record *me, bool)
 {
-  winsock_fd_set ws_readfds, ws_writefds, ws_exceptfds;
-  struct timeval tv = {0, 0};
-  WINSOCK_FD_ZERO (&ws_readfds);
-  WINSOCK_FD_ZERO (&ws_writefds);
-  WINSOCK_FD_ZERO (&ws_exceptfds);
-
-  HANDLE h;
-  set_handle_or_return_if_not_open (h, me);
-  select_printf ("considering handle %p", h);
-
-  if (me->read_selected && !me->read_ready)
+  fhandler_socket *fh = (fhandler_socket *) me->fh;
+  long events;
+  long evt_mask = (FD_CLOSE
+		   | (me->read_selected ? (FD_READ | FD_ACCEPT) : 0)
+		   | (me->write_selected ? (FD_WRITE | FD_CONNECT) : 0)
+		   | (me->except_selected ? (FD_OOB | FD_CONNECT) : 0));
+  int ret = fh->evaluate_events (evt_mask, events, false);
+  if (me->read_selected)
+    me->read_ready |= !!(events & (FD_READ | FD_ACCEPT | FD_CLOSE));
+  if (me->write_selected)
     {
-      select_printf ("adding read fd_set %s, fd %d", me->fh->get_name (),
-		     me->fd);
-      WINSOCK_FD_SET (h, &ws_readfds);
-    }
-  if (me->write_selected && !me->write_ready)
-    {
-      select_printf ("adding write fd_set %s, fd %d", me->fh->get_name (),
-		     me->fd);
-      WINSOCK_FD_SET (h, &ws_writefds);
-    }
-  if ((me->except_selected || me->except_on_write) && !me->except_ready)
-    {
-      select_printf ("adding except fd_set %s, fd %d", me->fh->get_name (),
-		     me->fd);
-      WINSOCK_FD_SET (h, &ws_exceptfds);
-    }
-  int r;
-  if ((me->read_selected && !me->read_ready)
-      || (me->write_selected && !me->write_ready)
-      || ((me->except_selected || me->except_on_write) && !me->except_ready))
-    {
-      r = WINSOCK_SELECT (0, &ws_readfds, &ws_writefds, &ws_exceptfds, &tv);
-      select_printf ("WINSOCK_SELECT returned %d", r);
-      if (r == -1)
-	{
-	  select_printf ("error %d", WSAGetLastError ());
-	  set_winsock_errno ();
-	  return 0;
-	}
-      if (WINSOCK_FD_ISSET (h, &ws_readfds))
-	me->read_ready = true;
-      if (WINSOCK_FD_ISSET (h, &ws_writefds))
+      if ((events & FD_CONNECT) && !ret)
 	me->write_ready = true;
-      if (WINSOCK_FD_ISSET (h, &ws_exceptfds))
-	me->except_ready = true;
+      else
+	me->write_ready |= !!(events & (FD_WRITE | FD_CLOSE));
     }
+  if (me->except_selected)
+    me->except_ready |= ret || !!(events & (FD_OOB | FD_CLOSE));
+
   return me->read_ready || me->write_ready || me->except_ready;
 }
 
 static int start_thread_socket (select_record *, select_stuff *);
 
+struct socketinf
+  {
+    cygthread *thread;
+    int num_w4;
+    HANDLE w4[MAXIMUM_WAIT_OBJECTS];
+    select_record *start;
+  };
+
 static DWORD WINAPI
 thread_socket (void *arg)
 {
   socketinf *si = (socketinf *) arg;
+  bool event = false;
 
-  select_printf ("stuff_start %p", &si->start);
-  int r = WINSOCK_SELECT (0, &si->readfds, &si->writefds, &si->exceptfds, NULL);
-  select_printf ("Win32 select returned %d", r);
-  if (r == -1)
-    select_printf ("error %d", WSAGetLastError ());
-  select_record *s = si->start;
-  while ((s = s->next))
-    if (s->startup == start_thread_socket)
-	{
-	  HANDLE h = s->fh->get_handle ();
-	  select_printf ("s %p, testing fd %d (%s)", s, s->fd, s->fh->get_name ());
-	  if (WINSOCK_FD_ISSET (h, &si->readfds))
+  select_printf ("stuff_start %p", si->start);
+  while (!event)
+    {
+      for (select_record *s = si->start; (s = s->next); )
+	if (s->startup == start_thread_socket)
+	  if (peek_socket (s, false))
+	    event = true;
+      if (!event)
+        {
+	  switch (WaitForMultipleObjects (si->num_w4, si->w4, FALSE, 50))
 	    {
-	      select_printf ("read_ready");
-	      s->read_ready = true;
-	    }
-	  if (WINSOCK_FD_ISSET (h, &si->writefds))
-	    {
-	      select_printf ("write_ready");
-	      s->write_ready = true;
-	    }
-	  if (WINSOCK_FD_ISSET (h, &si->exceptfds))
-	    {
-	      select_printf ("except_ready");
-	      s->except_ready = true;
+	    case WAIT_OBJECT_0:
+	    case WAIT_FAILED:
+	      goto out;
+	    case WAIT_TIMEOUT:
+	    default:
+	      break;
 	    }
 	}
-
-  if (WINSOCK_FD_ISSET (si->exitsock, &si->readfds))
-    select_printf ("saw exitsock read");
+    }
+out:
+  select_printf ("leaving thread_socket");
   return 0;
 }
 
@@ -1374,68 +1337,29 @@ start_thread_socket (select_record *me, select_stuff *stuff)
     }
 
   si = new socketinf;
-  WINSOCK_FD_ZERO (&si->readfds);
-  WINSOCK_FD_ZERO (&si->writefds);
-  WINSOCK_FD_ZERO (&si->exceptfds);
   select_record *s = &stuff->start;
+  if (_my_tls.locals.select_sockevt != INVALID_HANDLE_VALUE)
+    si->w4[0] = _my_tls.locals.select_sockevt;
+  else if (!(si->w4[0] = CreateEvent (&sec_none_nih, TRUE, FALSE, NULL)))
+    return 1;
+  else
+    _my_tls.locals.select_sockevt = si->w4[0];
+  si->num_w4 = 1;
   while ((s = s->next))
     if (s->startup == start_thread_socket)
       {
-	HANDLE h = s->fh->get_handle ();
-	select_printf ("Handle %p", h);
-	if (s->read_selected && !s->read_ready)
-	  {
-	    WINSOCK_FD_SET (h, &si->readfds);
-	    select_printf ("Added to readfds");
-	  }
-	if (s->write_selected && !s->write_ready)
-	  {
-	    WINSOCK_FD_SET (h, &si->writefds);
-	    select_printf ("Added to writefds");
-	  }
-	if ((s->except_selected || s->except_on_write) && !s->except_ready)
-	  {
-	    WINSOCK_FD_SET (h, &si->exceptfds);
-	    select_printf ("Added to exceptfds");
-	  }
+	HANDLE evt = ((fhandler_socket *) me->fh)->wsock_evt;
+	/* No event/socket should show up multiple times. */
+	for (int i = 1; i < si->num_w4; ++i)
+	  if (si->w4[i] == evt)
+	    goto continue_outer_loop;
+	if (si->num_w4 < MAXIMUM_WAIT_OBJECTS)
+	  si->w4[si->num_w4++] = evt;
+	else /* for now */
+	  goto err;
+      continue_outer_loop:
+	;
       }
-
-  if (_my_tls.locals.exitsock != INVALID_SOCKET)
-    si->exitsock = _my_tls.locals.exitsock;
-  else
-    {
-      si->exitsock = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-      if (si->exitsock == INVALID_SOCKET)
-	{
-	  set_winsock_errno ();
-	  select_printf ("cannot create socket, %E");
-	  return 0;
-	}
-      int sin_len = sizeof (_my_tls.locals.exitsock_sin);
-      memset (&_my_tls.locals.exitsock_sin, 0, sin_len);
-      _my_tls.locals.exitsock_sin.sin_family = AF_INET;
-      _my_tls.locals.exitsock_sin.sin_addr.s_addr = htonl (INADDR_LOOPBACK);
-      if (bind (si->exitsock, (struct sockaddr *) &_my_tls.locals.exitsock_sin, sin_len) < 0)
-	{
-	  select_printf ("cannot bind socket %p, %E", si->exitsock);
-	  goto err;
-	}
-
-      if (getsockname (si->exitsock, (struct sockaddr *) &_my_tls.locals.exitsock_sin, &sin_len) < 0)
-	{
-	  select_printf ("getsockname error");
-	  goto err;
-	}
-      if (wincap.has_set_handle_information ())
-	SetHandleInformation ((HANDLE) si->exitsock, HANDLE_FLAG_INHERIT, 0);
-      /* else
-	   too bad? */
-      select_printf ("opened new socket %p", si->exitsock);
-      _my_tls.locals.exitsock = si->exitsock;
-    }
-
-  select_printf ("exitsock %p", si->exitsock);
-  WINSOCK_FD_SET ((HANDLE) si->exitsock, &si->readfds);
   stuff->device_specific_socket = (void *) si;
   si->start = &stuff->start;
   select_printf ("stuff_start %p", &stuff->start);
@@ -1444,8 +1368,7 @@ start_thread_socket (select_record *me, select_stuff *stuff)
   return 1;
 
 err:
-  set_winsock_errno ();
-  closesocket (si->exitsock);
+  CloseHandle (si->w4[0]);
   return 0;
 }
 
@@ -1456,17 +1379,10 @@ socket_cleanup (select_record *, select_stuff *stuff)
   select_printf ("si %p si->thread %p", si, si ? si->thread : NULL);
   if (si && si->thread)
     {
-      char buf[] = "";
-      int res = sendto (_my_tls.locals.exitsock, buf, 1, 0,
-			(sockaddr *) &_my_tls.locals.exitsock_sin,
-			sizeof (_my_tls.locals.exitsock_sin));
-      select_printf ("sent a byte to exitsock %p, res %d", _my_tls.locals.exitsock, res);
+      SetEvent (si->w4[0]);
       /* Wait for thread to go away */
       si->thread->detach ();
-      /* empty the socket */
-      select_printf ("reading a byte from exitsock %p", si->exitsock);
-      res = recv (si->exitsock, buf, 1, 0);
-      select_printf ("recv returned %d", res);
+      ResetEvent (si->w4[0]);
       stuff->device_specific_socket = NULL;
       delete si;
     }

winsup/cygwin/Makefile.in
@@ -133,11 +133,11 @@ DLL_OFILES:=assert.o autoload.o bsdlib.o ctype.o cxx.o cygheap.o cygthread.o \
 	fhandler_serial.o fhandler_socket.o fhandler_tape.o fhandler_termios.o \
 	fhandler_tty.o fhandler_virtual.o fhandler_windows.o fhandler_zero.o \
 	flock.o fnmatch.o fork.o fts.o ftw.o getopt.o glob.o grp.o heap.o \
-	hookapi.o inet_addr.o inet_network.o init.o ioctl.o ipc.o iruserok.o \
-	localtime.o lsearch.o malloc_wrapper.o memmem.o miscfuncs.o mktemp.o \
-	mmap.o msg.o net.o netdb.o nftw.o ntea.o passwd.o path.o pinfo.o pipe.o \
-	poll.o pthread.o regcomp.o regerror.o regexec.o regfree.o registry.o \
-	resource.o rexec.o scandir.o sched.o sec_acl.o sec_helper.o security.o \
+	hookapi.o inet_addr.o inet_network.o init.o ioctl.o ipc.o localtime.o \
+	lsearch.o malloc_wrapper.o memmem.o miscfuncs.o mktemp.o mmap.o msg.o \
+	net.o netdb.o nftw.o ntea.o passwd.o path.o pinfo.o pipe.o poll.o \
+	pthread.o regcomp.o regerror.o regexec.o regfree.o registry.o resource.o \
+	rexec.o rcmd.o scandir.o sched.o sec_acl.o sec_helper.o security.o \
 	select.o sem.o shared.o shm.o sigfe.o signal.o sigproc.o smallprint.o \
 	spawn.o strace.o strptime.o strsep.o strsig.o sync.o syscalls.o \
 	sysconf.o syslog.o termios.o thread.o timelocal.o timer.o times.o tty.o \

winsup/cygwin/cygtls.cc
@@ -106,7 +106,7 @@ _cygtls::init_thread (void *x, DWORD (*func) (void *, void *))
 
   thread_id = GetCurrentThreadId ();
   initialized = CYGTLS_INITIALIZED;
-  locals.exitsock = INVALID_SOCKET;
+  locals.select_sockevt = INVALID_HANDLE_VALUE;
   errno_addr = &(local_clib._errno);
 
   if ((void *) func == (void *) cygthread::stub
@@ -137,7 +137,7 @@ _cygtls::fixup_after_fork ()
       sig = 0;
     }
   stacklock = spinning = 0;
-  locals.exitsock = INVALID_SOCKET;
+  locals.select_sockevt = INVALID_HANDLE_VALUE;
   wq.thread_ev = NULL;
 }
 
@@ -152,7 +152,7 @@ void
 _cygtls::remove (DWORD wait)
 {
   initialized = 0;
-  if (!locals.exitsock || exit_state >= ES_FINAL)
+  if (!locals.select_sockevt || exit_state >= ES_FINAL)
     return;
 
   debug_printf ("wait %p", wait);
@@ -160,10 +160,10 @@ _cygtls::remove (DWORD wait)
     {
       /* FIXME: Need some sort of atthreadexit function to allow things like
 	 select to control this themselves. */
-      if (locals.exitsock != INVALID_SOCKET)
+      if (locals.select_sockevt != INVALID_HANDLE_VALUE)
 	{
-	  closesocket (locals.exitsock);
-	  locals.exitsock = (SOCKET) NULL;
+	  CloseHandle (locals.select_sockevt);
+	  locals.select_sockevt = (HANDLE) NULL;
 	}
       free_local (process_ident);
       free_local (ntoa_buf);

winsup/cygwin/cygtls.h
@@ -18,10 +18,6 @@ details. */
 #undef _NOMNTENT_FUNCS
 #include <setjmp.h>
 #include <exceptions.h>
-#ifndef _WINSOCK_H
-#include <netinet/in.h>
-typedef unsigned int SOCKET;
-#endif
 
 #define CYGTLS_INITIALIZED 0xc763173f
 
@@ -69,8 +65,7 @@ struct _local_storage
   char mnt_dir[CYG_MAX_PATH];
 
   /* select.cc */
-  SOCKET exitsock;
-  struct sockaddr_in exitsock_sin;
+  HANDLE select_sockevt;
 
   /* strerror */
   char strerror_buf[sizeof ("Unknown error 4294967295")];

winsup/cygwin/cygwin.din
@@ -11,6 +11,8 @@ __cygwin_user_data DATA
 _daylight DATA
 h_errno DATA
 _impure_ptr DATA
+in6addr_any DATA
+in6addr_loopback DATA
 __mb_cur_max DATA
 optarg DATA
 opterr DATA
@@ -145,6 +147,8 @@ _bcmp = bcmp NOSIGFE
 bcopy NOSIGFE
 _bcopy = bcopy NOSIGFE
 bind = cygwin_bind SIGFE
+bindresvport = cygwin_bindresvport SIGFE
+bindresvport_sa = cygwin_bindresvport_sa SIGFE
 bsearch NOSIGFE
 _bsearch = bsearch NOSIGFE
 btowc NOSIGFE
@@ -471,6 +475,7 @@ fread SIGFE
 _fread = fread SIGFE
 free SIGFE
 _free = free SIGFE
+freeaddrinfo = cygwin_freeaddrinfo SIGFE
 freopen SIGFE
 _freopen = freopen SIGFE
 _freopen64 = freopen64 SIGFE
@@ -524,6 +529,7 @@ funlockfile SIGFE
 futimes SIGFE
 fwrite SIGFE
 _fwrite = fwrite SIGFE
+gai_strerror = cygwin_gai_strerror NOSIGFE
 gamma NOSIGFE
 _gamma = gamma NOSIGFE
 gamma_r NOSIGFE
@@ -538,6 +544,7 @@ gcvtf SIGFE
 _gcvtf = gcvtf SIGFE
 get_osfhandle SIGFE
 _get_osfhandle = get_osfhandle SIGFE
+getaddrinfo = cygwin_getaddrinfo SIGFE
 getc SIGFE
 _getc = getc SIGFE
 getc_unlocked SIGFE
@@ -594,6 +601,7 @@ getmntent SIGFE
 _getmntent = getmntent SIGFE
 getmode SIGFE
 _getmode = getmode SIGFE
+getnameinfo = cygwin_getnameinfo SIGFE
 getopt SIGFE
 getopt_long SIGFE
 getopt_long_only SIGFE
@@ -714,6 +722,7 @@ _ioctl = ioctl SIGFE
 iprintf SIGFE
 _iprintf = iprintf SIGFE
 iruserok SIGFE
+iruserok_sa SIGFE
 isalnum NOSIGFE
 _isalnum = isalnum NOSIGFE
 isalpha NOSIGFE
@@ -967,6 +976,7 @@ poll SIGFE
 _poll = poll SIGFE
 popen SIGFE
 _popen = popen SIGFE
+posix_openpt SIGFE
 posix_regcomp SIGFE
 posix_regerror SIGFE
 posix_regexec SIGFE
@@ -1086,6 +1096,7 @@ _rand = rand NOSIGFE
 rand_r NOSIGFE
 random NOSIGFE
 rcmd = cygwin_rcmd SIGFE
+rcmd_af = cygwin_rcmd_af SIGFE
 read SIGFE
 _read = read SIGFE
 readdir SIGFE
@@ -1128,6 +1139,7 @@ _rmdir = rmdir SIGFE
 round NOSIGFE
 roundf NOSIGFE
 rresvport = cygwin_rresvport SIGFE
+rresvport_af = cygwin_rresvport_af SIGFE
 ruserok SIGFE
 sbrk SIGFE
 _sbrk = sbrk SIGFE

winsup/cygwin/exceptions.cc
@@ -526,12 +526,13 @@ _cygtls::handle_exceptions (EXCEPTION_RECORD *e, exception_list *frame, CONTEXT 
       break;
 
     case STATUS_ACCESS_VIOLATION:
-      switch (mmap_is_attached_or_noreserve_page (e->ExceptionInformation[1]))
+      switch (mmap_is_attached_or_noreserve ((void *)e->ExceptionInformation[1],
+					     1))
         {
-	case 2:		/* MAP_NORESERVE page, now commited. */
+	case MMAP_NORESERVE_COMMITED:
 	  return 0;
-	case 1:		/* MAP_NORESERVE page, commit failed, or
-			   access to mmap page beyond EOF. */
+	case MMAP_RAISE_SIGBUS:	/* MAP_NORESERVE page, commit failed, or
+				   access to mmap page beyond EOF. */
 	  si.si_signo = SIGBUS;
 	  si.si_code = BUS_OBJERR;
 	  break;

winsup/cygwin/fhandler.cc
@@ -223,8 +223,10 @@ fhandler_base::raw_read (void *ptr, size_t& ulen)
 
   HANDLE h = NULL;	/* grumble */
   int prio = 0;		/* ditto */
+  int try_noreserve = 1;
   DWORD len = ulen;
 
+retry:
   ulen = (size_t) -1;
   if (read_state)
     {
@@ -259,6 +261,20 @@ fhandler_base::raw_read (void *ptr, size_t& ulen)
 	      bytes_read = 0;
 	      break;
 	    }
+	  if (try_noreserve)
+	    {
+	      try_noreserve = 0;
+	      switch (mmap_is_attached_or_noreserve (ptr, len))
+		{
+		case MMAP_NORESERVE_COMMITED:
+		  goto retry;
+		case MMAP_RAISE_SIGBUS:
+		  raise(SIGBUS);
+		case MMAP_NONE:
+		  break;
+		}
+	    }
+	  /*FALLTHRU*/
 	case ERROR_INVALID_FUNCTION:
 	case ERROR_INVALID_PARAMETER:
 	case ERROR_INVALID_HANDLE:

commit dedbbd74d0a8f3b7dfae6188321703a47bb8a2b3

Cygwin: select: workaround FD_WRITE network event handling
The FD_WRITE event is a false friend.  It indicates ready to write
even if the next send fails with WSAEWOULDBLOCK.  *After* the fact,
FD_WRITE will be cleared until sending is again possible, but that
is too late for a select/write loop.

Workaround that by using the WinSock select function when peeking
at a socket and FD_WRITE gets indicated. WinSock select fortunately
indicates writability correctly.

Fixes: 70e476d27be8 ("(peek_socket): Use event handling for peeking socket.")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/autoload.cc
@@ -645,6 +645,7 @@ LoadDLLfunc (getsockname, ws2_32)
 LoadDLLfunc (getsockopt, ws2_32)
 LoadDLLfunc (ioctlsocket, ws2_32)
 LoadDLLfunc (listen, ws2_32)
+LoadDLLfunc_pfx_only (select, ws2_32)
 LoadDLLfunc (setsockopt, ws2_32)
 LoadDLLfunc (shutdown, ws2_32)
 LoadDLLfunc (socket, ws2_32)

winsup/cygwin/select.cc
@@ -1757,6 +1757,15 @@ fhandler_base::select_except (select_stuff *ss)
   return s;
 }
 
+struct wfd_set
+{
+  u_int   fd_count;
+  SOCKET  fd_array[1];
+};
+
+/* autoload exposes the Winsock select function only with a _win32_ prefix. */
+extern "C" int _win32_select(int, wfd_set *, wfd_set *, wfd_set *, TIMEVAL *);
+
 static int
 peek_socket (select_record *me, bool)
 {
@@ -1772,9 +1781,24 @@ peek_socket (select_record *me, bool)
   if (me->read_selected)
     me->read_ready |= ret || !!(events & (FD_READ | FD_ACCEPT | FD_CLOSE));
   if (me->write_selected)
-    /* Don't check for FD_CLOSE here.  Only an error case (ret == -1)
-       will set ready for writing. */
-    me->write_ready |= ret || !!(events & (FD_WRITE | FD_CONNECT));
+    {
+      /* The FD_WRITE event is a false friend. It indicates ready to write
+         even if the next send fails with WSAEWOULDBLOCK.  *After* the fact,
+	 FD_WRITE will be cleared until sending is again possible.
+	 For the time being, workaround that here by using the WinSock
+	 select function.  It indicates writability correctly. */
+      if (events & FD_WRITE)
+	{
+	  wfd_set w = { 1, { fh->get_socket () } };
+	  TIMEVAL t = { 0 };
+
+	  if (_win32_select (0, NULL, &w, NULL, &t) == 0)
+	    events &= ~FD_WRITE;
+	}
+      /* Don't check for FD_CLOSE here.  Only an error case (ret == -1)
+	 will set ready for writing. */
+      me->write_ready |= ret || !!(events & (FD_WRITE | FD_CONNECT));
+    }
   if (me->except_selected)
     me->except_ready |= !!(events & FD_OOB);
 