picolibc/picolibc

commit 93508e5bb841138911ed3dee3c92cc18be43d9ca

Cygwin: open_shared: don't reuse shared_locations parameter as output
For ages, open_shared uses the shared_locations parameter as
output to indicate if the mapping for a shared region has been
created or just opened.  Split this into two parameters.  Use
the shared_locations parameter as input only, return the creation
state of the mapping in a bool reference parameter.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/mm/shared.cc
@@ -127,54 +127,57 @@ void *
 open_shared (const WCHAR *name, int n, HANDLE& shared_h, DWORD size,
 	     shared_locations m, PSECURITY_ATTRIBUTES psa, DWORD access)
 {
-  return open_shared (name, n, shared_h, size, &m, psa, access);
+  bool created_dummy;
+  return open_shared (name, n, shared_h, size, m, created_dummy, psa, access);
 }
 
 void *
 open_shared (const WCHAR *name, int n, HANDLE& shared_h, DWORD size,
-	     shared_locations *m, PSECURITY_ATTRIBUTES psa, DWORD access)
+	     shared_locations m, bool &created, PSECURITY_ATTRIBUTES psa,
+	     DWORD access)
 {
-  void *shared;
-
-  void *addr;
-  if (*m == SH_JUSTCREATE || *m == SH_JUSTOPEN)
-    addr = NULL;
-  else
-    addr = (void *) region_address[*m];
-
   WCHAR map_buf[MAX_PATH];
   WCHAR *mapname = NULL;
+  void *shared;
+  void *addr;
 
-  if (shared_h)
-    *m = SH_JUSTOPEN;
+  if (m == SH_JUSTCREATE || m == SH_JUSTOPEN)
+    addr = NULL;
   else
+    addr = (void *) region_address[m];
+
+  created = false;
+  if (!shared_h)
     {
       if (name)
 	mapname = shared_name (map_buf, name, n);
-      if (*m == SH_JUSTOPEN)
-	shared_h = OpenFileMappingW (access, FALSE, mapname);
+      if (m == SH_JUSTOPEN)
+	shared_h = OpenFileMappingW (FILE_MAP_READ | FILE_MAP_WRITE, FALSE,
+				     mapname);
       else
 	{
+	  created = true;
 	  shared_h = CreateFileMappingW (INVALID_HANDLE_VALUE, psa,
 					PAGE_READWRITE, 0, size, mapname);
 	  if (GetLastError () == ERROR_ALREADY_EXISTS)
-	    *m = SH_JUSTOPEN;
+	    created = false;
 	}
       if (shared_h)
 	/* ok! */;
-      else if (*m != SH_JUSTOPEN)
+      else if (m != SH_JUSTOPEN)
 	api_fatal ("CreateFileMapping %W, %E.  Terminating.", mapname);
       else
 	return NULL;
     }
 
-  shared = (shared_info *) MapViewOfFileEx (shared_h, access, 0, 0, 0, addr);
+  shared = MapViewOfFileEx (shared_h, FILE_MAP_READ | FILE_MAP_WRITE,
+			    0, 0, 0, addr);
 
   if (!shared)
     api_fatal ("MapViewOfFileEx '%W'(%p), %E.  Terminating.", mapname, shared_h);
 
-  debug_printf ("name %W, n %d, shared %p (wanted %p), h %p, *m %d",
-		mapname, n, shared, addr, shared_h, *m);
+  debug_printf ("name %W, n %d, shared %p (wanted %p), h %p, m %d",
+		mapname, n, shared, addr, shared_h, m);
 
   return shared;
 }

winsup/cygwin/fhandler/console.cc
@@ -211,15 +211,14 @@ beep ()
 }
 
 fhandler_console::console_state *
-fhandler_console::open_shared_console (HWND hw, HANDLE& h, bool& create)
+fhandler_console::open_shared_console (HWND hw, HANDLE& h, bool& created)
 {
   wchar_t namebuf[(sizeof "XXXXXXXXXXXXXXXXXX-consNNNNNNNNNN")];
   __small_swprintf (namebuf, L"%S-cons%p", &cygheap->installation_key, hw);
 
-  shared_locations m = create ? SH_SHARED_CONSOLE : SH_JUSTOPEN;
+  shared_locations m = created ? SH_SHARED_CONSOLE : SH_JUSTOPEN;
   console_state *res = (console_state *)
-    open_shared (namebuf, 0, h, sizeof (console_state), &m);
-  create = m != SH_JUSTOPEN;
+    open_shared (namebuf, 0, h, sizeof (console_state), m, created);
   return res;
 }
 

winsup/cygwin/local_includes/shared_info.h
@@ -89,9 +89,9 @@ HANDLE get_session_parent_dir ();
 char *shared_name (char *, const char *, int);
 WCHAR *shared_name (WCHAR *, const WCHAR *, int);
 void *open_shared (const WCHAR *, int, HANDLE&, DWORD,
-			     shared_locations, PSECURITY_ATTRIBUTES = &sec_all,
-			     DWORD = FILE_MAP_READ | FILE_MAP_WRITE);
+		   shared_locations, PSECURITY_ATTRIBUTES = &sec_all,
+		   DWORD = FILE_MAP_READ | FILE_MAP_WRITE);
 void *open_shared (const WCHAR *, int, HANDLE&, DWORD,
-			     shared_locations *, PSECURITY_ATTRIBUTES = &sec_all,
-			     DWORD = FILE_MAP_READ | FILE_MAP_WRITE);
+		   shared_locations, bool &, PSECURITY_ATTRIBUTES = &sec_all,
+		   DWORD = FILE_MAP_READ | FILE_MAP_WRITE);
 extern void user_shared_create (bool reinit);

winsup/cygwin/pinfo.cc
@@ -387,8 +387,10 @@ pinfo::init (pid_t n, DWORD flag, HANDLE h0)
 
   for (int i = 0; i < 20; i++)
     {
+      bool created;
+
       procinfo = (_pinfo *) open_shared (L"cygpid", n, h0, sizeof (_pinfo),
-					 &shloc, sec_attribs, access);
+					 shloc, created, sec_attribs, access);
       if (!h0)
 	{
 	  if (createit)
@@ -408,8 +410,6 @@ pinfo::init (pid_t n, DWORD flag, HANDLE h0)
 	  yield ();
 	  continue;
 	}
-
-      bool created = shloc != SH_JUSTOPEN;
 
       /* Just fetching info for ps or /proc, don't do anything rash. */
       if (!created && !(flag & PID_NEW) && !procinfo->ppid

commit 65d34484e9097fd9036dd577028423355cb5f5bc

Cygwin: shared: Fix access permissions setting in open_shared().
After the commit 93508e5bb841, the access permissions argument passed
to open_shared() is ignored and always replaced with (FILE_MAP_READ |
FILE_MAP_WRITE). This causes the weird behaviour that sshd service
process loses its cygwin PID. This triggers the failure in pty that
transfer_input() does not work properly.

This patch resumes the access permission settings to fix that.

Fixes: 93508e5bb841 ("Cygwin: open_shared: don't reuse shared_locations parameter as output")
Reviewed-by: Corinna Vinschen <corinna@vinschen.de>
Signedd-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/mm/shared.cc
@@ -139,8 +139,7 @@ open_shared (const WCHAR *name, int n, HANDLE& shared_h, DWORD size,
       if (name)
 	mapname = shared_name (map_buf, name, n);
       if (m == SH_JUSTOPEN)
-	shared_h = OpenFileMappingW (FILE_MAP_READ | FILE_MAP_WRITE, FALSE,
-				     mapname);
+	shared_h = OpenFileMappingW (access, FALSE, mapname);
       else
 	{
 	  created = true;
@@ -165,8 +164,7 @@ open_shared (const WCHAR *name, int n, HANDLE& shared_h, DWORD size,
   do
     {
       addr = (void *) next_address;
-      shared = MapViewOfFileEx (shared_h, FILE_MAP_READ | FILE_MAP_WRITE,
-				0, 0, 0, addr);
+      shared = MapViewOfFileEx (shared_h, access, 0, 0, 0, addr);
       next_address += wincap.allocation_granularity ();
       if (next_address >= SHARED_REGIONS_ADDRESS_HIGH)
 	{

winsup/cygwin/release/3.4.8
@@ -17,3 +17,6 @@ Bug Fixes
 
 - Fix memory leak in printf() regarding gdtoa-based _ldtoa_r().
   Addresses: https://cygwin.com/pipermail/cygwin/2023-July/254054.html
+
+- Fix a bug introduced in cygwin 3.4.5 that open_shared() does not set
+  access permissions as requested by its argument.