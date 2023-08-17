c6260ede26276bb933be80203426bcd49bbebdcf
Keith Packard <keithp@keithp.com>
2023-08-17 16:49:52 -0700

    test: Directly test fmaf, fma and fmal
    Test vectors are computed using a nickle program.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- test/test-fma.c
+++ test/test-fma.c
@@ -41,28 +41,13 @@
 #include <stdbool.h>
 #include <fenv.h>
 
-#if DBL_MANT_DIG == 53
-
-static float
-fmaf_slow(float x, float y, float z)
-{
-    return (float) ((double) x * (double) y + (double) z);
-}
-
-static const float valuef = 0x1.000002p0;
-
-static bool
-equalf(float x, float y)
-{
-    if (isnan(x) && isnan(y))
-        return true;
-    return x == y;
-}
+#if FLT_MANT_DIG == 24
 
 static int roundings[] = {
-    -1,
 #ifdef FE_TONEAREST
     FE_TONEAREST,
+#else
+    -1,
 #endif
 #ifdef FE_UPWARD
     FE_UPWARD,
@@ -76,9 +61,10 @@ static int roundings[] = {
 };
 
 static const char *rounding_names[] = {
-    "default",
 #ifdef FE_TONEAREST
     "FE_TONEAREST",
+#else
+    "default",
 #endif
 #ifdef FE_UPWARD
     "FE_UPWARD",
@@ -107,32 +93,46 @@ static const char *rounding_names[] = {
 #define NROUND_FLOAT NROUND
 #endif
 
-#define FIRST_EXPF      (FLT_MIN_EXP - FLT_MANT_DIG - 2)
-#define LAST_EXPF       (FLT_MAX_EXP + 2)
+#define nanf __builtin_nanf("")
+#define inff INFINITY
+#define nan __builtin_nan("")
+#define inf ((double) INFINITY)
+#define nanl __builtin_nanl("")
+#define infl ((long double) INFINITY)
 
-/*
- * We want to test transitional exponent values, so we test +/-2
- * relative relative to four points:
- *
- *  minimum
- *  smallest normal
- *  zero
- *  maximum
- */
+struct fmaf_vec {
+    float       x, y, z;
+    float       r[4];
+};
 
-static int
-next_expf(int e)
+struct fma_vec {
+    double      x, y, z;
+    double      r[4];
+};
+
+#if LDBL_MANT_DIG != 0
+struct fmal_vec {
+    long double      x, y, z;
+    long double      r[4];
+};
+#endif
+
+#include "fma_vec.h"
+
+#define NUM_FMAF_VEC (sizeof(fmaf_vec)/sizeof(fmaf_vec[0]))
+#define NUM_FMA_VEC (sizeof(fma_vec)/sizeof(fma_vec[0]))
+#define NUM_FMAL_VEC (sizeof(fmal_vec)/sizeof(fmal_vec[0]))
+
+#endif
+
+#if __FLT_EVAL_METHOD__ == 0 && FLT_MANT_DIG == 24
+
+static bool
+equalf(float x, float y)
 {
-    switch (e) {
-    case FIRST_EXPF + 4:
-        return FLT_MIN_EXP - 2;
-    case FLT_MIN_EXP + 2:
-        return -2;
-    case 2:
-        return LAST_EXPF - 4;
-    default:
-        return e + 1;
-    }
+    if (isnan(x) && isnan(y))
+        return true;
+    return x == y;
 }
 
 #if defined(__PICOLIBC__) && !defined(TINY_STDIO)
@@ -146,110 +146,47 @@ int strfromf(char *str, size_t n, const char *format, float f)
 static int
 test_fmaf(void)
 {
-    int x_exp, y_exp, z_exp;
     char        xs[20], ys[20], zs[20], rs[20], ss[20];
-    int x_sign, y_sign, z_sign;
     int ret = 0;
     unsigned int ro;
     int defround = fegetround();
+    unsigned int t;
 
-    for (ro = 0; ro < NROUND_FLOAT; ro++) {
-        if (roundings[ro] == -1)
-            fesetround(defround);
-        else
-            fesetround(roundings[ro]);
-        printf("float %s\n", rounding_names[ro]);
-        for (z_sign = -1; z_sign <= 1; z_sign += 2) {
-            for (z_exp = FIRST_EXPF; z_exp <= LAST_EXPF; z_exp = next_expf(z_exp)) {
-                float z = ldexpf(valuef * z_sign, z_exp);
-                for (y_sign = -1; y_sign <= 1; y_sign += 2) {
-                    for (y_exp = FIRST_EXPF; y_exp <= LAST_EXPF; y_exp = next_expf(y_exp)) {
-                        float y = ldexpf(valuef * y_sign, y_exp);
-                        for (x_sign = -1; x_sign <= 1; x_sign += 2) {
-                            for (x_exp = FIRST_EXPF; x_exp <= LAST_EXPF; x_exp = next_expf(x_exp)) {
-                                float x = ldexpf(valuef * x_sign, x_exp);
-
-                                float r = fmaf(x, y, z);
-                                float s = fmaf_slow(x, y, z);
-                                if (!equalf(r, s)) {
-                                    strfromf(xs, sizeof(xs), "%a", x);
-                                    strfromf(ys, sizeof(xs), "%a", y);
-                                    strfromf(zs, sizeof(xs), "%a", z);
-                                    strfromf(rs, sizeof(xs), "%a", r);
-                                    strfromf(ss, sizeof(xs), "%a", s);
-                                    printf("round %s slow %s %s %s -> got %s want %s\n",
-                                           rounding_names[ro],
-                                           xs, ys, zs, rs, ss);
-                                    ret = 1;
-                                }
-                            }
-                        }
-                    }
-                }
+    printf("float\n");
+    for (t = 0; t < NUM_FMAF_VEC; t++) {
+        for (ro = 0; ro < NROUND_FLOAT; ro++) {
+            if (roundings[ro] == -1)
+                fesetround(defround);
+            else
+                fesetround(roundings[ro]);
+            float x = fmaf_vec[t].x;
+            float y = fmaf_vec[t].y;
+            float z = fmaf_vec[t].z;
+            float r = fmaf(x, y, z);
+            float s = fmaf_vec[t].r[ro];
+            if (!equalf(r, s)) {
+                strfromf(xs, sizeof(xs), "%a", x);
+                strfromf(ys, sizeof(xs), "%a", y);
+                strfromf(zs, sizeof(xs), "%a", z);
+                strfromf(rs, sizeof(xs), "%a", r);
+                strfromf(ss, sizeof(xs), "%a", s);
+                printf("%u: round %s %s %s %s -> got %s want %s\n",
+                       t, rounding_names[ro],
+                       xs, ys, zs, rs, ss);
+                ret = 1;
             }
         }
     }
     fesetround(defround);
     return ret;
 }
-
 #else
 #define test_fmaf() 0
 #endif
 
-/*
- * If long double is large enough, we can use it to test
- * double FMA
- */
-#if LDBL_MANT_DIG  == 113 && LDBL_MAX_EXP > DBL_MAX_EXP
+#if DBL_MANT_DIG == 53 && __FLT_EVAL_METHOD__ == 0
 
-static double
-fma_slow(double x, double y, double z)
-{
-    return (double) ((long double) x * (long double) y + (long double) z);
-}
-
-#define FIRST_EXP      (DBL_MIN_EXP - DBL_MANT_DIG - 2)
-#define LAST_EXP       (DBL_MAX_EXP + 1)
-
-#define RANGE_EXP      10
-
-/*
- * We want to test transitional exponent values, so we test +/-2
- * relative relative to four points:
- *
- *  minimum
- *  smallest normal
- *  zero
- *  maximum
- */
-
-static int
-next_exp(int e)
-{
-    switch (e) {
-    case FIRST_EXP + 4:
-        return DBL_MIN_EXP - 2;
-    case DBL_MIN_EXP + 2:
-        return -2;
-    case 2:
-        return LAST_EXP - 4;
-    default:
-        return e + 1;
-    }
-}
-
-static bool
-equal(double x, double y)
-{
-    if (isnan(x) && isnan(y))
-        return true;
-    return x == y;
-}
-
-static const double value = 0x1.0000000000001p0;
-
-#ifdef PICOLIBC_LONG_DOUBLE_NOEXCEPT
+#ifdef PICOLIBC_DOUBLE_NOEXCEPT
 /*
  * Assume that a lack of exceptions for doubles also means a lack of
  * support for non-default rounding modes for doubles.
@@ -263,51 +200,106 @@ static const double value = 0x1.0000000000001p0;
 #define NROUND_DOUBLE NROUND
 #endif
 
+static bool
+equal(double x, double y)
+{
+    if (isnan(x) && isnan(y))
+        return true;
+    return x == y;
+}
+
 static int
 test_fma(void)
 {
-    int x_exp, y_exp, z_exp;
-    int x_sign, y_sign, z_sign;
     int ret = 0;
     unsigned int ro;
     int defround = fegetround();
+    unsigned int t;
 
-    for (ro = 0; ro < NROUND_DOUBLE; ro++) {
-        if (roundings[ro] == -1)
-            fesetround(defround);
-        else
-            fesetround(roundings[ro]);
-        printf("double %s\n", rounding_names[ro]);
-        for (z_sign = -1; z_sign <= 1; z_sign += 2) {
-            for (z_exp = FIRST_EXP; z_exp <= LAST_EXP; z_exp = next_exp(z_exp)) {
-                double z = ldexp(value * z_sign, z_exp);
-                for (y_sign = -1; y_sign <= 1; y_sign += 2) {
-                    for (y_exp = FIRST_EXP; y_exp <= LAST_EXP; y_exp = next_exp(y_exp)) {
-                        double y = ldexp(value * y_sign, y_exp);
-                        for (x_sign = -1; x_sign <= 1; x_sign += 2) {
-                            for (x_exp = FIRST_EXP; x_exp <= LAST_EXP; x_exp = next_exp(x_exp)) {
-                                double x = ldexp(value * x_sign, x_exp);
-
-                                double r = fma(x, y, z);
-                                double s = fma_slow(x, y, z);
-                                if (!equal(r, s)) {
-                                    printf("round %s slow %a %a %a -> got %a want %a\n",
-                                           rounding_names[ro], x, y, z, r, s);
-                                    ret = 1;
-                                }
-                            }
-                        }
-                    }
-                }
+    printf("double\n");
+    for (t = 0; t < NUM_FMA_VEC; t++) {
+        for (ro = 0; ro < NROUND_DOUBLE; ro++) {
+            if (roundings[ro] == -1)
+                fesetround(defround);
+            else
+                fesetround(roundings[ro]);
+            double x = fma_vec[t].x;
+            double y = fma_vec[t].y;
+            double z = fma_vec[t].z;
+            double r = fma(x, y, z);
+            double s = fma_vec[t].r[ro];
+            if (!equal(r, s)) {
+                printf("%u: round %s %a * %a + %a -> got %a want %a\n",
+                       t, rounding_names[ro],
+                       x, y, z, r, s);
+                ret = 1;
             }
         }
     }
     fesetround(defround);
     return ret;
 }
-
 #else
 #define test_fma() 0
+#endif
+
+#if defined (_TEST_LONG_DOUBLE) && (LDBL_MANT_DIG == 64 || LDBL_MANT_DIG == 113) && LDBL_MIN_EXP == -16381
+
+#ifdef PICOLIBC_LONG_DOUBLE_NOEXCEPT
+/*
+ * Assume that a lack of exceptions for doubles also means a lack of
+ * support for non-default rounding modes for doubles.
+ */
+#define SKIP_ROUNDINGS_LONG_DOUBLE
+#endif
+
+#ifdef SKIP_ROUNDINGS_LONG_DOUBLE
+#define NROUND_LONG_DOUBLE    1
+#else
+#define NROUND_LONG_DOUBLE NROUND
+#endif
+
+static bool
+equall(long double x, long double y)
+{
+    if (isnan(x) && isnan(y))
+        return true;
+    return x == y;
+}
+
+static int
+test_fmal(void)
+{
+    int ret = 0;
+    unsigned int ro;
+    int defround = fegetround();
+    unsigned int t;
+
+    printf("long double\n");
+    for (t = 0; t < NUM_FMAL_VEC; t++) {
+        for (ro = 0; ro < NROUND_LONG_DOUBLE; ro++) {
+            if (roundings[ro] == -1)
+                fesetround(defround);
+            else
+                fesetround(roundings[ro]);
+            long double x = fmal_vec[t].x;
+            long double y = fmal_vec[t].y;
+            long double z = fmal_vec[t].z;
+            long double r = fmal(x, y, z);
+            long double s = fmal_vec[t].r[ro];
+            if (!equall(r, s)) {
+                printf("%u: round %s %La * %La + %La -> got %La want %La\n",
+                       t, rounding_names[ro],
+                       x, y, z, r, s);
+                ret = 1;
+            }
+        }
+    }
+    fesetround(defround);
+    return ret;
+}
+#else
+#define test_fmal() 0
 #endif
 
 int
@@ -322,5 +314,7 @@ main(void)
         return 77;
     }
 #endif
-    return test_fmaf() || test_fma();
+    (void) rounding_names;
+    (void) roundings;
+    return test_fmaf() || test_fma() || test_fmal();
 }
