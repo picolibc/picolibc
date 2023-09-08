picolibc/picolibc

commit 622fb0776ea333dd708ff312f08ec98311138fbe

Cygwin: enable usage of FIFOs on NFS
FIFOs on NFS were never recogized as such in path handling.

stat(2) indicated native FIFOs as FIFOs but the path handling
code didn't set the matching values in the inner symlink checking
code, so the followup behaviour was wrong.

Basically for the same reason, Cygwin-created FIFOs were just treated
as symlinks with weird content by stat(2) as well as path handling.

Add code to enable both types of FIFOs on NFS as Cygwin FIFOs.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/fhandler/disk_file.cc
@@ -210,13 +210,14 @@ fhandler_base::fstat_by_nfs_ea (struct stat *buf)
   cyg_ldap cldap;
   bool ldap_open = false;
 
-  if (get_handle ())
+  /* NFS stumbles over its own caching.  If you write to the file,
+     a subsequent fstat does not return the actual size of the file,
+     but the size at the time the handle has been opened.  Unless
+     access through another handle invalidates the caching within the
+     NFS client.  Skip this for Cygwin-created Symlinks playing FIFOs
+     (this sets the filler1 member to NF3FIFO). */
+  if (get_handle () && nfs_attr->filler1 != NF3FIFO)
     {
-      /* NFS stumbles over its own caching.  If you write to the file,
-	 a subsequent fstat does not return the actual size of the file,
-	 but the size at the time the handle has been opened.  Unless
-	 access through another handle invalidates the caching within the
-	 NFS client. */
       if (get_access () & GENERIC_WRITE)
 	FlushFileBuffers (get_handle ());
       pc.get_finfo (get_handle ());
@@ -357,8 +358,13 @@ fhandler_base::fstat_fs (struct stat *buf)
 
   if (get_stat_handle ())
     {
-      if (!nohandle () && (!is_fs_special () || get_flags () & O_PATH))
-	res = pc.fs_is_nfs () ? fstat_by_nfs_ea (buf) : fstat_by_handle (buf);
+      if (!nohandle ())
+	{
+	  if (pc.fs_is_nfs ())
+	    res = fstat_by_nfs_ea (buf);
+	  else if (!is_fs_special () || get_flags () & O_PATH)
+	    res = fstat_by_handle (buf);
+	}
       if (res)
 	res = fstat_by_name (buf);
       return res;

winsup/cygwin/path.cc
@@ -3513,11 +3513,41 @@ restart:
 	}
 
       /* If the file is on an NFS share and could be opened with extended
-	 attributes, check if it's a symlink.  Only files can be symlinks
-	 (which can be symlinks to directories). */
-      else if (fs.is_nfs () && (conv_hdl.nfsattr ()->type & 7) == NF3LNK)
+	 attributes, check if it's a symlink or FIFO. */
+      else if (fs.is_nfs ())
 	{
-	  res = check_nfs_symlink (h);
+	  /* Make sure filler1 is 0, so we can use it safely as a marker. */
+	  conv_hdl.nfsattr ()->filler1 = 0;
+	  switch (conv_hdl.nfsattr ()->type & 7)
+	    {
+	    case NF3LNK:
+	      res = check_nfs_symlink (h);
+	      /* Enable Cygwin-created FIFOs to be recognized as FIFOs.
+		 We have to overwrite the NFS fattr3 data, otherwise the
+		 info returned by Cygwin's stat will still claim the file
+		 is a symlink. */
+	      if (res && contents[0] == ':' && contents[1] == '\\'
+		  && parse_device (contents) && major == _major (FH_FIFO))
+		{
+		  conv_hdl.nfsattr ()->type = NF3FIFO;
+		  conv_hdl.nfsattr ()->mode = mode;
+		  conv_hdl.nfsattr ()->size = 0;
+		  /* Marker for fhandler_base::fstat_by_nfs_ea not to override
+		     the cached fattr3 data with fresh data from the filesystem,
+		     even if the handle is used for other purposes than stat. */
+		  conv_hdl.nfsattr ()->filler1 = NF3FIFO;
+		}
+	      break;
+	    case NF3FIFO:
+	      /* Enable real FIFOs recognized as such. */
+	      major = _major (FH_FIFO);
+	      minor = _minor (FH_FIFO);
+	      mode = S_IFIFO | (conv_hdl.nfsattr ()->mode & ~S_IFMT);
+	      isdevice = true;
+	      break;
+	    default:
+	      break;
+	    }
 	  if (res)
 	    break;
 	}

commit bedefff9e25b6b2bdc13e0268a437f82dc4b4798

Cygwin: fix chmod on native NFS FIFOs
By handling native NFS FIFOs as actual FIFOs, chmod on a FIFO
suddenly called fhandler_base::fchmod, which is insufficient
to handle FIFO files on any filesystem.

Note that this does not fix Cygwin FIFOs on NFS or AFS yet.

Fixes: 622fb0776ea3 ("Cygwin: enable usage of FIFOs on NFS")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/fhandler/base.cc
@@ -613,7 +613,10 @@ fhandler_base::open (int flags, mode_t mode)
 	options |= FILE_OPEN_REPARSE_POINT;
     }
 
-  if (get_device () == FH_FS)
+  /* If the file is a FIFO, open has been called for an operation on the file
+     constituting the FIFO, e. g., chmod or statvfs.  Handle it like a normal
+     file.  Eespecially the access flags have to be set correctly. */
+  if (get_device () == FH_FS || get_device () == FH_FIFO)
     {
       /* O_TMPFILE files are created with delete-on-close semantics, as well
 	 as with FILE_ATTRIBUTE_TEMPORARY.  The latter speeds up file access,
@@ -1710,7 +1713,10 @@ int
 fhandler_base::fchmod (mode_t mode)
 {
   if (pc.is_fs_special ())
-    return chmod_device (pc, mode);
+    {
+      fhandler_disk_file fh (pc);
+      return fh.fchmod (mode);
+    }
   /* By default, just succeeds. */
   return 0;
 }

winsup/cygwin/fhandler/disk_file.cc
@@ -715,7 +715,10 @@ fhandler_disk_file::fchmod (mode_t mode)
   NTSTATUS status;
   IO_STATUS_BLOCK io;
 
-  if (pc.is_fs_special ())
+  if (pc.is_fs_special ()
+      /* For NFS, only handle Cygwin FIFOs specially.  Changing mode of
+	 native FIFOs will work with the default code below. */
+      && (!pc.fs_is_nfs () || pc.nfsattr ()->filler1 == NF3FIFO))
     return chmod_device (pc, mode);
 
   if (!get_handle ())