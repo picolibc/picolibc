picolibc/picolibc

commit 622fb0776ea333dd708ff312f08ec98311138fbe

Cygwin: enable usage of FIFOs on NFS
FIFOs on NFS were never recogized as such in path handling.

stat(2) indicated native FIFOs as FIFOs but the path handling
code didn't set the matching values in the inner symlink checking
code, so the followup behaviour was wrong.

Basically for the same reason, Cygwin-created FIFOs were just treated
as symlinks with weird content by stat(2) as well as path handling.

Add code to enable both types of FIFOs on NFS as Cygwin FIFOs.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/fhandler/disk_file.cc
@@ -210,13 +210,14 @@ fhandler_base::fstat_by_nfs_ea (struct stat *buf)
   cyg_ldap cldap;
   bool ldap_open = false;
 
-  if (get_handle ())
+  /* NFS stumbles over its own caching.  If you write to the file,
+     a subsequent fstat does not return the actual size of the file,
+     but the size at the time the handle has been opened.  Unless
+     access through another handle invalidates the caching within the
+     NFS client.  Skip this for Cygwin-created Symlinks playing FIFOs
+     (this sets the filler1 member to NF3FIFO). */
+  if (get_handle () && nfs_attr->filler1 != NF3FIFO)
     {
-      /* NFS stumbles over its own caching.  If you write to the file,
-	 a subsequent fstat does not return the actual size of the file,
-	 but the size at the time the handle has been opened.  Unless
-	 access through another handle invalidates the caching within the
-	 NFS client. */
       if (get_access () & GENERIC_WRITE)
 	FlushFileBuffers (get_handle ());
       pc.get_finfo (get_handle ());
@@ -357,8 +358,13 @@ fhandler_base::fstat_fs (struct stat *buf)
 
   if (get_stat_handle ())
     {
-      if (!nohandle () && (!is_fs_special () || get_flags () & O_PATH))
-	res = pc.fs_is_nfs () ? fstat_by_nfs_ea (buf) : fstat_by_handle (buf);
+      if (!nohandle ())
+	{
+	  if (pc.fs_is_nfs ())
+	    res = fstat_by_nfs_ea (buf);
+	  else if (!is_fs_special () || get_flags () & O_PATH)
+	    res = fstat_by_handle (buf);
+	}
       if (res)
 	res = fstat_by_name (buf);
       return res;

winsup/cygwin/path.cc
@@ -3513,11 +3513,41 @@ restart:
 	}
 
       /* If the file is on an NFS share and could be opened with extended
-	 attributes, check if it's a symlink.  Only files can be symlinks
-	 (which can be symlinks to directories). */
-      else if (fs.is_nfs () && (conv_hdl.nfsattr ()->type & 7) == NF3LNK)
+	 attributes, check if it's a symlink or FIFO. */
+      else if (fs.is_nfs ())
 	{
-	  res = check_nfs_symlink (h);
+	  /* Make sure filler1 is 0, so we can use it safely as a marker. */
+	  conv_hdl.nfsattr ()->filler1 = 0;
+	  switch (conv_hdl.nfsattr ()->type & 7)
+	    {
+	    case NF3LNK:
+	      res = check_nfs_symlink (h);
+	      /* Enable Cygwin-created FIFOs to be recognized as FIFOs.
+		 We have to overwrite the NFS fattr3 data, otherwise the
+		 info returned by Cygwin's stat will still claim the file
+		 is a symlink. */
+	      if (res && contents[0] == ':' && contents[1] == '\\'
+		  && parse_device (contents) && major == _major (FH_FIFO))
+		{
+		  conv_hdl.nfsattr ()->type = NF3FIFO;
+		  conv_hdl.nfsattr ()->mode = mode;
+		  conv_hdl.nfsattr ()->size = 0;
+		  /* Marker for fhandler_base::fstat_by_nfs_ea not to override
+		     the cached fattr3 data with fresh data from the filesystem,
+		     even if the handle is used for other purposes than stat. */
+		  conv_hdl.nfsattr ()->filler1 = NF3FIFO;
+		}
+	      break;
+	    case NF3FIFO:
+	      /* Enable real FIFOs recognized as such. */
+	      major = _major (FH_FIFO);
+	      minor = _minor (FH_FIFO);
+	      mode = S_IFIFO | (conv_hdl.nfsattr ()->mode & ~S_IFMT);
+	      isdevice = true;
+	      break;
+	    default:
+	      break;
+	    }
 	  if (res)
 	    break;
 	}

commit c0aa6ac30ec483ad6da6af701625e7aea7536663

Cygwin: NFS: create devices (especially FIFOs) as shortcut files
Creating real NFS symlinks for device files has a  major downside:
The way we store device info requires to change the symlink target
in case of calling chmod(2). This falls flat in two ways:

- It requires to remove and recreate the symlink, so it doesn't
  exist for a short period of time, and
- removing fails badly if there's another open handle to the symlink.

Therefore, change this to create FIFOs as shortcut files, just as on
most other filesystems.  Make sure to recognize these new shortcuts
on NFS (for devices only) in path handling and readdir.

Fixes: 622fb0776ea3 ("Cygwin: enable usage of FIFOs on NFS")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/fhandler/disk_file.cc
@@ -2348,9 +2348,11 @@ go_ahead:
 		 And, since some filesystems choke on the EAs, we don't
 		 use them unconditionally. */
 	      f_status = (dir->__flags & dirent_nfs_d_ino)
-			 ? NtCreateFile (&hdl, READ_CONTROL, &attr, &io,
-					 NULL, 0, FILE_SHARE_VALID_FLAGS,
-					 FILE_OPEN, FILE_OPEN_FOR_BACKUP_INTENT,
+			 ? NtCreateFile (&hdl,
+					 READ_CONTROL | FILE_READ_ATTRIBUTES,
+					 &attr, &io, NULL, 0,
+					 FILE_SHARE_VALID_FLAGS, FILE_OPEN,
+					 FILE_OPEN_FOR_BACKUP_INTENT,
 					 &nfs_aol_ffei, sizeof nfs_aol_ffei)
 			 : NtOpenFile (&hdl, READ_CONTROL, &attr, &io,
 				       FILE_SHARE_VALID_FLAGS,
@@ -2364,6 +2366,16 @@ go_ahead:
 		  FILE_INTERNAL_INFORMATION fii;
 		  f_status = NtQueryInformationFile (hdl, &io, &fii, sizeof fii,
 						     FileInternalInformation);
+		  /* On NFS fetch the (faked, but useful) DOS attribute.
+		     We need it to recognize shortcut FIFOs. */
+		  if ((dir->__flags & dirent_nfs_d_ino))
+		    {
+		      FILE_BASIC_INFORMATION fbi;
+
+		      if (NT_SUCCESS (NtQueryInformationFile (hdl, &io, &fbi,
+				      sizeof fbi, FileBasicInformation)))
+			FileAttributes = fbi.FileAttributes;
+		    }
 		  NtClose (hdl);
 		  if (NT_SUCCESS (f_status))
 		    {

winsup/cygwin/path.cc
@@ -2005,7 +2005,7 @@ symlink_worker (const char *oldpath, path_conv &win32_newpath, bool isdevice)
 	 variable.  Device files are always shortcuts. */
       wsym_type = isdevice ? WSYM_lnk : allow_winsymlinks;
       /* NFS has its own, dedicated way to create symlinks. */
-      if (win32_newpath.fs_is_nfs ())
+      if (win32_newpath.fs_is_nfs () && !isdevice)
 	wsym_type = WSYM_nfs;
       /* MVFS doesn't handle the SYSTEM DOS attribute, but it handles the R/O
 	 attribute. Therefore we create symlinks on MVFS always as shortcuts. */
@@ -3477,6 +3477,16 @@ restart:
 	  else if (contents[0] != ':' || contents[1] != '\\'
 		   || !parse_device (contents))
 	    break;
+	  if (fs.is_nfs () && major == _major (FH_FIFO))
+	    {
+	      conv_hdl.nfsattr ()->type = NF3FIFO;
+	      conv_hdl.nfsattr ()->mode = mode;
+	      conv_hdl.nfsattr ()->size = 0;
+	      /* Marker for fhandler_base::fstat_by_nfs_ea not to override
+		 the cached fattr3 data with fresh data from the filesystem,
+		 even if the handle is used for other purposes than stat. */
+	      conv_hdl.nfsattr ()->filler1 = NF3FIFO;
+	    }
 	}
 
       /* If searching for `foo' and then finding a `foo.lnk' which is