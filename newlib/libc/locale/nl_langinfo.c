picolibc/picolibc

commit d47d5b850bed398357e7f05d8fef508be1bb7f51

Extend locale support to maintain wide char values of native strings 	if __HAVE_LOCALE_INFO_EXTENDED__ is defined. 	* libc/include/langinfo.h (enum __nl_item): New type.  Define all 	native values accessible through nl_langinfo.  Define previously 	existing POSIX-compatible values as macros as well. 	* libc/include/stdlib.h (__mb_cur_max): Drop declaration. 	(__locale_mb_cur_max): Declare. 	(MB_CUR_MAX): Re-define calling __locale_mb_cur_max. 	* libc/locale/Makefile.am (ELIX_SOURCES): Add lctype.c. 	* libc/locale/Makefile.in: Regenerate. 	* libc/locale/lctype.c: New file to define and load LC_CTYPE category. 	* libc/locale/lctype.h: New file, matching header. 	* libc/locale/lmessages.c (_C_messages_locale): Add default values for 	wide char members. 	(__messages_load_locale): Add _C_messages_locale in call to 	__set_lc_messages_from_win. 	* libc/locale/lmessages.h (struct lc_messages_T): Add wide char members. 	* libc/locale/lmonetary.c (_C_monetary_locale):  Add default values for 	wide char members. 	(__monetary_load_locale): Add _C_monetary_locale in call to 	__set_lc_monetary_from_win. 	* libc/locale/lmonetary.h (struct lc_monetary_T): Add wide char members. 	Add numerical values for international currency formatting per 	POSIX-1.2008, if __HAVE_LOCALE_INFO_EXTENDED__ is defined. 	* libc/locale/lnumeric.c (_C_numeric_locale): Add default values for 	wide char members. 	(__numeric_load_locale): Add _C_numeric_locale in call to 	__set_lc_numeric_from_win. 	* libc/locale/lnumeric.h (struct lc_numeric_T): Add wide char members. 	* libc/locale/locale.c (loadlocale): Return doing nothing if category 	locale didn't change.  Convert category if chain to switch statement. 	Call __ctype_load_locale in LC_CTYPE case. 	(__locale_charset): Add (but disable for now) returning codeset from 	__get_current_ctype_locale. 	(__locale_mb_cur_max): Add (but disable for now) returning mb_cur_max 	from __get_current_ctype_locale. 	(__locale_msgcharset): Add returning codeset from 	__ge

newlib/libc/locale/nl_langinfo.c
@@ -32,6 +32,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "lctype.h"
 #include "timelocal.h"
 #include "lnumeric.h"
 #include "lmonetary.h"
@@ -40,6 +41,138 @@
 #ifndef __CYGWIN__
 #define TRANSITION_PERIOD_HACK
 #endif
+
+#undef offsetoff
+#define _O(TYPE, MEMBER)  __builtin_offsetof (TYPE, MEMBER)
+
+#define _NLITEM(cat,memb) { { cat:__get_current_##cat##_locale }, \
+			      _O (struct lc_##cat##_T, memb) }
+
+#ifdef __HAVE_LOCALE_INFO_EXTENDED__
+static struct _nl_item_t
+{
+  union {
+    struct lc_ctype_T *    (*ctype)(void);
+    struct lc_time_T *     (*time)(void);
+    struct lc_numeric_T *  (*numeric)(void);
+    struct lc_monetary_T * (*monetary)(void);
+    struct lc_messages_T * (*messages)(void);
+    void *		   (*base)(void);
+  };
+  _off_t offset;
+} nl_ext[] =
+{
+  /* First element has an nl_item value of _NL_LOCALE_EXTENDED_FIRST_ENTRY */
+  _NLITEM (ctype, outdigits[0]),
+  _NLITEM (ctype, outdigits[1]),
+  _NLITEM (ctype, outdigits[2]),
+  _NLITEM (ctype, outdigits[3]),
+  _NLITEM (ctype, outdigits[4]),
+  _NLITEM (ctype, outdigits[5]),
+  _NLITEM (ctype, outdigits[6]),
+  _NLITEM (ctype, outdigits[7]),
+  _NLITEM (ctype, outdigits[8]),
+  _NLITEM (ctype, outdigits[9]),
+  _NLITEM (ctype, woutdigits[0]),
+  _NLITEM (ctype, woutdigits[1]),
+  _NLITEM (ctype, woutdigits[2]),
+  _NLITEM (ctype, woutdigits[3]),
+  _NLITEM (ctype, woutdigits[4]),
+  _NLITEM (ctype, woutdigits[5]),
+  _NLITEM (ctype, woutdigits[6]),
+  _NLITEM (ctype, woutdigits[7]),
+  _NLITEM (ctype, woutdigits[8]),
+  _NLITEM (ctype, woutdigits[9]),
+  _NLITEM (time, codeset),
+  _NLITEM (time, wmon[1]),
+  _NLITEM (time, wmon[2]),
+  _NLITEM (time, wmon[3]),
+  _NLITEM (time, wmon[4]),
+  _NLITEM (time, wmon[5]),
+  _NLITEM (time, wmon[6]),
+  _NLITEM (time, wmon[7]),
+  _NLITEM (time, wmon[8]),
+  _NLITEM (time, wmon[9]),
+  _NLITEM (time, wmon[10]),
+  _NLITEM (time, wmon[11]),
+  _NLITEM (time, wmon[12]),
+  _NLITEM (time, wmonth[1]),
+  _NLITEM (time, wmonth[2]),
+  _NLITEM (time, wmonth[3]),
+  _NLITEM (time, wmonth[4]),
+  _NLITEM (time, wmonth[5]),
+  _NLITEM (time, wmonth[6]),
+  _NLITEM (time, wmonth[7]),
+  _NLITEM (time, wmonth[8]),
+  _NLITEM (time, wmonth[9]),
+  _NLITEM (time, wmonth[10]),
+  _NLITEM (time, wmonth[11]),
+  _NLITEM (time, wmonth[12]),
+  _NLITEM (time, wwday[1]),
+  _NLITEM (time, wwday[2]),
+  _NLITEM (time, wwday[3]),
+  _NLITEM (time, wwday[4]),
+  _NLITEM (time, wwday[5]),
+  _NLITEM (time, wwday[6]),
+  _NLITEM (time, wwday[7]),
+  _NLITEM (time, wweekday[1]),
+  _NLITEM (time, wweekday[2]),
+  _NLITEM (time, wweekday[3]),
+  _NLITEM (time, wweekday[4]),
+  _NLITEM (time, wweekday[5]),
+  _NLITEM (time, wweekday[6]),
+  _NLITEM (time, wweekday[7]),
+  _NLITEM (time, wX_fmt),
+  _NLITEM (time, wx_fmt),
+  _NLITEM (time, wc_fmt),
+  _NLITEM (time, wam_pm[0]),
+  _NLITEM (time, wam_pm[1]),
+  _NLITEM (time, wdate_fmt),
+  _NLITEM (time, wampm_fmt),
+  _NLITEM (time, wera),
+  _NLITEM (time, wera_d_fmt),
+  _NLITEM (time, wera_d_t_fmt),
+  _NLITEM (time, wera_t_fmt),
+  _NLITEM (time, walt_digits),
+  _NLITEM (numeric, codeset),
+  _NLITEM (numeric, grouping),
+  _NLITEM (numeric, wdecimal_point),
+  _NLITEM (numeric, wthousands_sep),
+  _NLITEM (monetary, int_curr_symbol),
+  _NLITEM (monetary, currency_symbol),
+  _NLITEM (monetary, mon_decimal_point),
+  _NLITEM (monetary, mon_thousands_sep),
+  _NLITEM (monetary, mon_grouping),
+  _NLITEM (monetary, positive_sign),
+  _NLITEM (monetary, negative_sign),
+  _NLITEM (monetary, int_frac_digits),
+  _NLITEM (monetary, frac_digits),
+  _NLITEM (monetary, p_cs_precedes),
+  _NLITEM (monetary, p_sep_by_space),
+  _NLITEM (monetary, n_cs_precedes),
+  _NLITEM (monetary, n_sep_by_space),
+  _NLITEM (monetary, p_sign_posn),
+  _NLITEM (monetary, n_sign_posn),
+  _NLITEM (monetary, int_p_cs_precedes),
+  _NLITEM (monetary, int_p_sep_by_space),
+  _NLITEM (monetary, int_n_cs_precedes),
+  _NLITEM (monetary, int_n_sep_by_space),
+  _NLITEM (monetary, int_p_sign_posn),
+  _NLITEM (monetary, int_n_sign_posn),
+  _NLITEM (monetary, codeset),
+  _NLITEM (monetary, wint_curr_symbol),
+  _NLITEM (monetary, wcurrency_symbol),
+  _NLITEM (monetary, wmon_decimal_point),
+  _NLITEM (monetary, wmon_thousands_sep),
+  _NLITEM (monetary, wpositive_sign),
+  _NLITEM (monetary, wnegative_sign),
+  _NLITEM (messages, codeset),
+  _NLITEM (messages, wyesexpr),
+  _NLITEM (messages, wnoexpr),
+  _NLITEM (messages, wyesstr),
+  _NLITEM (messages, wnostr),
+};
+#endif /* __HAVE_LOCALE_INFO_EXTENDED__ */
 
 #define _REL(BASE) ((int)item-BASE)
 
@@ -57,9 +190,36 @@ _DEFUN(nl_langinfo, (item),
    char *nptr;
 
    switch (item) {
+#ifdef __HAVE_LOCALE_INFO__
+	case _NL_MESSAGES_CODESET:
+	      	ret = (char *) __get_current_messages_locale ()->codeset;
+		goto do_codeset;
+#ifdef __HAVE_LOCALE_INFO_EXTENDED__
+	case _NL_TIME_CODESET:
+	      	ret = (char *) __get_current_time_locale ()->codeset;
+		goto do_codeset;
+	case _NL_NUMERIC_CODESET:
+	      	ret = (char *) __get_current_numeric_locale ()->codeset;
+		goto do_codeset;
+	case _NL_MONETARY_CODESET:
+	      	ret = (char *) __get_current_monetary_locale ()->codeset;
+		goto do_codeset;
+#ifdef __CYGWIN__
+	case _NL_COLLATE_CODESET:
+		{
+		  extern const char *__get_current_collate_codeset (void);
+		  ret = (char *) __get_current_collate_codeset ();
+		  goto do_codeset;
+		}
+#endif /* __CYGWIN__ */
+#endif /* __HAVE_LOCALE_INFO_EXTENDED__ */
+#endif /* __HAVE_LOCALE_INFO__ */
 	case CODESET:
 #ifdef __CYGWIN__
 		ret = __locale_charset ();
+#endif
+do_codeset:
+#ifdef __CYGWIN__
 		/* Convert charset to Linux compatible codeset string. */
 		if (ret[0] == 'A'/*SCII*/)
 		  ret = "ANSI_X3.4-1968";
@@ -252,7 +412,18 @@ _DEFUN(nl_langinfo, (item),
 	case D_MD_ORDER:        /* local extension */
 		ret = (char *) __get_current_time_locale()->md_order;
 		break;
+	case _NL_CTYPE_MB_CUR_MAX:
+		ret = (char *) __get_current_ctype_locale()->mb_cur_max;
+		break;
 	default:
+#ifdef __HAVE_LOCALE_INFO_EXTENDED__
+		if (item > _NL_LOCALE_EXTENDED_FIRST_ENTRY
+		    && item < _NL_LOCALE_EXTENDED_LAST_ENTRY) {
+			int idx = item - _NL_LOCALE_EXTENDED_FIRST_ENTRY - 1;
+			return *(char **) ((char *) (*nl_ext[idx].base)()
+					   + nl_ext[idx].offset);
+		}
+#endif
 		ret = "";
    }
    return (ret);

newlib/libc/include/langinfo.h
@@ -29,112 +29,284 @@
 #ifndef _LANGINFO_H_
 #define	_LANGINFO_H_
 
+#include <newlib.h>
+#include <sys/config.h>
 #include <sys/cdefs.h>
 
 typedef int nl_item;
 
-/* Extract the category and item index from a constructed `nl_item' value.  */
-#define _NL_ITEM_CATEGORY(item)		((int) (item) >> 16)
-#define _NL_ITEM_INDEX(item)		((int) (item) & 0xffff)
-
-#define	CODESET		0	/* codeset name */
-#define	D_T_FMT		1	/* string for formatting date and time */
-#define	D_FMT		2	/* date format string */
-#define	T_FMT		3	/* time format string */
-#define	T_FMT_AMPM	4	/* a.m. or p.m. time formatting string */
-#define	AM_STR		5	/* Ante Meridian affix */
-#define	PM_STR		6	/* Post Meridian affix */
+enum __nl_item
+{
+  /* POSIX and BSD defined items have to stick to the original values
+     to maintain backward compatibility. */
+  _NL_CTYPE_CODESET_NAME = 0,	/* codeset name */
+#define CODESET _NL_CTYPE_CODESET_NAME
+  D_T_FMT,			/* string for formatting date and time */
+#define D_T_FMT D_T_FMT
+  D_FMT,			/* date format string */
+#define D_FMT D_FMT
+  T_FMT,			/* time format string */
+#define T_FMT T_FMT
+  T_FMT_AMPM,			/* a.m. or p.m. time formatting string */
+#define T_FMT_AMPM T_FMT_AMPM
+  AM_STR,			/* Ante Meridian affix */
+#define AM_STR AM_STR
+  PM_STR,			/* Post Meridian affix */
+#define PM_STR PM_STR
 
 /* week day names */
-#define	DAY_1		7
-#define	DAY_2		8
-#define	DAY_3		9
-#define	DAY_4		10
-#define	DAY_5		11
-#define	DAY_6		12
-#define	DAY_7		13
+  DAY_1,
+#define DAY_1 DAY_1
+  DAY_2,
+#define DAY_2 DAY_2
+  DAY_3,
+#define DAY_3 DAY_3
+  DAY_4,
+#define DAY_4 DAY_4
+  DAY_5,
+#define DAY_5 DAY_5
+  DAY_6,
+#define DAY_6 DAY_6
+  DAY_7,
+#define DAY_7 DAY_7
 
 /* abbreviated week day names */
-#define	ABDAY_1		14
-#define	ABDAY_2		15
-#define	ABDAY_3		16
-#define	ABDAY_4		17
-#define	ABDAY_5		18
-#define	ABDAY_6		19
-#define	ABDAY_7		20
+  ABDAY_1,
+#define ABDAY_1 ABDAY_1
+  ABDAY_2,
+#define ABDAY_2 ABDAY_2
+  ABDAY_3,
+#define ABDAY_3 ABDAY_3
+  ABDAY_4,
+#define ABDAY_4 ABDAY_4
+  ABDAY_5,
+#define ABDAY_5 ABDAY_5
+  ABDAY_6,
+#define ABDAY_6 ABDAY_6
+  ABDAY_7,
+#define ABDAY_7 ABDAY_7
 
 /* month names */
-#define	MON_1		21
-#define	MON_2		22
-#define	MON_3		23
-#define	MON_4		24
-#define	MON_5		25
-#define	MON_6		26
-#define	MON_7		27
-#define	MON_8		28
-#define	MON_9		29
-#define	MON_10		30
-#define	MON_11		31
-#define	MON_12		32
+  MON_1,
+#define MON_1 MON_1
+  MON_2,
+#define MON_2 MON_2
+  MON_3,
+#define MON_3 MON_3
+  MON_4,
+#define MON_4 MON_4
+  MON_5,
+#define MON_5 MON_5
+  MON_6,
+#define MON_6 MON_6
+  MON_7,
+#define MON_7 MON_7
+  MON_8,
+#define MON_8 MON_8
+  MON_9,
+#define MON_9 MON_9
+  MON_10,
+#define MON_10 MON_10
+  MON_11,
+#define MON_11 MON_11
+  MON_12,
+#define MON_12 MON_12
 
 /* abbreviated month names */
-#define	ABMON_1		33
-#define	ABMON_2		34
-#define	ABMON_3		35
-#define	ABMON_4		36
-#define	ABMON_5		37
-#define	ABMON_6		38
-#define	ABMON_7		39
-#define	ABMON_8		40
-#define	ABMON_9		41
-#define	ABMON_10	42
-#define	ABMON_11	43
-#define	ABMON_12	44
+  ABMON_1,
+#define ABMON_1 ABMON_1
+  ABMON_2,
+#define ABMON_2 ABMON_2
+  ABMON_3,
+#define ABMON_3 ABMON_3
+  ABMON_4,
+#define ABMON_4 ABMON_4
+  ABMON_5,
+#define ABMON_5 ABMON_5
+  ABMON_6,
+#define ABMON_6 ABMON_6
+  ABMON_7,
+#define ABMON_7 ABMON_7
+  ABMON_8,
+#define ABMON_8 ABMON_8
+  ABMON_9,
+#define ABMON_9 ABMON_9
+  ABMON_10,
+#define ABMON_10 ABMON_10
+  ABMON_11,
+#define ABMON_11 ABMON_11
+  ABMON_12,
+#define ABMON_12 ABMON_12
 
-#define	ERA		45	/* era description segments */
-#define	ERA_D_FMT	46	/* era date format string */
-#define	ERA_D_T_FMT	47	/* era date and time format string */
-#define	ERA_T_FMT	48	/* era time format string */
-#define	ALT_DIGITS	49	/* alternative symbols for digits */
+  ERA,				/* era description segments */
+#define ERA ERA
+  ERA_D_FMT,			/* era date format string */
+#define ERA_D_FMT ERA_D_FMT
+  ERA_D_T_FMT,			/* era date and time format string */
+#define ERA_D_T_FMT ERA_D_T_FMT
+  ERA_T_FMT,			/* era time format string */
+#define ERA_T_FMT ERA_T_FMT
+  ALT_DIGITS,			/* alternative symbols for digits */
+#define ALT_DIGITS ALT_DIGITS
 
-#define	RADIXCHAR	50	/* radix char */
-#define	THOUSEP		51	/* separator for thousands */
+  RADIXCHAR,			/* radix char */
+#define RADIXCHAR RADIXCHAR
+  THOUSEP,			/* separator for thousands */
+#define THOUSEP THOUSEP
 
-#define	YESEXPR		52	/* affirmative response expression */
-#define	NOEXPR		53	/* negative response expression */
-#define	YESSTR		54	/* affirmative response for yes/no queries */
-#define	NOSTR		55	/* negative response for yes/no queries */
+  YESEXPR,			/* affirmative response expression */
+#define YESEXPR YESEXPR
+  NOEXPR,			/* negative response expression */
+#define NOEXPR NOEXPR
+  YESSTR,			/* affirmative response for yes/no queries */
+#define YESSTR YESSTR
+  NOSTR,			/* negative response for yes/no queries */
+#define NOSTR NOSTR
 
-#define	CRNCYSTR	56	/* currency symbol */
+  CRNCYSTR,			/* currency symbol */
+#define CRNCYSTR CRNCYSTR
 
-#define	D_MD_ORDER	57	/* month/day order (local extension) */
+  D_MD_ORDER,			/* month/day order (BSD extension) */
+#define D_MD_ORDER D_MD_ORDER
 
-#define _NL_CTYPE_TRANSLIT_TAB_SIZE 58
-#define _NL_CTYPE_TRANSLIT_FROM_IDX 59
-#define _NL_CTYPE_TRANSLIT_FROM_TBL 60
-#define _NL_CTYPE_TRANSLIT_TO_IDX 61
-#define _NL_CTYPE_TRANSLIT_TO_TBL 62
-#define _NL_CTYPE_TRANSLIT_DEFAULT_MISSING_LEN 63
-#define _NL_CTYPE_TRANSLIT_DEFAULT_MISSING 64
-#define _NL_CTYPE_TRANSLIT_IGNORE_LEN 65
-#define _NL_CTYPE_TRANSLIT_IGNORE 66
-#define _NL_CTYPE_EXTRA_MAP_1 70
-#define _NL_CTYPE_EXTRA_MAP_2 71
-#define _NL_CTYPE_EXTRA_MAP_3 72
-#define _NL_CTYPE_EXTRA_MAP_4 73
-#define _NL_CTYPE_EXTRA_MAP_5 74
-#define _NL_CTYPE_EXTRA_MAP_6 75
-#define _NL_CTYPE_EXTRA_MAP_7 76
-#define _NL_CTYPE_EXTRA_MAP_8 77
-#define _NL_CTYPE_EXTRA_MAP_9 78
-#define _NL_CTYPE_EXTRA_MAP_10 79
-#define _NL_CTYPE_EXTRA_MAP_11 80
-#define _NL_CTYPE_EXTRA_MAP_12 81
-#define _NL_CTYPE_EXTRA_MAP_13 82
-#define _NL_CTYPE_EXTRA_MAP_14 83
-
-#define _NL_TIME_DATE_FMT 84
+  _NL_TIME_DATE_FMT = 84,	/* date fmt used by date(1) (GNU extension) */
 #define _DATE_FMT _NL_TIME_DATE_FMT
+
+#ifdef __HAVE_LOCALE_INFO__
+  _NL_CTYPE_MB_CUR_MAX,
+  _NL_MESSAGES_CODESET,
+
+#ifdef __HAVE_LOCALE_INFO_EXTENDED__
+
+  /* NOTE:
+  
+     Always maintain the order and position of existing entries!
+     Always append new entry to the list, prior to the definition
+     of _NL_LOCALE_EXTENDED_LAST_ENTRY. */
+
+  _NL_LOCALE_EXTENDED_FIRST_ENTRY,
+
+  _NL_CTYPE_OUTDIGITS0_MB,
+  _NL_CTYPE_OUTDIGITS1_MB,
+  _NL_CTYPE_OUTDIGITS2_MB,
+  _NL_CTYPE_OUTDIGITS3_MB,
+  _NL_CTYPE_OUTDIGITS4_MB,
+  _NL_CTYPE_OUTDIGITS5_MB,
+  _NL_CTYPE_OUTDIGITS6_MB,
+  _NL_CTYPE_OUTDIGITS7_MB,
+  _NL_CTYPE_OUTDIGITS8_MB,
+  _NL_CTYPE_OUTDIGITS9_MB,
+  _NL_CTYPE_OUTDIGITS0_WC,
+  _NL_CTYPE_OUTDIGITS1_WC,
+  _NL_CTYPE_OUTDIGITS2_WC,
+  _NL_CTYPE_OUTDIGITS3_WC,
+  _NL_CTYPE_OUTDIGITS4_WC,
+  _NL_CTYPE_OUTDIGITS5_WC,
+  _NL_CTYPE_OUTDIGITS6_WC,
+  _NL_CTYPE_OUTDIGITS7_WC,
+  _NL_CTYPE_OUTDIGITS8_WC,
+  _NL_CTYPE_OUTDIGITS9_WC,
+
+  _NL_TIME_CODESET,
+  _NL_TIME_WMON_1,
+  _NL_TIME_WMON_2,
+  _NL_TIME_WMON_3,
+  _NL_TIME_WMON_4,
+  _NL_TIME_WMON_5,
+  _NL_TIME_WMON_6,
+  _NL_TIME_WMON_7,
+  _NL_TIME_WMON_8,
+  _NL_TIME_WMON_9,
+  _NL_TIME_WMON_10,
+  _NL_TIME_WMON_11,
+  _NL_TIME_WMON_12,
+  _NL_TIME_WMONTH_1,
+  _NL_TIME_WMONTH_2,
+  _NL_TIME_WMONTH_3,
+  _NL_TIME_WMONTH_4,
+  _NL_TIME_WMONTH_5,
+  _NL_TIME_WMONTH_6,
+  _NL_TIME_WMONTH_7,
+  _NL_TIME_WMONTH_8,
+  _NL_TIME_WMONTH_9,
+  _NL_TIME_WMONTH_10,
+  _NL_TIME_WMONTH_11,
+  _NL_TIME_WMONTH_12,
+  _NL_TIME_WWDAY_1,
+  _NL_TIME_WWDAY_2,
+  _NL_TIME_WWDAY_3,
+  _NL_TIME_WWDAY_4,
+  _NL_TIME_WWDAY_5,
+  _NL_TIME_WWDAY_6,
+  _NL_TIME_WWDAY_7,
+  _NL_TIME_WWEEKDAY_1,
+  _NL_TIME_WWEEKDAY_2,
+  _NL_TIME_WWEEKDAY_3,
+  _NL_TIME_WWEEKDAY_4,
+  _NL_TIME_WWEEKDAY_5,
+  _NL_TIME_WWEEKDAY_6,
+  _NL_TIME_WWEEKDAY_7,
+  _NL_TIME_WT_FMT,
+  _NL_TIME_WD_FMT,
+  _NL_TIME_WD_T_FMT,
+  _NL_TIME_WAM_STR,
+  _NL_TIME_WPM_STR,
+  _NL_TIME_WDATE_FMT,
+  _NL_TIME_WT_FMT_AMPM,
+  _NL_TIME_WERA,
+  _NL_TIME_WERA_D_FMT,
+  _NL_TIME_WERA_D_T_FMT,
+  _NL_TIME_WERA_T_FMT,
+  _NL_TIME_WALT_DIGITS,
+
+  _NL_NUMERIC_CODESET,
+  _NL_NUMERIC_GROUPING,
+  _NL_NUMERIC_DECIMAL_POINT_WC,
+  _NL_NUMERIC_THOUSANDS_SEP_WC,
+
+  _NL_MONETARY_INT_CURR_SYMBOL,
+  _NL_MONETARY_CURRENCY_SYMBOL,
+  _NL_MONETARY_MON_DECIMAL_POINT,
+  _NL_MONETARY_MON_THOUSANDS_SEP,
+  _NL_MONETARY_MON_GROUPING,
+  _NL_MONETARY_POSITIVE_SIGN,
+  _NL_MONETARY_NEGATIVE_SIGN,
+  _NL_MONETARY_INT_FRAC_DIGITS,
+  _NL_MONETARY_FRAC_DIGITS,
+  _NL_MONETARY_P_CS_PRECEDES,
+  _NL_MONETARY_P_SEP_BY_SPACE,
+  _NL_MONETARY_N_CS_PRECEDES,
+  _NL_MONETARY_N_SEP_BY_SPACE,
+  _NL_MONETARY_P_SIGN_POSN,
+  _NL_MONETARY_N_SIGN_POSN,
+  _NL_MONETARY_INT_P_CS_PRECEDES,
+  _NL_MONETARY_INT_P_SEP_BY_SPACE,
+  _NL_MONETARY_INT_N_CS_PRECEDES,
+  _NL_MONETARY_INT_N_SEP_BY_SPACE,
+  _NL_MONETARY_INT_P_SIGN_POSN,
+  _NL_MONETARY_INT_N_SIGN_POSN,
+  _NL_MONETARY_CODESET,
+  _NL_MONETARY_WINT_CURR_SYMBOL,
+  _NL_MONETARY_WCURRENCY_SYMBOL,
+  _NL_MONETARY_WMON_DECIMAL_POINT,
+  _NL_MONETARY_WMON_THOUSANDS_SEP,
+  _NL_MONETARY_WPOSITIVE_SIGN,
+  _NL_MONETARY_WNEGATIVE_SIGN,
+
+  _NL_MESSAGES_WYESEXPR,
+  _NL_MESSAGES_WNOEXPR,
+  _NL_MESSAGES_WYESSTR,
+  _NL_MESSAGES_WNOSTR,
+
+  _NL_COLLATE_CODESET,
+
+  /* This MUST be the last entry since it's used to check for an array
+     index in nl_langinfo(). */
+  _NL_LOCALE_EXTENDED_LAST_ENTRY
+};
+
+#endif /* __HAVE_LOCALE_INFO_EXTENDED__ */
+#endif /* __HAVE_LOCALE_INFO__ */
 
 __BEGIN_DECLS
 char	*nl_langinfo(nl_item);

newlib/libc/include/stdlib.h
@@ -55,9 +55,9 @@ typedef struct
 
 #define RAND_MAX __RAND_MAX
 
-extern __IMPORT int __mb_cur_max;
+int	_EXFUN(__locale_mb_cur_max,(_VOID));
 
-#define MB_CUR_MAX __mb_cur_max
+#define MB_CUR_MAX __locale_mb_cur_max()
 
 _VOID	_EXFUN(abort,(_VOID) _ATTRIBUTE ((noreturn)));
 int	_EXFUN(abs,(int));

newlib/libc/locale/Makefile.am
@@ -17,7 +17,8 @@ ELIX_SOURCES = \
 	lnumeric.c \
 	lmonetary.c \
 	nl_langinfo.c \
-	timelocal.c
+	timelocal.c \
+	lctype.c
 endif
 
 liblocale_la_LDFLAGS = -Xcompiler -nostdlib

newlib/libc/locale/Makefile.in
@@ -62,7 +62,8 @@ am__objects_1 = lib_a-locale.$(OBJEXT)
 @ELIX_LEVEL_1_FALSE@	lib_a-lnumeric.$(OBJEXT) \
 @ELIX_LEVEL_1_FALSE@	lib_a-lmonetary.$(OBJEXT) \
 @ELIX_LEVEL_1_FALSE@	lib_a-nl_langinfo.$(OBJEXT) \
-@ELIX_LEVEL_1_FALSE@	lib_a-timelocal.$(OBJEXT)
+@ELIX_LEVEL_1_FALSE@	lib_a-timelocal.$(OBJEXT) \
+@ELIX_LEVEL_1_FALSE@	lib_a-lctype.$(OBJEXT)
 @USE_LIBTOOL_FALSE@am_lib_a_OBJECTS = $(am__objects_1) \
 @USE_LIBTOOL_FALSE@	$(am__objects_2)
 lib_a_OBJECTS = $(am_lib_a_OBJECTS)
@@ -71,7 +72,7 @@ liblocale_la_LIBADD =
 am__objects_3 = locale.lo
 @ELIX_LEVEL_1_FALSE@am__objects_4 = fix_grouping.lo ldpart.lo \
 @ELIX_LEVEL_1_FALSE@	lmessages.lo lnumeric.lo lmonetary.lo \
-@ELIX_LEVEL_1_FALSE@	nl_langinfo.lo timelocal.lo
+@ELIX_LEVEL_1_FALSE@	nl_langinfo.lo timelocal.lo lctype.lo
 @USE_LIBTOOL_TRUE@am_liblocale_la_OBJECTS = $(am__objects_3) \
 @USE_LIBTOOL_TRUE@	$(am__objects_4)
 liblocale_la_OBJECTS = $(am_liblocale_la_OBJECTS)
@@ -168,6 +169,7 @@ PACKAGE_BUGREPORT = @PACKAGE_BUGREPORT@
 PACKAGE_NAME = @PACKAGE_NAME@
 PACKAGE_STRING = @PACKAGE_STRING@
 PACKAGE_TARNAME = @PACKAGE_TARNAME@
+PACKAGE_URL = @PACKAGE_URL@
 PACKAGE_VERSION = @PACKAGE_VERSION@
 PATH_SEPARATOR = @PATH_SEPARATOR@
 RANLIB = @RANLIB@
@@ -247,7 +249,8 @@ GENERAL_SOURCES = timelocal.h ldpart.h locale.c lnumeric.h lmonetary.h lmessages
 @ELIX_LEVEL_1_FALSE@	lnumeric.c \
 @ELIX_LEVEL_1_FALSE@	lmonetary.c \
 @ELIX_LEVEL_1_FALSE@	nl_langinfo.c \
-@ELIX_LEVEL_1_FALSE@	timelocal.c
+@ELIX_LEVEL_1_FALSE@	timelocal.c \
+@ELIX_LEVEL_1_FALSE@	lctype.c
 
 @ELIX_LEVEL_1_TRUE@ELIX_SOURCES = 
 liblocale_la_LDFLAGS = -Xcompiler -nostdlib
@@ -378,6 +381,12 @@ lib_a-timelocal.o: timelocal.c
 
 lib_a-timelocal.obj: timelocal.c
 	$(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) $(lib_a_CFLAGS) $(CFLAGS) -c -o lib_a-timelocal.obj `if test -f 'timelocal.c'; then $(CYGPATH_W) 'timelocal.c'; else $(CYGPATH_W) '$(srcdir)/timelocal.c'; fi`
+
+lib_a-lctype.o: lctype.c
+	$(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) $(lib_a_CFLAGS) $(CFLAGS) -c -o lib_a-lctype.o `test -f 'lctype.c' || echo '$(srcdir)/'`lctype.c
+
+lib_a-lctype.obj: lctype.c
+	$(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) $(lib_a_CFLAGS) $(CFLAGS) -c -o lib_a-lctype.obj `if test -f 'lctype.c'; then $(CYGPATH_W) 'lctype.c'; else $(CYGPATH_W) '$(srcdir)/lctype.c'; fi`
 
 mostlyclean-libtool:
 	-rm -f *.lo

newlib/libc/locale/lctype.c
@@ -1,0 +1,108 @@
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <limits.h>
+#include "lctype.h"
+#include "ldpart.h"
+
+#define LCCTYPE_SIZE (sizeof(struct lc_ctype_T) / sizeof(char *))
+
+static char	numone[] = { '\1', '\0'};
+
+static const struct lc_ctype_T _C_ctype_locale = {
+	"ASCII",			/* codeset */
+	numone				/* mb_cur_max */
+#ifdef __HAVE_LOCALE_INFO_EXTENDED__
+	, "0", "1", "2", "3", "4",	/* outdigits */
+	"5", "6", "7", "8", "9",
+	L"0", L"1", L"2", L"3", L"4",	/* woutdigits */
+	L"5", L"6", L"7", L"8", L"9"
+#endif
+};
+
+static struct lc_ctype_T _ctype_locale;
+static int	_ctype_using_locale;
+#ifdef __HAVE_LOCALE_INFO_EXTENDED__
+static char	*_ctype_locale_buf;
+#else
+/* Max encoding_len + NUL byte + 1 byte mb_cur_max plus trailing NUL byte */
+#define _CTYPE_BUF_SIZE	(ENCODING_LEN + 3)
+static char _ctype_locale_buf[_CTYPE_BUF_SIZE];
+#endif
+
+int
+__ctype_load_locale(const char *name, void *f_wctomb, const char *charset,
+		    int mb_cur_max)
+{
+	int ret;
+
+#ifdef __CYGWIN__
+	extern int __set_lc_ctype_from_win (const char *,
+					    const struct lc_ctype_T *,
+					    struct lc_ctype_T *, char **,
+					    void *, const char *, int);
+	int old_ctype_using_locale = _ctype_using_locale;
+	_ctype_using_locale = 0;
+	ret = __set_lc_ctype_from_win (name, &_C_ctype_locale, &_ctype_locale,
+				       &_ctype_locale_buf, f_wctomb, charset,
+				       mb_cur_max);
+	/* ret == -1: error, ret == 0: C/POSIX, ret > 0: valid */
+	if (ret < 0)
+	  _ctype_using_locale = old_ctype_using_locale;
+	else
+	  {
+	    _ctype_using_locale = ret;
+	    ret = 0;
+	  }
+#elif !defined (__HAVE_LOCALE_INFO_EXTENDED__)
+	if (!strcmp (name, "C"))
+	  _ctype_using_locale = 0;
+	else
+	  {
+	    _ctype_locale.codeset = _ctype_locale_buf;
+	    _ctype_locale.mb_cur_max = _ctype_locale_buf + _CTYPE_BUF_SIZE - 2;
+	    strcpy (_ctype_locale.codeset, charset);
+	    _ctype_locale.mb_cur_max[0] = mb_cur_max;
+	    _ctype_locale.mb_cur_max[1] = '\0';
+	    _ctype_using_locale = 1;
+	  }
+	ret = 0;
+#else
+	ret = __part_load_locale(name, &_ctype_using_locale,
+		_ctype_locale_buf, "LC_CTYPE",
+		LCCTYPE_SIZE, LCCTYPE_SIZE,
+		(const char **)&_ctype_locale);
+	if (ret == 0 && _ctype_using_locale)
+		_ctype_locale.grouping =
+			__fix_locale_grouping_str(_ctype_locale.grouping);
+#endif
+	return ret;
+}
+
+struct lc_ctype_T *
+__get_current_ctype_locale(void) {
+
+	return (_ctype_using_locale
+		? &_ctype_locale
+		: (struct lc_ctype_T *)&_C_ctype_locale);
+}

newlib/libc/locale/lctype.h
@@ -1,7 +1,4 @@
-/*-
- * Copyright (c) 2000, 2001 Alexey Zelkin <phantom@FreeBSD.org>
- * All rights reserved.
- *
+/*
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -22,12 +19,10 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD: src/lib/libc/locale/lnumeric.h,v 1.3 2001/12/20 18:28:52 phantom Exp $
  */
 
-#ifndef _LNUMERIC_H_
-#define	_LNUMERIC_H_
+#ifndef _LCTYPE_H_
+#define	_LCTYPE_H_
 
 #include <_ansi.h>
 #include <sys/cdefs.h>
@@ -35,20 +30,18 @@
 
 __BEGIN_DECLS
 
-struct lc_numeric_T {
-	const char	*decimal_point;
-	const char	*thousands_sep;
-	const char	*grouping;
-#ifdef __HAVE_LOCALE_INFO_EXTENDED__
+struct lc_ctype_T {
 	const char	*codeset;	 /* codeset for mbtowc conversion */
-	const wchar_t	*wdecimal_point;
-	const wchar_t	*wthousands_sep;
+	const char	*mb_cur_max;
+#ifdef __HAVE_LOCALE_INFO_EXTENDED__
+	const char	*outdigits[10];
+	const wchar_t	*woutdigits[10];
 #endif
 };
 
-struct lc_numeric_T *__get_current_numeric_locale(void);
-int	__numeric_load_locale(const char *, void *, const char *);
+struct lc_ctype_T *__get_current_ctype_locale(void);
+int	__ctype_load_locale(const char *, void *, const char *, int);
 
 __END_DECLS
 
-#endif /* !_LNUMERIC_H_ */
+#endif /* !_LCTYPE_H_ */

newlib/libc/locale/lmessages.c
@@ -42,6 +42,13 @@ static const struct lc_messages_T _C_messages_locale = {
 	"^[nN]" ,	/* noexpr */
 	"yes" , 	/* yesstr */
 	"no"		/* nostr */
+	"ASCII"		/* codeset */
+#ifdef __HAVE_LOCALE_INFO_EXTENDED__
+	, L"^[yY]" ,	/* wyesexpr */
+	L"^[nN]" ,	/* wnoexpr */
+	L"yes" , 	/* wyesstr */
+	L"no"		/* wnostr */
+#endif
 };
 
 static struct lc_messages_T _messages_locale;
@@ -53,13 +60,15 @@ __messages_load_locale (const char *name, void *f_wctomb, const char *charset)
 {
 #ifdef __CYGWIN__
 	extern int __set_lc_messages_from_win (const char *,
+					       const struct lc_messages_T *,
 					       struct lc_messages_T *, char **,
 					       void *, const char *);
 	int ret;
 
 	int old_messages_using_locale = _messages_using_locale;
 	_messages_using_locale = 0;
-	ret = __set_lc_messages_from_win (name, &_messages_locale,
+	ret = __set_lc_messages_from_win (name, &_C_messages_locale,
+					  &_messages_locale,
 					  &_messages_locale_buf,
 					  f_wctomb, charset);
 	/* ret == -1: error, ret == 0: C/POSIX, ret > 0: valid */

commit 0e961f582a315fd6f13b8e924f58af6057558348

newlib: nl_langinfo: Fix a bug of time stuff.
Previously, e.g. nl_langinfo(_NL_TIME_WMONTH_1) returns "February"
due to the bug. Similarly, nl_langinfo(_NL_TIME_WWDAY_1) returns
"Mon". This occurs because wide char month and weekday arrays are
pointed off-by-one (e.g. the array wmon[12] is reffered as wmon[1-12]
rather than wmon[0-11]). This patch fixes that.

Fixes: d47d5b850bed ("Extend locale support to maintain wide char values of native strings")
Reviewed-by: Corinna Vinschen <corinna@vinschen.de>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

newlib/libc/locale/nl_langinfo.c
@@ -78,6 +78,7 @@ static struct _nl_item_t
   _NLITEM (ctype, woutdigits[8]),
   _NLITEM (ctype, woutdigits[9]),
   _NLITEM (time, codeset),
+  _NLITEM (time, wmon[0]),
   _NLITEM (time, wmon[1]),
   _NLITEM (time, wmon[2]),
   _NLITEM (time, wmon[3]),
@@ -89,7 +90,7 @@ static struct _nl_item_t
   _NLITEM (time, wmon[9]),
   _NLITEM (time, wmon[10]),
   _NLITEM (time, wmon[11]),
-  _NLITEM (time, wmon[12]),
+  _NLITEM (time, wmonth[0]),
   _NLITEM (time, wmonth[1]),
   _NLITEM (time, wmonth[2]),
   _NLITEM (time, wmonth[3]),
@@ -101,21 +102,20 @@ static struct _nl_item_t
   _NLITEM (time, wmonth[9]),
   _NLITEM (time, wmonth[10]),
   _NLITEM (time, wmonth[11]),
-  _NLITEM (time, wmonth[12]),
+  _NLITEM (time, wwday[0]),
   _NLITEM (time, wwday[1]),
   _NLITEM (time, wwday[2]),
   _NLITEM (time, wwday[3]),
   _NLITEM (time, wwday[4]),
   _NLITEM (time, wwday[5]),
   _NLITEM (time, wwday[6]),
-  _NLITEM (time, wwday[7]),
+  _NLITEM (time, wweekday[0]),
   _NLITEM (time, wweekday[1]),
   _NLITEM (time, wweekday[2]),
   _NLITEM (time, wweekday[3]),
   _NLITEM (time, wweekday[4]),
   _NLITEM (time, wweekday[5]),
   _NLITEM (time, wweekday[6]),
-  _NLITEM (time, wweekday[7]),
   _NLITEM (time, wX_fmt),
   _NLITEM (time, wx_fmt),
   _NLITEM (time, wc_fmt),