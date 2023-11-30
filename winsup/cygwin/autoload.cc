picolibc/picolibc

commit a3ae2a7348927a6afd88d518797a42ebc4d588c6

Cygwin: don't autoload some kernel32 functions
DiscardVirtualMemory, PrefetchVirtualMemory and
GetSystemTimePreciseAsFileTime are available since
Windows 8.1.  Merge PseudoConsole functions into
kernel32 function block.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/autoload.cc
@@ -462,10 +462,10 @@ LoadDLLfunc (GetUdpTable, 12, iphlpapi)
 LoadDLLfunc (if_indextoname, 8, iphlpapi)
 LoadDLLfunc (if_nametoindex, 4, iphlpapi)
 
-LoadDLLfuncEx2 (DiscardVirtualMemory, 8, kernel32, 1, 127)
-LoadDLLfunc (GetSystemTimePreciseAsFileTime, 4, kernel32)
+LoadDLLfuncEx (ClosePseudoConsole, 4, kernel32, 1)
+LoadDLLfuncEx (CreatePseudoConsole, 20, kernel32, 1)
 LoadDLLfuncEx (IsWow64Process2, 12, kernel32, 1)
-LoadDLLfuncEx (PrefetchVirtualMemory, 16, kernel32, 1)
+LoadDLLfuncEx (ResizePseudoConsole, 8, kernel32, 1)
 
 /* MSDN claims these are exported by kernel32.dll, but only
    QueryUnbiasedInterruptTime actually is.  The others are only
@@ -637,7 +637,4 @@ LoadDLLfunc (PdhAddEnglishCounterW, 16, pdh)
 LoadDLLfunc (PdhCollectQueryData, 4, pdh)
 LoadDLLfunc (PdhGetFormattedCounterValue, 16, pdh)
 LoadDLLfunc (PdhOpenQueryW, 12, pdh)
-LoadDLLfuncEx (CreatePseudoConsole, 20, kernel32, 1)
-LoadDLLfuncEx (ResizePseudoConsole, 8, kernel32, 1)
-LoadDLLfuncEx (ClosePseudoConsole, 4, kernel32, 1)
 }

commit 013de6b097feb94b58386a2a51c32ecab5e80db7

Cygwin: revert autoloading of DiscardVirtualMemory
Commit a3ae2a734892 ("Cygwin: don't autoload some kernel32 functions")
erroneously removed DiscardVirtualMemory from the list of autloaded
functions.  DiscardVirtualMemory is not available on Windows 8.1.

Fixes: a3ae2a734892 ("Cygwin: don't autoload some kernel32 functions")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/autoload.cc
@@ -465,6 +465,7 @@ LoadDLLfunc (GetUdpTable, iphlpapi)
 LoadDLLfunc (if_indextoname, iphlpapi)
 LoadDLLfunc (if_nametoindex, iphlpapi)
 
+LoadDLLfuncEx2 (DiscardVirtualMemory, kernel32, 1, 127)
 LoadDLLfuncEx (ClosePseudoConsole, kernel32, 1)
 LoadDLLfuncEx (CreatePseudoConsole, kernel32, 1)
 LoadDLLfuncEx (IsWow64Process2, kernel32, 1)