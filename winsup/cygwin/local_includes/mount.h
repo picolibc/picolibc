picolibc/picolibc

commit 8b01c5d6903fbcbb14c52db45c12494b53493c5a

Cygwin: fs_info: check for SSD
During fs_info::update, check for the file being on an SSD.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/local_includes/mount.h
@@ -69,6 +69,7 @@ class fs_info
     ULONG name_len;		/* MaximumComponentNameLength */
     fs_info_type fs_type;	/* Filesystem type */
     unsigned is_remote_drive		: 1;
+    unsigned is_ssd			: 1;
     unsigned has_acls			: 1;
     unsigned hasgood_inode		: 1;
     unsigned caseinsensitive		: 1;
@@ -79,6 +80,8 @@ class fs_info
   } status;
   ULONG sernum;			/* Volume Serial Number */
   char fsn[80];			/* Windows filesystem name */
+
+  void check_ssd (HANDLE);
 
  public:
   void clear ()
@@ -93,6 +96,7 @@ class fs_info
   IMPLEMENT_STATUS_FLAG (ULONG, samba_version)
   IMPLEMENT_STATUS_FLAG (ULONG, name_len)
   IMPLEMENT_STATUS_FLAG (bool, is_remote_drive)
+  IMPLEMENT_STATUS_FLAG (bool, is_ssd)
   IMPLEMENT_STATUS_FLAG (bool, has_acls)
   IMPLEMENT_STATUS_FLAG (bool, hasgood_inode)
   IMPLEMENT_STATUS_FLAG (bool, caseinsensitive)

winsup/cygwin/mount.cc
@@ -523,8 +523,22 @@ fs_info::update (PUNICODE_STRING upath, HANDLE in_vol)
   caseinsensitive ((!(flags () & FILE_CASE_SENSITIVE_SEARCH) || is_samba ())
 		   && !is_nfs ());
 
+  /* Check for being an SSD */
+  if (!is_remote_drive () && !is_cdrom ())
+    {
+      /* Theoretically FileFsVolumeFlagsInformation would be sufficient,
+	 but apparently it's not exposed into userspace. */
+      FILE_FS_SECTOR_SIZE_INFORMATION ffssi;
+
+      status = NtQueryVolumeInformationFile (vol, &io, &ffssi, sizeof ffssi,
+					     FileFsSectorSizeInformation);
+      if (NT_SUCCESS (status))
+	is_ssd (!!(ffssi.Flags & SSINFO_FLAGS_NO_SEEK_PENALTY));
+    }
+
   if (!in_vol)
     NtClose (vol);
+
   fsi_cache.add (hash, this);
   return true;
 }

commit 10da64688016c0146e049eef3533a35ff19e6b6e

Cygwin: drop stray fs_info::check_ssd declaration
This is just a leftover from development

Fixes: 8b01c5d6903f ("Cygwin: fs_info: check for SSD")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/local_includes/mount.h
@@ -81,8 +81,6 @@ class fs_info
   ULONG sernum;			/* Volume Serial Number */
   char fsn[80];			/* Windows filesystem name */
 
-  void check_ssd (HANDLE);
-
  public:
   void clear ()
   {