7382f54405aa81ca4420a58bb805ab21ef1e0b6b
Jeff Law <jeffreyalaw@gmail.com>
2023-12-22 20:19:58 -0700

    Fix v850 libgloss for c99/gcc-14
    So the prior patch for sysnecv850 was in newlib.  This time we're fixing v850
    bits in libgloss.
    
    Like other patches submitted recently, this converts implicit ints to explicit
    ints, adds a missing prototype and header includes.

--- libgloss/v850/kill.c
+++ libgloss/v850/kill.c
@@ -10,6 +10,7 @@ int __trap0 (int function, int p1, int p2, int p3);
 
 #define TRAP0(f, p1, p2, p3) __trap0(f, (int)(p1), (int)(p2), (int)(p3))
 
+int
 _kill (pid, sig)
      pid_t pid;
      int sig;
