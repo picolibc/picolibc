bb6a41ba2b0ef8fb7426b1033c0e626d4111564d
Keith Packard <keithp@keithp.com>
2024-01-05 22:33:03 -0800

    posix: Change "INFINITY" constant in regex code to REGEX_INFINITY
    Avoid using the math symbol in case that has gotten included somehow.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/posix/utils.h
+++ newlib/libc/posix/utils.h
@@ -36,7 +36,7 @@
 
 /* utility definitions */
 #define	DUPMAX		_POSIX2_RE_DUP_MAX	/* xxx is this right? */
-#define	INFINITY	(DUPMAX + 1)
+#define	REGEX_INFINITY	(DUPMAX + 1)
 #define	NC		(CHAR_MAX - CHAR_MIN + 1)
 typedef unsigned char uch;
 
