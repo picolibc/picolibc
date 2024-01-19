picolibc/picolibc

commit 09119463a1445be498f8a6ce3834d462361c7dd3

stdio: split byte- and wide-char-oriented low-level output functions
Introduce function __swbufw_r and macros/inline-functions called
__swputc_r.  Call these functions/macros exclusively from wide-char
functions.

This allows to set and test the stream orientation correctly even if
output is only performed using byte-oriented macros from stdio.h.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

newlib/libc/stdio/fputwc.c
@@ -155,7 +155,7 @@ __fputwc (struct _reent *ptr,
     }
 
   for (i = 0; i < len; i++)
-    if (__sputc_r (ptr, (unsigned char) buf[i], fp) == EOF)
+    if (__swputc_r (ptr, (unsigned char) buf[i], fp) == EOF)
       return WEOF;
 
   return (wint_t) wc;
@@ -169,10 +169,7 @@ _fputwc_r (struct _reent *ptr,
   wint_t r;
 
   _newlib_flockfile_start (fp);
-  if (ORIENT(fp, 1) != 1)
-    r = WEOF;
-  else
-    r = __fputwc(ptr, wc, fp);
+  __fputwc(ptr, wc, fp);
   _newlib_flockfile_end (fp);
   return r;
 }

newlib/libc/stdio/Makefile.inc
@@ -117,6 +117,7 @@ libc_a_SOURCES += \
 	%D%/vsprintf.c \
 	%D%/vsscanf.c \
 	%D%/wbuf.c \
+	%D%/wbufw.c \
 	%D%/wsetup.c
 
 ## The following are EL/IX level 2 interfaces

newlib/libc/stdio/fputws.c
@@ -143,7 +143,7 @@ error:
 	goto error;
       while (i < nbytes)
         {
-	  if (__sputc_r (ptr, buf[i], fp) == EOF)
+	  if (__swputc_r (ptr, buf[i], fp) == EOF)
 	    goto error;
 	  i++;
         }

newlib/libc/stdio/fvwrite.c
@@ -70,21 +70,43 @@ __sfvwrite_r (struct _reent *ptr,
   len = 0;
 
 #ifdef __SCLE
+  /* This only affects Cygwin, so calling __sputc_r *and* __swputc_r
+   * from here doesn't matter.
+   */
   if (fp->_flags & __SCLE) /* text mode */
     {
-      do
-        {
-          GETIOV (;);
-          while (len > 0)
-            {
-              if (__sputc_r (ptr, *p, fp) == EOF)
-                return EOF;
-              p++;
-              len--;
-              uio->uio_resid--;
-            }
-        }
-      while (uio->uio_resid > 0);
+      if (fp->_flags2 & __SWID)
+	{
+	  do
+	    {
+	      GETIOV (;);
+	      while (len > 0)
+		{
+		  if (__swputc_r (ptr, *p, fp) == EOF)
+		    return EOF;
+		  p++;
+		  len--;
+		  uio->uio_resid--;
+		}
+	    }
+	  while (uio->uio_resid > 0);
+	}
+      else
+	{
+	  do
+	    {
+	      GETIOV (;);
+	      while (len > 0)
+		{
+		  if (__sputc_r (ptr, *p, fp) == EOF)
+		    return EOF;
+		  p++;
+		  len--;
+		  uio->uio_resid--;
+		}
+	    }
+	  while (uio->uio_resid > 0);
+	}
       return 0;
     }
 #endif

newlib/libc/stdio/local.h
@@ -251,6 +251,39 @@ extern _READ_WRITE_RETURN_TYPE __swrite64 (struct _reent *, void *,
 #define ORIENT(fp,ori) (-1)
 #endif
 
+/* Same thing as the functions in stdio.h, but these are to be called
+   from inside the wide-char functions. */
+int	__swbufw_r (struct _reent *, int, FILE *);
+#ifdef __GNUC__
+_ELIDABLE_INLINE int __swputc_r(struct _reent *_ptr, int _c, FILE *_p) {
+#ifdef __SCLE
+	if ((_p->_flags & __SCLE) && _c == '\n')
+	  __swputc_r (_ptr, '\r', _p);
+#endif
+	if (--_p->_w >= 0 || (_p->_w >= _p->_lbfsize && (char)_c != '\n'))
+		return (*_p->_p++ = _c);
+	else
+		return (__swbufw_r(_ptr, _c, _p));
+}
+#else
+#define       __swputc_raw_r(__ptr, __c, __p) \
+	(--(__p)->_w < 0 ? \
+		(__p)->_w >= (__p)->_lbfsize ? \
+			(*(__p)->_p = (__c)), *(__p)->_p != '\n' ? \
+				(int)*(__p)->_p++ : \
+				__swbufw_r(__ptr, '\n', __p) : \
+			__swbufw_r(__ptr, (int)(__c), __p) : \
+		(*(__p)->_p = (__c), (int)*(__p)->_p++))
+#ifdef __SCLE
+#define __swputc_r(__ptr, __c, __p) \
+        ((((__p)->_flags & __SCLE) && ((__c) == '\n')) \
+          ? __swputc_raw_r(__ptr, '\r', (__p)) : 0 , \
+        __swputc_raw_r((__ptr), (__c), (__p)))
+#else
+#define __swputc_r(__ptr, __c, __p) __swputc_raw_r(__ptr, __c, __p)
+#endif
+#endif
+
 /* WARNING: _dcvt is defined in the stdlib directory, not here!  */
 
 char *_dcvt (struct _reent *, char *, double, int, int, char, int);

newlib/libc/stdio/wbufw.c
@@ -27,47 +27,28 @@ static char sccsid[] = "%W% (Berkeley) %G%";
 #include "fvwrite.h"
 
 /*
- * Write the given character into the (probably full) buffer for
- * the given file.  Flush the buffer out if it is or becomes full,
- * or if c=='\n' and the file is line buffered.
+ * Note that this is the same function as __swbuf_r, just to be called
+ * from wide-char functions!
+ *
+ * The only difference is that we set and test the orientation differently.
  */
 
 int
-__swbuf_r (struct _reent *ptr,
+__swbufw_r (struct _reent *ptr,
        register int c,
        register FILE *fp)
 {
   register int n;
 
-  /* Ensure stdio has been initialized.  */
-
   CHECK_INIT (ptr, fp);
-
-  /*
-   * In case we cannot write, or longjmp takes us out early,
-   * make sure _w is 0 (if fully- or un-buffered) or -_bf._size
-   * (if line buffered) so that we will get called again.
-   * If we did not do this, a sufficient number of putc()
-   * calls might wrap _w from negative to positive.
-   */
 
   fp->_w = fp->_lbfsize;
   if (cantwrite (ptr, fp))
     return EOF;
   c = (unsigned char) c;
 
-  if (ORIENT (fp, -1) != -1)
+  if (ORIENT (fp, 1) != 1)
     return EOF;
-
-  /*
-   * If it is completely full, flush it out.  Then, in any case,
-   * stuff c into the buffer.  If this causes the buffer to fill
-   * completely, or if c is '\n' and the file is line buffered,
-   * flush it (perhaps a second time).  The second flush will always
-   * happen on unbuffered streams, where _bf._size==1; fflush()
-   * guarantees that putc() will always call wbuf() by setting _w
-   * to 0, so we need not do anything else.
-   */
 
   n = fp->_p - fp->_bf._base;
   if (n >= fp->_bf._size)
@@ -82,14 +63,4 @@ __swbuf_r (struct _reent *ptr,
     if (_fflush_r (ptr, fp))
       return EOF;
   return c;
-}
-
-/* This function isn't any longer declared in stdio.h, but it's
-   required for backward compatibility with applications built against
-   earlier dynamically built newlib libraries. */
-int
-__swbuf (register int c,
-       register FILE *fp)
-{
-  return __swbuf_r (_REENT, c, fp);
 }

commit d13d9220bf47f7e91b02c5c8e214780bda840606

_fputwc_r: actually return result of __fputwc
Compiling with -Wall uncovered a bug in _fputwc_r introduced in
commit 09119463a1445 ("stdio: split byte- and wide-char-oriented
low-level output functions").  The underlying function __fputwc
has been accidentally called without fetching its return value.
So the return value of _fputwc_r (and thus fputwc) was undefined.

Fixes: 09119463a1445 ("stdio: split byte- and wide-char-oriented low-level output functions"
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

newlib/libc/stdio/fputwc.c
@@ -169,7 +169,7 @@ _fputwc_r (struct _reent *ptr,
   wint_t r;
 
   _newlib_flockfile_start (fp);
-  __fputwc(ptr, wc, fp);
+  r = __fputwc(ptr, wc, fp);
   _newlib_flockfile_end (fp);
   return r;
 }