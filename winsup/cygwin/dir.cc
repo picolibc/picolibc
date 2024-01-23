picolibc/picolibc

commit 3f3bd10104550243781f0b4d9248975e35d91ac7

* Throughout, use __try/__except/__endtry blocks, rather than myfault 	handler. 	* cygtls.cc (_cygtls::remove): Accommodate the fact that pathbufs 	has been moved from _local_storage to _cygtls. 	* cygtls.h (class tls_pathbuf): Add comment to hint to gendef usage 	of counters.  Change type of counters to uint32_t for clarity. 	Remove _cygtls as friend class. 	(struct _local_storage): Move pathbufs from here... 	(struct _cygtls): ...to here, allowing to access it from _sigbe. 	(class san): Only define on 32 bit.  Remove errno, _c_cnt and _w_cnt 	members. 	(san::setup): Drop parameter.  Don't initialize removed members. 	(san::leave): Don't set removed members. 	(class myfault): Only define on 32 bit. 	(myfault::faulted): Only keep implementation not taking any parameter. 	Drop argument in call to sebastian.setup. 	(__try/__leave/__except/__endtry): Implement to support real SEH.  For 	now stick to SJLJ on 32 bit. 	* dcrt0.cc (dll_crt0_0): Drop 64 bit call to 	exception::install_myfault_handler. 	* exception.h (exception_handler): Define with EXCEPTION_DISPOSITION 	as return type. 	(PDISPATCHER_CONTEXT): Define as void * on 32 bit.  Define as pointer 	to _DISPATCHER_CONTEXT on 64 bit. 	(class exception): Define separately for 32 and 64 bit. 	(exception::myfault): Add handler for myfault SEH handling on 64 bit. 	(exception::exception): Fix mangled method name to account for change 	in type of last parameter. 	(exception::install_myfault_handler): Remove. 	* exceptions.cc (exception::myfault_handle): Remove. 	(exception::myfault): New SEH handler for 64 bit. 	* gendef (_sigbe): Set tls_pathbuf counters to 0 explicitely when 	returning to the caller. 	* ntdll.h: Move a comment to a better place. 	(struct _SCOPE_TABLE): Define on 64 bit. 	* thread.cc (verifyable_object_isvalid): Remove gcc 4.7 workaround. 	* tls_pbuf.cc (tls_pbuf): Fix to accommodate new place of pathbufs. 	(tls_pathbuf::destroy): Change type of loop variables to uint32_t. 	* tls_pbuf.h (class tmp_pathbuf

winsup/cygwin/dir.cc
@@ -28,16 +28,16 @@ details. */
 extern "C" int
 dirfd (DIR *dir)
 {
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return -1;
-  if (dir->__d_cookie != __DIRENT_COOKIE)
+  __try
     {
-      set_errno (EINVAL);
+      if (dir->__d_cookie == __DIRENT_COOKIE)
+	return dir->__d_fd;
       syscall_printf ("-1 = dirfd (%p)", dir);
-      return -1;
+      set_errno (EINVAL);
     }
-  return dir->__d_fd;
+  __except (EFAULT) {}
+  __endtry
+  return -1;
 }
 
 /* Symbol kept for backward compatibility.  Don't remove.  Don't declare
@@ -93,79 +93,86 @@ fdopendir (int fd)
 static int
 readdir_worker (DIR *dir, dirent *de)
 {
-  myfault efault;
-  if (efault.faulted ())
-    return EFAULT;
+  int res = 0;
 
-  if (dir->__d_cookie != __DIRENT_COOKIE)
+  __try
     {
-      syscall_printf ("%p = readdir (%p)", NULL, dir);
-      return EBADF;
-    }
-
-  de->d_ino = 0;
-  de->d_type = DT_UNKNOWN;
-  memset (&de->__d_unused1, 0, sizeof (de->__d_unused1));
-
-  int res = ((fhandler_base *) dir->__fh)->readdir (dir, de);
-
-  if (res == ENMFILE)
-    {
-      if (!(dir->__flags & dirent_saw_dot))
+      if (dir->__d_cookie != __DIRENT_COOKIE)
 	{
-	  strcpy (de->d_name, ".");
-	  dir->__flags |= dirent_saw_dot;
-	  dir->__d_position++;
-	  res = 0;
-	}
-      else if (!(dir->__flags & dirent_saw_dot_dot))
-	{
-	  strcpy (de->d_name, "..");
-	  dir->__flags |= dirent_saw_dot_dot;
-	  dir->__d_position++;
-	  res = 0;
-	}
-    }
-
-  if (!res && !de->d_ino)
-    {
-      bool is_dot = false;
-      bool is_dot_dot = false;
-
-      if (de->d_name[0] == '.')
-	{
-	  if (de->d_name[1] == '\0')
-	    is_dot = true;
-	  else if (de->d_name[1] == '.' && de->d_name[2] == '\0')
-	    is_dot_dot = true;
+	  syscall_printf ("%p = readdir (%p)", NULL, dir);
+	  res = EBADF;
+	  __leave;
 	}
 
-      if (is_dot_dot && !(dir->__flags & dirent_isroot))
-	de->d_ino = readdir_get_ino (((fhandler_base *) dir->__fh)->get_name (),
-				     true);
-      else
+      de->d_ino = 0;
+      de->d_type = DT_UNKNOWN;
+      memset (&de->__d_unused1, 0, sizeof (de->__d_unused1));
+
+      res = ((fhandler_base *) dir->__fh)->readdir (dir, de);
+
+      if (res == ENMFILE)
 	{
-	  /* Compute d_ino by combining filename hash with directory hash. */
-	  de->d_ino = ((fhandler_base *) dir->__fh)->get_ino ();
-	  if (!is_dot && !is_dot_dot)
+	  if (!(dir->__flags & dirent_saw_dot))
 	    {
-	      PUNICODE_STRING w32name =
-		  ((fhandler_base *) dir->__fh)->pc.get_nt_native_path ();
-	      DWORD devn = ((fhandler_base *) dir->__fh)->get_device ();
-	      /* Paths below /proc don't have a Win32 pendant. */
-	      if (isproc_dev (devn))
-		de->d_ino = hash_path_name (de->d_ino, L"/");
-	      else if (w32name->Buffer[w32name->Length / sizeof (WCHAR) - 1]
-		       != L'\\')
-		de->d_ino = hash_path_name (de->d_ino, L"\\");
-	      de->d_ino = hash_path_name (de->d_ino, de->d_name);
+	      strcpy (de->d_name, ".");
+	      dir->__flags |= dirent_saw_dot;
+	      dir->__d_position++;
+	      res = 0;
+	    }
+	  else if (!(dir->__flags & dirent_saw_dot_dot))
+	    {
+	      strcpy (de->d_name, "..");
+	      dir->__flags |= dirent_saw_dot_dot;
+	      dir->__d_position++;
+	      res = 0;
 	    }
 	}
-    }
-  /* This fills out the old 32 bit d_ino field for old applications,
-     build under Cygwin before 1.5.x. */
-  de->__d_internal1 = de->d_ino;
 
+      if (!res && !de->d_ino)
+	{
+	  bool is_dot = false;
+	  bool is_dot_dot = false;
+
+	  if (de->d_name[0] == '.')
+	    {
+	      if (de->d_name[1] == '\0')
+		is_dot = true;
+	      else if (de->d_name[1] == '.' && de->d_name[2] == '\0')
+		is_dot_dot = true;
+	    }
+
+	  if (is_dot_dot && !(dir->__flags & dirent_isroot))
+	    de->d_ino = readdir_get_ino (((fhandler_base *)
+	    				 dir->__fh)->get_name (),
+					 true);
+	  else
+	    {
+	      /* Compute d_ino by combining filename hash with directory hash. */
+	      de->d_ino = ((fhandler_base *) dir->__fh)->get_ino ();
+	      if (!is_dot && !is_dot_dot)
+		{
+		  PUNICODE_STRING w32name =
+		      ((fhandler_base *) dir->__fh)->pc.get_nt_native_path ();
+		  DWORD devn = ((fhandler_base *) dir->__fh)->get_device ();
+		  /* Paths below /proc don't have a Win32 pendant. */
+		  if (isproc_dev (devn))
+		    de->d_ino = hash_path_name (de->d_ino, L"/");
+		  else if (w32name->Buffer[w32name->Length / sizeof (WCHAR) - 1]
+			   != L'\\')
+		    de->d_ino = hash_path_name (de->d_ino, L"\\");
+		  de->d_ino = hash_path_name (de->d_ino, de->d_name);
+		}
+	    }
+	}
+      /* This fills out the old 32 bit d_ino field for old applications,
+	 build under Cygwin before 1.5.x. */
+      de->__d_internal1 = de->d_ino;
+    }
+  __except (NO_ERROR)
+    {
+      res = EFAULT;
+    }
+  __endtry
   return res;
 }
 
@@ -200,16 +207,15 @@ readdir_r (DIR *__restrict dir, dirent *__restrict de, dirent **__restrict ode)
 extern "C" long
 telldir (DIR *dir)
 {
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return -1;
-
-  if (dir->__d_cookie != __DIRENT_COOKIE)
+  __try
     {
+      if (dir->__d_cookie == __DIRENT_COOKIE)
+	return ((fhandler_base *) dir->__fh)->telldir (dir);
       set_errno (EBADF);
-      return -1;
     }
-  return ((fhandler_base *) dir->__fh)->telldir (dir);
+  __except (EFAULT) {}
+  __endtry
+  return -1;
 }
 
 /* telldir was never defined using off_t in POSIX, only in early versions
@@ -225,14 +231,17 @@ telldir64 (DIR *dir)
 extern "C" void
 seekdir (DIR *dir, long loc)
 {
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return;
-
-  if (dir->__d_cookie != __DIRENT_COOKIE)
-    return;
-  dir->__flags &= dirent_info_mask;
-  return ((fhandler_base *) dir->__fh)->seekdir (dir, loc);
+  __try
+    {
+      if (dir->__d_cookie == __DIRENT_COOKIE)
+	{
+	  dir->__flags &= dirent_info_mask;
+	  ((fhandler_base *) dir->__fh)->seekdir (dir, loc);
+	}
+      set_errno (EINVAL);	/* Diagnosis */
+    }
+  __except (EFAULT) {}
+  __endtry
 }
 
 /* seekdir was never defined using off_t in POSIX, only in early versions
@@ -248,42 +257,45 @@ seekdir64 (DIR *dir, off_t loc)
 extern "C" void
 rewinddir (DIR *dir)
 {
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return;
-
-  if (dir->__d_cookie != __DIRENT_COOKIE)
-    return;
-  dir->__flags &= dirent_info_mask;
-  return ((fhandler_base *) dir->__fh)->rewinddir (dir);
+  __try
+    {
+      if (dir->__d_cookie == __DIRENT_COOKIE)
+	{
+	  dir->__flags &= dirent_info_mask;
+	  ((fhandler_base *) dir->__fh)->rewinddir (dir);
+	}
+      set_errno (EINVAL);	/* Diagnosis */
+    }
+  __except (EFAULT) {}
+  __endtry
 }
 
 /* closedir: POSIX 5.1.2.1 */
 extern "C" int
 closedir (DIR *dir)
 {
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return -1;
-
-  if (dir->__d_cookie != __DIRENT_COOKIE)
+  __try
     {
+      if (dir->__d_cookie == __DIRENT_COOKIE)
+	{
+	  /* Reset the marker in case the caller tries to use `dir' again.  */
+	  dir->__d_cookie = 0;
+
+	  int res = ((fhandler_base *) dir->__fh)->closedir (dir);
+
+	  close (dir->__d_fd);
+	  free (dir->__d_dirname);
+	  free (dir->__d_dirent);
+	  free (dir);
+	  syscall_printf ("%R = closedir(%p)", res, dir);
+	  return res;
+	}
       set_errno (EBADF);
-      syscall_printf ("%R = closedir(%p)", -1, dir);
-      return -1;
     }
-
-  /* Reset the marker in case the caller tries to use `dir' again.  */
-  dir->__d_cookie = 0;
-
-  int res = ((fhandler_base *) dir->__fh)->closedir (dir);
-
-  close (dir->__d_fd);
-  free (dir->__d_dirname);
-  free (dir->__d_dirent);
-  free (dir);
-  syscall_printf ("%R = closedir(%p)", res, dir);
-  return res;
+  __except (EFAULT) {}
+  __endtry
+  syscall_printf ("%R = closedir(%p)", -1, dir);
+  return -1;
 }
 
 /* mkdir: POSIX 5.4.1.1 */
@@ -294,43 +306,42 @@ mkdir (const char *dir, mode_t mode)
   fhandler_base *fh = NULL;
   tmp_pathbuf tp;
 
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return -1;
-
-  /* POSIX says mkdir("symlink-to-missing/") should create the
-     directory "missing", but Linux rejects it with EEXIST.  Copy
-     Linux behavior for now.  */
-
-  if (!*dir)
+  __try
     {
-      set_errno (ENOENT);
-      goto done;
-    }
-  if (isdirsep (dir[strlen (dir) - 1]))
-    {
-      /* This converts // to /, but since both give EEXIST, we're okay.  */
-      char *buf;
-      char *p = stpcpy (buf = tp.c_get (), dir) - 1;
-      dir = buf;
-      while (p > dir && isdirsep (*p))
-	*p-- = '\0';
-    }
-  if (!(fh = build_fh_name (dir, PC_SYM_NOFOLLOW)))
-    goto done;   /* errno already set */;
+      /* POSIX says mkdir("symlink-to-missing/") should create the
+	 directory "missing", but Linux rejects it with EEXIST.  Copy
+	 Linux behavior for now.  */
 
-  if (fh->error ())
-    {
-      debug_printf ("got %d error from build_fh_name", fh->error ());
-      set_errno (fh->error ());
-    }
-  else if (has_dot_last_component (dir, true))
-    set_errno (fh->exists () ? EEXIST : ENOENT);
-  else if (!fh->mkdir (mode))
-    res = 0;
-  delete fh;
+      if (!*dir)
+	{
+	  set_errno (ENOENT);
+	  __leave;
+	}
+      if (isdirsep (dir[strlen (dir) - 1]))
+	{
+	  /* This converts // to /, but since both give EEXIST, we're okay.  */
+	  char *buf;
+	  char *p = stpcpy (buf = tp.c_get (), dir) - 1;
+	  dir = buf;
+	  while (p > dir && isdirsep (*p))
+	    *p-- = '\0';
+	}
+      if (!(fh = build_fh_name (dir, PC_SYM_NOFOLLOW)))
+	__leave;   /* errno already set */;
 
- done:
+      if (fh->error ())
+	{
+	  debug_printf ("got %d error from build_fh_name", fh->error ());
+	  set_errno (fh->error ());
+	}
+      else if (has_dot_last_component (dir, true))
+	set_errno (fh->exists () ? EEXIST : ENOENT);
+      else if (!fh->mkdir (mode))
+	res = 0;
+      delete fh;
+    }
+  __except (EFAULT) {}
+  __endtry
   syscall_printf ("%R = mkdir(%s, %d)", res, dir, mode);
   return res;
 }
@@ -342,30 +353,28 @@ rmdir (const char *dir)
   int res = -1;
   fhandler_base *fh = NULL;
 
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return -1;
-
-  if (!(fh = build_fh_name (dir, PC_SYM_NOFOLLOW)))
-    goto done;   /* errno already set */;
-
-  if (fh->error ())
+  __try
     {
-      debug_printf ("got %d error from build_fh_name", fh->error ());
-      set_errno (fh->error ());
+      if (!(fh = build_fh_name (dir, PC_SYM_NOFOLLOW)))
+	__leave;   /* errno already set */;
+
+      if (fh->error ())
+	{
+	  debug_printf ("got %d error from build_fh_name", fh->error ());
+	  set_errno (fh->error ());
+	}
+      else if (!fh->exists ())
+	set_errno (ENOENT);
+      else if (has_dot_last_component (dir, false))
+	set_errno (EINVAL);
+      else if (isdev_dev (fh->dev ()))
+	set_errno (ENOTEMPTY);
+      else if (!fh->rmdir ())
+	res = 0;
+      delete fh;
     }
-  else if (!fh->exists ())
-    set_errno (ENOENT);
-  else if (has_dot_last_component (dir, false))
-    set_errno (EINVAL);
-  else if (isdev_dev (fh->dev ()))
-    set_errno (ENOTEMPTY);
-  else if (!fh->rmdir ())
-    res = 0;
-
-  delete fh;
-
- done:
+  __except (EFAULT) {}
+  __endtry
   syscall_printf ("%R = rmdir(%s)", res, dir);
   return res;
 }

winsup/cygwin/cygheap.cc
@@ -638,10 +638,7 @@ init_cygheap::find_tls (int sig, bool& issig_wait)
   _cygtls *t = NULL;
   issig_wait = false;
 
-  myfault efault;
-  if (efault.faulted ())
-    threadlist[ix]->remove (INFINITE);
-  else
+  __try
     {
       ix = -1;
       /* Scan thread list looking for valid signal-delivery candidates */
@@ -652,11 +649,15 @@ init_cygheap::find_tls (int sig, bool& issig_wait)
 	  {
 	    t = cygheap->threadlist[ix];
 	    issig_wait = true;
-	    goto out;
+	    __leave;
 	  }
 	else if (!t && !sigismember (&(threadlist[ix]->sigmask), sig))
 	  t = cygheap->threadlist[ix];
     }
-out:
+  __except (NO_ERROR)
+    {
+      threadlist[ix]->remove (INFINITE);
+    }
+  __endtry
   return t;
 }

winsup/cygwin/cygtls.cc
@@ -193,7 +193,7 @@ _cygtls::remove (DWORD wait)
   free_local (servent_buf);
   free_local (hostent_buf);
   /* Free temporary TLS path buffers. */
-  locals.pathbufs.destroy ();
+  pathbufs.destroy ();
   /* Close timer handle. */
   if (locals.cw_timer)
     NtClose (locals.cw_timer);

winsup/cygwin/cygtls.h
@@ -44,18 +44,21 @@ details. */
 #else
 #pragma pack(push,4)
 #endif
+
 /* Defined here to support auto rebuild of tlsoffsets.h. */
 class tls_pathbuf
 {
-  int c_cnt;
-  int w_cnt;
+  /* Make sure that c_cnt and w_cnt are always the first two members of this
+     class, and never change the size (32 bit), unless you also change the
+     mov statements in sigbe! */
+  uint32_t c_cnt;
+  uint32_t w_cnt;
   char  *c_buf[TP_NUM_C_BUFS];
   WCHAR *w_buf[TP_NUM_W_BUFS];
 
 public:
   void destroy ();
   friend class tmp_pathbuf;
-  friend class _cygtls;
   friend class san;
 };
 
@@ -129,8 +132,6 @@ struct _local_storage
   /* thread.cc */
   HANDLE cw_timer;
 
-  /* All functions requiring temporary path buffers. */
-  tls_pathbuf pathbufs;
   char ttybuf[32];
 };
 
@@ -188,6 +189,7 @@ public:
   struct pthread *tid;
   class cygthread *_ctinfo;
   class san *andreas;
+  tls_pathbuf pathbufs;
   waitq wq;
   int sig;
   unsigned incyg;
@@ -281,34 +283,25 @@ const int CYGTLS_PADSIZE = 12700;
 extern PVOID _tlsbase __asm__ ("%fs:4");
 extern PVOID _tlstop __asm__ ("%fs:8");
 #endif
+
 #define _my_tls (*((_cygtls *) ((char *)_tlsbase - CYGTLS_PADSIZE)))
 extern _cygtls *_main_tls;
 extern _cygtls *_sig_tls;
 
+#ifndef __x86_64__
 class san
 {
   san *_clemente;
   jmp_buf _context;
-  int _errno;
-  unsigned _c_cnt;
-  unsigned _w_cnt;
 public:
-  int setup (int myerrno = 0) __attribute__ ((always_inline))
+  int setup () __attribute__ ((always_inline))
   {
     _clemente = _my_tls.andreas;
     _my_tls.andreas = this;
-    _errno = myerrno;
-    _c_cnt = _my_tls.locals.pathbufs.c_cnt;
-    _w_cnt = _my_tls.locals.pathbufs.w_cnt;
     return __sjfault (_context);
   }
   void leave () __attribute__ ((always_inline))
   {
-    if (_errno)
-      set_errno (_errno);
-    /* Restore tls_pathbuf counters in case of error. */
-    _my_tls.locals.pathbufs.c_cnt = _c_cnt;
-    _my_tls.locals.pathbufs.w_cnt = _w_cnt;
     __ljfault (_context, 1);
   }
   void reset () __attribute__ ((always_inline))
@@ -324,17 +317,73 @@ public:
   ~myfault () __attribute__ ((always_inline)) { sebastian.reset (); }
   inline int faulted () __attribute__ ((always_inline))
   {
-    return sebastian.setup (0);
-  }
-  inline int faulted (void const *obj, int myerrno = 0) __attribute__ ((always_inline))
-  {
-    return !obj || !(*(const char **) obj) || sebastian.setup (myerrno);
-  }
-  inline int faulted (int myerrno) __attribute__ ((always_inline))
-  {
-    return sebastian.setup (myerrno);
+    return sebastian.setup ();
   }
 };
+#endif
+
+/* Exception handling macros.  These are required because SEH differs a lot
+   between 32 and 64 bit.  Essentially, on 64 bit, we have to create compile
+   time SEH tables which define the handler and try/except labels, while on
+   32 bit we can simply set up an SJLJ handler within the myfault class. */
+#define __mem_barrier	__asm__ __volatile__ ("" ::: "memory")
+#ifdef __x86_64__
+#define __try \
+  { \
+    __label__ __l_try, __l_except, __l_endtry; \
+    __mem_barrier; \
+    __asm__ goto ("\n" \
+      "  .seh_handler _ZN9exception7myfaultEP17_EXCEPTION_RECORDPvP8_CONTEXTP19_DISPATCHER_CONTEXT, @except						\n" \
+      "  .seh_handlerdata						\n" \
+      "  .long 1							\n" \
+      "  .rva %l[__l_try],%l[__l_endtry],%l[__l_except],%l[__l_except]	\n" \
+      "  .seh_code							\n" \
+      : : : : __l_try, __l_endtry, __l_except); \
+    { \
+      __l_try: \
+	__mem_barrier;
+
+#define __leave	\
+      goto __l_endtry
+
+#define __except(__errno) \
+      goto __l_endtry; \
+    } \
+    { \
+      __l_except: \
+	__mem_barrier; \
+	if (__errno) \
+	  set_errno (__errno);
+
+#define __endtry \
+    } \
+    __l_endtry: \
+      __mem_barrier; \
+  }
+
+#else /* !__x86_64__ */
+#define __try \
+  { \
+    myfault efault; \
+    if (!efault.faulted ()) \
+      {
+
+#define __leave	\
+	goto __l_endtry
+
+#define __except(__errno) \
+	  goto __l_endtry; \
+      } \
+      { \
+	if (__errno) \
+	  set_errno (__errno);
+
+#define __endtry \
+      } \
+    __l_endtry: \
+      __mem_barrier; \
+  }
+#endif /* __x86_64__ */
 
 class set_signal_arrived
 {

winsup/cygwin/dcrt0.cc
@@ -796,10 +796,6 @@ dll_crt0_0 ()
 
   _main_tls = &_my_tls;
 
-#ifdef __x86_64__
-  exception::install_myfault_handler ();
-#endif
-
   /* Initialize signal processing here, early, in the hopes that the creation
      of a thread early in the process will cause more predictability in memory
      layout for the main thread. */

winsup/cygwin/environ.cc
@@ -2,7 +2,7 @@
    process's environment.
 
    Copyright 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
-   2008, 2009, 2010, 2011, 2012, 2013 Red Hat, Inc.
+   2008, 2009, 2010, 2011, 2012, 2013, 2014 Red Hat, Inc.
 
 This software is a copyrighted work licensed under the terms of the
 Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
@@ -664,19 +664,22 @@ _addenv (const char *name, const char *value, int overwrite)
 extern "C" int
 putenv (char *str)
 {
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return -1;
-  if (*str)
+  __try
     {
-      char *eq = strchr (str, '=');
-      if (eq)
-	return _addenv (str, eq + 1, -1);
+      if (*str)
+	{
+	  char *eq = strchr (str, '=');
+	  if (eq)
+	    return _addenv (str, eq + 1, -1);
 
-      /* Remove str from the environment. */
-      unsetenv (str);
+	  /* Remove str from the environment. */
+	  unsetenv (str);
+	}
+      return 0;
     }
-  return 0;
+  __except (EFAULT) {}
+  __endtry
+  return -1;
 }
 
 /* Set the value of the environment variable "name" to be
@@ -684,15 +687,18 @@ putenv (char *str)
 extern "C" int
 setenv (const char *name, const char *value, int overwrite)
 {
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return -1;
-  if (!name || !*name || strchr (name, '='))
+  __try
     {
-      set_errno (EINVAL);
-      return -1;
+      if (!name || !*name || strchr (name, '='))
+	{
+	  set_errno (EINVAL);
+	  __leave;
+	}
+      return _addenv (name, value, !!overwrite);
     }
-  return _addenv (name, value, !!overwrite);
+  __except (EFAULT) {}
+  __endtry
+  return -1;
 }
 
 /* Delete environment variable "name".  */
@@ -701,22 +707,26 @@ unsetenv (const char *name)
 {
   register char **e;
   int offset;
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return -1;
-  if (!name || *name == '\0' || strchr (name, '='))
+
+  __try
     {
-      set_errno (EINVAL);
-      return -1;
+      if (!name || *name == '\0' || strchr (name, '='))
+	{
+	  set_errno (EINVAL);
+	  __leave;
+	}
+
+      while (my_findenv (name, &offset))	/* if set multiple times */
+	/* Move up the rest of the array */
+	for (e = cur_environ () + offset; ; e++)
+	  if (!(*e = *(e + 1)))
+	    break;
+
+      return 0;
     }
-
-  while (my_findenv (name, &offset))	/* if set multiple times */
-    /* Move up the rest of the array */
-    for (e = cur_environ () + offset; ; e++)
-      if (!(*e = *(e + 1)))
-	break;
-
-  return 0;
+  __except (EFAULT) {}
+  __endtry
+  return -1;
 }
 
 /* Minimal list of Windows vars which must be converted to uppercase.
@@ -822,96 +832,101 @@ environ_init (char **envp, int envc)
   bool envp_passed_in;
   bool got_something_from_registry;
   static char NO_COPY cygterm[] = "TERM=cygwin";
-  myfault efault;
   tmp_pathbuf tp;
 
-  if (efault.faulted ())
-    api_fatal ("internal error reading the windows environment - too many environment variables?");
-
-  char *tmpbuf = tp.t_get ();
-  got_something_from_registry = regopt (L"default", tmpbuf);
-  if (myself->progname[0])
-    got_something_from_registry = regopt (myself->progname, tmpbuf)
-				  || got_something_from_registry;
-
-  if (!envp)
-    envp_passed_in = 0;
-  else
+  __try
     {
-      envc++;
-      envc *= sizeof (char *);
-      char **newenv = (char **) malloc (envc);
-      memcpy (newenv, envp, envc);
-      cfree (envp);
+      char *tmpbuf = tp.t_get ();
+      got_something_from_registry = regopt (L"default", tmpbuf);
+      if (myself->progname[0])
+	got_something_from_registry = regopt (myself->progname, tmpbuf)
+				      || got_something_from_registry;
 
-      /* Older applications relied on the fact that cygwin malloced elements of the
-	 environment list.  */
-      envp = newenv;
-      if (ENVMALLOC)
-	for (char **e = newenv; *e; e++)
-	  {
-	    char *p = *e;
-	    *e = strdup (p);
-	    cfree (p);
-	  }
-      envp_passed_in = 1;
-      goto out;
+      if (!envp)
+	envp_passed_in = 0;
+      else
+	{
+	  envc++;
+	  envc *= sizeof (char *);
+	  char **newenv = (char **) malloc (envc);
+	  memcpy (newenv, envp, envc);
+	  cfree (envp);
+
+	  /* Older applications relied on the fact that cygwin malloced elements of the
+	     environment list.  */
+	  envp = newenv;
+	  if (ENVMALLOC)
+	    for (char **e = newenv; *e; e++)
+	      {
+		char *p = *e;
+		*e = strdup (p);
+		cfree (p);
+	      }
+	  envp_passed_in = 1;
+	  goto out;
+	}
+
+      /* Allocate space for environment + trailing NULL + CYGWIN env. */
+      lastenviron = envp = (char **) malloc ((4 + (envc = 100)) * sizeof (char *));
+
+      rawenv = GetEnvironmentStringsW ();
+      if (!rawenv)
+	{
+	  system_printf ("GetEnvironmentStrings returned NULL, %E");
+	  return;
+	}
+      debug_printf ("GetEnvironmentStrings returned %p", rawenv);
+
+      /* Current directory information is recorded as variables of the
+	 form "=X:=X:\foo\bar; these must be changed into something legal
+	 (we could just ignore them but maybe an application will
+	 eventually want to use them).  */
+      for (i = 0, w = rawenv; *w != L'\0'; w = wcschr (w, L'\0') + 1, i++)
+	{
+	  sys_wcstombs_alloc (&newp, HEAP_NOTHEAP, w);
+	  if (i >= envc)
+	    envp = (char **) realloc (envp, (4 + (envc += 100)) * sizeof (char *));
+	  envp[i] = newp;
+	  if (*newp == '=')
+	    *newp = '!';
+	  char *eq = strchrnul (newp, '=');
+	  ucenv (newp, eq);	/* uppercase env vars which need it */
+	  if (*newp == 'T' && strncmp (newp, "TERM=", 5) == 0)
+	    sawTERM = 1;
+	  else if (*newp == 'C' && strncmp (newp, "CYGWIN=", 7) == 0)
+	    parse_options (newp + 7);
+	  if (*eq)
+	    posify_maybe (envp + i, *++eq ? eq : --eq, tmpbuf);
+	  debug_printf ("%p: %s", envp[i], envp[i]);
+	}
+
+      if (!sawTERM)
+	envp[i++] = strdup (cygterm);
+      envp[i] = NULL;
+      FreeEnvironmentStringsW (rawenv);
+
+    out:
+      findenv_func = (char * (*)(const char*, int*)) my_findenv;
+      __cygwin_environ = envp;
+      update_envptrs ();
+      if (envp_passed_in)
+	{
+	  p = getenv ("CYGWIN");
+	  if (p)
+	    parse_options (p);
+	}
+
+      if (got_something_from_registry)
+	parse_options (NULL);	/* possibly export registry settings to
+				       environment */
+      MALLOC_CHECK;
     }
-
-  /* Allocate space for environment + trailing NULL + CYGWIN env. */
-  lastenviron = envp = (char **) malloc ((4 + (envc = 100)) * sizeof (char *));
-
-  rawenv = GetEnvironmentStringsW ();
-  if (!rawenv)
+  __except (NO_ERROR)
     {
-      system_printf ("GetEnvironmentStrings returned NULL, %E");
-      return;
+      api_fatal ("internal error reading the windows environment "
+		 "- too many environment variables?");
     }
-  debug_printf ("GetEnvironmentStrings returned %p", rawenv);
-
-  /* Current directory information is recorded as variables of the
-     form "=X:=X:\foo\bar; these must be changed into something legal
-     (we could just ignore them but maybe an application will
-     eventually want to use them).  */
-  for (i = 0, w = rawenv; *w != L'\0'; w = wcschr (w, L'\0') + 1, i++)
-    {
-      sys_wcstombs_alloc (&newp, HEAP_NOTHEAP, w);
-      if (i >= envc)
-	envp = (char **) realloc (envp, (4 + (envc += 100)) * sizeof (char *));
-      envp[i] = newp;
-      if (*newp == '=')
-	*newp = '!';
-      char *eq = strchrnul (newp, '=');
-      ucenv (newp, eq);	/* uppercase env vars which need it */
-      if (*newp == 'T' && strncmp (newp, "TERM=", 5) == 0)
-	sawTERM = 1;
-      else if (*newp == 'C' && strncmp (newp, "CYGWIN=", 7) == 0)
-	parse_options (newp + 7);
-      if (*eq)
-	posify_maybe (envp + i, *++eq ? eq : --eq, tmpbuf);
-      debug_printf ("%p: %s", envp[i], envp[i]);
-    }
-
-  if (!sawTERM)
-    envp[i++] = strdup (cygterm);
-  envp[i] = NULL;
-  FreeEnvironmentStringsW (rawenv);
-
-out:
-  findenv_func = (char * (*)(const char*, int*)) my_findenv;
-  __cygwin_environ = envp;
-  update_envptrs ();
-  if (envp_passed_in)
-    {
-      p = getenv ("CYGWIN");
-      if (p)
-	parse_options (p);
-    }
-
-  if (got_something_from_registry)
-    parse_options (NULL);	/* possibly export registry settings to
-				   environment */
-  MALLOC_CHECK;
+  __endtry
 }
 
 /* Function called by qsort to sort environment strings.  */

commit 02572ff089dcefed1e2e25f420be975f1ee7340c

Cygwin: seekdir: don't set errno
Commit 3f3bd1010455 ("* Throughout, use __try/__except/__endtry blocks [...]")
introduced setting EINVAL, marked as "Diagnosis".  The reason
for this is lost in time and space, but looks very much like
a debug helper which was supposed to be removed before release.
It's rather pointless, so remove it.

Fixes: 3f3bd1010455 ("* Throughout, use __try/__except/__endtry blocks [...]")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/dir.cc
@@ -235,7 +235,6 @@ seekdir (DIR *dir, long loc)
 	  dir->__flags &= dirent_info_mask;
 	  ((fhandler_base *) dir->__fh)->seekdir (dir, loc);
 	}
-      set_errno (EINVAL);	/* Diagnosis */
     }
   __except (EFAULT) {}
   __endtry