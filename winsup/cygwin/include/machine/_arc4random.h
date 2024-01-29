picolibc/picolibc

commit 030a762535c1e18bf5a7ecc73b0f49898a30b157

Cygwin: fix arc4random after fork(2)
After using fork(), arc4random does not reseed itself, which
causes the results to become predictable.  Activate droppingfork-recognition

Fixes: e0fc33322d50 ("Delete Cygwin's arc4random in favor of new Newlib implementation")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/include/machine/_arc4random.h
@@ -25,4 +25,15 @@ extern int __isthreaded;
 	    __lock_release (_arc4random_mutex);	\
         } while (0)
 
+static inline void
+_arc4random_forkdetect(void)
+{
+  extern int __in_forkee;
+
+  if (__in_forkee)
+    rs = NULL;
+}
+#define _ARC4RANDOM_FORKDETECT() _arc4random_forkdetect ()
+
+__END_DECLS
 #endif /* _MACHINE_ARC4RANDOM_H */

winsup/cygwin/release/3.5.0
@@ -67,3 +67,9 @@ What changed:
 
 - The default RLIMIT_CORE is now 0, disabling the generation of core dump or
   stackdump files.
+
+Fixes:
+------
+
+- Fix arc4random reseeding after fork(2).
+  Addresses: https://cygwin.com/pipermail/cygwin/2024-January/255245.html

commit 6252fd0265347032c46c95367895ddb838856bd2

Cygwin: machine/_arc4random.h: Fix copy/paste bug
Remove a stray  __END_DECLS.  It didn't hurt in the only
(plain C) file including this header, but still...

Fixes: 030a762535c1 ("Cygwin: fix arc4random after fork(2)")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/include/machine/_arc4random.h
@@ -35,5 +35,4 @@ _arc4random_forkdetect(void)
 }
 #define _ARC4RANDOM_FORKDETECT() _arc4random_forkdetect ()
 
-__END_DECLS
 #endif /* _MACHINE_ARC4RANDOM_H */