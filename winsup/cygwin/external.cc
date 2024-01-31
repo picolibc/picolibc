picolibc/picolibc

commit 831d6fa520ed43d055924607a2d9be7fcc3813e9

* external.cc (cygwin_internal): Implement CW_CMDLINE. * pinfo.h (SIGCOMMUNE): New signal type. (commune_result): New structure for commune functions. (picom): New enum for commune functions. (_pinfo::hello_pid): New.  Pid who's communicating with me. (_pinfo::tothem): New.  Handle of communicating pipe. (_pinfo::fromthem): Ditto. (_pinfo::commune_recv): Declare. (_pinfo::commune_send): Declare. (_pinfo::alive): Declare. (_pinfo::cmdline): Declare. (_pinfo::lock): Declare. * pinfo.cc (set_myself): Initialize new _pinfo lock. (_pinfo::alive): Define.  Determines if process still exists. (_pinfo::commune_recv): Define.  Receive info from another cooperating process. (_pinfo::commune_send): Define.  Send info to another cooperating process. (_pinfo::cmdline): Define.  Determine command line of a given process. * include/sys/cygwin.h (CW_CMDLINE): Define. *sigproc.cc (talktome): Communicate with any processes who want to talk to me. (wait_sig): Honor __SIGCOMMUNE. * fhandler.cc (fhandler_virtual::fixup_after_exec): Declare. * fhandler_proc.cc: Use malloc/free/realloc throughout rather than cmalloc since buffers don't need to be propagated to subprocesses. * fhandler_registry.cc: Ditto. * fhandler_virtual.cc: Ditto. (fhandler_virtual::fixup_after_exec): Define. * fhandler_process.cc: Ditto for malloc/free/realloc. (process_listin): Add "cmdline". (fhandler_process::fill_filebuf): Implement PROCESS_CMDLINE. * miscfuncs.cc (isalpha_array): New array populated with xor values for alpha characters to switch from one case to another. * string.h (cygwin_strcasematch): New asm implementation of case match. * string.h (cygwin_nstrcasematch): New asm implementation of counted case match.

winsup/cygwin/external.cc
@@ -241,6 +241,13 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	  extract_nt_dom_user (pw, domain, user);
 	  return 0;
 	}
+      case CW_CMDLINE:
+	{
+	  size_t n;
+	  pid_t pid = va_arg (arg, pid_t);
+	  pinfo p (pid);
+	  return (DWORD) p->cmdline (n);
+	}
       default:
 	return (DWORD) -1;
     }

winsup/cygwin/include/sys/cygwin.h
@@ -70,7 +70,8 @@ typedef enum
     CW_STRACE_TOGGLE,
     CW_STRACE_ACTIVE,
     CW_CYGWIN_PID_TO_WINPID,
-    CW_EXTRACT_DOMAIN_AND_USER
+    CW_EXTRACT_DOMAIN_AND_USER,
+    CW_CMDLINE
   } cygwin_getinfo_types;
 
 #define CW_NEXTPID	0x80000000	// or with pid to get next one

winsup/cygwin/fhandler.h
@@ -1107,6 +1107,7 @@ class fhandler_virtual : public fhandler_base
   int close (void);
   int __stdcall fstat (struct stat *buf, path_conv *pc) __attribute__ ((regparm (3)));
   virtual bool fill_filebuf ();
+  void fixup_after_exec (HANDLE);
 };
 
 class fhandler_proc: public fhandler_virtual

winsup/cygwin/fhandler_proc.cc
@@ -327,7 +327,7 @@ fhandler_proc::fill_filebuf ()
 	    uname (&uts_name);
 		bufalloc = strlen (uts_name.sysname) + 1 + strlen (uts_name.release) +
 			  1 + strlen (uts_name.version) + 2;
-	    filebuf = (char *) cmalloc (HEAP_BUF, bufalloc);
+	    filebuf = (char *) realloc (filebuf, bufalloc);
 		filesize = __small_sprintf (filebuf, "%s %s %s\n", uts_name.sysname,
 			     uts_name.release, uts_name.version);
 	  }
@@ -335,15 +335,13 @@ fhandler_proc::fill_filebuf ()
       }
     case PROC_UPTIME:
       {
-	if (!filebuf)
-	  filebuf = (char *) cmalloc (HEAP_BUF, bufalloc = 80);
+	filebuf = (char *) realloc (filebuf, bufalloc = 80);
 	filesize = format_proc_uptime (filebuf, bufalloc);
 	break;
       }
     case PROC_STAT:
       {
-	if (!filebuf)
-	  filebuf = (char *) cmalloc (HEAP_BUF, bufalloc = 2048);
+	filebuf = (char *) realloc (filebuf, bufalloc = 2048);
 	filesize = format_proc_stat (filebuf, bufalloc);
 	break;
       }
@@ -354,16 +352,14 @@ fhandler_proc::fill_filebuf ()
 	 * Windows 95/98/me does have the KERNEL/CPUUsage performance counter
 	 * which is similar.
 	 */
-	if (!filebuf)
-	  filebuf = (char *) cmalloc (HEAP_BUF, bufalloc = 16);
+	filebuf = (char *) realloc (filebuf, bufalloc = 16);
 	filesize = __small_sprintf (filebuf, "%u.%02u %u.%02u %u.%02u\n",
 				    0, 0, 0, 0, 0, 0);
 	break;
       }
     case PROC_MEMINFO:
       {
-	if (!filebuf)
-	  filebuf = (char *) cmalloc (HEAP_BUF, bufalloc = 2048);
+	filebuf = (char *) realloc (filebuf, bufalloc = 2048);
 	filesize = format_proc_meminfo (filebuf, bufalloc);
 	break;
       }

winsup/cygwin/fhandler_process.cc
@@ -42,6 +42,7 @@ static const int PROCESS_SID = 10;
 static const int PROCESS_CTTY = 11;
 static const int PROCESS_STAT = 12;
 static const int PROCESS_STATM = 13;
+static const int PROCESS_CMDLINE = 14;
 
 static const char * const process_listing[] =
 {
@@ -59,6 +60,7 @@ static const char * const process_listing[] =
   "ctty",
   "stat",
   "statm",
+  "cmdline",
   NULL
 };
 
@@ -264,8 +266,7 @@ fhandler_process::fill_filebuf ()
     case PROCESS_CTTY:
     case PROCESS_PPID:
       {
-	if (!filebuf)
-	  filebuf = (char *) cmalloc (HEAP_BUF, bufalloc = 40);
+	filebuf = (char *) realloc (filebuf, bufalloc = 40);
 	int num;
 	switch (fileid)
 	  {
@@ -295,10 +296,18 @@ fhandler_process::fill_filebuf ()
 	filesize = strlen (filebuf);
 	break;
       }
+    case PROCESS_CMDLINE:
+      {
+	if (filebuf)
+	  free (filebuf);
+	filebuf = p->cmdline (filesize);
+	if (!*filebuf)
+	  filebuf = strdup ("<defunct>");
+	break;
+      }
     case PROCESS_EXENAME:
       {
-	if (!filebuf)
-	  filebuf = (char *) cmalloc (HEAP_BUF, bufalloc = MAX_PATH);
+	filebuf = (char *) realloc (filebuf, bufalloc = MAX_PATH);
 	if (p->process_state & (PID_ZOMBIE | PID_EXITED))
 	  strcpy (filebuf, "<defunct>");
 	else
@@ -317,8 +326,7 @@ fhandler_process::fill_filebuf ()
       }
     case PROCESS_WINPID:
       {
-	if (!filebuf)
-	  filebuf = (char *) cmalloc (HEAP_BUF, bufalloc = 40);
+	filebuf = (char *) realloc (filebuf, bufalloc = 40);
 	__small_sprintf (filebuf, "%d\n", p->dwProcessId);
 	filesize = strlen (filebuf);
 	break;
@@ -326,8 +334,7 @@ fhandler_process::fill_filebuf ()
     case PROCESS_WINEXENAME:
       {
 	int len = strlen (p->progname);
-	if (!filebuf)
-	  filebuf = (char *) cmalloc (HEAP_BUF, bufalloc = (len + 2));
+	filebuf = (char *) realloc (filebuf, bufalloc = (len + 2));
 	strcpy (filebuf, p->progname);
 	filebuf[len] = '\n';
 	filesize = len + 1;
@@ -335,22 +342,19 @@ fhandler_process::fill_filebuf ()
       }
     case PROCESS_STATUS:
       {
-	if (!filebuf)
-	  filebuf = (char *) cmalloc (HEAP_BUF, bufalloc = 2048);
+	filebuf = (char *) realloc (filebuf, bufalloc = 2048);
 	filesize = format_process_status (*p, filebuf, bufalloc);
 	break;
       }
     case PROCESS_STAT:
       {
-	if (!filebuf)
-	  filebuf = (char *) cmalloc (HEAP_BUF, bufalloc = 2048);
+	filebuf = (char *) realloc (filebuf, bufalloc = 2048);
 	filesize = format_process_stat (*p, filebuf, bufalloc);
 	break;
       }
     case PROCESS_STATM:
       {
-	if (!filebuf)
-	  filebuf = (char *) cmalloc (HEAP_BUF, bufalloc = 2048);
+	filebuf = (char *) realloc (filebuf, bufalloc = 2048);
 	filesize = format_process_statm (*p, filebuf, bufalloc);
 	break;
       }

winsup/cygwin/fhandler_registry.cc
@@ -555,7 +555,7 @@ fhandler_registry::fill_filebuf ()
 	  goto value_not_found;
 	}
       bufalloc = size;
-      filebuf = (char *) cmalloc (HEAP_BUF, bufalloc);
+      filebuf = (char *) malloc (bufalloc);
       error =
 	RegQueryValueEx (handle, value_name, NULL, NULL, (BYTE *) filebuf,
 			 &size);
@@ -572,14 +572,9 @@ fhandler_registry::fill_filebuf ()
       do
 	{
 	  bufalloc += 1000;
-	  if (filebuf)
-	    {
-	      cfree (filebuf);
-	      filebuf = (char *) cmalloc (HEAP_BUF, bufalloc);
-	    }
-	  error =
-	    RegQueryValueEx (handle, value_name, NULL, &type,
-			     (BYTE *) filebuf, &size);
+	  filebuf = (char *) realloc (filebuf, bufalloc);
+	  error = RegQueryValueEx (handle, value_name, NULL, &type,
+				   (BYTE *) filebuf, &size);
 	  if (error != ERROR_SUCCESS && error != ERROR_MORE_DATA)
 	    {
 	      if (error != ERROR_FILE_NOT_FOUND)

winsup/cygwin/fhandler_virtual.cc
@@ -34,8 +34,15 @@ fhandler_virtual::fhandler_virtual (DWORD devtype):
 fhandler_virtual::~fhandler_virtual ()
 {
   if (filebuf)
-    cfree (filebuf);
+    free (filebuf);
   filebuf = NULL;
+}
+
+void
+fhandler_virtual::fixup_after_exec (HANDLE)
+{
+  if (filebuf)
+    filebuf = NULL;
 }
 
 DIR *
@@ -143,7 +150,7 @@ fhandler_virtual::dup (fhandler_base * child)
   if (!ret)
     {
       fhandler_virtual *fhproc_child = (fhandler_virtual *) child;
-      fhproc_child->filebuf = (char *) cmalloc (HEAP_BUF, filesize);
+      fhproc_child->filebuf = (char *) malloc (filesize);
       fhproc_child->bufalloc = fhproc_child->filesize = filesize;
       fhproc_child->position = position;
       memcpy (fhproc_child->filebuf, filebuf, filesize);
@@ -156,7 +163,7 @@ int
 fhandler_virtual::close ()
 {
   if (filebuf)
-    cfree (filebuf);
+    free (filebuf);
   filebuf = NULL;
   bufalloc = (size_t) -1;
   cygwin_shared->delqueue.process_queue ();

winsup/cygwin/miscfuncs.cc
@@ -57,7 +57,28 @@ const char case_folded_upper[] NO_COPY = {
  240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255
 };
 
+const char isalpha_array[] NO_COPY = {
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,
+0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,   0,   0,   0,   0,   0,
+   0,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,
+0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,   0,   0,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0
+};
+
 #define ch_case_eq(ch1, ch2) (cyg_tolower(ch1) == cyg_tolower(ch2))
+
+#if 0
 
 /* Return TRUE if two strings match up to length n */
 extern "C" int __stdcall
@@ -91,6 +112,7 @@ strcasematch (const char *s1, const char *s2)
     }
   return *s2 == '\0';
 }
+#endif
 
 extern "C" char * __stdcall
 strcasestr (const char *searchee, const char *lookfor)

commit 732afede935832beab181ae421c1366df2ae6af5

Cygwin: redefine CW_CMDLINE to CW_CMDLINE_ALLOC
Make sure to

- append a trailing \0 as with Windows multistrings, so the end of
  the string can be recognized by the caller, and

- allocate cmdline on the user heap so the caller can free the
  multistring after usage.

Fixes: 831d6fa520ed4 ("* external.cc (cygwin_internal): Implement CW_CMDLINE.")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/external.cc
@@ -344,20 +344,37 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	  res = 0;
 	}
 	break;
-      case CW_CMDLINE:
+
+      case CW_CMDLINE_ALLOC:
 	{
 	  size_t n;
+	  char *cmdline_cheap;
+	  char *cmdline;
+
 	  pid_t pid = va_arg (arg, pid_t);
 	  pinfo p (pid);
-	  res = (uintptr_t) (p ? p->cmdline (n) : NULL);
+	  cmdline_cheap = (p ? p->cmdline (n) : NULL);
+	  if (cmdline_cheap)
+	    {
+	      cmdline = (char *) malloc (n + 1);
+	      if (cmdline)
+		{
+		  memcpy (cmdline, cmdline_cheap, n);
+		  cmdline[n] = '\0';
+		}
+	      cfree (cmdline_cheap);
+	    }
+	  res = (uintptr_t) cmdline;
 	}
 	break;
+
       case CW_CHECK_NTSEC:
 	{
 	  char *filename = va_arg (arg, char *);
 	  res = check_ntsec (filename);
 	}
 	break;
+
       case CW_GET_ERRNO_FROM_WINERROR:
 	{
 	  int error = va_arg (arg, int);
@@ -365,6 +382,7 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	  res = geterrno_from_win_error (error, deferrno);
 	}
 	break;
+
       case CW_GET_POSIX_SECURITY_ATTRIBUTE:
 	{
 	  path_conv dummy;
@@ -383,23 +401,27 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	    }
 	}
 	break;
+
       case CW_GET_SHMLBA:
 	{
 	  res = wincap.allocation_granularity ();
 	}
 	break;
+
       case CW_GET_UID_FROM_SID:
 	{
 	  cygpsid psid = va_arg (arg, PSID);
 	  res = psid.get_uid (NULL);
 	}
 	break;
+
       case CW_GET_GID_FROM_SID:
 	{
 	  cygpsid psid = va_arg (arg, PSID);
 	  res = psid.get_gid (NULL);
 	}
 	break;
+
       case CW_GET_BINMODE:
 	{
 	  const char *path = va_arg (arg, const char *);
@@ -413,6 +435,7 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	    res = p.binmode ();
 	}
 	break;
+
       case CW_HOOK:
 	{
 	  const char *name = va_arg (arg, const char *);
@@ -421,32 +444,39 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	  res = (uintptr_t) hook_or_detect_cygwin (name, hookfn, subsys);
 	}
 	break;
+
       case CW_ARGV:
 	{
 	  child_info_spawn *ci = (child_info_spawn *) get_cygwin_startup_info ();
 	  res = (uintptr_t) (ci ? ci->moreinfo->argv : NULL);
 	}
 	break;
+
       case CW_ENVP:
 	{
 	  child_info_spawn *ci = (child_info_spawn *) get_cygwin_startup_info ();
 	  res = (uintptr_t) (ci ? ci->moreinfo->envp : NULL);
 	}
 	break;
+
       case CW_DEBUG_SELF:
 	error_start_init (va_arg (arg, const char *));
 	res = try_to_debug ();
 	break;
+
       case CW_SYNC_WINENV:
 	create_winenv (NULL);
 	res = 0;
 	break;
+
       case CW_CYGTLS_PADSIZE:
 	res = __CYGTLS_PADSIZE__;
 	break;
+
       case CW_SET_DOS_FILE_WARNING:
 	res = 0;
 	break;
+
       case CW_SET_PRIV_KEY:
 	{
 	  const char *passwd = va_arg (arg, const char *);
@@ -454,6 +484,7 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	  res = setlsapwd (passwd, username);
 	}
 	break;
+
       case CW_SETERRNO:
 	{
 	  const char *file = va_arg (arg, const char *);
@@ -462,6 +493,7 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	  res = 0;
 	}
 	break;
+
       case CW_EXIT_PROCESS:
 	{
 	  UINT status = va_arg (arg, UINT);
@@ -476,6 +508,7 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	  res = 0;
 	}
 	break;
+
       case CW_GET_INSTKEY:
 	{
 	  PWCHAR dest = va_arg (arg, PWCHAR);
@@ -483,6 +516,7 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	  res = 0;
 	}
 	break;
+
       case CW_INT_SETLOCALE:
 	{
 	  extern void internal_setlocale ();
@@ -490,6 +524,7 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	  res = 0;
 	}
 	break;
+
       case CW_CVT_MNT_OPTS:
 	{
 	  extern bool fstab_read_flags (char **, unsigned &, bool);
@@ -509,6 +544,7 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	    }
 	}
 	break;
+
       case CW_LST_MNT_OPTS:
 	{
 	  extern char *fstab_list_flags ();
@@ -523,6 +559,7 @@ cygwin_internal (cygwin_getinfo_types t, ...)
 	    }
 	}
 	break;
+
       case CW_STRERROR:
 	{
 	  int err = va_arg (arg, int);

winsup/cygwin/include/sys/cygwin.h
@@ -95,7 +95,7 @@ typedef enum
     CW_STRACE_ACTIVE,
     CW_CYGWIN_PID_TO_WINPID,
     CW_EXTRACT_DOMAIN_AND_USER,
-    CW_CMDLINE,
+    CW_CMDLINE_ALLOC,
     CW_CHECK_NTSEC,
     CW_GET_ERRNO_FROM_WINERROR,
     CW_GET_POSIX_SECURITY_ATTRIBUTE,
@@ -159,7 +159,7 @@ typedef enum
 #define CW_STRACE_ACTIVE CW_STRACE_ACTIVE
 #define CW_CYGWIN_PID_TO_WINPID CW_CYGWIN_PID_TO_WINPID
 #define CW_EXTRACT_DOMAIN_AND_USER CW_EXTRACT_DOMAIN_AND_USER
-#define CW_CMDLINE CW_CMDLINE
+#define CW_CMDLINE_ALLOC CW_CMDLINE_ALLOC
 #define CW_CHECK_NTSEC CW_CHECK_NTSEC
 #define CW_GET_ERRNO_FROM_WINERROR CW_GET_ERRNO_FROM_WINERROR
 #define CW_GET_POSIX_SECURITY_ATTRIBUTE CW_GET_POSIX_SECURITY_ATTRIBUTE