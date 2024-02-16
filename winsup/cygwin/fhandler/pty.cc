picolibc/picolibc

commit f907b5f405a3229a473e59df0ef1c2166e8f6e5e

Cygwin: pty: Fix failure to revert from nat-pipe in disable_pcon.
Fixes: a9038789488e ("Cygwin: pty: Additional fix for transferring input at exit.");
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/fhandler/pty.cc
@@ -4108,8 +4108,6 @@ fhandler_pty_slave::cleanup_for_non_cygwin_app (handle_set_t *p, tty *ttyp,
   WaitForSingleObject (p->pipe_sw_mutex, INFINITE);
   if (!switch_to)
     switch_to = get_console_process_id (current_pid, false, true, true);
-  if (!switch_to)
-    switch_to = get_console_process_id (current_pid, false, true, false);
   if ((!switch_to && (ttyp->pcon_activated || stdin_is_ptys))
       && ttyp->pty_input_state_eq (tty::to_nat))
     {

commit f9542a2e8e7532626298f02015337fdeb4e7bac0

Cygwin: pty: Re-fix the last bug regarding nat-pipe.
Fixes: f907b5f405a3 ("Cygwin: pty: Fix failure to revert from nat-pipe in disable_pcon.")
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/fhandler/pty.cc
@@ -4103,11 +4103,8 @@ fhandler_pty_slave::cleanup_for_non_cygwin_app (handle_set_t *p, tty *ttyp,
 						DWORD force_switch_to)
 {
   ttyp->wait_fwd ();
-  DWORD current_pid = myself->exec_dwProcessId ?: myself->dwProcessId;
-  DWORD switch_to = force_switch_to;
   WaitForSingleObject (p->pipe_sw_mutex, INFINITE);
-  if (!switch_to)
-    switch_to = get_console_process_id (current_pid, false, true, true);
+  DWORD switch_to = get_winpid_to_hand_over (ttyp, force_switch_to);
   if ((!switch_to && (ttyp->pcon_activated || stdin_is_ptys))
       && ttyp->pty_input_state_eq (tty::to_nat))
     {