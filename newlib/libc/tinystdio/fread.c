9992bdff491bc3103eeee8782e17f430a40b4134
Keith Packard <keithp@keithp.com>
2024-04-30 10:21:56 -0700

    tinystdio: Mirror bufio stdin/stdout flush fix to fast fread
    The fast-bufio path in fread has the same stdin/stdout semantics as
    found in __bufio_get, so the fix applied there for weak stdin/stdout
    needs to be applied to fread as well. Take advantage of this to copy
    the slightly more efficient test from fread to __bufio_get.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/tinystdio/fread.c
+++ newlib/libc/tinystdio/fread.c
@@ -82,7 +82,7 @@ fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
                                 /* Flush stdout if reading from stdin */
                                 if (!flushed) {
                                         flushed = true;
-                                        if (stream == stdin && stdout != NULL) {
+                                        if (&stdin != NULL && &stdout != NULL && stream == stdin) {
                                                 __bufio_unlock(stream);
                                                 fflush(stdout);
                                                 goto again;
