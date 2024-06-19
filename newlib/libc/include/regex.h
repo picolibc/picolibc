7905738ae2aeb7cccdeb5896cc3deb5d3d428b23
Keith Packard <keithp@keithp.com>
2024-06-18 20:51:23 -0700

    regex: Make header file usable from C++
    Use __restrict_arr instead of __restrict as that is an internal
    version which is defined to nothing on C++ (which doesn't support
    __restrict attributes on arrays).
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/include/regex.h
+++ newlib/libc/include/regex.h
@@ -96,7 +96,7 @@ __BEGIN_DECLS
 int	regcomp(regex_t *__restrict, const char *__restrict, int);
 size_t	regerror(int, const regex_t *__restrict, char *__restrict, size_t);
 int	regexec(const regex_t *__restrict, const char *__restrict,
-			size_t, regmatch_t [__restrict], int);
+			size_t, regmatch_t [__restrict_arr], int);
 void	regfree(regex_t *);
 __END_DECLS
 
