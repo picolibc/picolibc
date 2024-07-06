picolibc/picolibc

commit c77a5689f7bd4520f6d87481fd08591747b5da0f

Cygwin: console: Do not unmap shared console memory belonging to ctty.
In the condition that console setup for CTTY and close run at the
sametime, accessing shared console memory which is already unmapped
may occur. With this patch, to avoid this race issue, shared console
memory which belongs to contorolling terminal (CTTY) is kept mapped
as before.
Addresses: https://cygwin.com/pipermail/cygwin/2024-February/255561.html

Fixes: 3721a756b0d8 ("Cygwin: console: Make the console accessible from other terminals.")
Reported-by: Kate Deplaix <kit-ty-kate@outlook.com>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/fhandler/console.cc
@@ -1926,9 +1926,11 @@ fhandler_console::close ()
 	  || get_device () == (dev_t) myself->ctty))
     free_console ();
 
-  if (shared_console_info[unit])
-    UnmapViewOfFile ((void *) shared_console_info[unit]);
-  shared_console_info[unit] = NULL;
+  if (shared_console_info[unit] && myself->ctty != tc ()->ntty)
+    {
+      UnmapViewOfFile ((void *) shared_console_info[unit]);
+      shared_console_info[unit] = NULL;
+    }
 
   return 0;
 }

winsup/cygwin/release/3.5.2
@@ -1,0 +1,7 @@
+Fixes:
+------
+
+- Fix the problem that console setup accesses shared memory which
+  is already unmapped due to race condition. To avoid this issue,
+  shared console memory will be kept mapped if it belongs to CTTY.
+  Addresses:  https://cygwin.com/pipermail/cygwin/2024-February/255561.html

commit baf2764dca435d77fdd25e19146c831214a2da73

Cygwin: console: Fixes an issue that tmux can not run on the console.
After the commit c77a5689f7bd, tmux can not run on the console.
This patch replaces the countermeasure for the race issue between
console setup and close with another mechanism using a mutex.

Fixes: c77a5689f7bd ("Cygwin: console: Do not unmap shared console memory belonging to ctty.")
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/fhandler/console.cc
@@ -48,6 +48,9 @@ details. */
 		  con.b.srWindow.Top + con.scroll_region.Bottom)
 #define con_is_legacy (shared_console_info[unit] && con.is_legacy)
 
+static HANDLE NO_COPY shared_info_mutex;
+static int NO_COPY shared_info_state;
+
 #define CONS_THREAD_SYNC "cygcons.thread_sync"
 static bool NO_COPY master_thread_started = false;
 
@@ -665,6 +668,12 @@ fhandler_console::set_unit ()
   else if (myself->ctty != CTTY_UNINITIALIZED)
     unit = device::minor (myself->ctty);
 
+  if (!shared_info_mutex)
+    shared_info_mutex = CreateMutex (&sec_none_nih, FALSE, NULL);
+
+  WaitForSingleObject (shared_info_mutex, INFINITE);
+  shared_info_state++;
+
   if (shared_console_info[unit])
     ; /* Do nothing */
   else if (generic_console
@@ -698,6 +707,8 @@ fhandler_console::set_unit ()
 	    }
 	}
     }
+  ReleaseMutex (shared_info_mutex);
+
   if (shared_console_info[unit])
     {
       devset = (fh_devices) shared_console_info[unit]->tty_min_state.getntty ();
@@ -1963,11 +1974,13 @@ fhandler_console::close ()
   CloseHandle (output_mutex);
   output_mutex = NULL;
 
-  if (shared_console_info[unit] && myself->ctty != tc ()->ntty)
+  WaitForSingleObject (shared_info_mutex, INFINITE);
+  if (--shared_info_state == 0 && shared_console_info[unit])
     {
       UnmapViewOfFile ((void *) shared_console_info[unit]);
       shared_console_info[unit] = NULL;
     }
+  ReleaseMutex (shared_info_mutex);
 
   return 0;
 }