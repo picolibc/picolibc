342e2758249c62618b4a3cbec79e31dd7f65dd98
Keith Packard <keithp@keithp.com>
2024-09-22 17:58:54 -0700

    include/sys: Use standard names for thread local storage class
    Use thread_local for C++, _Thread_local for C >= C11 and otherwise
    keep using __thread.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/include/sys/config.h
+++ newlib/libc/include/sys/config.h
@@ -273,7 +273,13 @@ SUCH DAMAGE.
 #endif
 
 #ifdef PICOLIBC_TLS
+#ifdef __cplusplus
+#define NEWLIB_THREAD_LOCAL thread_local
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__) >= 201112L
+#define NEWLIB_THREAD_LOCAL _Thread_local
+#else
 #define NEWLIB_THREAD_LOCAL __thread
+#endif
 #else
 #define NEWLIB_THREAD_LOCAL
 #endif
