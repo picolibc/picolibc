cfea2433a829dc230ce08f0d8285931313e710b4
Keith Packard <keithp@keithp.com>
2024-10-21 17:06:56 -0700

    string: declare (but do not define) strerrorname_np and strerrordesc_np
    Provide declarations for these functions; maybe we'll provide an
    implementation someday.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/include/string.h
+++ newlib/libc/include/string.h
@@ -149,6 +149,10 @@ int	__xpg_strerror_r (int, char *, size_t);
 #  define strerror_r __xpg_strerror_r
 # endif
 #endif
+#if __GNU_VISIBLE
+const char *strerrorname_np(int errnum);
+const char *strerrordesc_np(int errnum);
+#endif
 #if __BSD_VISIBLE
 size_t   strlcat (char *, const char *, size_t);
 #endif
