0618facc4549fc873a162636b82fc88df6776835
Keith Packard <keithp@keithp.com>
2024-10-21 17:05:47 -0700

    posix: Add pathconf and fpathconf
    These POSIX apis provide for path-dependent limits.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/include/sys/syslimits.h
+++ newlib/libc/include/sys/syslimits.h
@@ -59,7 +59,49 @@
 #define	LINE_MAX		 2048	/* max bytes in an input line */
 #define	RE_DUP_MAX		  255	/* max RE's in interval notation */
 
+
+#define _POSIX_AIO_LISTIO_MAX	2
+#define _POSIX_AIO_MAX	        1
+#define _POSIX_ARG_MAX	        4096
+#define _POSIX_CHILD_MAX	6
+#define _POSIX_DELAYTIMER_MAX	32
+#define _POSIX_LINK_MAX	        8
+#define _POSIX_LOGIN_NAME_MAX	9
+#define _POSIX_MAX_CANON	255
+#define _POSIX_MAX_INPUT	255
+#define _POSIX_MQ_OPEN_MAX	8
+#define _POSIX_MQ_PRIO_MAX	32
+#define _POSIX_NAME_MAX	        14
+#define _POSIX_NGROUPS_MAX	0
+#define _POSIX_OPEN_MAX	        16
+#define _POSIX_PATH_MAX	        255
+#define _POSIX_PIPE_BUF	        512
+#define _POSIX_RTSIG_MAX	8
+#define _POSIX_SEM_NSEMS_MAX	256
+#define _POSIX_SEM_VALUE_MAX	32767
+#define _POSIX_SIGQUEUE_MAX	32
+#define _POSIX_SSIZE_MAX	32767
+#define _POSIX_STREAM_MAX	8
+#define _POSIX_THREAD_DESTRUCTOR_ITERATIONS	4
+#define _POSIX_THREAD_KEYS_MAX	128
+#define _POSIX_THREAD_THREADS_MAX	64
+#define _POSIX_TIMER_MAX	32
+#define _POSIX_TTY_NAME_MAX	9
+#define _POSIX_TZNAME_MAX	3
+#define _POSIX2_BC_BASE_MAX	99
+#define _POSIX2_BC_DIM_MAX	2048
+#define _POSIX2_BC_SCALE_MAX	99
+#define _POSIX2_BC_STRING_MAX	1000
+#define _POSIX2_COLL_WEIGHTS_MAX	2
+#define _POSIX2_EXPR_NEST_MAX	32
+#define _POSIX2_LINE_MAX	2048
+#define _POSIX2_RE_DUP_MAX	255
+
 #endif /* __POSIX_VISIBLE */
+
+#ifdef _XOPEN_SOURCE
+#define _XOPEN_IOV_MAX          16
+#endif
 
 #if __MISC_VISIBLE
 #define NSIG_MAX       __LONG_WIDTH__   /* max signal number */
