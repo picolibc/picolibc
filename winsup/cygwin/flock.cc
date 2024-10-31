picolibc/picolibc

commit 31390e4ca643782a365c9fba0a1ca188c9bb854e

* flock.cc (lockf_t::operator new): Add operator taking a pointer.  Call 	cmalloc instead of ccalloc in the default operator.  Add comments. 	(inode_t::operator new): Call cmalloc instead of ccalloc. 	(inode_t::get_all_locks_list): Return lockf_t pointer. 	(inode_t::del_all_locks_list): Delete.  Remove calls throughout. 	(inode_t::get): Handle failing new gracefully. 	(MAX_LOCKF_CNT): Define. 	(inode_t::get_all_locks_list): Use pre-allocated buffer in i_all_lf 	instead of allocating every lock.  Return pointer to start of linked 	list of locks. 	(lockf_t::open_lock_obj): Create event object non-inheritable. 	(fhandler_disk_file::lock): Handle failing new gracefully. 	(lf_setlock): Allocate temporary buffer for node->i_all_lf from TLS. 	Remove erroneous NtClose call. 	(lf_getlock): Allocate temporary buffer for node->i_all_lf from TLS. 	(lf_getblock): Set lf to return value of get_all_locks_list.

winsup/cygwin/flock.cc
@@ -326,8 +326,13 @@ class lockf_t
     {}
     ~lockf_t ();
 
+    /* Used to create all locks list in a given TLS buffer. */
+    void *operator new (size_t size, void *p)
+    { return p; }
+    /* Used to store own lock list in the cygheap. */
     void *operator new (size_t size)
-    { return ccalloc (HEAP_FHANDLER, 1, sizeof (lockf_t)); }
+    { return cmalloc (HEAP_FHANDLER, sizeof (lockf_t)); }
+    /* Never call on node->i_all_lf! */
     void operator delete (void *p)
     { cfree (p); }
 
@@ -360,7 +365,7 @@ class inode_t
     ~inode_t ();
 
     void *operator new (size_t size)
-    { return ccalloc (HEAP_FHANDLER, 1, sizeof (inode_t)); }
+    { return cmalloc (HEAP_FHANDLER, sizeof (inode_t)); }
     void operator delete (void *p)
     { cfree (p); }
 
@@ -369,8 +374,7 @@ class inode_t
     void LOCK () { WaitForSingleObject (i_mtx, INFINITE); }
     void UNLOCK () { ReleaseMutex (i_mtx); }
 
-    void get_all_locks_list ();
-    void del_all_locks_list () { del_locks (&i_all_lf); }
+    lockf_t *get_all_locks_list ();
 
     void del_my_locks () { LOCK (); del_locks (&i_lockf); UNLOCK ();
 }
@@ -445,7 +449,8 @@ inode_t::get (dev_t dev, ino_t ino)
   if (!node)
     {
       node = new inode_t (dev, ino);
-      LIST_INSERT_HEAD (&cygheap->inode_list, node, i_next);
+      if (node)
+	LIST_INSERT_HEAD (&cygheap->inode_list, node, i_next);
     }
   INODE_LIST_UNLOCK ();
   return node;
@@ -505,7 +510,12 @@ inode_t::del_locks (lockf_t **head)
 /* Enumerate all lock event objects for this file and create a lockf_t
    list in the i_all_lf member.  This list is searched in lf_getblock
    for locks which potentially block our lock request. */
-void
+
+/* Number of lockf_t structs which fit in the temporary buffer. */
+#define MAX_LOCKF_CNT	((intptr_t)((NT_MAX_PATH * sizeof (WCHAR)) \
+				    / sizeof (lockf_t)))
+
+lockf_t *
 inode_t::get_all_locks_list ()
 {
   struct fdbi
@@ -515,8 +525,8 @@ inode_t::get_all_locks_list ()
   } f;
   ULONG context;
   NTSTATUS status;
+  lockf_t *lock = i_all_lf;
 
-  del_all_locks_list ();
   for (BOOLEAN restart = TRUE;
        NT_SUCCESS (status = NtQueryDirectoryObject (i_dir, &f, sizeof f, TRUE,
 						    restart, &context, NULL));
@@ -547,12 +557,20 @@ inode_t::get_all_locks_list ()
       DWORD wid = wcstoul (endptr + 1, &endptr, 16);
       if (endptr && *endptr != L'\0')
       	continue;
-      lockf_t *lock = new lockf_t (this, &i_all_lf, flags, type, start, end,
-				   id, wid);
-      if (i_all_lf)
-	lock->lf_next = i_all_lf;
-      i_all_lf = lock;
+      if (lock - i_all_lf >= MAX_LOCKF_CNT)
+        {
+	  system_printf ("Warning, can't handle more than %d locks per file.",
+			 MAX_LOCKF_CNT);
+	  break;
+	}
+      if (lock > i_all_lf)
+        lock[-1].lf_next = lock;
+      new (lock++) lockf_t (this, &i_all_lf, flags, type, start, end, id, wid);
     }
+  /* If no lock has been found, return NULL. */
+  if (lock == i_all_lf)
+    return NULL;
+  return i_all_lf;
 }
 
 /* Create the lock event object in the file's subdir in the NT global
@@ -595,8 +613,7 @@ lockf_t::open_lock_obj () const
 			  lf_end, lf_id, lf_wid);
   RtlInitCountedUnicodeString (&uname, name,
 			       LOCK_OBJ_NAME_LEN * sizeof (WCHAR));
-  InitializeObjectAttributes (&attr, &uname, OBJ_INHERIT, lf_inode->i_dir,
-			      NULL);
+  InitializeObjectAttributes (&attr, &uname, 0, lf_inode->i_dir, NULL);
   status = NtOpenEvent (&obj, FLOCK_EVENT_ACCESS, &attr);
   if (!NT_SUCCESS (status))
     {
@@ -659,7 +676,7 @@ static int maxlockdepth = MAXDEPTH;
 #define OTHERS  0x2
 static int      lf_clearlock (lockf_t *, lockf_t **);
 static int      lf_findoverlap (lockf_t *, lockf_t *, int, lockf_t ***, lockf_t **);
-static lockf_t   *lf_getblock (lockf_t *, inode_t *node);
+static lockf_t *lf_getblock (lockf_t *, inode_t *node);
 static int      lf_getlock (lockf_t *, inode_t *, struct __flock64 *);
 static int      lf_setlock (lockf_t *, inode_t *, lockf_t **);
 static void     lf_split (lockf_t *, lockf_t *, lockf_t **);
@@ -773,6 +790,11 @@ fhandler_disk_file::lock (int a_op, struct __flock64 *fl)
   if (!node)
     {
       node = inode_t::get (stat.st_dev, stat.st_ino);
+      if (!node)
+        {
+	  set_errno (ENOLCK);
+	  return -1;
+	}
       need_fork_fixup (true);
     }
   /* Unlock the fd table which has been locked in fcntl_worker, otherwise
@@ -797,12 +819,24 @@ fhandler_disk_file::lock (int a_op, struct __flock64 *fl)
    */
   lockf_t *clean = NULL;
   if (a_op == F_SETLK || a_op == F_UNLCK)
-    clean = new lockf_t ();
+    {
+      clean = new lockf_t ();
+      if (!clean)
+        {
+	  set_errno (ENOLCK);
+	  return -1;
+	}
+    }
   /*
    * Create the lockf_t structure
    */
   lockf_t *lock = new lockf_t (node, head, a_flags, type, start, end,
 			       getpid (), myself->dwProcessId);
+  if (!lock)
+    {
+      set_errno (ENOLCK);
+      return -1;
+    }
 
   node->LOCK ();
   switch (a_op)
@@ -854,6 +888,7 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean)
   lockf_t **head = lock->lf_head;
   lockf_t **prev, *overlap;
   int ovcase, priority, old_prio, needtolink;
+  tmp_pathbuf tp;
 
   /*
    * Set the priority
@@ -864,6 +899,8 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean)
   /*
    * Scan lock list for this file looking for locks that would block us.
    */
+  /* Create temporary space for the all locks list. */
+  node->i_all_lf = (lockf_t *) tp.w_get ();
   while ((block = lf_getblock(lock, node)))
     {
       /*
@@ -871,7 +908,6 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean)
        */
       if ((lock->lf_flags & F_WAIT) == 0)
 	{
-	  node->del_all_locks_list ();
 	  lock->lf_next = *clean;
 	  *clean = lock;
 	  return EAGAIN;
@@ -921,7 +957,6 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean)
 	     Treat this as a deadlock-like situation for now. */
 	  system_printf ("Can't sync with lock object hold by "
 			 "Win32 pid %lu: %E", block->lf_wid);
-	  NtClose (obj);
 	  return EDEADLK;
 	}
       HANDLE proc = OpenProcess (SYNCHRONIZE, FALSE, block->lf_wid);
@@ -936,7 +971,6 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean)
 	  return EDEADLK;
 	}
       HANDLE w4[3] = { obj, proc, signal_arrived };
-      node->del_all_locks_list ();
       //SetThreadPriority (GetCurrentThread (), priority);
       node->UNLOCK ();
       DWORD ret = WaitForMultipleObjects (3, w4, FALSE, INFINITE);
@@ -959,7 +993,6 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean)
 	  return geterrno_from_win_error ();
 	}
     }
-  node->del_all_locks_list ();
   allow_others_to_sync ();
   /*
    * No blocks!!  Add the lock.  Note that we will
@@ -1169,7 +1202,10 @@ static int
 lf_getlock (lockf_t *lock, inode_t *node, struct __flock64 *fl)
 {
   lockf_t *block;
+  tmp_pathbuf tp;
 
+  /* Create temporary space for the all locks list. */
+  node->i_all_lf = (lockf_t *) tp.w_get ();
   if ((block = lf_getblock (lock, node)))
     {
       fl->l_type = block->lf_type;
@@ -1186,7 +1222,6 @@ lf_getlock (lockf_t *lock, inode_t *node, struct __flock64 *fl)
     }
   else
     fl->l_type = F_UNLCK;
-  node->del_all_locks_list ();
   return 0;
 }
 
@@ -1197,8 +1232,8 @@ lf_getlock (lockf_t *lock, inode_t *node, struct __flock64 *fl)
 static lockf_t *
 lf_getblock (lockf_t *lock, inode_t *node)
 {   
-  node->get_all_locks_list ();
-  lockf_t **prev, *overlap, *lf = node->i_all_lf;
+  lockf_t **prev, *overlap;
+  lockf_t *lf = node->get_all_locks_list ();
   int ovcase;
 
   prev = lock->lf_head;

commit ae181b0ff1226cf38be8a7f03ff19bf869c87f54

Cygwin: lockf: Make lockf() return ENOLCK when too many locks
Previously, lockf() printed a warning message when the number of locks
per file exceeds the limit (MAX_LOCKF_CNT). This patch makes lockf()
return ENOLCK in that case rather than printing the warning message.

Addresses: https://cygwin.com/pipermail/cygwin/2024-October/256528.html
Fixes: 31390e4ca643 ("(inode_t::get_all_locks_list): Use pre-allocated buffer in i_all_lf instead of allocating every lock.  Return pointer to start of linked list of locks.")
Reported-by: Christian Franke <Christian.Franke@t-online.de>
Reviewed-by: Corinna Vinschen <corinna@vinschen.de>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/flock.cc
@@ -297,6 +297,7 @@ class inode_t
     HANDLE		 i_dir;
     HANDLE		 i_mtx;
     uint32_t		 i_cnt;    /* # of threads referencing this instance. */
+    uint32_t		 i_lock_cnt; /* # of locks for this file */
 
   public:
     inode_t (dev_t dev, ino_t ino);
@@ -321,6 +322,8 @@ class inode_t
     void unlock_and_remove_if_unused ();
 
     lockf_t *get_all_locks_list ();
+    uint32_t get_lock_count () /* needs get_all_locks_list() */
+    { return i_lock_cnt; }
 
     bool del_my_locks (long long id, HANDLE fhdl);
 };
@@ -503,7 +506,8 @@ inode_t::get (dev_t dev, ino_t ino, bool create_if_missing, bool lock)
 }
 
 inode_t::inode_t (dev_t dev, ino_t ino)
-: i_lockf (NULL), i_all_lf (NULL), i_dev (dev), i_ino (ino), i_cnt (0L)
+: i_lockf (NULL), i_all_lf (NULL), i_dev (dev), i_ino (ino), i_cnt (0L),
+  i_lock_cnt (0)
 {
   HANDLE parent_dir;
   WCHAR name[48];
@@ -610,17 +614,16 @@ inode_t::get_all_locks_list ()
 	  dbi->ObjectName.Buffer[LOCK_OBJ_NAME_LEN] = L'\0';
 	  if (!newlock.from_obj_name (this, &i_all_lf, dbi->ObjectName.Buffer))
 	    continue;
-	  if (lock - i_all_lf >= MAX_LOCKF_CNT)
-	    {
-	      system_printf ("Warning, can't handle more than %d locks per file.",
-			     MAX_LOCKF_CNT);
-	      break;
-	    }
+	  /* This should not be happen. The number of locks is limitted
+	     in lf_setlock() and lf_clearlock() so that it does not
+	     exceed MAX_LOCKF_CNT. */
+	  assert (lock - i_all_lf < MAX_LOCKF_CNT);
 	  if (lock > i_all_lf)
 	    lock[-1].lf_next = lock;
 	  new (lock++) lockf_t (newlock);
 	}
     }
+  i_lock_cnt = lock - i_all_lf;
   /* If no lock has been found, return NULL. */
   if (lock == i_all_lf)
     return NULL;
@@ -1190,6 +1193,12 @@ restart:	/* Entry point after a restartable signal came in. */
   return -1;
 }
 
+/* The total number of locks shall not exceed MAX_LOCKF_CNT.
+   If once it exceeds, lf_fildoverlap() cannot work correctly.
+   Therefore, lf_setlock() and lf_clearlock() control the
+   total number of locks not to exceed MAX_LOCKF_CNT. When
+   they detect that the operation will cause excess, they
+   return ENOCLK. */
 /*
  * Set a byte-range lock.
  */
@@ -1346,14 +1355,31 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean, HANDLE fhdl)
    *
    * Handle any locks that overlap.
    */
+  node->get_all_locks_list (); /* Update lock count */
+  uint32_t lock_cnt = node->get_lock_count ();
+  /* lf_clearlock() sometimes increases the number of locks. Without
+     this room, the unlocking will never succeed in some situation. */
+  const uint32_t room_for_clearlock = 2;
+  const int incr[] = {1, 1, 2, 2, 3, 2};
+  int decr = 0;
+
   prev = head;
   block = *head;
   needtolink = 1;
   for (;;)
     {
       ovcase = lf_findoverlap (block, lock, SELF, &prev, &overlap);
+      /* Estimate the maximum increase in number of the locks that
+	 can occur here. If this possibly exceeds the MAX_LOCKF_CNT,
+	 return ENOLCK. */
       if (ovcase)
-	block = overlap->lf_next;
+	{
+	  block = overlap->lf_next;
+	  HANDLE ov_obj = overlap->lf_obj;
+	  decr = (ov_obj && get_obj_handle_count (ov_obj) == 1) ? 1 : 0;
+	}
+      if (needtolink)
+	lock_cnt += incr[ovcase] - decr;
       /*
        * Six cases:
        *  0) no overlap
@@ -1368,6 +1394,8 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean, HANDLE fhdl)
 	case 0: /* no overlap */
 	  if (needtolink)
 	    {
+	      if (lock_cnt > MAX_LOCKF_CNT - room_for_clearlock)
+		return ENOLCK;
 	      *prev = lock;
 	      lock->lf_next = overlap;
 	      lock->create_lock_obj ();
@@ -1380,6 +1408,8 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean, HANDLE fhdl)
 	   * able to acquire it.
 	   * Cygwin: Always wake lock.
 	   */
+	  if (lock_cnt > MAX_LOCKF_CNT - room_for_clearlock)
+	    return ENOLCK;
 	  lf_wakelock (overlap, fhdl);
 	  overlap->lf_type = lock->lf_type;
 	  overlap->create_lock_obj ();
@@ -1397,6 +1427,11 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean, HANDLE fhdl)
 	      *clean = lock;
 	      break;
 	    }
+	  if (overlap->lf_start < lock->lf_start
+	      && overlap->lf_end > lock->lf_end)
+	    lock_cnt++;
+	  if (lock_cnt > MAX_LOCKF_CNT - room_for_clearlock)
+	    return ENOLCK;
 	  if (overlap->lf_start == lock->lf_start)
 	    {
 	      *prev = lock;
@@ -1413,6 +1448,8 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean, HANDLE fhdl)
 	  break;
 
 	case 3: /* lock contains overlap */
+	  if (needtolink && lock_cnt > MAX_LOCKF_CNT - room_for_clearlock)
+	    return ENOLCK;
 	  /*
 	   * If downgrading lock, others may be able to
 	   * acquire it, otherwise take the list.
@@ -1440,6 +1477,8 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean, HANDLE fhdl)
 	  /*
 	   * Add lock after overlap on the list.
 	   */
+	  if (lock_cnt > MAX_LOCKF_CNT - room_for_clearlock)
+	    return ENOLCK;
 	  lock->lf_next = overlap->lf_next;
 	  overlap->lf_next = lock;
 	  overlap->lf_end = lock->lf_start - 1;
@@ -1456,6 +1495,8 @@ lf_setlock (lockf_t *lock, inode_t *node, lockf_t **clean, HANDLE fhdl)
 	   */
 	  if (needtolink)
 	    {
+	      if (lock_cnt > MAX_LOCKF_CNT - room_for_clearlock)
+		return ENOLCK;
 	      *prev = lock;
 	      lock->lf_next = overlap;
 	      lock->create_lock_obj ();
@@ -1483,12 +1524,35 @@ lf_clearlock (lockf_t *unlock, lockf_t **clean, HANDLE fhdl)
   lockf_t *lf = *head;
   lockf_t *overlap, **prev;
   int ovcase;
+  inode_t *node = lf->lf_inode;
+  tmp_pathbuf tp;
+  node->i_all_lf = (lockf_t *) tp.w_get ();
+  node->get_all_locks_list (); /* Update lock count */
+  uint32_t lock_cnt = node->get_lock_count ();
+  bool first_loop = true;
 
   if (lf == NOLOCKF)
     return 0;
   prev = head;
   while ((ovcase = lf_findoverlap (lf, unlock, SELF, &prev, &overlap)))
     {
+      /* Estimate the maximum increase in number of the locks that
+	 can occur here. If this possibly exceeds the MAX_LOCKF_CNT,
+	 return ENOLCK. */
+      HANDLE ov_obj = overlap->lf_obj;
+      if (first_loop)
+	{
+	  const int incr[] = {0, 0, 1, 1, 2, 1};
+	  int decr = (ov_obj && get_obj_handle_count (ov_obj) == 1) ? 1 : 0;
+	  lock_cnt += incr[ovcase] - decr;
+	  if (ovcase == 2
+	      && overlap->lf_start < unlock->lf_start
+	      && overlap->lf_end > unlock->lf_end)
+	    lock_cnt++;
+	  if (lock_cnt > MAX_LOCKF_CNT)
+	    return ENOLCK;
+	}
+
       /*
        * Wakeup the list of locks to be retried.
        */
@@ -1521,6 +1585,7 @@ lf_clearlock (lockf_t *unlock, lockf_t **clean, HANDLE fhdl)
 	  lf = overlap->lf_next;
 	  overlap->lf_next = *clean;
 	  *clean = overlap;
+	  first_loop = false;
 	  continue;
 
 	case 4: /* overlap starts before lock */
@@ -1528,6 +1593,7 @@ lf_clearlock (lockf_t *unlock, lockf_t **clean, HANDLE fhdl)
 	    prev = &overlap->lf_next;
 	    lf = overlap->lf_next;
 	    overlap->create_lock_obj ();
+	    first_loop = false;
 	    continue;
 
 	case 5: /* overlap ends after lock */

winsup/cygwin/release/3.5.5
@@ -16,3 +16,7 @@ Fixes:
 - Fix lockf() error which occurs when adding a new lock over multiple
   locks.
   Addresses: https://cygwin.com/pipermail/cygwin/2024-October/256528.html
+
+- Make lockf() return ENOLCK when the number of locks exceeds
+  MAX_LOCKF_CNT rather than printing a warning message.
+  Addresses: https://cygwin.com/pipermail/cygwin/2024-October/256528.html