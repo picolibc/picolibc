37920d33accdc1f841da25c1ecd8d0ac35c41682
Lenard Mollenkopf <newlib@lenardmollenkopf.de>
2024-11-01 15:06:36 +0100

    Replace references to C2x with C23
    Signed-off-by: Lenard Mollenkopf <newlib@lenardmollenkopf.de>

--- winsup/cygwin/include/uchar.h
+++ winsup/cygwin/include/uchar.h
@@ -4,7 +4,7 @@
 #include <sys/cdefs.h>
 #include <wchar.h>
 
-/* Either C2x or if C++ doesn't already define char8_t */
+/* Either C23 or if C++ doesn't already define char8_t */
 #if __ISO_C_VISIBLE >= 2020 && !defined (__cpp_char8_t)
 typedef unsigned char		char8_t;
 #endif
@@ -17,7 +17,7 @@ typedef	__uint_least32_t	char32_t;
 
 __BEGIN_DECLS
 
-/* Either C2x or if C++ defines char8_t */
+/* Either C23 or if C++ defines char8_t */
 #if __ISO_C_VISIBLE >= 2020 || defined (__cpp_char8_t)
 size_t  c8rtomb(char * __restrict, char8_t, mbstate_t * __restrict);
 size_t	mbrtoc8(char8_t * __restrict, const char * __restrict, size_t,
