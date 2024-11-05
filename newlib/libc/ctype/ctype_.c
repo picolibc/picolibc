b0cbbba96a8198dcb44d6be787f87fb304e5a1fd
Mohamed Moawad <moawad@synopsys.com>
2024-11-05 17:29:16 +0200

    ctype: Classify horizontal tab as a blank character in _CTYPE_DATA
    The issue in picolibc was that the horizontal tab character (0x09) was not
    classified as a blank character in the _CTYPE_DATA array. This led to incorr
    locale behavior in C++, where the classic_table locale could not detect the
    as a blank character.
    According to C standard definition, blank characters are used to separate wo
    and include space (0x20) and horizontal tab (0x09).
    
    The solution involves creating a migration strategy that
    keeps picolibc using the narrow ctype table with
    the special isblank implementation while allowing for
    the introduction of a new, wider table.
    This new table will incorporate the updated _T flag for the tab character,
    ensuring compatibility with existing C++ libraries that depend on
    ctype::classic_table().
    This approach enables the addition of the fix
    without disrupting current functionality.
    
    Signed-off-by: Mohamed Moawad <moawad@synopsys.com>

--- newlib/libc/ctype/ctype_.c
+++ newlib/libc/ctype/ctype_.c
@@ -51,6 +51,24 @@
 	_L,	_L,	_L,	_L,	_L,	_L,	_L,	_L, \
 	_L,	_L,	_L,	_P,	_P,	_P,	_P,	_C
 
+#define _CTYPE_DATA_0_127_WIDE \
+	_C,	_C,	_C,	_C,	_C,	_C,	_C,	_C, \
+	_C,	_C|_S|_T, _C|_S, _C|_S,	_C|_S,	_C|_S,	_C,	_C, \
+	_C,	_C,	_C,	_C,	_C,	_C,	_C,	_C, \
+	_C,	_C,	_C,	_C,	_C,	_C,	_C,	_C, \
+	_S|_B,	_P,	_P,	_P,	_P,	_P,	_P,	_P, \
+	_P,	_P,	_P,	_P,	_P,	_P,	_P,	_P, \
+	_N,	_N,	_N,	_N,	_N,	_N,	_N,	_N, \
+	_N,	_N,	_P,	_P,	_P,	_P,	_P,	_P, \
+	_P,	_U|_X,	_U|_X,	_U|_X,	_U|_X,	_U|_X,	_U|_X,	_U, \
+	_U,	_U,	_U,	_U,	_U,	_U,	_U,	_U, \
+	_U,	_U,	_U,	_U,	_U,	_U,	_U,	_U, \
+	_U,	_U,	_U,	_P,	_P,	_P,	_P,	_P, \
+	_P,	_L|_X,	_L|_X,	_L|_X,	_L|_X,	_L|_X,	_L|_X,	_L, \
+	_L,	_L,	_L,	_L,	_L,	_L,	_L,	_L, \
+	_L,	_L,	_L,	_L,	_L,	_L,	_L,	_L, \
+	_L,	_L,	_L,	_P,	_P,	_P,	_P,	_C
+
 #define _CTYPE_DATA_128_255 \
 	0,	0,	0,	0,	0,	0,	0,	0, \
 	0,	0,	0,	0,	0,	0,	0,	0, \
@@ -97,6 +115,12 @@ const char _ctype_[1 + 256] = {
 };
 
 #endif	/* !ALLOW_NEGATIVE_CTYPE_INDEX */
+
+const short _ctype_wide[1 + 256] = {
+	0,
+	_CTYPE_DATA_0_127_WIDE,
+	_CTYPE_DATA_128_255
+};
 
 #if defined(_MB_CAPABLE)
 /* Cygwin has its own implementation which additionally maintains backward
