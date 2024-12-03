picolibc/picolibc

commit 6f05b327678f5081df227a6b11aed66837b82971

* sigproc.cc (sigproc_init): Set aside more buffer space for signal pipes. (sig_send): Retry WriteFiles which fail when there is no error but packbytes have not been sent.

winsup/cygwin/sigproc.cc
@@ -426,7 +426,7 @@ sigproc_init ()
   char char_sa_buf[1024];
   PSECURITY_ATTRIBUTES sa = sec_user_nih ((PSECURITY_ATTRIBUTES) char_sa_buf, cygheap->user.sid());
   DWORD err = fhandler_pipe::create (sa, &my_readsig, &my_sendsig,
-				     sizeof (sigpacket), "sigwait",
+				     NSIG * sizeof (sigpacket), "sigwait",
 				     PIPE_ADD_PID);
   if (err)
     {
@@ -635,7 +635,19 @@ sig_send (_pinfo *p, siginfo_t& si, _cygtls *tls)
     }
 
   DWORD nb;
-  if (!WriteFile (sendsig, leader, packsize, &nb, NULL))
+  BOOL res;
+  /* Try multiple times to send if packsize != nb since that probably
+     means that the pipe buffer is full.  */
+  for (int i = 0; i < 100; i++)
+    {
+      res = WriteFile (sendsig, leader, packsize, &nb, NULL);
+      if (!res || packsize == nb)
+	break;
+      Sleep (10);
+      res = 0;
+    }
+
+  if (!res)
     {
       /* Couldn't send to the pipe.  This probably means that the
 	 process is exiting.  */

commit 2544e753963e9c15a9e3fe35188d8dea7b39d748

Cygwin: signal: Fix a short period of deadlock
The main thread waits for the sig thread to read the signal pipe by
calling Sleep(10) if writing to the signal pipe has failed. However,
if the signal thread waiting for another signal being handled in the
main thread, the sig thread does not read the signal pipe. To avoid
such a situation, this patch replaces Sleep(10) to cygwait().

Addresses: https://cygwin.com/pipermail/cygwin/2024-November/256744.html
Fixes: 6f05b327678f ("(sig_send): Retry WriteFiles which fail when there is no error but packbytes have not been sent.")
Reported-by: Christian Franke <Christian.Franke@t-online.de>
Reviewed-by: Corinna Vinschen <corinna@vinschen.de>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/sigproc.cc
@@ -727,7 +727,8 @@ sig_send (_pinfo *p, siginfo_t& si, _cygtls *tls)
       res = WriteFile (sendsig, leader, packsize, &nb, NULL);
       if (!res || packsize == nb)
 	break;
-      Sleep (10);
+      if (cygwait (NULL, 10, cw_sig_eintr) == WAIT_SIGNALED)
+	_my_tls.call_signal_handler ();
       res = 0;
     }
 