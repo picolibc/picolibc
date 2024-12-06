picolibc/picolibc

commit 474048c26edf5ad4fa269ab1801bf4994bf95083

* path.cc (normalize_posix_path): Reorganize to short circuit to DOS path handling whenever a '\' is detected. * signal.cc (sigaction): Make strace output more informative. * sigproc.cc (pending_signals::add): Just index directly into signal array rather than treating the array as a heap. (pending_signals::del): Ditto. (wait_sig): Don't send signal if we already have a similar signal queued. * sigproc.h (call_signal_handler_now): Remove obsolete declaration.

winsup/cygwin/signal.cc
@@ -349,16 +349,21 @@ extern "C" int
 sigaction (int sig, const struct sigaction *newact, struct sigaction *oldact)
 {
   sig_dispatch_pending ();
-  sigproc_printf ("signal %d, newact %p, oldact %p", sig, newact, oldact);
   /* check that sig is in right range */
   if (sig < 0 || sig >= NSIG)
     {
       set_errno (EINVAL);
+      sigproc_printf ("signal %d, newact %p, oldact %p", sig, newact, oldact);
       syscall_printf ("SIG_ERR = sigaction signal %d out of range", sig);
       return -1;
     }
 
   struct sigaction oa = global_sigs[sig];
+
+  if (newact)
+    sigproc_printf ("signal %d, newact %p (handler %p), oa %p", sig, newact, newact->sa_handler, oa, oa.sa_handler);
+  else
+    sigproc_printf ("signal %d, newact %p, oa %p", sig, newact, oa, oa.sa_handler);
 
   if (newact)
     {

winsup/cygwin/sigproc.cc
@@ -48,31 +48,11 @@ details. */
 
 #define NZOMBIES	256
 
-class pending_signals
-{
-  sigpacket sigs[NSIG + 1];
-  sigpacket start;
-  sigpacket *end;
-  sigpacket *prev;
-  sigpacket *curr;
-  int empty;
-public:
-  void reset () {curr = &start; prev = &start;}
-  void add (sigpacket&);
-  void del ();
-  sigpacket *next ();
-  sigpacket *save () const {return curr;}
-  void restore (sigpacket *saved) {curr = saved;}
-  friend void __stdcall sig_dispatch_pending (bool);
-};
-
-static pending_signals sigq;
-
-struct sigaction *global_sigs;
-
 /*
  * Global variables
  */
+struct sigaction *global_sigs;
+
 const char *__sp_fn ;
 int __sp_ln;
 
@@ -114,15 +94,37 @@ muto NO_COPY *sync_proc_subproc = NULL;	// Control access to subproc stuff
 
 DWORD NO_COPY sigtid = 0;		// ID of the signal thread
 
-/* Functions
- */
+/* Function declarations */
 static int __stdcall checkstate (waitq *) __attribute__ ((regparm (1)));
 static __inline__ bool get_proc_lock (DWORD, DWORD);
 static void __stdcall remove_zombie (int);
-static DWORD WINAPI wait_sig (VOID *arg);
 static int __stdcall stopped_or_terminated (waitq *, _pinfo *);
 static DWORD WINAPI wait_subproc (VOID *);
+static DWORD WINAPI wait_sig (VOID *arg);
 
+/* wait_sig bookkeeping */
+
+class pending_signals
+{
+  sigpacket sigs[NSIG + 1];
+  sigpacket start;
+  sigpacket *end;
+  sigpacket *prev;
+  sigpacket *curr;
+public:
+  void reset () {curr = &start; prev = &start;}
+  void add (sigpacket&);
+  void del ();
+  sigpacket *next ();
+  sigpacket *save () const {return curr;}
+  void restore (sigpacket *saved) {curr = saved;}
+  friend void __stdcall sig_dispatch_pending (bool);
+  friend DWORD WINAPI wait_sig (VOID *arg);
+};
+
+static pending_signals sigq;
+
+/* Functions */
 void __stdcall
 sigalloc ()
 {
@@ -1012,25 +1014,13 @@ talktome ()
 	pids[i]->commune_recv ();
 }
 
-/* Process signals by waiting for a semaphore to become signaled.
-   Then scan an in-memory array representing queued signals.
-   Executes in a separate thread.
-
-   Signals sent from this process are sent a completion signal so
-   that returns from kill/raise do not occur until the signal has
-   has been handled, as per POSIX.  */
-
 void
 pending_signals::add (sigpacket& pack)
 {
   sigpacket *se;
-  for (se = start.next; se; se = se->next)
-    if (se->si.si_signo == pack.si.si_signo)
-      return;
-  while (sigs[empty].si.si_signo)
-    if (++empty == NSIG)
-      empty = 0;
-  se = sigs + empty;
+  if (sigs[pack.si.si_signo].si.si_signo)
+    return;
+  se = sigs + pack.si.si_signo;
   *se = pack;
   se->mask = &myself->getsigmask ();
   se->next = NULL;
@@ -1039,7 +1029,6 @@ pending_signals::add (sigpacket& pack)
   end = se;
   if (!start.next)
     start.next = se;
-  empty++;
 }
 
 void
@@ -1053,7 +1042,6 @@ pending_signals::del ()
 #endif
   if (end == curr)
     end = prev;
-  empty = curr - sigs;
   curr = next;
 }
 
@@ -1174,7 +1162,7 @@ wait_sig (VOID *self)
 	default:
 	  if (pack.si.si_signo < 0)
 	    sig_clear (-pack.si.si_signo);
-	  else
+	  else if (!sigq.sigs[pack.si.si_signo].si.si_signo)
 	    {
 	      int sig = pack.si.si_signo;
 	      int sigres = pack.process ();

winsup/cygwin/sigproc.h
@@ -66,7 +66,6 @@ int __stdcall handle_sigprocmask (int sig, const sigset_t *set,
   __attribute__ ((regparm (3)));
 
 extern "C" void __stdcall reset_signal_arrived ();
-extern "C" int __stdcall call_signal_handler_now ();
 void __stdcall sig_clear (int) __attribute__ ((regparm (1)));
 void __stdcall sig_set_pending (int) __attribute__ ((regparm (1)));
 int __stdcall handle_sigsuspend (sigset_t);

winsup/cygwin/path.cc
@@ -188,7 +188,6 @@ pathmatch (const char *path1, const char *path2)
 #define isslash(c) ((c) == '/')
 
 /* Normalize a POSIX path.
-   \'s are converted to /'s in the process.
    All duplicate /'s, except for 2 leading /'s, are deleted.
    The result is 0 for success, or an errno error value.  */
 
@@ -200,14 +199,11 @@ normalize_posix_path (const char *src, char *dst)
 
   syscall_printf ("src %s", src);
 
+  const char *in_src = src;
+  char *in_dst = dst;
+
   if (isdrive (src) || slash_unc_prefix_p (src))
-    {
-      int err = normalize_win32_path (src, dst);
-      if (!err)
-	for (char *p = dst; (p = strchr (p, '\\')); p++)
-	  *p = '/';
-      return err;
-    }
+    goto win32_path;
 
   if (!isslash (src[0]))
     {
@@ -247,6 +243,8 @@ normalize_posix_path (const char *src, char *dst)
 
   while (*src)
     {
+      if (*src == '\\')
+	goto win32_path;
       /* Strip runs of /'s.  */
       if (!isslash (*src))
 	*dst++ = *src++;
@@ -309,6 +307,13 @@ done:
 
   debug_printf ("%s = normalize_posix_path (%s)", dst_start, src_start);
   return 0;
+
+win32_path:
+  int err = normalize_win32_path (in_src, in_dst);
+  if (!err)
+    for (char *p = in_dst; (p = strchr (p, '\\')); p++)
+      *p = '/';
+  return err;
 }
 
 inline void

commit 496fa7b2ce0052550eab8900723ebb59c33d25e7

Cygwin: signal: Introduce a lock for the signal queue
Currently, the signal queue is touched by the thread sig as well as
other threads that call sigaction_worker(). This potentially has
a possibility to destroy the signal queue chain. A possible worst
result may be a self-loop chain which causes infinite loop. With
this patch, lock()/unlock() are introduce to avoid such a situation.

Fixes: 474048c26edf ("* sigproc.cc (pending_signals::add): Just index directly into signal array rather than treating the array as a heap.")
Suggested-by: Corinna Vinschen <corinna@vinschen.de>
Reviewed-by: Corinna Vinschen <corinna@vinschen.de>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/exceptions.cc
@@ -1450,10 +1450,10 @@ _cygtls::handle_SIGCONT (threadlist_t * &tl_entry)
 	sigsent = true;
       }
   /* Clear pending stop signals */
-  sig_clear (SIGSTOP);
-  sig_clear (SIGTSTP);
-  sig_clear (SIGTTIN);
-  sig_clear (SIGTTOU);
+  sig_clear (SIGSTOP, false);
+  sig_clear (SIGTSTP, false);
+  sig_clear (SIGTTIN, false);
+  sig_clear (SIGTTOU, false);
 }
 
 int
@@ -1554,14 +1554,14 @@ sigpacket::process ()
     goto exit_sig;
   if (si.si_signo == SIGSTOP)
     {
-      sig_clear (SIGCONT);
+      sig_clear (SIGCONT, false);
       goto stop;
     }
 
   /* Clear pending SIGCONT on stop signals */
   if (si.si_signo == SIGTSTP || si.si_signo == SIGTTIN
       || si.si_signo == SIGTTOU)
-    sig_clear (SIGCONT);
+    sig_clear (SIGCONT, false);
 
   if (handler == (void *) SIG_DFL)
     {

winsup/cygwin/local_includes/sigproc.h
@@ -62,7 +62,7 @@ void set_signal_mask (sigset_t&, sigset_t);
 int handle_sigprocmask (int sig, const sigset_t *set,
 				  sigset_t *oldset, sigset_t& opmask);
 
-void sig_clear (int);
+void sig_clear (int, bool);
 void sig_set_pending (int);
 int handle_sigsuspend (sigset_t);
 

winsup/cygwin/signal.cc
@@ -451,9 +451,9 @@ sigaction_worker (int sig, const struct sigaction *newact,
 	      if (!(gs.sa_flags & SA_NODEFER))
 		gs.sa_mask |= SIGTOMASK(sig);
 	      if (gs.sa_handler == SIG_IGN)
-		sig_clear (sig);
+		sig_clear (sig, true);
 	      if (gs.sa_handler == SIG_DFL && sig == SIGCHLD)
-		sig_clear (sig);
+		sig_clear (sig, true);
 	      if (sig == SIGCHLD)
 		{
 		  myself->process_state &= ~PID_NOCLDSTOP;

winsup/cygwin/sigproc.cc
@@ -106,12 +106,16 @@ class pending_signals
 {
   sigpacket sigs[_NSIG + 1];
   sigpacket start;
+  SRWLOCK queue_lock;
   bool retry;
+  void lock () { AcquireSRWLockExclusive (&queue_lock); }
+  void unlock () { ReleaseSRWLockExclusive (&queue_lock); }
 
 public:
+  pending_signals (): queue_lock (SRWLOCK_INIT) {}
   void add (sigpacket&);
   bool pending () {retry = !!start.next; return retry;}
-  void clear (int sig);
+  void clear (int sig, bool need_lock);
   void clear (_cygtls *tls);
   friend void sig_dispatch_pending (bool);
   friend void wait_sig (VOID *arg);
@@ -427,23 +431,27 @@ proc_terminate ()
 
 /* Clear pending signal */
 void
-sig_clear (int sig)
+sig_clear (int sig, bool need_lock)
 {
-  sigq.clear (sig);
+  sigq.clear (sig, need_lock);
 }
 
 /* Clear pending signals of specific si_signo.
    Called from sigpacket::process(). */
 void
-pending_signals::clear (int sig)
+pending_signals::clear (int sig, bool need_lock)
 {
   sigpacket *q = sigs + sig;
   if (!sig || !q->si.si_signo)
     return;
+  if (need_lock)
+    lock ();
   q->si.si_signo = 0;
   q->prev->next = q->next;
   if (q->next)
     q->next->prev = q->prev;
+  if (need_lock)
+    unlock ();
 }
 
 /* Clear pending signals of specific thread.  Called under TLS lock from
@@ -453,6 +461,7 @@ pending_signals::clear (_cygtls *tls)
 {
   sigpacket *q = &start;
 
+  lock ();
   while ((q = q->next))
     if (q->sigtls == tls)
       {
@@ -461,6 +470,7 @@ pending_signals::clear (_cygtls *tls)
 	if (q->next)
 	  q->next->prev = q->prev;
       }
+  unlock ();
 }
 
 /* Clear pending signals of specific thread.  Called from _cygtls::remove */
@@ -1313,11 +1323,13 @@ pending_signals::add (sigpacket& pack)
   if (se->si.si_signo)
     return;
   *se = pack;
+  lock ();
   se->next = start.next;
   se->prev = &start;
   se->prev->next = se;
   if (se->next)
     se->next->prev = se;
+  unlock ();
 }
 
 /* Process signals by waiting for signal data to arrive in a pipe.
@@ -1398,6 +1410,7 @@ wait_sig (VOID *)
 	    bool issig_wait;
 
 	    *pack.mask = 0;
+	    sigq.lock ();
 	    while ((q = q->next))
 	      {
 		_cygtls *sigtls = q->sigtls ?: _main_tls;
@@ -1411,6 +1424,7 @@ wait_sig (VOID *)
 		      }
 		  }
 	      }
+	    sigq.unlock ();
 	  }
 	  break;
 	case __SIGPENDING:
@@ -1419,6 +1433,7 @@ wait_sig (VOID *)
 
 	    *pack.mask = 0;
 	    tl_entry = cygheap->find_tls (pack.sigtls);
+	    sigq.lock ();
 	    while ((q = q->next))
 	      {
 		/* Skip thread-specific signals for other threads. */
@@ -1427,6 +1442,7 @@ wait_sig (VOID *)
 		if (pack.sigtls->sigmask & (bit = SIGTOMASK (q->si.si_signo)))
 		  *pack.mask |= bit;
 	      }
+	    sigq.unlock ();
 	    cygheap->unlock_tls (tl_entry);
 	  }
 	  break;
@@ -1461,7 +1477,7 @@ wait_sig (VOID *)
 	  break;
 	default:	/* Normal (positive) signal */
 	  if (pack.si.si_signo < 0)
-	    sig_clear (-pack.si.si_signo);
+	    sig_clear (-pack.si.si_signo, true);
 	  else
 	    sigq.add (pack);
 	  fallthrough;
@@ -1474,6 +1490,7 @@ wait_sig (VOID *)
 	    {
 	      /* Check the queue for signals.  There will always be at least one
 		 thing on the queue if this was a valid signal.  */
+	      sigq.lock ();
 	      while ((q = q->next))
 		{
 		  if (q->si.si_signo && q->process () > 0)
@@ -1484,6 +1501,7 @@ wait_sig (VOID *)
 			q->next->prev = q->prev;
 		    }
 		}
+	      sigq.unlock ();
 	      /* At least one signal still queued?  The event is used in select
 		 only, and only to decide if WFMO should wake up in case a
 		 signalfd is waiting via select/poll for being ready to read a