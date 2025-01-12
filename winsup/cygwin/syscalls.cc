picolibc/picolibc

commit fe2545e9faaf4bf9586f61a7b83d5cb5af501194

Cygwin: don't use unlink/rename POSIX semantics on certain NTFS
If a host NTFS is mapped into a Hyper-V isolated container, the
OPEN_BY_FILE_ID filesystem flag is missing, just as if that NTFS
is a remote drive.  However, NtQueryVolumeInformationFile claims
the drive is a local drive.

We can use this fact to learn that the process is running under
Hyper-V, and that the Hyper-V isolated process can't use rename/unlink
with POSIX semantics.  Strange enough, the POSIX_UNLINK_RENAME filesystem
flag is still set...

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/syscalls.cc
@@ -691,10 +691,12 @@ unlink_nt (path_conv &pc, bool shareable)
   pc.get_object_attr (attr, sec_none_nih);
 
   /* First check if we can use POSIX unlink semantics: W10 1709+, local NTFS.
-     With POSIX unlink semantics the entire job gets MUCH easier and faster.
-     Just try to do it and if it fails, it fails. */
+     For the OPEN_BY_FILE_ID flag, see MINIMAL_WIN_NTFS_FLAGS comment in
+     fs_info::update.  With POSIX unlink semantics the entire job gets MUCH
+     easier and faster.  Just try to do it and if it fails, it fails. */
   if (wincap.has_posix_unlink_semantics ()
-      && !pc.isremote () && pc.fs_is_ntfs ())
+      && !pc.isremote () && pc.fs_is_ntfs ()
+      && pc.has_attribute (FILE_SUPPORTS_OPEN_BY_FILE_ID))
     {
       FILE_DISPOSITION_INFORMATION_EX fdie;
 
@@ -2407,10 +2409,12 @@ rename2 (const char *oldpath, const char *newpath, unsigned int at2flags)
 	  __leave;
 	}
 
-      /* POSIX semantics only on local NTFS drives. */
+      /* POSIX semantics only on local NTFS drives. For the OPEN_BY_FILE_ID
+         flag, see MINIMAL_WIN_NTFS_FLAGS comment in fs_info::update. */
       use_posix_semantics = wincap.has_posix_rename_semantics ()
 			    && !oldpc.isremote ()
-			    && oldpc.fs_is_ntfs ();
+			    && oldpc.fs_is_ntfs ()
+			    && oldpc.has_attribute (FILE_SUPPORTS_OPEN_BY_FILE_ID);
 
       /* Opening the file must be part of the transaction.  It's not sufficient
 	 to call only NtSetInformationFile under the transaction.  Therefore we

commit 264544bf72f6ed85530a1b058e9efdb73ab72e90

Cygwin: unlink/rename: fix skipping deletion with POSIX semantics
unlink_nt() and rename2 () both check for the FILE_SUPPORTS_OPEN_BY_FILE_ID
flag to use POSIX delete/rename semantics.  Both erroneously check the flag
against the file attributes using has_attributes().

Given that this flag is a filesystem flag, check using fs_flags() instead.

Fixes: fe2545e9faaf ("Cygwin: don't use unlink/rename POSIX semantics on certain NTFS")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/release/3.5.6
@@ -4,3 +4,6 @@ Fixes:
 - Fix a regression in 3.5.5 when checking for execute permissions in
   execve(2) and access(2).
   Addresses: https://cygwin.com/pipermail/cygwin/2024-December/256972.html
+
+- Fix a regression since 3.5.0 which fails to use POSIX semantics in
+  unlink/rename on NTFS.

winsup/cygwin/syscalls.cc
@@ -740,7 +740,7 @@ unlink_nt (path_conv &pc, bool shareable)
      easier and faster.  Just try to do it and if it fails, it fails. */
   if (wincap.has_posix_unlink_semantics ()
       && !pc.isremote () && pc.fs_is_ntfs ()
-      && pc.has_attribute (FILE_SUPPORTS_OPEN_BY_FILE_ID))
+      && (pc.fs_flags () & FILE_SUPPORTS_OPEN_BY_FILE_ID))
     {
       FILE_DISPOSITION_INFORMATION_EX fdie;
 
@@ -2522,9 +2522,9 @@ rename2 (const char *oldpath, const char *newpath, unsigned int at2flags)
       /* POSIX semantics only on local NTFS drives. For the OPEN_BY_FILE_ID
          flag, see MINIMAL_WIN_NTFS_FLAGS comment in fs_info::update. */
       use_posix_semantics = wincap.has_posix_rename_semantics ()
-			    && !oldpc.isremote ()
-			    && oldpc.fs_is_ntfs ()
-			    && oldpc.has_attribute (FILE_SUPPORTS_OPEN_BY_FILE_ID);
+			&& !oldpc.isremote ()
+			&& oldpc.fs_is_ntfs ()
+			&& (oldpc.fs_flags () & FILE_SUPPORTS_OPEN_BY_FILE_ID);
 
 ignore_posix_semantics_retry:
       /* Opening the file must be part of the transaction.  It's not sufficient