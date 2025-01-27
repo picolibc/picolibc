picolibc/picolibc

commit d870655f570f25393dcefbaf0b1dc807f277749c

Cygwin: path_conv: set on-disk-device flag for message queue files
This simplifies further checks for on-disk-devices in places with
special handling for such files.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/path.cc
@@ -1273,7 +1273,10 @@ path_conv::check (const char *src, unsigned opt,
 
 	  /* FIXME: bad hack alert!!!  We need a better solution */
 	  if (!strncmp (path_copy, MQ_PATH, MQ_LEN) && path_copy[MQ_LEN])
-	    dev.parse (FH_MQUEUE);
+	    {
+	      dev.parse (FH_MQUEUE);
+	      dev.setfs (1);
+	    }
 	}
 
       if (opt & PC_NOFULL)

commit b940faa144cae9cd29295d8fdaac163a17f334cc

Cygwin: message queues are not devices
Message queues are basically just files and in most cases can be handled
like normal files.  So it was a mistake to set the "on-disk-device" flag
for them to fix the mq_unlink() problem reported in
https://cygwin.com/pipermail/cygwin/2025-January/257119.html

Rather, given that unlink() just checks if the object to be deleted
has an on-disk representation, make sure message queues are added to
the path_conv::isondisk() predicate.

This also reverts commit d870655f570f25393dcefbaf0b1dc807f277749c.

Fixes: d870655f570f ("Cygwin: path_conv: set on-disk-device flag for message queue files")
Reported-by: Christian Franke <Christian.Franke@t-online.de>
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/local_includes/devices.h
@@ -376,7 +376,8 @@ public:
   }
   fh_devices operator = (fh_devices n) {return d.devn_fh_devices = n;}
   inline void setfs (bool x) {dev_on_fs = x;}
-  inline bool isfs () const {return dev_on_fs || d.devn == FH_FS;}
+  inline bool isfs () const {return dev_on_fs || d.devn == FH_FS
+				    || d.devn == FH_MQUEUE;}
   inline bool is_fs_special () const {return dev_on_fs && d.devn != FH_FS;}
   inline bool is_dev_resident () const {return lives_in_dev;}
   inline int exists () const {return exists_func (*this);}

winsup/cygwin/local_includes/path.h
@@ -235,8 +235,7 @@ class path_conv
      /proc. */
   int isspecial () const {return dev.not_device (FH_FS);}
   /* Devices with representation on disk. This includes local sockets, FIFOs,
-     message queues and devices created with mknod.  It does not include
-     the /proc hierarchy. */
+     devices created with mknod.  It does not include the /proc hierarchy. */
   int is_fs_special () const {return dev.is_fs_special ();}
   /* Like is_fs_special but excluding local sockets. */
   int is_lnk_special () const {return is_fs_special () && !issocket ();}

winsup/cygwin/path.cc
@@ -1273,10 +1273,7 @@ path_conv::check (const char *src, unsigned opt,
 
 	  /* FIXME: bad hack alert!!!  We need a better solution */
 	  if (!strncmp (path_copy, MQ_PATH, MQ_LEN) && path_copy[MQ_LEN])
-	    {
-	      dev.parse (FH_MQUEUE);
-	      dev.setfs (1);
-	    }
+	    dev.parse (FH_MQUEUE);
 	}
 
       if (opt & PC_NOFULL)

winsup/cygwin/release/3.5.7
@@ -1,0 +1,5 @@
+Fixes:
+------
+
+- Fix stat() on message queues.
+  Addresses: https://cygwin.com/pipermail/cygwin/2025-January/257186.html