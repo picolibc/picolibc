705f624c7004b6568200536599cdf3bcbfa469a6
Keith Packard <keithp@keithp.com>
2025-01-26 16:51:25 -0800

    stdlib: Elide extended charset declarations as appropriate
    Don't declare symbols which won't ever be defined.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/stdlib/local.h
+++ newlib/libc/stdlib/local.h
@@ -21,11 +21,17 @@ typedef wctomb_f *wctomb_p;
 wctomb_f __ascii_wctomb;
 #ifdef _MB_CAPABLE
 wctomb_f __utf8_wctomb;
+#ifdef _MB_EXTENDED_CHARSETS_JIS
 wctomb_f __sjis_wctomb;
 wctomb_f __eucjp_wctomb;
 wctomb_f __jis_wctomb;
+#endif
+#ifdef _MB_EXTENDED_CHARSETS_ISO
 wctomb_p __iso_wctomb (int val);
+#endif
+#ifdef _MB_EXTENDED_CHARSETS_WINDOWS
 wctomb_p __cp_wctomb (int val);
+#endif
 #endif
 
 #define __WCTOMB (__get_current_locale()->wctomb)
@@ -37,21 +43,31 @@ typedef mbtowc_f *mbtowc_p;
 mbtowc_f __ascii_mbtowc;
 #ifdef _MB_CAPABLE
 mbtowc_f __utf8_mbtowc;
+#ifdef _MB_EXTENDED_CHARSETS_JIS
 mbtowc_f __sjis_mbtowc;
 mbtowc_f __eucjp_mbtowc;
 mbtowc_f __jis_mbtowc;
+#endif
+#ifdef _MB_EXTENDED_CHARSETS_ISO
 mbtowc_p __iso_mbtowc (int val);
+#endif
+#ifdef _MB_EXTENDED_CHARSETS_WINDOWS
 mbtowc_p __cp_mbtowc (int val);
+#endif
 #endif
 
 #define __MBTOWC (__get_current_locale()->mbtowc)
 
+#ifdef _MB_EXTENDED_CHARSETS_ISO
 extern wchar_t __iso_8859_conv[14][0x60];
 int __iso_8859_val_index (int);
 int __iso_8859_index (const char *);
+#endif
 
+#ifdef _MB_EXTENDED_CHARSETS_WINDOWS
 extern wchar_t __cp_conv[][0x80];
 int __cp_val_index (int);
 int __cp_index (const char *);
+#endif
 
 #endif
