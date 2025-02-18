668cb92aab81e4f79c5960e537034b409973d888
Keith Packard <keithp@keithp.com>
2025-02-17 22:14:48 -0800

    uchar: Fix surrogate handling in c16tomb
    Was using | 0x10000 instead of + 0x10000, so high surrogates with
    the sixth bit set were generating the wrong values.
    
    Generate an error when a high surrogate is not immediately followed by
    a low surrogate.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newlib/libc/uchar/c16rtomb.c
+++ newlib/libc/uchar/c16rtomb.c
@@ -48,23 +48,20 @@ c16rtomb (char *s, char16_t c16, mbstate_t *ps)
 #elif __SIZEOF_WCHAR_T__ == 4
     char32_t    c32;
 
-    /* High surrogate */
-    if (char16_is_high_surrogate(c16)) {
-        if (ps->__count != 0) {
-            errno = EILSEQ;
-            return (size_t) -1;
-        }
-        ps->__value.__ucs = ((char32_t) (c16 & 0x3ff) << 10) | 0x10000;
-        ps->__count = -1;
-        return 0;
-    } else if (char16_is_low_surrogate(c16)) {
-        if (ps->__count == -1) {
+    if (ps->__count == -1) {
+        ps->__count = 0;
+        /* Check for low surrogate */
+        if (char16_is_low_surrogate(c16)) {
             c32 = ps->__value.__ucs | (c16 & 0x3ff);
-            ps->__count = 0;
         } else {
             errno = EILSEQ;
             return (size_t) -1;
         }
+    } else if (char16_is_high_surrogate(c16)) {
+        /* High surrogate */
+        ps->__value.__ucs = ((char32_t) (c16 & 0x3ff) << 10) + 0x10000;
+        ps->__count = -1;
+        return 0;
     } else {
         c32 = c16;
     }
