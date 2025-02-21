picolibc/picolibc

commit df307349a09e6f1ba75c78b1427dcea40f435e6e

Cygwin: fhandler_netdrive::exists: add debug statement
Add a debug_printf printing the return code from GetAddrInfoW.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/fhandler/netdrive.cc
@@ -432,7 +432,7 @@ fhandler_netdrive::exists ()
   if (strlen (get_name ()) == 2)
     return virt_rootdir;
 
-  wchar_t name[MAX_PATH];
+  wchar_t name[CYG_MAX_PATH];
   struct addrinfoW *ai;
   INT ret;
   DWORD protocol = 0;
@@ -460,7 +460,10 @@ fhandler_netdrive::exists ()
   sys_mbstowcs (name, CYG_MAX_PATH, get_name ());
   ret = GetAddrInfoW (name + 2, NULL, NULL, &ai);
   if (!ret)
-    FreeAddrInfoW (ai);
+    {
+      debug_printf ("GetAddrInfoW(%W) returned %d", ret);
+      FreeAddrInfoW (ai);
+    }
 
   return ret ? virt_none : virt_directory;
 }

commit 8cc55d819bb78a775e3bab3359ca4d0510e9f16a

Cygwin: fhandler_netdrive::exists: fix new debug statement
The debug statement was wrong twice: Not only was it in the wrong
spot, it was also supposed to print the share name and missed to
add said argument.

Fixes: df307349a09e ("Cygwin: fhandler_netdrive::exists: add debug statement")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/fhandler/netdrive.cc
@@ -459,13 +459,14 @@ fhandler_netdrive::exists ()
      doesn't exist, or about 8 secs if DNS is unavailable. */
   sys_mbstowcs (name, CYG_MAX_PATH, get_name ());
   ret = GetAddrInfoW (name + 2, NULL, NULL, &ai);
-  if (!ret)
+  if (ret)
     {
-      debug_printf ("GetAddrInfoW(%W) returned %d", ret);
-      FreeAddrInfoW (ai);
+      debug_printf ("GetAddrInfoW(%W) returned %d", name + 2, ret);
+      return virt_none;
     }
 
-  return ret ? virt_none : virt_directory;
+  FreeAddrInfoW (ai);
+  return virt_directory;
 }
 
 fhandler_netdrive::fhandler_netdrive ():