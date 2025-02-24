picolibc/picolibc

commit 8cebbb2b42bfa65ad13c282d47b472b0e73484e3

Cygwin: fhandler_netdrive::exists: handle WebDAV URLs
WebDAV URLs may contain an at-sign followed by a port number or SSL.
This will throw GetAddrInfoW, so remove the at-sign prior to calling
GetAddrInfoW.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/fhandler/netdrive.cc
@@ -432,7 +432,7 @@ fhandler_netdrive::exists ()
   if (strlen (get_name ()) == 2)
     return virt_rootdir;
 
-  wchar_t name[CYG_MAX_PATH];
+  wchar_t name[CYG_MAX_PATH], *dav_at;
   struct addrinfoW *ai;
   INT ret;
   DWORD protocol = 0;
@@ -458,6 +458,11 @@ fhandler_netdrive::exists ()
      into IP addresses.  This may take up to about 3 secs if the name
      doesn't exist, or about 8 secs if DNS is unavailable. */
   sys_mbstowcs (name, CYG_MAX_PATH, get_name ());
+  /* Webdav URLs contain a @ after the hostname, followed by stuff.
+     Drop @ for GetAddrInfoW to succeed. */
+  if ((dav_at = wcschr (name, L'@')) != NULL)
+    *dav_at = L'\0';
+
   ret = GetAddrInfoW (name + 2, NULL, NULL, &ai);
   if (ret)
     {

winsup/cygwin/release/3.6.0
@@ -44,9 +44,9 @@ What changed:
   ls //server now also enumerates NFS shares.  If "server" is given
   as a flat name, only SMB shares are enumerated.
 
-- Expose //tsclient (Microsoft Terminal Services) shares as well as
-  //wsl$ (Plan 9 Network Provider) shares, i. e., WSL installation
-  root dirs.
+- Expose WebDav shares, //tsclient (Microsoft Terminal Services)
+  shares as well as //wsl$ (Plan 9 Network Provider) shares, i. e.,
+  WSL installation root dirs.
 
 - Redesign pipe handling to minimize toggling blocking mode.
   The query_hdl stuff is no longer needed in new implementation.

winsup/doc/new-features.xml
@@ -78,8 +78,9 @@ name, only SMB shares are enumerated.
 </para></listitem>
 
 <listitem><para>
-Expose //tsclient (Microsoft Terminal Services) shares as well as wsl$
-(Plan 9 Network Provider) shares, i. e., WSL installation root dirs.
+Expose WebDav shares, //tsclient (Microsoft Terminal Services) shares as
+well as //wsl$ (Plan 9 Network Provider) shares, i. e., WSL installation
+root dirs.
 </para></listitem>
 
 <listitem><para>

commit 7124d1c23dd1b8805d17aca83dbe5fc9a41aae81

Cygwin: fhandler_netdrive: don't enumerate NFS shares on WebDAV server
WebDAV resources trigger the code trying to enumerate NFS shares
on the server.  Disable this by checking for an at-sign as well.

Fixes: 8cebbb2b42bf ("Cygwin: fhandler_netdrive::exists: handle WebDAV URLs")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/fhandler/netdrive.cc
@@ -393,8 +393,8 @@ create_thread_and_wait (DIR *dir)
   /* For shares, use WNet functions. */
 
   /* Try NFS first, if the name contains a dot (i. e., supposedly is a FQDN
-     as used in NFS server enumeration). */
-  if (strchr (dir->__d_dirname, '.'))
+     as used in NFS server enumeration) but no at-sign. */
+  if (strchr (dir->__d_dirname, '.') && !strchr (dir->__d_dirname + 2, '@'))
     {
       ndi.provider = WNNC_NET_MS_NFS;
       ndi.sem = CreateSemaphore (&sec_none_nih, 0, 2, NULL);