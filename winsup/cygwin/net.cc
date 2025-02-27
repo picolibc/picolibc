picolibc/picolibc

commit b91d38db37d8088901e47d73a65dadac51e227fb

Cygwin: net: Make if_nametoindex, etc. consistent with if_nameindex.
Currently, if_nametoindex() and if_indextoname() handle interface names
such as "ethernet_32777", while if_nameindex() returns the names such
as "{5AF7ACD0-D52E-4DFC-A4D0-54D3E6D6B2AC}". This patch unifies the
interface names to the latter.

Fixes: c356901f0d69 ("Rename if_indextoname to cygwin_if_indextoname (analag for if_nametoindex)")
Reviewed-by: Corinna Vinschen <corinna@vinschen.de>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/net.cc
@@ -2001,13 +2001,40 @@ get_ifconf (struct ifconf *ifc, int what)
 extern "C" unsigned
 cygwin_if_nametoindex (const char *name)
 {
-  return (unsigned) ::if_nametoindex (name);
+  PIP_ADAPTER_ADDRESSES pa0 = NULL, pap;
+  if (get_adapters_addresses (&pa0, AF_UNSPEC))
+    for (pap = pa0; pap; pap = pap->Next)
+      if (strcmp (name, pap->AdapterName) == 0)
+	{
+	  free (pa0);
+	  return pap->IfIndex;
+	}
+  if (pa0)
+    free (pa0);
+  return 0;
 }
 
 extern "C" char *
 cygwin_if_indextoname (unsigned ifindex, char *ifname)
 {
-  return ::if_indextoname (ifindex, ifname);
+  if (ifindex == 0 || ifname == NULL)
+    {
+      set_errno (ENXIO);
+      return NULL;
+    }
+  PIP_ADAPTER_ADDRESSES pa0 = NULL, pap;
+  if (get_adapters_addresses (&pa0, AF_UNSPEC))
+    for (pap = pa0; pap; pap = pap->Next)
+      if (ifindex == pap->IfIndex)
+	{
+	  strcpy (ifname, pap->AdapterName);
+	  free (pa0);
+	  return ifname;
+	}
+  if (pa0)
+    free (pa0);
+  set_errno (ENXIO);
+  return NULL;
 }
 
 extern "C" struct if_nameindex *

winsup/cygwin/autoload.cc
@@ -462,8 +462,6 @@ LoadDLLfunc (GetNetworkParams, iphlpapi)
 LoadDLLfunc (GetTcpTable, iphlpapi)
 LoadDLLfunc (GetTcp6Table, iphlpapi)
 LoadDLLfunc (GetUdpTable, iphlpapi)
-LoadDLLfunc (if_indextoname, iphlpapi)
-LoadDLLfunc (if_nametoindex, iphlpapi)
 
 LoadDLLfuncEx2 (DiscardVirtualMemory, kernel32, 1, 127)
 LoadDLLfuncEx (ClosePseudoConsole, kernel32, 1)

winsup/cygwin/release/3.5.1
@@ -3,3 +3,6 @@ Bug Fixes
 
 - Fix exit code for non-cygwin process running in console. The bug
   was introduced in 3.5.0.
+
+- Make the interface names handled by if_nametoindex() and if_indextoname()
+  consistent with that of if_nameindex().

commit 5e435a20b5c851ecc7bea160aaf0b9fa9aec2363

Cygwin: if_nametoindex: fix a 'use-after-free" uncovered by gcc-14
Fixes: b91d38db37d8 ("Cygwin: net: Make if_nametoindex, etc. consistent with if_nameindex.")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/net.cc
@@ -2006,8 +2006,9 @@ cygwin_if_nametoindex (const char *name)
     for (pap = pa0; pap; pap = pap->Next)
       if (strcmp (name, pap->AdapterName) == 0)
 	{
+	  unsigned idx = pap->IfIndex;
 	  free (pa0);
-	  return pap->IfIndex;
+	  return idx;
 	}
   if (pa0)
     free (pa0);