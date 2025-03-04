picolibc/picolibc

commit 1cca343e4714d11c53e23c8a3a17deba3e5531db

Drop redundant arc4random prototypes from cygwin/stdlib.h
Newlib's default stdlib.h now defines these functions so we can
drop them from the Cygwin-specific header.  Remove the arc4random_stir
and arc4random_uniform prototypes entirely.  They shouldn't be used
by applications anymore.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/include/cygwin/stdlib.h
@@ -19,12 +19,6 @@ extern "C"
 {
 #endif
 
-__uint32_t arc4random(void);
-void arc4random_addrandom(unsigned char *, int);
-void arc4random_buf(void *, size_t);
-void arc4random_stir(void);
-__uint32_t arc4random_uniform(__uint32_t);
-
 const char *getprogname (void);
 void	setprogname (const char *);
 

commit a8891c932192eaf2b19f14958ccc662d37ec7236

Cygwin: stdlib.h: resurrect arc4random_stir() definition
An autoconf test for arc4random_stir() succeeds on Cygwin, because
we export the symbol for backward compatibility.  However, when
building with -Werror afterwards, the definition of arc4random_stir()
is missing in the stdlib.h header, so there's a chance to encounter
an error like this with newer GCC:

sshd.c:1113:25: error: implicit declaration of function ‘arc4random_stir’; did you mean ‘arc4random_buf’? [-Wimplicit-function-declaration]
 1113 |                         arc4random_stir();
      |                         ^~~~~~~~~~~~~~~
      |                         arc4random_buf

This occurs for instance when building OpenSSH 9.9p2 with GCC 15.0.

Define arc4random_stir() as an empty macro in cygwin/stdlib.h
to avoid the above compiler warning.

Do not define arc4random_addrandom() yet, in the hope that this function
call is really, really not used anymore.

Fixes: 1cca343e4714 ("Drop redundant arc4random prototypes from cygwin/stdlib.h")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/include/cygwin/stdlib.h
@@ -20,6 +20,11 @@ extern "C"
 const char *getprogname (void);
 void	setprogname (const char *);
 
+#if __BSD_VISIBLE
+/* Cygwin exports arc4random_stir() for backward compatibility.  Define
+   here as empty macro to avoid compiler warnings. */
+#define arc4random_stir()
+#endif
 #if __GNU_VISIBLE
 char *canonicalize_file_name (const char *);
 #endif