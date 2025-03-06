picolibc/picolibc

commit 3c1308ed890eac4333d0a16f5795203cee4f38b2

Cygwin: signal: Fix a problem that process hangs on exit
The process that receives many SIGSTOP/SIGCONT signals sometimes hangs
on exit in sig_dispatch_pending(). This patch skips processing signals
in call_signal_handler() when exit_state > ES_EXIT_STARTING to avoid
that situation.

Addresses: https://cygwin.com/pipermail/cygwin/2025-February/257473.html
Fixes: d243e51ef1d3 ("Cygwin: signal: Fix deadlock between main thread and sig thread")
Reported-by: Christian Franke <Christian.Franke@t-online.de>
Reviewed-by: Corinna Vinschen <corinna@vinschen.de>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/exceptions.cc
@@ -1675,7 +1675,7 @@ _cygtls::call_signal_handler ()
   while (1)
     {
       lock ();
-      if (!current_sig)
+      if (!current_sig || exit_state > ES_EXIT_STARTING)
 	{
 	  unlock ();
 	  break;

commit a3863bfeb73f3c3832038685498c7c4148a06890

Cygwin: signal: Add one more guard to stop signal handling on exit().
The commit 3c1308ed890e adds a guard to stop signal handling on exit()
in call_signal_handler(). However, the signal that is already queued
but does not use signal handler may be going to process even with that
patch.
This patch add one more guard at the begining of sigpacket::process()
to avoid that situation.

Fixes: 3c1308ed890e ("Cygwin: signal: Fix a problem that process hangs on exit")
Reviewed-by: Corinna Vinschen <corinna@vinschen.de>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/exceptions.cc
@@ -1457,7 +1457,7 @@ sigpacket::process ()
 
   /* Don't try to send signals if we're just starting up since signal masks
      may not be available.  */
-  if (!cygwin_finished_initializing)
+  if (!cygwin_finished_initializing || exit_state > ES_EXIT_STARTING)
     {
       rc = -1;
       goto done;