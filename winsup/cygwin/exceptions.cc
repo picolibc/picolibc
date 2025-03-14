picolibc/picolibc

commit 1d97f8aa43851df24c88c1d3f32071b1c16f0e0d

Cygwin: signals: don't evaluate SA_SIGINFO context after handler returns
After return from a signal handler created with the SA_SIGINFO flag,
Cygwin checks if the handler changed the context.  If so, Cygwin
erroneously uses the context in a call to setcontext().

Drop this special behaviour.  Neither Linux man pages, nor POSIX
documentation treat the context pointer given to the handler as
anything other than a read-only area.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/exceptions.cc
@@ -1850,14 +1850,6 @@ _cygtls::call_signal_handler ()
 					? context.uc_sigmask : this_oldmask);
       if (this_errno >= 0)
 	set_errno (this_errno);
-      if (this_sa_flags & SA_SIGINFO)
-	{
-	  /* If more than just the sigmask in the context has been changed by
-	     the signal handler, call setcontext. */
-	  context_copy.uc_sigmask = context.uc_sigmask;
-	  if (memcmp (&context, &context_copy, sizeof context) != 0)
-	    setcontext (&context);
-	}
     }
 
   /* FIXME: Since 2011 this return statement always returned 1 (meaning

commit 0f02ac19102dd8e5c5722cac2f6069e646f0f42c

Cygwin: signal: Remove context_copy in call_signal_handler()
After the commit 1d97f8aa4385, context_copy in call_signal_handle()
is not used anymore. This patch removes it.

Fixes: 1d97f8aa4385 ("Cygwin: signals: don't evaluate SA_SIGINFO context after handler returns")
Reviewed-by: Corinna Vinschen <corinna@vinschen.de>
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/exceptions.cc
@@ -1694,7 +1694,7 @@ _cygtls::call_signal_handler ()
       siginfo_t thissi = infodata;
       void (*thisfunc) (int, siginfo_t *, void *) = func;
 
-      ucontext_t *thiscontext = NULL, context_copy;
+      ucontext_t *thiscontext = NULL;
 
       /* Only make a context for SA_SIGINFO handlers */
       if (this_sa_flags & SA_SIGINFO)
@@ -1752,7 +1752,6 @@ _cygtls::call_signal_handler ()
 				     ? (uintptr_t) thissi.si_addr : 0;
 
 	  thiscontext = &context1;
-	  context_copy = context1;
 	}
 
       int this_errno = saved_errno;