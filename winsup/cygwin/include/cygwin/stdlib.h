picolibc/picolibc

commit ad29c33229e5fdd9a93469933d4ab77a733877af

Cygwin: stdlib.h: revert declaring arc4random_stir() as function
The idea to define arc4random_stir() as macro to avoid autoconf
problems in commit a8891c932192 ("Cygwin: stdlib.h: resurrect
arc4random_stir() definition") worked fine for OpenSSH, but not
for libbsd.  During the build process the macro shadows the
definition of arc4random_stir() and results in an error message
like this:

  error: macro "arc4random_stir" passed 1 arguments, but takes just 0

Give up and just declare arc4random_stir() in a compatible way with
libbsd again.

Fixes: a8891c932192 ("Cygwin: stdlib.h: resurrect arc4random_stir() definition")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/include/cygwin/stdlib.h
@@ -21,9 +21,9 @@ const char *getprogname (void);
 void	setprogname (const char *);
 
 #if __BSD_VISIBLE
-/* Cygwin exports arc4random_stir() for backward compatibility.  Define
-   here as empty macro to avoid compiler warnings. */
-#define arc4random_stir()
+/* Cygwin exports arc4random_stir() for backward compatibility.
+   Keep the declaration compatible with libbsd. */
+void arc4random_stir(void);
 #endif
 #if __GNU_VISIBLE
 char *canonicalize_file_name (const char *);

commit 5ec497dc80bcb7ad78cc07bb919b2624b361f017

Cygwin: don't export arc4random_addrandom/arc4random_stir
Keep the functions for backward compatibility, but don't export
them from libcygwin.a for new applications.  Drop the workaround
from commit ad29c33229e5 ("Cygwin: stdlib.h: revert declaring
arc4random_stir() as function").

Fixes: ad29c33229e5 ("Cygwin: stdlib.h: revert declaring arc4random_stir() as function")
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/Makefile.am
@@ -459,6 +459,8 @@ VERSION_OFILES = version.o winver.o
 #
 
 NEW_FUNCTIONS=$(addprefix --replace=,\
+	arc4random_addrandom= \
+	arc4random_stir= \
 	atexit= \
 	timezone= \
 	uname=uname_x \

winsup/cygwin/include/cygwin/stdlib.h
@@ -20,11 +20,6 @@ extern "C"
 const char *getprogname (void);
 void	setprogname (const char *);
 
-#if __BSD_VISIBLE
-/* Cygwin exports arc4random_stir() for backward compatibility.
-   Keep the declaration compatible with libbsd. */
-void arc4random_stir(void);
-#endif
 #if __GNU_VISIBLE
 char *canonicalize_file_name (const char *);
 #endif