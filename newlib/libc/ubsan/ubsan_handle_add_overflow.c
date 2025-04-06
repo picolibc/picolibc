43aa47b2a2a7ddd64ac5c2d47c25bf32a40a31f1
Keith Packard <keithp@keithp.com>
2025-04-05 23:45:16 -0700

    ubsan: Provide complete set of handlers for -fsanitize=undefined
    These are all of the undefind behavior handlers for both gcc and
    clang. When building applications with -fsanitize=undefined, these
    will present error messages and call abort() to terminate the
    application.
    
    Negative shift operations with valid values are allowed so that
    arithmetic right shift continues to work. Right shift is
    implementation defined, while left shift is undefined behavior, but
    there doesn't appear to be a way to distinguish these in the handler.
    
    This uses the existing meson -Db_sanitize option while leaving the
    -Dsanitize=bounds option available as a compatibility change.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>
    
    fixup-flags

---
+++ newlib/libc/ubsan/ubsan_handle_add_overflow.c
@@ -0,0 +1,50 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright © 2025 Keith Packard
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "ubsan.h"
+
+void
+__ubsan_handle_add_overflow(void *_data,
+                            void *lhs,
+                            void *rhs)
+{
+    struct overflow_data *data = _data;
+    char lhs_str[VAL_STR_LEN];
+    char rhs_str[VAL_STR_LEN];
+    __ubsan_val_to_string(lhs_str, data->type, lhs);
+    __ubsan_val_to_string(rhs_str, data->type, rhs);
+    __ubsan_error(&data->location, "add_overflow", "%s + %s\n", lhs_str, rhs_str);
+}
+
