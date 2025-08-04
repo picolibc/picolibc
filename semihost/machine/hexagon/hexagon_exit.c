16028fc476e0cfe1e9029eb7b33a2cdc2f977cd3
Kushal Pal <kushpal@qti.qualcomm.com>
2025-08-04 14:45:03 +0530

    Fix hexagon_exit
    Signed-off-by: Kushal Pal <kushpal@qti.qualcomm.com>

--- semihost/machine/hexagon/hexagon_exit.c
+++ semihost/machine/hexagon/hexagon_exit.c
@@ -6,12 +6,9 @@ _exit(int status)
 {
     /* simulator reads exit status from r2 */
     /* simulator reads system call code from r0 */
-    register uintptr_t r0 __asm__("r0") = status;
-    __asm__ __volatile__("r2 = %0\n"
-                         "r0 = #24\n" SWI
-                         : "=r"(r0)
-                         :
-                         : "memory");
+    register uintptr_t r2 __asm__("r2") = status;
+    register uintptr_t r0 __asm__("r0") = SYS_EXIT;
+    __asm__ __volatile__(SWI : : "r"(r0), "r"(r2) : "memory");
     /* Should never execute */
     __builtin_unreachable();
 }
