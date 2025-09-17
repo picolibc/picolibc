94f1bdc48f9052c99650d88a78b2aa3b52a14ac2
Venkata Ramanaiah Nalamothu <vnalamot@qti.qualcomm.com>
2025-09-16 19:17:55 -0700

    Address feedback

--- newlib/libc/machine/riscv/memcpy.c
+++ newlib/libc/machine/riscv/memcpy.c
@@ -9,10 +9,88 @@
    http://www.opensource.org/licenses.
 */
 
-#include <picolibc.h>
+#include "./rv_memcpy.h"
 
-#if defined(__PREFER_SIZE_OVER_SPEED) || defined(__OPTIMIZE_SIZE__)
-# define _MACHINE_RISCV_MEMCPY_ASM_
-#else
-# define _MACHINE_RISCV_MEMCPY_C_
+#ifdef _MACHINE_RISCV_MEMCPY_C_
+#include <string.h>
+#include <stdint.h>
+#include "../../string/local.h"
+
+#define unlikely(X) __builtin_expect (!!(X), 0)
+
+#undef memcpy
+
+void *
+__no_builtin
+#if __riscv_misaligned_slow || __riscv_misaligned_fast
+__disable_sanitizer
 #endif
+memcpy(void *__restrict aa, const void *__restrict bb, size_t n)
+{
+  #define BODY(a, b, t) { \
+    t tt = *b; \
+    a++, b++; \
+    *(a - 1) = tt; \
+  }
+
+  char *a = (char *)aa;
+  const char *b = (const char *)bb;
+  char *end = a + n;
+  uintptr_t msk = sizeof (long) - 1;
+#if __riscv_misaligned_slow || __riscv_misaligned_fast
+  if (n < sizeof (long))
+#else
+  if (unlikely ((((uintptr_t)a & msk) != ((uintptr_t)b & msk))
+	       || n < sizeof (long)))
+#endif
+    {
+small:
+      if (__builtin_expect (a < end, 1))
+	while (a < end)
+	  BODY (a, b, char);
+      return aa;
+    }
+
+  if (unlikely (((uintptr_t)a & msk) != 0))
+    while ((uintptr_t)a & msk)
+      BODY (a, b, char);
+
+  long *la = (long *)a;
+  const long *lb = (const long *)b;
+  long *lend = (long *)((uintptr_t)end & ~msk);
+
+  if (unlikely (lend - la > 8))
+    {
+      while (lend - la > 8)
+	{
+	  long b0 = *lb++;
+	  long b1 = *lb++;
+	  long b2 = *lb++;
+	  long b3 = *lb++;
+	  long b4 = *lb++;
+	  long b5 = *lb++;
+	  long b6 = *lb++;
+	  long b7 = *lb++;
+	  long b8 = *lb++;
+	  *la++ = b0;
+	  *la++ = b1;
+	  *la++ = b2;
+	  *la++ = b3;
+	  *la++ = b4;
+	  *la++ = b5;
+	  *la++ = b6;
+	  *la++ = b7;
+	  *la++ = b8;
+	}
+    }
+
+  while (la < lend)
+    BODY (la, lb, long);
+
+  a = (char *)la;
+  b = (const char *)lb;
+  if (unlikely (a < end))
+    goto small;
+  return aa;
+}
+#endif /* _MACHINE_RISCV_MEMCPY_C_ */
