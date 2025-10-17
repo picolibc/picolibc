3cc54944695d8a98f59da109e1209529b6d2c9a5
Kushal Pal <kushpal@qti.qualcomm.com>
2025-10-17 10:20:28 +0530

    Add copyright headers for Hexagon files
    Signed-off-by: Kushal Pal <kushpal@qti.qualcomm.com>

--- picocrt/machine/hexagon/crt0_standalone.c
+++ picocrt/machine/hexagon/crt0_standalone.c
@@ -1,3 +1,8 @@
+/*
+Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+SPDX-License-Identifier: BSD-3-Clause-Clear
+*/
+
 #include <stdint.h>
 
 #include "../../crt0.h"
--- semihost/machine/hexagon/hexagon_lseek.c
+++ semihost/machine/hexagon/hexagon_lseek.c
@@ -1,3 +1,8 @@
+/*
+Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+SPDX-License-Identifier: BSD-3-Clause-Clear
+*/
+
 #include "hexagon_semihost.h"
 #include <errno.h>
 #include <unistd.h>
