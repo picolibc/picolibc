a5698c9ace8c274cd30a59c314988fc787bbfb00
Keith Packard <keithp@keithp.com>
2025-11-03 11:31:43 -0800

    libc-testsuite: Expand fnmatch testing
    Add tests from Zephyr, then re-enable tests for character classes,
    equivalence classes and collating sequences.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- test/libc-testsuite/fnmatch.c
+++ test/libc-testsuite/fnmatch.c
@@ -133,24 +133,121 @@ const struct {
     { "*/*", "a/.b", FNM_PATHNAME|FNM_PERIOD, FNM_NOMATCH },
     { "*?*/*", "a/.b", FNM_PERIOD, 0 },
     { "*[.]/b", "a./b", FNM_PATHNAME|FNM_PERIOD, 0 },
-#ifndef __PICOLIBC__
     { "*[[:alpha:]]/*[[:alnum:]]", "a/b", FNM_PATHNAME, 0 },
-#endif
-    /* These three tests should result in error according to SUSv3.
-     * See XCU 2.13.1, XBD 9.3.5, & fnmatch() */
-#if 0
-    { "*[![:digit:]]*/[![:d-d]", "a/b", FNM_PATHNAME, -FNM_NOMATCH },
-    { "*[![:digit:]]*/[[:d-d]", "a/[", FNM_PATHNAME, -FNM_NOMATCH },
+    { "*[![:digit:]]*/[![:d-d]", "a/b", FNM_PATHNAME, 0 },
+    { "*[![:digit:]]*/[[:d-d]", "a/[", FNM_PATHNAME, 0 },
     { "*[![:digit:]]*/[![:d-d]", "a/[", FNM_PATHNAME, -FNM_NOMATCH },
-#endif
     { "a?b", "a.b", FNM_PATHNAME|FNM_PERIOD, 0 },
     { "a*b", "a.b", FNM_PATHNAME|FNM_PERIOD, 0 },
     { "a[.]b", "a.b", FNM_PATHNAME|FNM_PERIOD, 0 },
+    { "[[=a=]]", "a", 0, 0 },
+    { "[[=a=]][[=b=]]", "ab", 0, 0 },
+    { "[[=a=]][[=c=]]", "ab", 0, FNM_NOMATCH },
+    { "[[.foo.]]", "f", 0, -FNM_NOMATCH },
+    { "[[.a.]]", "a", 0, 0 },
 };
+
+#define zassert_equal(x, v) do {                                        \
+        int __val = (x);                                                \
+        if (__val != (v)) {                                             \
+            printf("Test %d failed: got %d want %d. %s\n",              \
+                   __LINE__, __val, (v), #x);                           \
+            ret = 0;                                                    \
+        }                                                               \
+    } while(0)
+
+#define zassert_not_equal(x, v) do {                                    \
+        int __val = (x);                                                \
+        if (__val == (v)) {                                             \
+            printf("Test %d failed: unexpectedly got %d. %s\n",         \
+                   __LINE__, __val, #x);                                \
+            ret = 0;                                                    \
+        }                                                               \
+    } while(0)
+
+#define zassert_ok(x) zassert_equal(x, 0)
+
+/* Adopt Zephyr fnmatch tests for picolibc test suite */
+static int zephyr_test_fnmatch(void) {
+    int ret = 1;
+    zassert_ok(fnmatch("*.c", "foo.c", 0));
+    zassert_ok(fnmatch("*.c", ".c", 0));
+    zassert_equal(fnmatch("*.a", "foo.c", 0), FNM_NOMATCH);
+    zassert_ok(fnmatch("*.c", ".foo.c", 0));
+    zassert_equal(fnmatch("*.c", ".foo.c", FNM_PERIOD), FNM_NOMATCH);
+    zassert_ok(fnmatch("*.c", "foo.c", FNM_PERIOD));
+    zassert_equal(fnmatch("a\\*.c", "a*.c", FNM_NOESCAPE), FNM_NOMATCH);
+    zassert_equal(fnmatch("a\\*.c", "ax.c", 0), FNM_NOMATCH);
+    zassert_ok(fnmatch("a[xy].c", "ax.c", 0));
+    zassert_ok(fnmatch("a[!y].c", "ax.c", 0));
+    zassert_equal(fnmatch("a[a/z]*.c", "a/x.c", FNM_PATHNAME), FNM_NOMATCH);
+    zassert_ok(fnmatch("a/*.c", "a/x.c", FNM_PATHNAME));
+    zassert_equal(fnmatch("a*.c", "a/x.c", FNM_PATHNAME), FNM_NOMATCH);
+    zassert_ok(fnmatch("*/foo", "/foo", FNM_PATHNAME));
+    zassert_ok(fnmatch("-O[01]", "-O1", 0));
+    zassert_equal(fnmatch("[[?*\\]", "\\", 0), FNM_NOMATCH);    /* \\ escapes ], no bracket expr, no match */
+    zassert_ok(fnmatch("[[?*\\]", "\\", FNM_NOESCAPE));         /* \\ unescaped, match */
+    zassert_ok(fnmatch("[[?*\\\\]", "\\", 0));                  /* \\ escapes \\, match */
+    zassert_equal(fnmatch("[]?*\\]", "]", 0), FNM_NOMATCH);     /* \\ escapes ], no bracket expr, no match */
+    zassert_ok(fnmatch("[]?*\\]", "]", FNM_NOESCAPE));          /* \\ unescaped, match */
+    zassert_ok(fnmatch("[]?*\\\\]", "]", 0));                   /* \\ escapes \\, match */
+    zassert_ok(fnmatch("[!]a-]", "b", 0));
+    zassert_ok(fnmatch("[]-_]", "^", 0));
+    zassert_ok(fnmatch("[!]-_]", "X", 0));
+    zassert_equal(fnmatch("??", "-", 0), FNM_NOMATCH);
+    zassert_equal(fnmatch("*LIB*", "lib", FNM_PERIOD), FNM_NOMATCH);
+    zassert_ok(fnmatch("a[/]b", "a/b", 0));
+    zassert_equal(fnmatch("a[/]b", "a/b", FNM_PATHNAME), FNM_NOMATCH);
+    zassert_ok(fnmatch("[a-z]/[a-z]", "a/b", 0));
+    zassert_equal(fnmatch("*", "a/b", FNM_PATHNAME), FNM_NOMATCH);
+    zassert_equal(fnmatch("*[/]b", "a/b", FNM_PATHNAME), FNM_NOMATCH);
+    zassert_equal(fnmatch("*[b]", "a/b", FNM_PATHNAME), FNM_NOMATCH);
+    zassert_equal(fnmatch("[*]/b", "a/b", 0), FNM_NOMATCH);
+    zassert_ok(fnmatch("[*]/b", "*/b", 0));
+    zassert_equal(fnmatch("[?]/b", "a/b", 0), FNM_NOMATCH);
+    zassert_ok(fnmatch("[?]/b", "?/b", 0));
+    zassert_ok(fnmatch("[[a]/b", "a/b", 0));
+    zassert_ok(fnmatch("[[a]/b", "[/b", 0));
+    zassert_equal(fnmatch("\\*/b", "a/b", 0), FNM_NOMATCH);
+    zassert_ok(fnmatch("\\*/b", "*/b", 0));
+    zassert_equal(fnmatch("\\?/b", "a/b", 0), FNM_NOMATCH);
+    zassert_ok(fnmatch("\\?/b", "?/b", 0));
+    zassert_ok(fnmatch("[/b", "[/b", 0));
+    zassert_ok(fnmatch("\\[/b", "[/b", 0));
+    zassert_ok(fnmatch("??"
+                       "/b",
+                       "aa/b", 0));
+    zassert_ok(fnmatch("???b", "aa/b", 0));
+    zassert_equal(fnmatch("???b", "aa/b", FNM_PATHNAME), FNM_NOMATCH);
+    zassert_equal(fnmatch("?a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+    zassert_equal(fnmatch("a/?b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+    zassert_equal(fnmatch("*a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+    zassert_equal(fnmatch("a/*b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+    zassert_equal(fnmatch("[.]a/b", ".a/b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+    zassert_equal(fnmatch("a/[.]b", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+    zassert_ok(fnmatch("*/?", "a/b", FNM_PATHNAME | FNM_PERIOD));
+    zassert_ok(fnmatch("?/*", "a/b", FNM_PATHNAME | FNM_PERIOD));
+    zassert_ok(fnmatch(".*/?", ".a/b", FNM_PATHNAME | FNM_PERIOD));
+    zassert_ok(fnmatch("*/.?", "a/.b", FNM_PATHNAME | FNM_PERIOD));
+    zassert_equal(fnmatch("*/*", "a/.b", FNM_PATHNAME | FNM_PERIOD), FNM_NOMATCH);
+    zassert_ok(fnmatch("*?*/*", "a/.b", FNM_PERIOD));
+    zassert_ok(fnmatch("*[.]/b", "a./b", FNM_PATHNAME | FNM_PERIOD));
+    zassert_ok(fnmatch("*[[:alpha:]]/""*[[:alnum:]]", "a/b", FNM_PATHNAME));
+    zassert_ok(fnmatch("*[![:digit:]]*/[![:d-d]", "a/b", FNM_PATHNAME));
+    zassert_ok(fnmatch("*[![:digit:]]*/[[:d-d]", "a/[", FNM_PATHNAME));
+    zassert_not_equal(fnmatch("*[![:digit:]]*/[![:d-d]", "a/[", FNM_PATHNAME), 0);
+    zassert_ok(fnmatch("a?b", "a.b", FNM_PATHNAME | FNM_PERIOD));
+    zassert_ok(fnmatch("a*b", "a.b", FNM_PATHNAME | FNM_PERIOD));
+    zassert_ok(fnmatch("a[.]b", "a.b", FNM_PATHNAME | FNM_PERIOD));
+    return ret;
+}
 
 static int test_fnmatch(void) {
     int i;
     unsigned int failed = 0;
+
+    if (!zephyr_test_fnmatch())
+        return 1;
 
     for (i = 0; i < (int)(sizeof(tests) / sizeof(*tests)); i++) {
         int r, x;
