62b36abaa39b3e43762e5c61880351c2b2bde1b2
Keith Packard <keithp@keithp.com>
2026-01-25 16:34:53 -0800

    include: Remove spurious UNIX System Laboratory name from headers
    These appear to just be a reference to the org and not part of the
    license or list of copyright holders.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- libc/include/grp.h
+++ libc/include/grp.h
@@ -3,7 +3,6 @@
 /*-
  * Copyright (c) 1989, 1993
  *	The Regents of the University of California.  All rights reserved.
- * (c) UNIX System Laboratories, Inc.
  * All or some portions of this file are derived from material licensed
  * to the University of California by American Telephone and Telegraph
  * Co. or Unix System Laboratories, Inc. and are reproduced herein with
