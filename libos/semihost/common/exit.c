3012b33f3291b542a9bebd9468db41c4be5f922e
Keith Packard <keithp@keithp.com>
2026-07-19 17:33:30 -0700

    semihost/arm: Always try exit_extended first on 32-bit
    As every known semihosting implementation supports the extended exit
    call, and because FVP doesn't support the semihosting extension
    detection mechanism, just try the extended exit first and if that
    fails, fall back to the classic mechanism.
    
    Move translation of a non-zero exit code into
    ADP_Stopped_RunTimeErrorUnknown down to the sys_semihost_exit
    implementation so that we only do that on 32-bit targets which
    don't provide a way to pass the exit status.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- libos/semihost/common/exit.c
+++ libos/semihost/common/exit.c
@@ -40,14 +40,17 @@
 __noreturn void
 _exit(int code)
 {
-    if (sys_semihost_feature(SH_EXT_EXIT_EXTENDED)) {
+    if (sizeof(sh_param_t) != 8) {
+        /*
+         * On 32-bit arm, try the extended version first as
+         * SYS_EXIT doesn't pass the exit status along.
+         *
+         * We should use the semihosting extension mechanism to detect
+         * if this is supported, but FVP doesn't support detecting
+         * extensions, so we just give a try and if it fails, we fall
+         * back to the original path.
+         */
         sys_semihost_exit_extended(code);
-    } else {
-        uintptr_t value;
-        if (code == 0)
-            value = ADP_Stopped_ApplicationExit;
-        else
-            value = ADP_Stopped_RunTimeErrorUnknown;
-        sys_semihost_exit(value, code);
     }
+    sys_semihost_exit(ADP_Stopped_ApplicationExit, code);
 }
