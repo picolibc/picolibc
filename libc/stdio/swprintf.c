259288f3493bd5ef8bf687ed1028374a34e7bb6b
Zain Siddavatam <zsiddava@qti.qualcomm.com>
2026-08-04 23:56:25 +0530

    stdio: Add error-reporting wide string put function
    Unlike snprintf, swprintf and vswprintf must return a negative value
    when the generated output does not fit in the buffer.
    
    Add __file_wstr_put, which returns _FDEV_ERR when it reaches the slot
    reserved for the terminating null character. Add
    FDEV_SETUP_WSTRING_WRITE to use this function and mark zero-sized
    buffers as unwritable, ensuring empty output with a zero size also
    returns an error.
    
    Signed-off-by: Zain Siddavatam <zsiddava@qti.qualcomm.com>

--- libc/stdio/swprintf.c
+++ libc/stdio/swprintf.c
@@ -40,7 +40,7 @@ swprintf(wchar_t *s, size_t n, const wchar_t *fmt, ...)
 {
     va_list           ap;
     int               i;
-    struct __file_str f = FDEV_SETUP_STRING_WRITE((char *)s, (char *)FDEV_STRING_WRITE_END(s, n));
+    struct __file_str f = FDEV_SETUP_WSTRING_WRITE(s, n);
     f.file.flags |= __SWIDE;
 
     va_start(ap, fmt);
--- libc/stdio/vswprintf.c
+++ libc/stdio/vswprintf.c
@@ -38,7 +38,7 @@
 int
 vswprintf(wchar_t *s, size_t n, const wchar_t *fmt, va_list ap)
 {
-    struct __file_str f = FDEV_SETUP_STRING_WRITE((char *)s, (char *)FDEV_STRING_WRITE_END(s, n));
+    struct __file_str f = FDEV_SETUP_WSTRING_WRITE(s, n);
     int               i;
 
     f.file.flags |= __SWIDE;
